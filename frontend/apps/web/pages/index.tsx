67ef0532c832549bb6489b11e48b16e22ca7de5a
Rafael Motta <rafael@onefootprint.com>
2022-04-26 10:09:18 +0200

    Add prettier

--- frontend/apps/web/pages/index.tsx
+++ frontend/apps/web/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Weaab</h1>
+      <div>lorem</div>
       <Button />
     </div>
   );
