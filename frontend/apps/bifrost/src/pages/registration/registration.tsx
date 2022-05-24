12a4bc510fef0cdda067c13726e81ba91fb88d36
Belce Dogru <belce.dogru@gmail.com>
2022-05-24 06:55:31 -0700

    Added liveness check pages (#182)
    * Added liveness check pages
    
    * Updated PR based on feedback

--- frontend/apps/bifrost/src/pages/registration/registration.tsx
+++ frontend/apps/bifrost/src/pages/registration/registration.tsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import { Container } from 'ui';
 
 import BasicInformation from './components/basic-information';
+import LivenessCheck from './components/liveness-check';
 import PhoneRegistration from './components/phone-registration';
 import ResidentialAddress from './components/residential-address';
 import SSN from './components/ssn';
 
 enum Step {
-  phoneRegistration = 'phone-registration',
-  basicInformation = 'basic-information',
-  residentialAddress = 'residential-address',
+  phoneRegistration = 'phoneRegistration',
+  livenessCheck = 'livenessCheck',
+  basicInformation = 'basicInformation',
+  residentialAddress = 'residentialAddress',
   ssn = 'ssn',
 }
 
@@ -19,6 +21,7 @@ const Registration = () => {
     <Container sx={{ marginTop: 5 }}>
       {step === Step.basicInformation && <BasicInformation />}
       {step === Step.phoneRegistration && <PhoneRegistration />}
+      {step === Step.livenessCheck && <LivenessCheck />}
       {step === Step.residentialAddress && <ResidentialAddress />}
       {step === Step.ssn && <SSN />}
     </Container>
