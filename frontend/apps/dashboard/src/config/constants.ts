12eeb1f167809173cd5c1e758548c7add6b8ca73
Elliott Forde <elliott@onefootprint.com>
2022-05-27 14:21:24 -0700

    [dashboard] Proactively add /auth to LOGGED_OUT_ROUTES

--- frontend/apps/dashboard/src/config/constants.ts
+++ frontend/apps/dashboard/src/config/constants.ts
@@ -1,5 +1,10 @@
 export const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 export const COMMIT_SHA = process.env.NEXT_PUBLIC_COMMIT_SHA;
-export const LOGGED_OUT_ROUTES = ['/login', '/login/email', '/login/link-sent'];
+export const LOGGED_OUT_ROUTES = [
+  '/login',
+  '/login/email',
+  '/login/link-sent',
+  '/auth',
+];
 export const DEFAULT_LOGGED_IN_ROUTE = '/users';
 export const DEFAULT_LOGGED_OUT_ROUTE = '/login';
