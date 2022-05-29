ad5eb51f129ee9136310e6aea3a7775076948e31
Alex Grinman <me@alexgr.in>
2022-05-29 14:29:45 -0400

    improve observability logs

--- components/db/src/lib.rs
+++ components/db/src/lib.rs
@@ -26,24 +26,38 @@ pub type DbPool = Pool;
 
 /// Initialize our DB
 pub fn init(url: &str) -> Result<Pool, DbError> {
+    let init_instant = std::time::Instant::now();
+
     let manager = Manager::new(url, Runtime::Tokio1);
     let pool = Pool::builder(manager)
         .runtime(Runtime::Tokio1)
         .recycle_timeout(Some(Duration::from_secs(1)))
         .create_timeout(Some(Duration::from_secs(1)))
-        .post_create(Hook::sync_fn(|_, metrics| {
-            tracing::info!(
-                created = ?metrics.created,
-                "db_pool conn post_created"
-            );
+        .post_create(Hook::sync_fn(move |_, metrics| {
+            let created = metrics
+                .created
+                .duration_since(init_instant.clone())
+                .as_secs();
+
+            tracing::info!(db.pool.created_secs_ago = created, "db_pool.post_create");
             Ok(())
         }))
-        .post_recycle(Hook::sync_fn(|_, metrics| {
-            tracing::info!(
-                recycle_count = metrics.recycle_count,
-                created = ?metrics.created,
-                recycled = ?metrics.recycled,
-                "db_pool conn post_recycle"
+        .post_recycle(Hook::sync_fn(move |_, metrics| {
+            let recycled = metrics
+                .created
+                .duration_since(init_instant.clone())
+                .as_secs();
+
+            let created = metrics
+                .created
+                .duration_since(init_instant.clone())
+                .as_secs();
+
+            tracing::debug!(
+                db.pool.recycle_count = metrics.recycle_count,
+                db.pool.created_secs_ago = created,
+                db.pool.recycled_secs_ago = recycled,
+                "db_pool.post_recycle"
             );
             Ok(())
         }))
