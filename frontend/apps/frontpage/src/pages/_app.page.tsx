a481071b59133ae573748a64652a034b0fea095d
Rafael Michels Motta <rafael@onefootprint.com>
2022-05-31 21:43:22 +0200

    [FP-120] Setup fathom analytics for front-page (#265)
    [FP-120] Setup fathom analytics for frontpage

--- frontend/apps/frontpage/src/pages/_app.page.tsx
+++ frontend/apps/frontpage/src/pages/_app.page.tsx
@@ -1,11 +1,14 @@
 import '@typeform/embed/build/css/popup.css';
 
+import { IS_PROD } from 'global-constants';
+import Script from 'next/script';
 import React from 'react';
 import { createGlobalStyle } from 'styled';
 import { DesignSystemProvider, themes } from 'ui';
 
 import Layout from '../components/layout';
 import MDXProvider from '../components/mdx-provider';
+import { FATHOM_ANALYTICS_SITE } from '../config/constants';
 import configureReactI18next from '../config/initializers/react-i18next';
 
 configureReactI18next();
@@ -18,14 +21,23 @@ type AppProps = {
 const GlobalStyle = createGlobalStyle``;
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <DesignSystemProvider theme={themes.light}>
-    <GlobalStyle />
-    <Layout>
-      <MDXProvider>
-        <Component {...pageProps} />
-      </MDXProvider>
-    </Layout>
-  </DesignSystemProvider>
+  <>
+    {IS_PROD && (
+      <Script
+        data-site={FATHOM_ANALYTICS_SITE}
+        defer
+        src="https://cdn.usefathom.com/script.js"
+      />
+    )}
+    <DesignSystemProvider theme={themes.light}>
+      <GlobalStyle />
+      <Layout>
+        <MDXProvider>
+          <Component {...pageProps} />
+        </MDXProvider>
+      </Layout>
+    </DesignSystemProvider>
+  </>
 );
 
 export default App;
--- frontend/packages/global-constants/src/index.ts
+++ frontend/packages/global-constants/src/index.ts
@@ -1,5 +1,7 @@
 export const IS_BROWSER = typeof window !== 'undefined';
 export const IS_SERVER = typeof window === 'undefined';
+export const IS_DEV = process.env.NODE_ENV === 'development';
+export const IS_PROD = !IS_DEV;
 
 export const API_BASE_URL = 'https://api.dev.infra.footprint.dev';
 export const API_TIMEOUT = 60000;
