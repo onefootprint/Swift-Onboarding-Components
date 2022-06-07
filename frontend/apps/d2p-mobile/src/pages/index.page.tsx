586ffd7a29ef708e5bbb91ce4a3a50d6621d204c
Belce Dogru <belce.dogru@gmail.com>
2022-06-07 11:12:08 -0700

    Add bootstrapped d2p mobile app folder (cloned from identity-wallet) (#310)

---
+++ frontend/apps/d2p-mobile/src/pages/index.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Typography } from 'ui';
+
+const Root = () => (
+  <Container>
+    <Typography variant="heading-2" color="primary">
+      Hey there! 👋
+    </Typography>
+  </Container>
+);
+
+export default Root;
