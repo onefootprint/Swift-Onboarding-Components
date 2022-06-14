6adecf86915d3d19a6f90f14f8a39a215619e1c1
Elliott Forde <elliott@onefootprint.com>
2022-06-13 21:03:02 -0700

    [api] Rename commit.rs to complete.rs (#177)
    feature-branch: ef/style-guide

--- components/api/src/onboarding/mod.rs
+++ components/api/src/onboarding/mod.rs
@@ -1,13 +1,13 @@
 use paperclip::actix::web;
 
-pub mod commit;
+pub mod complete;
 pub mod d2p;
 pub mod init;
 
 pub fn routes() -> web::Scope {
     web::scope("/onboarding")
         .service(web::resource("").route(web::post().to(init::handler)))
-        .service(commit::handler)
-        .service(commit::handler)
+        .service(complete::handler)
+        .service(complete::handler)
         .service(d2p::routes())
 }
