b58e4ff5dd79253b2639262a4679d12d3fc80968
Elliott Forde <elliott@onefootprint.com>
2022-06-21 08:37:08 -0700

    [dashboard] Migrate users filter to Dialog component (#408)
    feature-branch: ef/users-filter-dialog

--- frontend/apps/dashboard/src/pages/users/components/users-filter/users-filter.tsx
+++ frontend/apps/dashboard/src/pages/users/components/users-filter/users-filter.tsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import Modal, { ModalCloseEvent } from 'src/components/modal';
 import { useFilters } from 'src/pages/users/hooks/use-filters';
-import {} from 'src/pages/users/hooks/use-get-onboardings';
 import { OnboardingStatus, statusToDisplayText } from 'src/types';
 import styled, { css } from 'styled-components';
-import { Button, Select, SelectOption } from 'ui';
+import { Button, Dialog, Select, SelectOption } from 'ui';
 
 const UsersFilter = () => {
   const { query, setFilter } = useFilters();
   const [selectedOption, setSelectedOption] = useState<
     SelectOption | null | undefined
   >();
-  const [showModal, setShowModal] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
 
-  // Any time the modal is opened, recompute what the currently displayed status should be based
+  // Any time the dialog is opened, recompute what the currently displayed status should be based
   // on the querystring
   useEffect(() => {
     // TODO this should be much simpler... can the selectedOption be just a value rather than
@@ -26,55 +24,59 @@ const UsersFilter = () => {
           } as SelectOption)
         : undefined;
     setSelectedOption(currentStatus);
-  }, [query, showModal]);
+  }, [query, showDialog]);
 
-  const handleCloseModal = (type: ModalCloseEvent) => {
-    if (type === ModalCloseEvent.Primary) {
-      setFilter({
-        status: selectedOption?.value as string,
-      });
-    } else if (type === ModalCloseEvent.Secondary) {
-      // Clear the filter
-      setFilter({
-        status: undefined,
-      });
-    }
-    setShowModal(false);
+  const handleApplyClick = () => {
+    setFilter({
+      status: selectedOption?.value as string,
+    });
+    setShowDialog(false);
+  };
+  const handleClearClick = () => {
+    // Clear the filter
+    setFilter({
+      status: undefined,
+    });
+    setShowDialog(false);
   };
 
   return (
     <>
-      {showModal && (
-        <Modal
-          size="compact"
-          headerText="Filters"
-          primaryButtonText="Apply"
-          secondaryButtonText="Clear"
-          secondaryButtonVariant="link"
-          onClose={handleCloseModal}
-        >
-          <Select
-            label="Status"
-            options={[
-              // TODO share these with the enum values we define
-              { label: 'Verified', value: OnboardingStatus.verified },
-              { label: 'Incomplete', value: OnboardingStatus.incomplete },
-              { label: 'Manual review', value: OnboardingStatus.manualReview },
-              { label: 'Processing', value: OnboardingStatus.processing },
-              { label: 'Failed', value: OnboardingStatus.failed },
-            ]}
-            value={selectedOption ? selectedOption.value : null}
-            onChange={option => {
-              setSelectedOption(option);
-            }}
-          />
-        </Modal>
-      )}
+      <Dialog
+        size="compact"
+        title="Filters"
+        primaryButton={{
+          label: 'Apply',
+          onClick: handleApplyClick,
+        }}
+        linkButton={{
+          label: 'Clear',
+          onClick: handleClearClick,
+        }}
+        onClose={() => setShowDialog(false)}
+        open={showDialog}
+      >
+        <Select
+          label="Status"
+          options={[
+            // TODO share these with the enum values we define
+            { label: 'Verified', value: OnboardingStatus.verified },
+            { label: 'Incomplete', value: OnboardingStatus.incomplete },
+            { label: 'Manual review', value: OnboardingStatus.manualReview },
+            { label: 'Processing', value: OnboardingStatus.processing },
+            { label: 'Failed', value: OnboardingStatus.failed },
+          ]}
+          value={selectedOption ? selectedOption.value : null}
+          onChange={option => {
+            setSelectedOption(option);
+          }}
+        />
+      </Dialog>
       <FilterButtonContainer>
         <Button
           size="small"
           variant="secondary"
-          onClick={() => setShowModal(true)}
+          onClick={() => setShowDialog(true)}
         >
           Filters
         </Button>
