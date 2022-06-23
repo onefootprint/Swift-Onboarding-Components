791fb007a7bc3b316d8238e7631a6c12e269c37e
Elliott Forde <elliott@onefootprint.com>
2022-06-23 13:09:20 -0700

    [dashboard] Use shimmer when loading encrypted field (#436)
    feature-branch: ef/shimmer
    Resolves FP-239

--- frontend/apps/dashboard/src/pages/users/components/encrypted-cell/encrypted-cell.tsx
+++ frontend/apps/dashboard/src/pages/users/components/encrypted-cell/encrypted-cell.tsx
@@ -1,16 +1,11 @@
 import IcoLock16 from 'icons/ico/ico-lock-16';
 import React from 'react';
-import { Box, LoadingIndicator, Typography } from 'ui';
+import { Box, Typography } from 'ui';
 
-type EncryptedCellProps = {
-  isLoading: boolean;
-};
-
-const EncryptedCell = ({ isLoading }: EncryptedCellProps) => (
+const EncryptedCell = () => (
   <Box sx={{ display: 'flex' }}>
     <Box sx={{ marginRight: 2 }}>
-      {/* TODO https://linear.app/footprint/issue/FP-239/add-a-shimmer-effect-for-loading-encrypted-fields */}
-      {isLoading ? <LoadingIndicator size="compact" /> : <IcoLock16 />}
+      <IcoLock16 />
     </Box>
     <Typography variant="body-3" color="primary" sx={{ userSelect: 'none' }}>
       •••••••••
--- frontend/apps/dashboard/src/pages/users/components/field-or-placeholder/field-or-placeholder.tsx
+++ frontend/apps/dashboard/src/pages/users/components/field-or-placeholder/field-or-placeholder.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import EncryptedCell from 'src/pages/users/components/encrypted-cell';
-import { Typography } from 'ui';
+import { Shimmer, Typography } from 'ui';
 
 import { UserData } from '../../hooks/use-user-data';
 
@@ -8,13 +8,17 @@ type FieldOrPlaceholderProps = {
   data: UserData;
 };
 
-const FieldOrPlaceholder = ({ data }: FieldOrPlaceholderProps) =>
-  data.exists && data.value === undefined ? (
-    <EncryptedCell isLoading={data.isLoading} />
+const FieldOrPlaceholder = ({ data }: FieldOrPlaceholderProps) => {
+  if (data.isLoading) {
+    return <Shimmer sx={{ width: '70px' }} />;
+  }
+  return data.exists && data.value === undefined ? (
+    <EncryptedCell />
   ) : (
     <Typography variant="body-3" color="primary" sx={{ whiteSpace: 'nowrap' }}>
       {data.value || '-'}
     </Typography>
   );
+};
 
 export default FieldOrPlaceholder;
