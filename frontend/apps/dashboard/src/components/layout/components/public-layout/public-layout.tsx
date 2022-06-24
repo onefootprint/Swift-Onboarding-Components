b8bb6d7beb64fe134a9143dd9076412f94a61991
Elliott Forde <elliott@onefootprint.com>
2022-06-24 00:07:39 -0700

    [dashboard] Make login page beautiful (#441)
    feature-branch: ef/gradient

--- frontend/apps/dashboard/src/components/layout/components/public-layout/public-layout.tsx
+++ frontend/apps/dashboard/src/components/layout/components/public-layout/public-layout.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type PublicLayoutProps = {
   children: React.ReactNode;
 };
 
 const PublicLayout = ({ children }: PublicLayoutProps) => (
-  <div data-testid="public-layout">{children}</div>
+  <Container data-testid="public-layout">{children}</Container>
 );
+
+const Container = styled.div`
+  background: linear-gradient(180deg, #f0edff 0%, #ffffff 100%);
+`;
 
 export default PublicLayout;
