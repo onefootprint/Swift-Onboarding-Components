705a5d23c590215d98b70bf1b659ae452a0cc41f
Rafael Michels Motta <rafael@onefootprint.com>
2022-06-24 12:18:41 +0200

    [FP-405] Bifrost error without key (#447)

--- frontend/apps/bifrost/src/pages/init/hooks/use-tenant-info/use-tenant-info.ts
+++ frontend/apps/bifrost/src/pages/init/hooks/use-tenant-info/use-tenant-info.ts
@@ -33,6 +33,12 @@ const getTenantInfo = async (payload: TenantInfoRequest) => {
 const useTenantInfo = (tenantPk: string) => {
   const [, send] = useBifrostMachine();
 
+  if (!tenantPk) {
+    send({
+      type: Events.tenantInfoRequestFailed,
+    });
+  }
+
   useQuery<TenantInfoResponse, RequestError>(
     ['tenantInfo', tenantPk],
     () => getTenantInfo({ tenantPk }),
