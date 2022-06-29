5189431090e42d6b7fc4f7f57dce54aa5ebcd590
Gabriela Merz <gabbi.merz@gmail.com>
2022-06-28 23:51:43 -0400

    actually fix

--- ci/integration_tests.py
+++ ci/integration_tests.py
@@ -143,7 +143,7 @@ def test_get_org_config(request, workos_tenant):
         headers=_client_pub_key_headers(workos_tenant["pk"]),
     )
     body = _assert_response(r)
-    assert body == {'data': {'name': 'workos_tenant', 'required_user_data': ['first_name', 'last_name', 'dob', 'ssn', 'street_address', 'street_address2', 'city', 'state', 'zip', 'country', 'email', 'phone_number'], 'settings': 'Empty'}}
+    assert body == {'data': {'name': 'Acme Bank', 'required_user_data': ['first_name', 'last_name', 'dob', 'ssn', 'street_address', 'street_address2', 'city', 'state', 'zip', 'country', 'email', 'phone_number'], 'settings': 'Empty'}}
 
 
 def test_identify_email(request):
