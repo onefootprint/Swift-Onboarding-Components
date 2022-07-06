80a92ae3cc65d2fdb105de7924e758549b4975e4
Belce Dogru <belce.dogru@gmail.com>
2022-07-06 10:32:47 -0700

    Add comment explaining ssn regex (#502)

--- frontend/apps/bifrost/src/pages/onboarding/pages/ssn/ssn.tsx
+++ frontend/apps/bifrost/src/pages/onboarding/pages/ssn/ssn.tsx
@@ -55,6 +55,9 @@ const SSN = () => {
           type="tel"
           {...register('ssn', {
             required: true,
+            // Numbers with all zeros in any digit group (000-##-####, ###-00-####, ###-##-0000) are not allowed.
+            // Numbers with 666 or 900–999 in the first digit group are not allowed.
+            // Also validates length & formatting.
             pattern: /^(?!(000|666|9))(\d{3}-?(?!(00))\d{2}-?(?!(0000))\d{4})$/,
           })}
         />
