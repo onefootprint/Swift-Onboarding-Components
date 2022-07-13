4d22c52cb9970423d5abaf824798806aa3c1a937
Elliott Forde <elliott@onefootprint.com>
2022-07-12 17:40:35 -0700

    [dashboard] Add preset reasons to decrypt modal (#542)
    feature-branch: ef/decrypt-reason
    
    Resolves FP-570

--- frontend/apps/dashboard/src/pages/users/pages/detail/components/decrypt-dialog/components/reason-screen/reason-screen.tsx
+++ frontend/apps/dashboard/src/pages/users/pages/detail/components/decrypt-dialog/components/reason-screen/reason-screen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Divider, TextArea, Typography } from 'ui';
+import React, { useEffect, useState } from 'react';
+import { Box, Divider, Select, SelectOption, TextArea, Typography } from 'ui';
 
 type ReasonScreenProps = {
   reason: string;
@@ -7,26 +7,80 @@ type ReasonScreenProps = {
   hasError: boolean;
 };
 
-const ReasonScreen = ({ reason, setReason, hasError }: ReasonScreenProps) => (
-  <>
-    <Typography variant="label-1" sx={{ marginBottom: 7 }}>
-      Briefly describe why you need to decrypt this data.
-    </Typography>
-    <TextArea
-      placeholder="Type the reason here..."
-      hasError={hasError}
-      hintText={hasError ? 'A reason is needed' : ''}
-      value={reason}
-      onChangeText={setReason}
-    />
-    <Box sx={{ marginTop: 7, marginBottom: 7 }}>
-      <Divider />
-    </Box>
-    <Typography variant="body-3" color="tertiary">
-      Please note that all data attribute access are logged for security
-      reasons.
-    </Typography>
-  </>
-);
+const options = [
+  {
+    value: 'Responding to customer support inquiry',
+    label: 'Responding to customer support inquiry',
+  },
+  {
+    value: 'Sending communication to customer',
+    label: 'Sending communication to customer',
+  },
+  {
+    value: 'Verifying customer identity',
+    label: 'Verifying customer identity',
+  },
+  {
+    value: 'Auditor review',
+    label: 'Auditor review',
+  },
+  {
+    value: 'Transaction over $5k',
+    label: 'Transaction over $5k',
+  },
+  {
+    value: 'Change of direct deposit',
+    label: 'Change of direct deposit',
+  },
+  {
+    value: 'Other',
+    label: 'Other',
+  },
+];
+
+const ReasonScreen = ({ reason, setReason, hasError }: ReasonScreenProps) => {
+  const [selectedOption, setSelectedOption] = useState<SelectOption<string>>();
+  useEffect(() => {
+    setReason(options[0].value);
+    setSelectedOption(options[0]);
+  }, [setReason, setSelectedOption]);
+  return (
+    <>
+      <Typography variant="label-1" sx={{ marginBottom: 7 }}>
+        Why are you decrypting this data?
+      </Typography>
+      <Select
+        label="Reason"
+        isSearchable={false}
+        value={selectedOption}
+        hasError={hasError}
+        onChange={option => {
+          setSelectedOption(option);
+          // Clear reason value when "Other" is selected
+          setReason(option.value !== 'Other' ? option.value : '');
+        }}
+        options={options}
+      />
+      {selectedOption?.value === 'Other' && (
+        <Box sx={{ marginTop: 4 }}>
+          <TextArea
+            placeholder="Type the reason here..."
+            hasError={hasError}
+            hintText={hasError ? 'A reason is needed' : ''}
+            value={reason}
+            onChangeText={setReason}
+          />
+        </Box>
+      )}
+      <Box sx={{ marginTop: 7, marginBottom: 7 }}>
+        <Divider />
+      </Box>
+      <Typography variant="body-3" color="tertiary">
+        Please note that all data attribute access are logged for security
+        reasons.
+      </Typography>
+    </>
+  );
+};
 
 export default ReasonScreen;
