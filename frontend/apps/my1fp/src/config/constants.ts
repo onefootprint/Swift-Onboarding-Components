f82b5ef6962243add1d0d9d0b3ccd523b5a1c462
Belce Dogru <belce.dogru@gmail.com>
2022-07-13 08:54:17 -0700

    [FP-525] Add page guard to my1fp (#544)
    Add page guard to my1fp

--- frontend/apps/my1fp/src/config/constants.ts
+++ frontend/apps/my1fp/src/config/constants.ts
@@ -1,7 +1,11 @@
 export const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 export const COMMIT_SHA = process.env.NEXT_PUBLIC_COMMIT_SHA;
-export const MY1FP_D2P_BASE_URL = process.env.NEXT_PUBLIC_D2P_BASE_URL;
 
+export const LOGGED_OUT_ROUTES = ['/login'];
+export const DEFAULT_LOGGED_IN_ROUTE = '/';
+export const DEFAULT_LOGGED_OUT_ROUTE = '/login';
+
+export const MY1FP_D2P_BASE_URL = process.env.NEXT_PUBLIC_D2P_BASE_URL;
 export const MY1FP_AUTH_HEADER = 'X-My1fp-Authorization';
 export const MY1FP_USER_AUTH_HEADER = 'X-Fpuser-Authorization';
 export const MY1FP_D2P_SCOPED_AUTH_HEADER = 'X-d2p-Authorization';
--- frontend/apps/my1fp/src/pages/_app.page.tsx
+++ frontend/apps/my1fp/src/pages/_app.page.tsx
@@ -5,6 +5,7 @@ import { createGlobalStyle } from 'styled-components';
 import themes from 'themes';
 import { DesignSystemProvider } from 'ui';
 
+import PageGuard from '../components/page-guard';
 import configureReactI18next from '../config/initializers/react-i18next';
 import queryClient from '../config/initializers/react-query';
 import configureSentry from '../config/initializers/sentry';
@@ -28,7 +29,9 @@ const App = ({ Component, pageProps }: AppProps) => (
     <QueryClientProvider client={queryClient}>
       <DesignSystemProvider theme={themes.light}>
         <GlobalStyle />
-        <Component {...pageProps} />
+        <PageGuard>
+          <Component {...pageProps} />
+        </PageGuard>
       </DesignSystemProvider>
     </QueryClientProvider>
   </>
