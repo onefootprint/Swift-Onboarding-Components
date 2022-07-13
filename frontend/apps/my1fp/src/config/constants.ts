32791299c96c009d130420d2d5713aa17c967dfa
Belce Dogru <belce.dogru@gmail.com>
2022-07-13 02:59:30 -0700

    Add d2p generate hook to my1fp (#548)

--- frontend/apps/my1fp/src/config/constants.ts
+++ frontend/apps/my1fp/src/config/constants.ts
@@ -3,4 +3,5 @@ export const COMMIT_SHA = process.env.NEXT_PUBLIC_COMMIT_SHA;
 export const MY1FP_D2P_BASE_URL = process.env.NEXT_PUBLIC_D2P_BASE_URL;
 
 export const MY1FP_AUTH_HEADER = 'X-My1fp-Authorization';
+export const MY1FP_USER_AUTH_HEADER = 'X-Fpuser-Authorization';
 export const MY1FP_D2P_SCOPED_AUTH_HEADER = 'X-d2p-Authorization';
---
+++ frontend/apps/my1fp/src/pages/liveness-check/qr-register/hooks/use-generate-d2p/index.ts
@@ -0,0 +1,1 @@
+export { default } from './use-generate-d2p';
