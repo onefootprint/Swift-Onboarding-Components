c805e30adf535d3da4ca051c708d848eaa6f99fb
Elliott Forde <elliott@onefootprint.com>
2022-07-13 20:42:26 -0700

    [db] Update migration to backfill can_access_data_kinds (#301)
    I ran this extra line manually in dev - hopefully this will just make folks' local envs less broken
    
    feature-branch: ef/migration

--- components/db/migrations/2022-07-13-232148_ob_configuration_data_kinds/up.sql
+++ components/db/migrations/2022-07-13-232148_ob_configuration_data_kinds/up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE ob_configurations RENAME COLUMN required_user_data TO must_collect_data_kinds;
 ALTER TABLE ob_configurations ADD COLUMN can_access_data_kinds TEXT[] NOT NULL DEFAULT CAST(ARRAY[] as TEXT[]);
 ALTER TABLE ob_configurations ALTER COLUMN can_access_data_kinds DROP DEFAULT;
+UPDATE ob_configurations SET can_access_data_kinds = must_collect_data_kinds;
