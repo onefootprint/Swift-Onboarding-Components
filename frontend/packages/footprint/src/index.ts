58befff80bba597343367135fb37fe6e0c2d71af
Rafael Michels Motta <rafael@onefootprint.com>
2022-07-15 12:47:17 +0200

    [FP-622] Fix bifrost url on footprint.js (#576)
    * [FP-622] Fix url
    
    * fix

--- frontend/packages/footprint/src/index.ts
+++ frontend/packages/footprint/src/index.ts
@@ -2,14 +2,27 @@ import vanillaIntegration from './adapters/vanilla';
 import Footprint from './footprint';
 import UiManager from './footprint/ui-manager';
 
-const getUrl = () =>
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://id.onefootprint.com';
+const getUrl = (env = 'local', branchName?: string) => {
+  if (env === 'local') {
+    return 'http://localhost:3000/';
+  }
+  if (env === 'development') {
+    return 'https://id.preview.onefootprint.com';
+  }
+  if (env === 'preview' && branchName) {
+    const slugBranch = branchName.toLowerCase().replaceAll('/', '-');
+    return `https://bifrost-git-${slugBranch}.preview.onefootprint.com`;
+  }
+  return 'https://id.onefootprint.com';
+};
 
-const footprintUrl = getUrl();
+const url = getUrl(
+  process.env.NEXT_PUBLIC_VERCEL_ENV,
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
+);
+
 const uiManager = new UiManager();
-const footprint = new Footprint(footprintUrl, uiManager);
+const footprint = new Footprint(url, uiManager);
 vanillaIntegration(footprint)();
 
 export default footprint;
--- frontend/packages/request/src/request.ts
+++ frontend/packages/request/src/request.ts
@@ -20,13 +20,6 @@ export const getErrorMessage = (error: RequestError): string =>
   error.response?.data.error.message || error.message;
 
 const request = <TData = any>(requestConfig: AxiosRequestConfig = {}) => {
-  console.log('api base url', process.env.NEXT_PUBLIC_API_BASE_URL);
-  console.log('NEXT_PUBLIC_VERCEL_ENV', process.env.NEXT_PUBLIC_VERCEL_ENV);
-  console.log(
-    'NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF',
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
-  );
-
   const client = applyCaseMiddleware(axios.create());
   return client.request<TData, RequestResponse<TData>>({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
