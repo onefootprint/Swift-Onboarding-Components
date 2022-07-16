7586006030c99176e395f1506c00f6c10c633303
Elliott Forde <elliott@onefootprint.com>
2022-07-15 17:35:27 -0700

    [db] Fix reverse migration for adding back check_sh_data constraint (#311)
    feature-branch: ef/migration
    
    This migration was failing to be rolled back when there was standing data in the dev database: https://github.com/onefootprint/footprint-core/runs/7366747229?check_suite_focus=true
    
    If there were rows that had fingerprints added by a future version of code, we weren't able to add the constraint that requires they don't have a fingerprint. This clears out the fingerprint for data types that shouldn't be fingerprinted in older versions of the DB

--- components/db/migrations/2022-07-07-214824_drop_user_data_constraints/down.sql
+++ components/db/migrations/2022-07-07-214824_drop_user_data_constraints/down.sql
@@ -1,7 +1,9 @@
--- allow more flexibility in what is fingerprinted/not, control in app logic
-alter table user_data add constraint check_sh_data check (
+-- Un-fingerprint data in order to roll back to the old constraint
+UPDATE user_data SET sh_data = NULL WHERE data_kind NOT IN ('Ssn', 'PhoneNumber', 'Email', 'FirstName', 'LastName', 'LastFourSsn');
+
+ALTER TABLE user_data ADD CONSTRAINT check_sh_data CHECK (
     ((sh_data IS NOT NULL) AND (data_kind IN ('Ssn', 'PhoneNumber', 'Email', 'FirstName', 'LastName', 'LastFourSsn')))
     OR ((sh_data IS NULL) AND (data_kind NOT IN ('Ssn', 'PhoneNumber', 'Email', 'FirstName', 'LastName', 'LastFourSsn')))
 );
-drop index user_data_unique_kind_fingerprint;
+DROP INDEX user_data_unique_kind_fingerprint;
 CREATE UNIQUE INDEX IF NOT EXISTS user_data_unique_kind_fingerprint ON user_data(data_kind, sh_data) WHERE is_verified = TRUE AND data_kind IN ('PhoneNumber', 'Email');
