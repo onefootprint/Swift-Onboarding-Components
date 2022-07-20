970c9668d5c74a9029391df731eaa746a11378a0
Elliott Forde <elliott@onefootprint.com>
2022-07-20 17:35:25 -0600

    Revert "Revert "Revert "Revert "Revert "[api] No-op change to test overlapping actions"""""
    This reverts commit 94449c2a2e1d642150a0d1e4ef6826f0ad9333ca.

--- components/api/src/main.rs
+++ components/api/src/main.rs
@@ -183,8 +183,6 @@ async fn main() -> std::io::Result<()> {
             // use custom error handler
             .error_handler(|err, _req| actix_web::Error::from(ApiError::InvalidJsonBody(err)));
 
-        tracing::info!("flerp");
-
         App::new()
             .app_data(web::Data::new(state.clone()))
             .wrap(
