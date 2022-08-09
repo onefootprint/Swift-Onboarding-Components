bb8a8326dc82579947eb7bfad5e706e404815e96
Belce Dogru <belce.dogru@gmail.com>
2022-08-09 14:22:51 -0700

    Move use-countdown to footprint-ui to use in phone-verification & qr-register pages for disabled challenges (#705)
    * Move use-countdown to footprint-ui
    
    * Add setSeconds & setDate methods on useCountdown
    
    * Add guards against back to back starting

--- frontend/apps/biometric/src/pages/success/success.tsx
+++ frontend/apps/biometric/src/pages/success/success.tsx
@@ -1,25 +1,32 @@
+import useCountdown from 'footprint-ui/src/hooks/use-countdown';
 import { useTranslation } from 'hooks';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import HeaderTitle from '../../components/header-title';
 import useOpener from '../../hooks/use-opener';
-import useCountdown from './hooks/use-countdown';
+
+const SUCCESS_COUNTER_SECONDS = 3;
 
 const Success = () => {
   const { t } = useTranslation('pages.success');
   const opener = useOpener();
   const shouldShowCounter = opener === 'mobile';
-  const seconds = useCountdown({
-    disabled: shouldShowCounter,
+  const { countdown, setSeconds } = useCountdown({
+    disabled: !shouldShowCounter,
     onCompleted: () => window.close(),
   });
+
+  useEffect(() => {
+    setSeconds(SUCCESS_COUNTER_SECONDS);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <HeaderTitle
       title={t('title')}
       subtitle={
         shouldShowCounter
-          ? t('subtitle.with-countdown', { seconds })
+          ? t('subtitle.with-countdown', { seconds: countdown })
           : t('subtitle.without-countdown')
       }
     />
--- frontend/packages/footprint-ui/src/hooks/index.ts
+++ frontend/packages/footprint-ui/src/hooks/index.ts
@@ -1,2 +1,3 @@
+export { default as useCountdown } from './use-countdown';
 export { default as useDeviceInfo } from './use-device-info';
 export * from './use-device-info';
