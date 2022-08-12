4e7a8e22d9d5de0c2a78da9995fe5d766ff85829
Elliott Forde <elliott@onefootprint.com>
2022-08-11 17:13:02 -0700

    [api] Alias /internal to /hosted URLs (#467)
    feature-branch: elliott/fp-1030-point-internal-and-hosted-to-same-place

--- components/api/src/internal/mod.rs
+++ components/api/src/internal/mod.rs
@@ -6,6 +6,13 @@ pub mod user;
 
 // Routes accessible only to internal applications - my1fp, bifrost
 pub fn routes() -> web::Scope {
+    web::scope("/hosted")
+        .service(user::routes())
+        .service(identify::routes())
+        .service(onboarding::routes())
+}
+
+pub fn old_routes() -> web::Scope {
     web::scope("/internal")
         .service(user::routes())
         .service(identify::routes())
