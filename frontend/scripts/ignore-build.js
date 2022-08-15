b9a5ee8199357ecf09b6cfacac17b9401b59526e
Rafael Michels Motta <rafael@onefootprint.com>
2022-08-15 13:33:14 +0200

    [FP-1053] Fix argument of vercel ignore build (#493)

--- frontend/scripts/ignore-build.js
+++ frontend/scripts/ignore-build.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const childProcess = require("child_process");
+const childProcess = require('child_process');
 
 // https://vercel.com/support/articles/how-do-i-use-the-ignored-build-step-field-on-vercel
 const ABORT_BUILD_CODE = 0;
@@ -7,36 +7,28 @@ const CONTINUE_BUILD_CODE = 1;
 
 const continueBuild = () => {
   process.exit(CONTINUE_BUILD_CODE);
-}
+};
 
 const abortBuild = () => {
   process.exit(ABORT_BUILD_CODE);
-}
+};
 
-const dir = process.argv[3] || path.basename(path.resolve());
+// argv is a custom argument we pass on vercel (e.g frontend)
+// the second value is the directory that we're calling this script from
+const dir = process.argv[2] || path.basename(path.resolve());
 
 const stepCheck = () => {
   if (!dir) {
     return abortBuild();
   }
-
   // get all file names changed in last commit
   const fileNameList = childProcess
-    .execSync("git diff --name-only HEAD~1")
+    .execSync('git diff --name-only HEAD~1')
     .toString()
     .trim()
-    .split("\n");
-
-  // check if any files in the app, or in any shared packages have changed
-  const shouldBuild = fileNameList.some(
-    (file) => file.startsWith(dir)
-  );
-
-  if (shouldBuild) {
-    return continueBuild();
-  }
-
-  return abortBuild();
+    .split('\n');
+  const shouldBuild = fileNameList.some(file => file.startsWith(dir));
+  return shouldBuild ? continueBuild() : abortBuild();
 };
 
 stepCheck();
