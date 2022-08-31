36033ecc19754bb2cd466522308aa2bd2cca0b51
Rafael Michels Motta <rafael@onefootprint.com>
2022-08-31 12:11:24 +0200

    Fix bundle of footprint-js (#680)

--- frontend/packages/footprint-js/tsup.config.ts
+++ frontend/packages/footprint-js/tsup.config.ts
@@ -1,3 +1,26 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({});
+export default defineConfig(config => ({
+  dts: true,
+  format: config.watch ? ['esm', 'cjs'] : ['esm', 'cjs', 'iife'],
+  minify: !config.watch,
+  clean: !config.watch,
+  entry: {
+    'footprint-js': 'src/index.ts',
+  },
+  outExtension({ format }) {
+    if (format === 'iife') {
+      return {
+        js: `.umd.js`,
+      };
+    }
+    return {
+      js: `.${format}.js`,
+    };
+  },
+  env: {
+    NODE_ENV: 'production',
+    NEXT_PUBLIC_VERCEL_ENV: '',
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF: '',
+  },
+}));
