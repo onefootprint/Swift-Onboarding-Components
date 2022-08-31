ebe63d7a4360f92e5070b2d9ac4e18b625334187
Rafael Michels Motta <rafael@onefootprint.com>
2022-08-31 15:02:17 +0200

    [FP-1244] Add not boring podcast to the media (#692)
    * [FP-1244] Add not boring podcast to the media
    
    * Fix date

--- frontend/apps/frontpage/src/pages/media/media.server.ts
+++ frontend/apps/frontpage/src/pages/media/media.server.ts
@@ -5,7 +5,18 @@ import type { Article } from './media.types';
 const getStaticProps: GetStaticProps = () => {
   const articles: Article[] = [
     {
-      id: '1',
+      id: '7',
+      url: 'https://open.spotify.com/show/6k1YLBvORRMyosKy3x1xIl',
+      imageUrl: '/media/not-boring.png',
+      imageAlt: 'Our founders, Eli and Alex',
+      createdAt: 'Aug 5, 2022',
+      website: 'Not Boring Podcast',
+      title: 'Not Boring founders: Eli Wachs & Alex Grinman',
+      excerpt:
+        "Eli Wachs & Alex Grinman are the co-founders of Footprint. Footprint's mission is to bring back trust on the internet. The company wants to put people in control of their identity while solving KYC, IDV, and PII storage problems for enterprises. Footprint just announced a $6M Seed Round led by Index Ventures.",
+    },
+    {
+      id: '6',
       url: 'https://techcrunch.com/2022/08/03/footprint-wants-to-change-how-companies-collect-store-and-share-personal-data/',
       imageUrl: '/media/techcrunch.png',
       imageAlt:
@@ -18,7 +29,7 @@ const getStaticProps: GetStaticProps = () => {
         'Anyone who has ever applied for an apartment or a mortgage knows that these companies tend to collect much more information than they need to determine if you can afford the monthly payment...',
     },
     {
-      id: '2',
+      id: '5',
       imageAlt: 'Eli, our co-founder, being interviewed by Shardul Shah',
       url: 'https://www.indexventures.com/perspectives/footprint-emerges-from-stealth-to-transform-digital-identity/',
       imageUrl: '/media/index-ventures.png',
@@ -29,7 +40,7 @@ const getStaticProps: GetStaticProps = () => {
         'Today, when consumers are asked to verify their identity (such as creating a bank account, applying for a credit card, or onboarding to a new job), they have to go through the same process of entering...',
     },
     {
-      id: '3',
+      id: '4',
       imageAlt: 'Green silhouette of a person made up of 0 and 1',
       url: 'https://www.darkreading.com/dr-tech/new-startup-footprint-tackles-identity-verification',
       imageUrl: '/media/dark-reading.png',
@@ -40,7 +51,7 @@ const getStaticProps: GetStaticProps = () => {
         'Whenever a person walks into a financial services institution to open a bank account, apply for a loan, or obtain a credit card, that institution needs to first verify that person’s identity. Personal info...',
     },
     {
-      id: '4',
+      id: '3',
       imageAlt: 'Footprint logo',
       url: 'https://lererhippeau.medium.com/please-welcome-footprint-the-last-identity-form-youll-ever-fill-out-cc917d252b4e',
       imageUrl: '/media/lerer-hippeau.png',
@@ -52,7 +63,7 @@ const getStaticProps: GetStaticProps = () => {
         'Being a digital citizen requires using identity credentials, including SSN, DL, DOB, education and employment history, income, and more, online. Current verification processes are broken, though. They add...',
     },
     {
-      id: '5',
+      id: '2',
       imageAlt: 'Footprint logo',
       url: 'https://www.finsmes.com/2022/08/footprint-raises-6m-in-seed-funding.html',
       imageUrl: '/media/finsmes.png',
@@ -63,7 +74,7 @@ const getStaticProps: GetStaticProps = () => {
         'Footprint, a NYC-based company that allows companies to securely onboard users, raised $6m in Seed funding.',
     },
     {
-      id: '6',
+      id: '1',
       imageAlt: 'Footprint logo',
       url: 'https://www.prweb.com/releases/2022/8/prweb18825423.htm',
       imageUrl: '/media/pr-web.png',
