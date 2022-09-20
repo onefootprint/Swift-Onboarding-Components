37ca1f382e09b8993d090d753a26a3c280fd55b9
Belce Dogru <belce.dogru@gmail.com>
2022-09-20 09:39:19 -0400

    Add the stoplight workflow script (#795)

--- frontend/apps/docs/src/pages/api-docs/api-docs.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-// TODO: implement this component
-const ApiDocs = () => <Container>Hello!</Container>;
-
-const Container = styled.div``;
-
-export default ApiDocs;
