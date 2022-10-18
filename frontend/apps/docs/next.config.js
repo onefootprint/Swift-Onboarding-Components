38de7b1e5607940765d07f14b3dc4d974c5643d6
Rafael Michels Motta <rafael@onefootprint.com>
2022-10-18 12:03:18 -0300

    [FP-1602] Add security headers to docs (#1045)

--- frontend/apps/docs/next.config.js
+++ frontend/apps/docs/next.config.js
@@ -1,10 +1,68 @@
 /** @type {import('next').NextConfig} */
 
+const ContentSecurityPolicy = `
+  child-src onefootprint.com;
+  connect-src 'self' vitals.vercel-insights.com *.pusher.com wss://*.pusher.com vercel.live *.usefathom.com *.ingest.sentry.io www.youtube.com getform.io; 
+  default-src 'self' vitals.vercel-insights.com;
+  font-src 'self' fonts.googleapis.com fonts.gstatic.com;
+  form-action 'self';
+  frame-ancestors 'self';
+  frame-src 'self' vercel.live www.youtube.com form.typeform.com;
+  img-src 'self' data: assets.vercel.com vercel.live vercel.com;
+  media-src 'self' https;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' vercel.live *.usefathom.com vitals.vercel-insights.com tagmanager.google.com www.googletagmanager.com platform.twitter.com www.youtube.com static.ads-twitter.com connect.facebook.net www.facebook.com;
+  style-src 'self' 'unsafe-inline' tagmanager.google.com fonts.googleapis.com;
+  worker-src 'self' blob:;
+`;
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
 module.exports = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
   async redirects() {
     return [
