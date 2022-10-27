95027a87f84fb4ad214ca5b197e5847d4e2b2ef3
Rafael Michels Motta <rafael@onefootprint.com>
2022-10-27 12:51:45 -0300

    [FP-1734] Fix toast/redirect error on dashboard (#1140)

--- frontend/apps/dashboard/src/config/initializers/react-query-provider.tsx
+++ frontend/apps/dashboard/src/config/initializers/react-query-provider.tsx
@@ -1,5 +1,4 @@
-import { isFootprintError, isLogoutError } from '@onefootprint/request';
-import { useToast } from '@onefootprint/ui';
+import { isLogoutError } from '@onefootprint/request';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -10,39 +9,13 @@ type ReactQueryProviderProps = {
 };
 
 const ReactQueryProvider = ({ children }: ReactQueryProviderProps) => {
-  const { show } = useToast();
   const { logOut } = useSessionUser();
   const router = useRouter();
 
-  // Provide some default onError handlers to show a toast when an API error occurs.
-  // These can be overridden in the specific useMutation or useQuery arguments
   const handleError = (error: unknown) => {
-    // @ts-ignore
-    if (isFootprintError(error) && error.response) {
-      // @ts-ignore
-      if (isLogoutError(error)) {
-        // If we receive an HTTP 401, assume the auth token has expired and we should prompt to re-log in
-        // TODO one day, check that this is a 401 from an expired token
-        show(
-          {
-            description: `To keep your session secure, your login has expired. Please log back in.`,
-            title: 'Logged out',
-            variant: 'error',
-          },
-          15_000,
-        );
-        logOut();
-        router.push('/login');
-      } else {
-        show({
-          // @ts-ignore can't type the QueryClient's defaultOptions sadly
-          description: `There was an error making your request: ${
-            error.response.statusText || error.message
-          }`,
-          title: 'Uh-oh!',
-          variant: 'error',
-        });
-      }
+    if (isLogoutError(error)) {
+      logOut();
+      router.push('/login?session_expired=true');
     }
   };
 
