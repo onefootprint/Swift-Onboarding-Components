f56e0aecaac49b723fcacb2b1f0998d75e07095f
Elliott Forde <elliott@onefootprint.com>
2022-11-08 14:29:26 -0500

    Fix migration

--- backend/components/db/migrations/2022-11-07-021620_add_liveness_insight_id/up.sql
+++ backend/components/db/migrations/2022-11-07-021620_add_liveness_insight_id/up.sql
@@ -1,9 +1,9 @@
 
-ALTER TABLE liveness_event ADD COLUMN insight_event_id uuid;
-DELETE FROM liveness_event where insight_event_id=null;
+ALTER TABLE liveness_event ADD COLUMN insight_event_id UUID;
+DELETE FROM liveness_event where insight_event_id IS NULL;
 ALTER TABLE liveness_event ALTER COLUMN insight_event_id SET NOT NULL;
 
-ALTER TABLE liveness_event ADD CONSTRAINT fk_liveness_insight_event_id
+ALTER TABLE liveness_event ADD CONSTRAINT fk_liveness_event_insight_event_id
         FOREIGN KEY(insight_event_id)
         REFERENCES insight_event(id);
 
