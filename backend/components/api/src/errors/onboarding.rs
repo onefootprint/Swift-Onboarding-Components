a63db987f72faacecf12d77f378f567aeb968203
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2022-11-18 15:50:24 -0500

    Small wording for 2x ob error (#1343)

--- backend/components/api/src/errors/onboarding.rs
+++ backend/components/api/src/errors/onboarding.rs
@@ -29,6 +29,6 @@ pub enum OnboardingError {
     NoPendingDocumentRequestFound(DocumentRequestId),
     #[error("No decision can be made from decision engine")]
     NoDecisionMade,
-    #[error("This user has already onboarded onto the tenant. Footprint does not yet support this use case")]
+    #[error("This user has already has an onboarding for the tenant. Footprint does not yet support this use case")]
     UserOnboardedOntoTenant,
 }
