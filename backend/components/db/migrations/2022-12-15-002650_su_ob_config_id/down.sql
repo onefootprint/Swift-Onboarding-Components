a351415be234e037113611ddb9e24a716c1a710b
Elliott Forde <elliott@onefootprint.com>
2022-12-15 16:37:12 -0800

    Fix master build
    https://onefootprint.slack.com/archives/C03BC0SJ9GD/p1671149754638909
    
    Main problem is that we run integration tests that created multiple scoped users for a single tenant, and then when we tried to run down.sql, we could create a unique constraint on (user, tenant).
    
    There's no easy way to delete one of the conflicting scoped users since there are so many related records...
    
    Then, in up.sql, we had some problems creating and index in the same transaction in which we backfill. So I tricked diesel to commit the transaction it starts, then open a new one to create the indexes.

--- backend/components/db/migrations/2022-12-15-002650_su_ob_config_id/down.sql
+++ backend/components/db/migrations/2022-12-15-002650_su_ob_config_id/down.sql
@@ -1,3 +1,4 @@
 ALTER TABLE scoped_user DROP COLUMN ob_configuration_id;
 
-CREATE UNIQUE INDEX scoped_users_unique_user_vault_id_tenant_id ON scoped_user(user_vault_id, tenant_id);
+-- Note, doesn't recreate the index since there may now be multiple scoped_users for one tenant
+-- CREATE UNIQUE INDEX scoped_users_unique_user_vault_id_tenant_id ON scoped_user(user_vault_id, tenant_id);
--- backend/components/db/migrations/2022-12-15-002650_su_ob_config_id/up.sql
+++ backend/components/db/migrations/2022-12-15-002650_su_ob_config_id/up.sql
@@ -10,13 +10,18 @@ UPDATE scoped_user
     FROM onboarding
     WHERE onboarding.scoped_user_id = scoped_user.id;
 
-CREATE INDEX scoped_user_ob_configuration_id ON scoped_user(ob_configuration_id);
-
 -- Drop old uniqueness index
-DROP INDEX scoped_users_unique_user_vault_id_tenant_id;
+DROP INDEX IF EXISTS scoped_users_unique_user_vault_id_tenant_id;
 
+-- Diesel migrations are always run inside of a transaction.
+-- Postgres will sometimes complain when you edit data (like backfilling ob_configuration_id) and the schema
+-- in the same transaction. So this just splits the part that was complaining into a separate PR
+COMMIT;
+BEGIN;
+
+CREATE INDEX IF NOT EXISTS scoped_user_ob_configuration_id ON scoped_user(ob_configuration_id);
 -- We need two different indexes to enforce uniqueness property since in postgres NULL != NULL.
 -- First, enforce that there's only one scoped user for each (user, ob_config) pair.
-CREATE UNIQUE INDEX scoped_users_unique_user_vault_id_ob_config_id ON scoped_user(user_vault_id, ob_configuration_id);
+CREATE UNIQUE INDEX IF NOT EXISTS scoped_user_unique_user_vault_id_ob_config_id ON scoped_user(user_vault_id, ob_configuration_id);
 -- Then, where ob_config is NULL, enforce there's only one scoped user for each (user, tenant).
-CREATE UNIQUE INDEX scoped_users_unique_user_vault_id_tenant_id_no_ob_config ON scoped_user(user_vault_id, tenant_id) WHERE ob_configuration_id IS NULL;
+CREATE UNIQUE INDEX IF NOT EXISTS scoped_user_unique_user_vault_id_tenant_id_no_ob_config ON scoped_user(user_vault_id, tenant_id) WHERE ob_configuration_id IS NULL;
