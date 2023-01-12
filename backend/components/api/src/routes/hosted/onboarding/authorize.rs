1724c53c2ccff34393cf53fc179ed4f9c75d8c81
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2023-01-12 14:47:45 -0500

    Return status in /authorize (#1813)
    ## Describe your changes
    
    Return onboarding status so that the frontend can render a page
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/api/src/routes/hosted/onboarding/authorize.rs
+++ backend/components/api/src/routes/hosted/onboarding/authorize.rs
@@ -5,7 +5,9 @@ use crate::errors::ApiError;
 use crate::hosted::onboarding::get_requirements;
 use crate::types::response::ResponseData;
 use crate::State;
+use db::models::onboarding::Onboarding;
 use db::models::onboarding::OnboardingUpdate;
+use newtypes::OnboardingStatus;
 use newtypes::SessionAuthToken;
 use paperclip::actix::{self, api_v2_operation, web, web::Json, Apiv2Schema};
 
@@ -13,6 +15,7 @@ use paperclip::actix::{self, api_v2_operation, web, web::Json, Apiv2Schema};
 pub struct CommitResponse {
     /// Footprint validation token
     validation_token: SessionAuthToken,
+    status: OnboardingStatus,
 }
 
 #[api_v2_operation(
@@ -27,7 +30,7 @@ pub async fn post(
     let user_auth = user_auth.check_permissions(vec![UserAuthScopeDiscriminant::OrgOnboarding])?;
 
     let session_key = state.session_sealing_key.clone();
-    let validation_token = state
+    let (validation_token, status) = state
         .db_pool
         .db_query(move |conn| -> Result<_, ApiError> {
             // Verify there are no unmet requirements
@@ -39,13 +42,25 @@ pub async fn post(
             }
 
             // Mark the onboarding as authorized and create validation token
-            let ob = ob.update(conn, OnboardingUpdate::is_authorized(true))?;
+            let ob_id = ob.id.clone();
+            ob.update(conn, OnboardingUpdate::is_authorized(true))?;
+
+            // Return status as well
+            let (ob, _, _, latest_decision) = Onboarding::get(conn, &ob_id)?;
+
+            let status = latest_decision
+                .and_then(|d| d.visible_status())
+                .ok_or(OnboardingError::NonTerminalState)?;
+
             let validation_token = super::create_onboarding_validation_token(conn, &session_key, ob.id)?;
-            Ok(validation_token)
+            Ok((validation_token, status))
         })
         .await??;
 
     Ok(Json(ResponseData {
-        data: CommitResponse { validation_token },
+        data: CommitResponse {
+            validation_token,
+            status,
+        },
     }))
 }
