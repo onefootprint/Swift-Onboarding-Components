ded0fda87c1de437826f55a4c0db6992d0fd7848
Elliott Forde <elliott@onefootprint.com>
2023-01-17 22:57:54 -0700

    Rm unnecessary TODO
    Resolves FP-2381

--- backend/components/api/src/routes/proxy/index.rs
+++ backend/components/api/src/routes/proxy/index.rs
@@ -190,8 +190,6 @@ async fn detokenize(
             .db_query(move |conn| -> ApiResult<_> {
                 let scoped_user = ScopedUser::get(conn, (&fp_id, &tenant_id, is_live))?;
                 let uvw = UserVaultWrapper::build_for_tenant(conn, &scoped_user.id)?;
-                // TODO how do we check perms for custom data? feels like always allowed, only gated
-                // by tenant_role. I think this will break rn
                 Ok((uvw, scoped_user))
             })
             .await??;
