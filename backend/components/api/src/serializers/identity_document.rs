28dbd3d72dae194c8c0e341d0bb0e34ee06320d3
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2023-01-19 17:28:51 -0500

    add identity doc type to user timeline (#1928)
    ## Describe your changes
    
    Forgot to add this in previous PR
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/api/src/serializers/identity_document.rs
+++ backend/components/api/src/serializers/identity_document.rs
@@ -4,14 +4,25 @@ use crate::utils::db2api::DbToApi;
 
 impl DbToApi<(IdentityDocument, DocumentRequest)> for api_wire_types::IdentityDocumentTimelineEvent {
     fn from_db((identity_doc, document_request): (IdentityDocument, DocumentRequest)) -> Self {
-        let IdentityDocument { id, created_at, .. } = identity_doc;
+        let IdentityDocument {
+            id,
+            created_at,
+            document_type,
+            ..
+        } = identity_doc;
 
-        let DocumentRequest { status, .. } = document_request;
+        let DocumentRequest {
+            status,
+            should_collect_selfie,
+            ..
+        } = document_request;
 
         Self {
             id,
             timestamp: created_at,
             status,
+            document_type,
+            selfie_collected: should_collect_selfie,
         }
     }
 }
--- backend/components/api_wire_types/src/resources/identity_document_timeline_event.rs
+++ backend/components/api_wire_types/src/resources/identity_document_timeline_event.rs
@@ -1,11 +1,13 @@
 use crate::{export_schema, Apiv2Schema, DateTime, Deserialize, JsonSchema, Serialize, Utc};
-use newtypes::{DocumentRequestStatus, IdentityDocumentId};
+use newtypes::{DocumentRequestStatus, IdDocKind, IdentityDocumentId};
 
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, Apiv2Schema)]
 pub struct IdentityDocumentTimelineEvent {
     pub id: IdentityDocumentId,
     pub timestamp: DateTime<Utc>,
     pub status: DocumentRequestStatus,
+    pub document_type: IdDocKind,
+    pub selfie_collected: bool,
 }
 
 export_schema!(IdentityDocumentTimelineEvent);
