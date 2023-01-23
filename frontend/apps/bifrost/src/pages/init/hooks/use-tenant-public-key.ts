a7e65c8cec2c09f9a498150193adb5d36853f48b
Belce Dogru <belce.dogru@gmail.com>
2023-01-23 11:41:59 -1000

    Bifrost init page should check if router is ready before getting tenantPk (#1967)
    ## Describe your changes
    - The bifrost app hangs on the initial loading page because of a timing
    issue where we are unable to fetch the onboarding config with an empty
    tenant pk (since router wasn't ready)
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- frontend/apps/bifrost/src/pages/init/hooks/use-tenant-public-key.ts
+++ frontend/apps/bifrost/src/pages/init/hooks/use-tenant-public-key.ts
@@ -2,7 +2,14 @@ import { useRouter } from 'next/router';
 
 const useTenantPublicKey = () => {
   const router = useRouter();
-  return (router.query.public_key || '') as string;
+  const tenantPk = router.query.public_key;
+  if (!router.isReady || !tenantPk) {
+    return '';
+  }
+  if (typeof tenantPk === 'string') {
+    return tenantPk;
+  }
+  return tenantPk[0];
 };
 
 export default useTenantPublicKey;
