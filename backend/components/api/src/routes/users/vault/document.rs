1065ece61872f433c2fc3a7a3ac70b9a1fd7b9b3
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2023-01-24 15:26:06 -0500

    Update decrypt doc endpoint to return uploaded_at and status (#1977)
    ## Describe your changes
    
    Updates the decrypt document endpoint to allow grouping by
    Failed/Success.
    
    In this PR, failure and success is determined purely by the document
    request status, which
    is just an indicator of whether the document was able to be accepted
    analyzed or not by a vendor.
    
    Although the Failure case is easier, Complete bifurcates into 2
    possibilities:
    * the document was accepted (and was verified)
    * the document was accepted (and was not verified)
    
    We don't have a good way of distinguishing between those 2 cases at this
    point, so that will be in a followup once the design is aligned on
    
    cc @belcedogru 
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/api/src/routes/users/vault/document.rs
+++ backend/components/api/src/routes/users/vault/document.rs
@@ -9,17 +9,22 @@ use crate::errors::ApiError;
 use crate::types::{JsonApiResponse, ResponseData};
 
 use crate::utils::headers::InsightHeaders;
+use crate::utils::user_vault_wrapper::identity_document::DecryptDocumentResult;
 use crate::utils::user_vault_wrapper::{DecryptRequest, TenantUvw, UserVaultWrapper};
 use crate::State;
 
 use api_wire_types::{
-    DecryptIdentityDocumentRequest, DecryptIdentityDocumentResponse, GetIdentityDocumentForDecryptResponse,
-    ImageData,
+    DecryptIdentityDocumentRequest, DecryptIdentityDocumentResponse, DecryptedDocumentStatus,
+    GetIdentityDocumentForDecryptResponse, ImageData,
 };
+use chrono::{DateTime, Utc};
+use db::models::document_request::DocumentRequest;
+use db::models::identity_document::IdentityDocument;
 use db::models::insight_event::CreateInsightEvent;
 use db::models::scoped_user::ScopedUser;
+use db::PgConnection;
 use itertools::Itertools;
-use newtypes::{DataIdentifier, FootprintUserId, IdDocKind};
+use newtypes::{DataIdentifier, DocumentRequestStatus, FootprintUserId, IdDocKind, IdentityDocumentId};
 
 use paperclip::actix::{self, api_v2_operation, web, web::Json, web::Path};
 use strum::IntoEnumIterator;
@@ -114,14 +119,10 @@ pub async fn post_decrypt(
         .decrypt_document(&state, document_type, include_selfie, req)
         .await?;
 
-    let images = decrypted_docs
-        .into_iter()
-        .map(|doc| ImageData {
-            front: doc.front.into_leak_base64().to_string_standard(),
-            back: doc.back.map(|p| p.into_leak_base64().to_string_standard()),
-            selfie: doc.selfie.map(|p| p.into_leak_base64().to_string_standard()),
-        })
-        .collect();
+    let images = state
+        .db_pool
+        .db_query(move |conn| create_image_data(conn, decrypted_docs))
+        .await??;
 
     let res = DecryptIdentityDocumentResponse {
         document_type,
@@ -135,4 +136,51 @@ pub async fn post_decrypt(
 /// expired/is_valid etc
 fn available_images_from_uvw(uvw: &TenantUvw) -> HashSet<IdDocKind> {
     uvw.identity_documents().iter().map(|i| i.document_type).collect()
+}
+
+type StatusAndUploadedAt = (DecryptedDocumentStatus, DateTime<Utc>);
+fn create_image_data(
+    conn: &mut PgConnection,
+    decrypted_docs: Vec<DecryptDocumentResult>,
+) -> Result<Vec<ImageData>, ApiError> {
+    // For this, we'll create the status just based on whether or not the upload was successful
+    let mut status_map: HashMap<IdentityDocumentId, StatusAndUploadedAt> =
+        IdentityDocument::get_bulk_with_requests(
+            conn,
+            decrypted_docs.iter().map(|d| &d.identity_document_id).collect(),
+        )?
+        .into_iter()
+        .filter_map(|(id, (id_doc, doc_request))| {
+            status_for_document(&doc_request).map(|s| (id, (s, id_doc.created_at)))
+        })
+        .collect();
+
+    let images = decrypted_docs
+        .into_iter()
+        .filter_map(|doc| {
+            status_map
+                .remove(&doc.identity_document_id)
+                .map(|(status, uploaded_at)| ImageData {
+                    front: doc.front.into_leak_base64().to_string_standard(),
+                    back: doc.back.map(|p| p.into_leak_base64().to_string_standard()),
+                    selfie: doc.selfie.map(|p| p.into_leak_base64().to_string_standard()),
+                    status,
+                    uploaded_at,
+                })
+        })
+        .collect();
+
+    Ok(images)
+}
+
+fn status_for_document(document_request: &DocumentRequest) -> Option<DecryptedDocumentStatus> {
+    match document_request.status {
+        DocumentRequestStatus::Failed => Some(DecryptedDocumentStatus::Fail),
+        DocumentRequestStatus::Complete => Some(DecryptedDocumentStatus::Success),
+        // If doc request is not in failed/complete, there's nothing to return
+        // this is kept completely enumerated so that we don't miss doc request status variants
+        DocumentRequestStatus::Pending => None,
+        DocumentRequestStatus::Uploaded => None,
+        DocumentRequestStatus::UploadFailed => None,
+    }
 }
--- backend/components/api_wire_types/src/requests/decrypt_document_request.rs
+++ backend/components/api_wire_types/src/requests/decrypt_document_request.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use newtypes::{flat_api_object_map_type, IdDocKind};
 use paperclip::actix::Apiv2Schema;
 use serde::{Deserialize, Serialize};
@@ -20,6 +21,17 @@ pub struct DecryptIdentityDocumentRequest {
     pub include_selfie: bool,
 }
 
+/// This status is used to group and display decrypted documents in the dashboard
+/// It is derived from `DocumentRequestStatus` in that it represents whether or not the document
+/// was successfully uploaded AND usable by our vendors to make a determination.
+///
+/// It does NOT represent whether the document was verified or not.
+#[derive(Debug, Clone, Apiv2Schema, Serialize)]
+pub enum DecryptedDocumentStatus {
+    Success,
+    Fail,
+}
+
 #[derive(Debug, Clone, Apiv2Schema, Serialize)]
 pub struct ImageData {
     // Base64 encoded image
@@ -28,6 +40,9 @@ pub struct ImageData {
     pub back: Option<String>,
     // Base64 encoded string
     pub selfie: Option<String>,
+    // the time the IdentityDocument was created in our database
+    pub uploaded_at: DateTime<Utc>,
+    pub status: DecryptedDocumentStatus,
 }
 
 #[derive(Debug, Clone, Apiv2Schema, Serialize)]
