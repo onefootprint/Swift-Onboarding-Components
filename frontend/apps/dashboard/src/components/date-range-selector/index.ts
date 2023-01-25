c3e9b0e09ad1697215b931be7739e5c1697dc6cf
Rafael Michels Motta <rafael@onefootprint.com>
2023-01-25 18:00:58 -0300

    Remove unused code (#2006)

--- frontend/apps/dashboard/src/components/date-range-selector/index.ts
+++
@@ -1,3 +0,0 @@
-export { default } from './date-range-selector';
-export type { DateRangeSelectorFormValues } from './date-range-selector.types';
-export { dateRangeSelectorFormSchema } from './date-range-selector.types';
