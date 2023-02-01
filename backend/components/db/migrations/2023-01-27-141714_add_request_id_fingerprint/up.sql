7bf05627412dab4c7ebab954af6facdce90930c7
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2023-01-31 22:08:22 -0500

    Fix fingerprint js table migration (#2099)
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/db/migrations/2023-01-27-141714_add_request_id_fingerprint/up.sql
+++ backend/components/db/migrations/2023-01-27-141714_add_request_id_fingerprint/up.sql
@@ -1,2 +1,4 @@
+TRUNCATE fingerprint_visit_event;
+
 ALTER TABLE fingerprint_visit_event ADD COLUMN request_id TEXT NOT NULL;
 CREATE INDEX IF NOT EXISTS fingerprint_visit_event_request_id ON fingerprint_visit_event(request_id);
