ff9b66a816005ba0fc2f3e55bae79cdb0ed3f97a
Belce Dogru <belce.dogru@gmail.com>
2023-02-10 10:01:07 -0500

    Add test for pagination component - was mostly done by Tensai, added a few more. (#2218)

---
+++ frontend/packages/ui/src/components/pagination/pagination.test.tsx
@@ -0,0 +1,110 @@
+import { customRender, screen, userEvent } from '@onefootprint/test-utils';
+import React from 'react';
+
+import Pagination, { PaginationProps } from './pagination';
+
+describe('<Pagination />', () => {
+  const renderPagination = ({
+    hasNextPage = false,
+    hasPrevPage = false,
+    pageIndex = 0,
+    pageSize = 10,
+    totalNumResults = 0,
+    onNextPage = jest.fn(),
+    onPrevPage = jest.fn(),
+  }: Partial<PaginationProps>) =>
+    customRender(
+      <Pagination
+        hasNextPage={hasNextPage}
+        hasPrevPage={hasPrevPage}
+        pageIndex={pageIndex}
+        pageSize={pageSize}
+        totalNumResults={totalNumResults}
+        onNextPage={onNextPage}
+        onPrevPage={onPrevPage}
+      />,
+    );
+
+  it('should render the text', () => {
+    renderPagination({});
+    expect(screen.getByText('No results')).toBeInTheDocument();
+  });
+
+  it('should render the text when there are results', () => {
+    renderPagination({
+      hasNextPage: true,
+      hasPrevPage: true,
+      pageIndex: 0,
+      pageSize: 10,
+      totalNumResults: 20,
+    });
+    expect(
+      screen.getByText('Showing 1 to 10 of 20 total results'),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the correct text when on the last page', () => {
+    renderPagination({
+      hasNextPage: false,
+      hasPrevPage: true,
+      pageIndex: 1,
+      pageSize: 10,
+      totalNumResults: 20,
+    });
+    expect(
+      screen.getByText('Showing 11 to 20 of 20 total results'),
+    ).toBeInTheDocument();
+  });
+
+  it('Disables previous button when there is no previous page', () => {
+    renderPagination({
+      hasNextPage: true,
+      hasPrevPage: false,
+      pageIndex: 0,
+      pageSize: 10,
+      totalNumResults: 20,
+    });
+    expect(screen.getByText('Previous')).toBeDisabled();
+  });
+
+  it('Disables next button when there is no next page', () => {
+    renderPagination({
+      hasNextPage: false,
+      hasPrevPage: true,
+      pageIndex: 1,
+      pageSize: 10,
+      totalNumResults: 20,
+    });
+    expect(screen.getByText('Next')).toBeDisabled();
+  });
+
+  it('should call the onNextPage function when the next button is clicked', async () => {
+    const onNextPage = jest.fn();
+    renderPagination({
+      hasNextPage: true,
+      hasPrevPage: true,
+      pageIndex: 0,
+      pageSize: 10,
+      totalNumResults: 20,
+      onNextPage,
+    });
+
+    await userEvent.click(screen.getByText('Next'));
+    expect(onNextPage).toHaveBeenCalled();
+  });
+
+  it('should call the onPrevPage function when the previous button is clicked', async () => {
+    const onPrevPage = jest.fn();
+    renderPagination({
+      hasNextPage: true,
+      hasPrevPage: true,
+      pageIndex: 1,
+      pageSize: 10,
+      totalNumResults: 20,
+      onPrevPage,
+    });
+
+    await userEvent.click(screen.getByText('Previous'));
+    expect(onPrevPage).toHaveBeenCalled();
+  });
+});
