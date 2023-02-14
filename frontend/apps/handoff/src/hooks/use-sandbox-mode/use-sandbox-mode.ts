9d6ebaf05bb35479c572403301c257f07583cc4f
Belce Dogru <belce.dogru@gmail.com>
2023-02-14 08:21:04 -0700

    [FP-2824] Add sandbox banner to handoff app (#2260)

--- frontend/apps/handoff/src/hooks/use-sandbox-mode/use-sandbox-mode.ts
+++ frontend/apps/handoff/src/hooks/use-sandbox-mode/use-sandbox-mode.ts
@@ -1,5 +1,9 @@
-const useSandboxMode = () =>
-  // TODO: implement later
-  ({ isSandbox: false });
+import useHandoffMachine from '../use-handoff-machine';
+
+const useSandboxMode = () => {
+  const [state] = useHandoffMachine();
+  const { onboardingConfig: tenant } = state.context;
+  return { isSandbox: tenant?.isLive === false };
+};
 
 export default useSandboxMode;
