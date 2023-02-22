e908b48705091df17cbb8d2698084d424a981e40
Belce Dogru <belce.dogru@gmail.com>
2023-02-22 14:04:15 -0500

    Implement bootstrap sms challenge (#2383)

--- frontend/apps/bifrost/src/pages/identify/pages/bootstrap-challenge/components/sms-challenge/sms-challenge.tsx
+++ frontend/apps/bifrost/src/pages/identify/pages/bootstrap-challenge/components/sms-challenge/sms-challenge.tsx
@@ -1,8 +1,159 @@
-import React from 'react';
+import {
+  useIdentifyVerify,
+  useLoginChallenge,
+} from '@onefootprint/footprint-elements';
+import { useRequestErrorToast, useTranslation } from '@onefootprint/hooks';
+import {
+  ChallengeKind,
+  IdentifyVerifyResponse,
+  LoginChallengeResponse,
+} from '@onefootprint/types';
+import { useToast } from '@onefootprint/ui';
+import React, { useState } from 'react';
+import useIdentifyMachine, { Events } from 'src/hooks/use-identify-machine';
+import SmsChallengeVerification from 'src/pages/identify/components/sms-challenge-verification';
+import { useEffectOnce } from 'usehooks-ts';
 
-const SmsChallenge = () => (
-  // TODO: implement using SmsChallengeVerification
-  <div>SmsChallenge</div>
-);
+const SUCCESS_EVENT_DELAY_MS = 1500;
+
+const SmsChallenge = () => {
+  const { t } = useTranslation('pages.bootstrap-challenge.sms-challenge');
+  const [state, send] = useIdentifyMachine();
+  const {
+    identify: { successfulIdentifier },
+    tenantPk,
+    challenge: { challengeData },
+  } = state.context;
+
+  const toast = useToast();
+  const showRequestErrorToast = useRequestErrorToast();
+
+  const loginChallengeMutation = useLoginChallenge();
+  const identifyVerifyMutation = useIdentifyVerify();
+
+  const [isSuccess, setSuccess] = useState(false);
+  const hasError = identifyVerifyMutation.isError;
+  const isLoading =
+    loginChallengeMutation.isLoading || identifyVerifyMutation.isLoading;
+
+  const handlePinValidationSucceeded = ({
+    authToken,
+  }: IdentifyVerifyResponse) => {
+    // Only send the user email to the backend if we are onboarding the user for
+    // the first time
+    if (!authToken) {
+      console.error(
+        'Received empty auth token from successful sms pin verification.',
+      );
+      return;
+    }
+
+    setSuccess(true);
+    setTimeout(() => {
+      send({
+        type: Events.challengeSucceeded,
+        payload: {
+          authToken,
+        },
+      });
+    }, SUCCESS_EVENT_DELAY_MS);
+  };
+
+  const handleRequestChallengeSuccess = (payload: LoginChallengeResponse) => {
+    if (payload.challengeData.challengeKind !== ChallengeKind.sms) {
+      console.error(
+        'Received biometric challenge after requesting bootstrap SMS challenge',
+      );
+      return;
+    }
+    send({
+      type: Events.challengeInitiated,
+      payload: {
+        challengeData: payload.challengeData,
+      },
+    });
+  };
+
+  const handleComplete = (pin: string) => {
+    if (!challengeData) {
+      return;
+    }
+
+    const { challengeToken } = challengeData;
+    identifyVerifyMutation.mutate(
+      {
+        challengeResponse: pin,
+        challengeToken,
+        tenantPk,
+      },
+      {
+        onSuccess: handlePinValidationSucceeded,
+        onError: handleRequestError,
+      },
+    );
+  };
+
+  const handleResend = () => {
+    if (!successfulIdentifier) {
+      console.error(
+        'No successful identifier found while resending bootstrap SMS challenge',
+      );
+      return;
+    }
+
+    loginChallengeMutation.mutate(
+      {
+        identifier: successfulIdentifier,
+        preferredChallengeKind: ChallengeKind.sms,
+      },
+      {
+        onSuccess: payload => {
+          toast.show({
+            title: t('resend.success.title'),
+            description: t('resend.success.description'),
+          });
+          handleRequestChallengeSuccess(payload);
+        },
+        onError: handleRequestError,
+      },
+    );
+  };
+
+  const handleRequestError = (error: unknown) => {
+    showRequestErrorToast(error);
+    console.error(error);
+  };
+
+  useEffectOnce(() => {
+    if (!successfulIdentifier) {
+      console.error(
+        'No successful identifier found while initiating bootstrap SMS challenge',
+      );
+      return;
+    }
+
+    loginChallengeMutation.mutate(
+      {
+        identifier: successfulIdentifier,
+        preferredChallengeKind: ChallengeKind.sms,
+      },
+      {
+        onSuccess: handleRequestChallengeSuccess,
+        onError: handleRequestError,
+      },
+    );
+  });
+
+  return (
+    <SmsChallengeVerification
+      isLoading={isLoading}
+      isSuccess={isSuccess}
+      hasError={hasError}
+      onComplete={handleComplete}
+      resendDisabledUntil={challengeData?.retryDisabledUntil}
+      onResend={handleResend}
+    />
+  );
+};
 
 export default SmsChallenge;
