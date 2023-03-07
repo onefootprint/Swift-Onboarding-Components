297e45845e7537fb52e9e82eb024ff33209d7c16
Elliott Forde <elliott@onefootprint.com>
2023-03-07 12:37:06 -0800

    Remove boolean flags for document and selfie in ob config list API (#2481)
    ## Describe your changes
    
    The reads for these were removed in #2480 - now can simplify the
    serialization of Ob configs!

--- backend/components/api/src/serializers/onboarding_configuration.rs
+++ backend/components/api/src/serializers/onboarding_configuration.rs
@@ -4,10 +4,6 @@ use crate::utils::db2api::DbToApi;
 
 impl DbToApi<(ObConfiguration, Tenant)> for api_wire_types::OnboardingConfiguration {
     fn from_db((ob_config, tenant): (ObConfiguration, Tenant)) -> Self {
-        let must_collect_identity_document = ob_config.must_collect_document();
-        let must_collect_selfie = ob_config.must_collect_selfie();
-        let can_access_identity_document_images = ob_config.can_access_document();
-        let can_access_selfie_image = ob_config.can_access_selfie();
         let ObConfiguration {
             id,
             key,
@@ -34,10 +30,6 @@ impl DbToApi<(ObConfiguration, Tenant)> for api_wire_types::OnboardingConfigurat
             privacy_policy_url,
             must_collect_data,
             can_access_data,
-            must_collect_identity_document,
-            can_access_identity_document_images,
-            must_collect_selfie,
-            can_access_selfie_image,
             is_live,
             created_at,
             status,
@@ -47,15 +39,9 @@ impl DbToApi<(ObConfiguration, Tenant)> for api_wire_types::OnboardingConfigurat
 
 impl DbToApi<ObConfiguration> for api_wire_types::LiteObConfiguration {
     fn from_db(ob_configuration: ObConfiguration) -> Self {
-        let must_collect_identity_document = ob_configuration.must_collect_document();
-        let must_collect_selfie = ob_configuration.must_collect_selfie();
         let ObConfiguration {
             must_collect_data, ..
         } = ob_configuration;
-        api_wire_types::LiteObConfiguration {
-            must_collect_data,
-            must_collect_identity_document,
-            must_collect_selfie,
-        }
+        api_wire_types::LiteObConfiguration { must_collect_data }
     }
 }
