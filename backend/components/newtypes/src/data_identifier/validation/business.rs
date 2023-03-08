07d49d0dfe045b87262ed09a5228bf563a21f1ee
Elliott Forde <elliott@onefootprint.com>
2023-03-08 07:34:13 -0800

    Add basic validation skeleton for business data (#2508)
    ## Describe your changes
    
    Very basic skeloton for validating business data

---
+++ backend/components/newtypes/src/data_identifier/validation/business.rs
@@ -0,0 +1,57 @@
+use super::VResult;
+use crate::{BusinessDataKind as BDK, PhoneNumber, PiiString};
+use crate::{NtResult, Validate};
+
+impl Validate for BDK {
+    fn validate(&self, value: PiiString, _for_bifrost: bool) -> NtResult<PiiString> {
+        let result = match self {
+            BDK::Name => value,
+            BDK::Website => value,
+            BDK::PhoneNumber => PhoneNumber::parse(value)?.e164_with_suffix(),
+            BDK::Ein => value,
+            BDK::AddressLine1 => value,
+            BDK::AddressLine2 => value,
+            BDK::City => value,
+            BDK::State => value,
+            BDK::Zip => super::clean_and_validate_zip(value)?,
+            BDK::Country => super::clean_and_validate_country(value)?,
+            BDK::BeneficialOwners => clean_and_validate_beneficial_owners(value)?,
+        };
+        Ok(result)
+    }
+}
+
+fn clean_and_validate_beneficial_owners(input: PiiString) -> VResult<PiiString> {
+    // TODO one day, json deserialize the BOs and make sure they follow some spec
+    Ok(input)
+}
+
+#[cfg(test)]
+mod test {
+    use super::BDK::*;
+    use crate::BusinessDataKind as BDK;
+    use crate::PiiString;
+    use crate::Validate;
+    use test_case::test_case;
+
+    // TODO more tests
+    #[test_case(AddressLine1, "100 Nitro Way@" => Some("100 Nitro Way@".to_owned()))]
+    #[test_case(AddressLine1, "100 Enclave Way" => Some("100 Enclave Way".to_owned()))]
+    #[test_case(AddressLine2, "#1" => Some("#1".to_owned()))]
+    #[test_case(City, "Footprint" => Some("Footprint".to_owned()))]
+    #[test_case(City, "_Footprint1" => Some("_Footprint1".to_owned()))] // We don't care about special chars
+    #[test_case(State, "CA" => Some("CA".to_owned()))]
+    #[test_case(State, "CA1" => Some("CA1".to_owned()))] // We don't care about special chars
+    #[test_case(Zip, "flerp!" => None)]
+    #[test_case(Zip, "12345" => Some("12345".to_owned()))]
+    #[test_case(Country, "BLERP" => None)]
+    #[test_case(Country, "US" => Some("US".to_owned()))]
+    #[test_case(PhoneNumber, "flerp" => None)]
+    #[test_case(PhoneNumber, "+1-555-555-5555" => Some("+15555555555".to_owned()))]
+    #[test_case(PhoneNumber, "+15555555555#sandbox" => Some("+15555555555#sandbox".to_owned()))] // Sandbox phone
+    fn test_clean_and_validate_field_not_bifrost(bdk: BDK, pii: &str) -> Option<String> {
+        bdk.validate(PiiString::new(pii.to_owned()), false)
+            .ok()
+            .map(|pii| pii.leak_to_string())
+    }
+}
--- backend/components/newtypes/src/data_identifier/validation/identity.rs
+++ backend/components/newtypes/src/data_identifier/validation/identity.rs
@@ -1,6 +1,5 @@
 use crate::{email::Email, NtResult, Validate};
 use chrono::{Datelike, NaiveDate};
-use regex::Regex;
 use std::str::FromStr;
 
 use crate::{IdentityDataKind as IDK, PhoneNumber, PiiString};
@@ -19,8 +18,8 @@ impl Validate for IDK {
             IDK::AddressLine2 => value,
             IDK::City => value,
             IDK::State => value, // maybe we'll want to validate state based on country some day
-            IDK::Zip => clean_and_validate_zip(value)?,
-            IDK::Country => clean_and_validate_country(value)?,
+            IDK::Zip => super::clean_and_validate_zip(value)?,
+            IDK::Country => super::clean_and_validate_country(value)?,
             IDK::Email => Email::from_str(value.leak())?.to_piistring(),
             IDK::PhoneNumber => PhoneNumber::parse(value)?.e164_with_suffix(),
         };
@@ -36,7 +35,7 @@ fn clean_and_validate_dob(input: PiiString, for_bifrost: bool) -> VResult<PiiStr
 }
 
 fn validate_name(input: PiiString, for_bifrost: bool) -> VResult<PiiString> {
-    if for_bifrost && input.leak().is_empty() || input.leak().len() > 1000 {
+    if for_bifrost && (input.leak().is_empty() || input.leak().len() > 1000) {
         return Err(Error::InvalidLength);
     }
 
@@ -44,7 +43,7 @@ fn validate_name(input: PiiString, for_bifrost: bool) -> VResult<PiiString> {
 }
 
 fn validate_address(input: PiiString, for_bifrost: bool) -> VResult<PiiString> {
-    if for_bifrost && input.leak().is_empty() || input.leak().len() > 1000 {
+    if for_bifrost && (input.leak().is_empty() || input.leak().len() > 1000) {
         return Err(Error::InvalidLength);
     }
 
@@ -69,48 +68,6 @@ fn clean_and_validate_ssn9(input: PiiString) -> VResult<PiiString> {
     }
     if input.leak().chars().any(|c| !c.is_ascii_digit()) {
         return Err(Error::NonDigitCharacter);
-    }
-    Ok(input)
-}
-
-lazy_static! {
-    // some zip codes are alphanumeric, can also contain -'s and spaces
-    // simple regex just to make sure input is base-level clean
-    // https://en.wikipedia.org/wiki/Postal_code#Alphanumeric_postal_codes
-    pub static ref ZIP_CHARS: Regex = Regex::new(r"^([A-Za-z0-9\- ]*)$").unwrap();
-}
-
-fn clean_and_validate_zip(input: PiiString) -> VResult<PiiString> {
-    if !ZIP_CHARS.is_match(input.leak()) {
-        return Err(Error::InvalidZipCharacter);
-    }
-    Ok(input)
-}
-
-/// list of valid iso3166-alpha-2 country codes, from https://datahub.io/core/country-codes#data
-/// eventually we should maybe just pony up and pay for the subscription to iso: https://www.iso.org/publication/PUB500001.html
-/// Channel islands does not have a country code
-const ISO_3166_COUNTRIES: [&str; 249] = [
-    "TW", "AF", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS",
-    "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "VG",
-    "BN", "BG", "BF", "BI", "CV", "KH", "CM", "CA", "KY", "CF", "TD", "CL", "CN", "HK", "MO", "CX", "CC",
-    "CO", "KM", "CG", "CK", "CR", "HR", "CU", "CW", "CY", "CZ", "CI", "KP", "CD", "DK", "DJ", "DM", "DO",
-    "EC", "EG", "SV", "GQ", "ER", "EE", "SZ", "ET", "FK", "FO", "FJ", "FI", "FR", "GF", "PF", "TF", "GA",
-    "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM",
-    "VA", "HN", "HU", "IS", "IN", "ID", "IR", "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ",
-    "KE", "KI", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MG", "MW", "MY", "MV",
-    "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA",
-    "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU", "NF", "MP", "NO", "OM", "PK", "PW", "PA", "PG",
-    "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "KR", "MD", "RO", "RU", "RW", "RE", "BL", "SH", "KN",
-    "LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB",
-    "SO", "ZA", "GS", "SS", "ES", "LK", "PS", "SD", "SR", "SJ", "SE", "CH", "SY", "TJ", "TH", "MK", "TL",
-    "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "TZ", "UM", "VI", "US",
-    "UY", "UZ", "VU", "VE", "VN", "WF", "EH", "YE", "ZM", "ZW", "AX",
-];
-
-fn clean_and_validate_country(input: PiiString) -> VResult<PiiString> {
-    if !ISO_3166_COUNTRIES.contains(&input.leak()) {
-        return Err(Error::InvalidCountry);
     }
     Ok(input)
 }
