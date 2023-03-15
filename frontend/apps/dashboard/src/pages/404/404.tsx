8a8d3dba479b739e10d5219ab34e343742906fa7
Rafael Michels Motta <rafael@onefootprint.com>
2023-03-15 06:49:47 -0300

    [FP-3056] Dashboard: add 404 page (#2622)
    ## Describe your changes
    
    We didn't have a dedicated 404 page for the dashboard, so when accessing
    a page like `/userslorem`, we would actually get an error.
    
    Before:
    <img width="1840" alt="Screenshot 2023-03-14 at 08 30 13"
    src="https://user-images.githubusercontent.com/446731/224987747-728c04c6-1da8-418f-b652-414db5b1a1d3.png">
    
    Now:
    <img width="1840" alt="Screenshot 2023-03-14 at 08 30 02"
    src="https://user-images.githubusercontent.com/446731/224987769-e47b02e0-0190-48d4-bafa-98097443ace0.png">
    
    
    ## Issue: FP-3056
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

---
+++ frontend/apps/dashboard/src/pages/404/404.tsx
@@ -0,0 +1,28 @@
+import { useTranslation } from '@onefootprint/hooks';
+import { Box, EmptyState } from '@onefootprint/ui';
+import Image from 'next/image';
+import React from 'react';
+
+const Page404 = () => {
+  const { t } = useTranslation('pages.404');
+
+  return (
+    <Box sx={{ marginTop: 12 }}>
+      <EmptyState
+        title={t('title')}
+        description={t('description')}
+        renderHeader={() => (
+          <Image
+            alt={t('image.alt')}
+            height={212.62}
+            src="/404.png"
+            width={298}
+            priority
+          />
+        )}
+      />
+    </Box>
+  );
+};
+
+export default Page404;
