6125f3c9970e4a9f7625a3ebed17f1324f42afea
Elliott Forde <elliott@onefootprint.com>
2023-03-15 11:34:45 -0700

    Add GET /entities/<>/vault endpoint (#2601)
    ## Describe your changes
    
    Port of GET /users/<>/vault endpoint to also work for business vaults.
    Accepting any new name for `entity` :)
    
    ## Caveats
    This endpoint needs to be duplicated because it's possible fractional is
    still using the old one... I'm thinking it would be much nicer to have a
    simpler interface for pointing a second route to the same handler,
    especially if we need to keep this around for the long run
    
    ## Test Plan
    
    Tested in future PR

--- backend/components/api/src/routes/entities/mod.rs
+++ backend/components/api/src/routes/entities/mod.rs
@@ -1,8 +1,10 @@
 use paperclip::actix::web;
 mod get;
+pub mod vault;
 
 pub use get::{get_entities, get_entity};
 
 pub fn routes(config: &mut web::ServiceConfig) {
+    vault::routes(config);
     config.service(get::get).service(get::get_detail);
 }
--- backend/components/api/src/routes/users/vault/get.rs
+++ backend/components/api/src/routes/users/vault/get.rs
@@ -1,30 +1,11 @@
-use crate::auth::tenant::{CheckTenantGuard, SecretTenantAuthContext, TenantGuard};
+use crate::auth::tenant::SecretTenantAuthContext;
 use crate::auth::{tenant::TenantSessionAuth, Either};
-use crate::types::{JsonApiResponse, ResponseData};
-use crate::utils::vault_wrapper::VaultWrapper;
-use crate::{errors::ApiError, State};
+use crate::routes::entities::vault::get::{get_inner, FieldsParams, GetVaultResponse};
+use crate::types::JsonApiResponse;
+use crate::State;
 use actix_web::web::Query;
-use db::models::scoped_vault::ScopedVault;
-use newtypes::flat_api_object_map_type;
-use newtypes::input::Csv;
-use newtypes::{DataIdentifier, FootprintUserId};
-use paperclip::actix::Apiv2Schema;
+use newtypes::FootprintUserId;
 use paperclip::actix::{self, api_v2_operation, web, web::Path};
-use serde::Deserialize;
-use std::collections::HashMap;
-
-#[derive(Debug, Clone, Deserialize, Apiv2Schema)]
-pub struct FieldsParams {
-    /// Comma separated list of fields to check. For example, `id.first_name,id.ssn4,custom.bank_account`
-    #[openapi(example = "id.last_name, custom.ach_account, id.dob, id.ssn9")]
-    fields: Option<Csv<DataIdentifier>>,
-}
-
-flat_api_object_map_type!(
-    GetUnifiedResponse<DataIdentifier, bool>,
-    description="A key-value map of identifier to whether the identifier exists in the vault",
-    example=r#"{ "id.last_name": "smith", "id.ssn9": "121121212", "custom.credit_card": "1234 1234 1234 1234" }"#
-);
 
 #[api_v2_operation(
     description = "Given a list of fields, checks for their existence in the vault without decrypting them.",
@@ -36,31 +17,7 @@ pub async fn get(
     path: Path<FootprintUserId>,
     request: Query<FieldsParams>,
     auth: Either<TenantSessionAuth, SecretTenantAuthContext>,
-) -> JsonApiResponse<GetUnifiedResponse> {
-    let footprint_user_id = path.into_inner();
-    let FieldsParams { fields } = request.into_inner();
-
-    let auth = auth.check_guard(TenantGuard::Read)?;
-    let is_live = auth.is_live()?;
-    let tenant_id = auth.tenant().id.clone();
-
-    let uvw = state
-        .db_pool
-        .db_query(move |conn| -> Result<_, ApiError> {
-            let scoped_user = ScopedVault::get(conn, (&footprint_user_id, &tenant_id, is_live))?;
-            let uvw = VaultWrapper::build_for_tenant(conn, &scoped_user.id)?;
-            Ok(uvw)
-        })
-        .await??;
-
-    let populated = uvw.get_visible_populated_fields();
-    let keys = if let Some(fields) = fields {
-        fields.to_vec()
-    } else {
-        populated.clone()
-    };
-    let results = HashMap::from_iter(keys.into_iter().map(|di| (di.clone(), populated.contains(&di))));
-    let out = GetUnifiedResponse { map: results };
-
-    ResponseData::ok(out).json()
+) -> JsonApiResponse<GetVaultResponse> {
+    let result = get_inner(state, path, request, auth).await?;
+    Ok(result)
 }
