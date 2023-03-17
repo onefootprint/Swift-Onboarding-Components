c879c311260ca2d5d961dbe4fcac91da353a1a21
Elliott Forde <elliott@onefootprint.com>
2023-03-17 15:52:02 -0700

    More advanced validation tests for IPKs (#2727)
    ## Describe your changes
    
    Check exact error rather than general 400

--- backend/ci/tests/test_investor_profile.py
+++ backend/ci/tests/test_investor_profile.py
@@ -26,22 +26,37 @@ def test_put_ip_info_valid(sandbox_user, ip_data):
 
 
 @pytest.mark.parametrize(
-    "patch_data",
+    "key,value,expected_error",
     [
-        {"investor_profile.occuptation": ""},
-        {"investor_profile.brokerage_firm_employer": ""},
-        {"investor_profile.annual_income": "10000000000"},
-        {"investor_profile.net_worth": "0"},
-        {"investor_profile.investment_goals": '["hi", "grow_long_term_wealth"]'},
-        {"investor_profile.investment_goals": "[]"},
-        {"investor_profile.risk_tolerance": "really high"},
-        {"investor_profile.declarations": '["hi", "grow_long_term_wealth"]'},
+        ("investor_profile.occupation", "", "Invalid length"),
+        ("investor_profile.brokerage_firm_employer", "", "Invalid length"),
+        ("investor_profile.annual_income", "10000000000", "Matching variant not found"),
+        ("investor_profile.net_worth", "0", "Matching variant not found"),
+        (
+            "investor_profile.investment_goals",
+            '["hi", "grow_long_term_wealth"]',
+            "Matching variant not found",
+        ),
+        ("investor_profile.investment_goals", "[]", "Invalid length"),
+        (
+            "investor_profile.risk_tolerance",
+            "really high",
+            "Matching variant not found",
+        ),
+        (
+            "investor_profile.declarations",
+            '["hi", "grow_long_term_wealth"]',
+            "Matching variant not found",
+        ),
     ],
 )
-def test_put_ip_info_invalid(sandbox_user, ip_data, patch_data):
+def test_put_ip_info_invalid(sandbox_user, ip_data, key, value, expected_error):
     ip_data = {
         **ip_data,
-        **patch_data,
+        key: value,
     }
-    post("hosted/user/vault/validate", ip_data, sandbox_user, status_code=400)
-    put("hosted/user/vault", ip_data, sandbox_user, status_code=400)
+    body = post("hosted/user/vault/validate", ip_data, sandbox_user, status_code=400)
+    assert expected_error in body["error"]["message"][key]
+
+    body = put("hosted/user/vault", ip_data, sandbox_user, status_code=400)
+    assert expected_error in body["error"]["message"][key]
