9c93e403ee5043da57cd3501a1b0cdc6218f9705
Elliott Forde <elliott@onefootprint.com>
2023-03-17 09:06:28 -0700

    Also publish preview APIs (#2707)

--- backend/ci/scripts/update_open_api.py
+++ backend/ci/scripts/update_open_api.py
@@ -4,36 +4,64 @@ import requests
 
 # Every API endpoint must have only one of these tag values
 IDENTIFYING_TAG_VALUES = ["Private", "PublicApi", "Hosted", "Preview"]
+PUBLICLY_VISIBLE_TAG_VALUES = ["PublicApi", "Preview"]
 
 
-def is_public_api(method, url, tags):
-    identifying_tags = set(tags) & set(IDENTIFYING_TAG_VALUES)
-    # Enforce that every API has one and only one "identifying tag"
-    assert (
-        identifying_tags
-    ), f"{method.upper()} {url} doesn't have an identifying tag: {tags}"
-    assert (
-        len(identifying_tags) == 1
-    ), f"{method.upper()} {url} has more than one identifying tags: {tags}"
-    return next(iter(identifying_tags)) == "PublicApi"
+class Endpoint:
+    """
+    In-memory representation of an Open API endpoint
+    """
+
+    def __init__(self, url, method, path_info):
+        self.url = url
+        self.method = method
+        self.path_info = path_info
+
+    def matches(self, tag):
+        tags = self.path_info["tags"]
+        identifying_tags = set(tags) & set(IDENTIFYING_TAG_VALUES)
+        # Enforce that every API has one and only one "identifying tag"
+        assert (
+            identifying_tags
+        ), f"{self.method.upper()} {self.url} doesn't have an identifying tag: {tags}"
+        assert (
+            len(identifying_tags) == 1
+        ), f"{self.method.upper()} {self.url} has more than one identifying tags: {tags}"
+        identifying_tag = next(iter(identifying_tags))
+        return identifying_tag == tag
+
+    def serialize(self):
+        """
+        Serializes the Endpoint back into open-api JSON path info
+        """
+        existing_description = self.path_info["description"]
+        new_description = f"This is a preview API. By using this, you consent to potentially breaking API changes.\n{existing_description}"
+        return {**self.path_info, "description": new_description}
 
 
-def get_public_apis(open_api_spec):
+def get_apis(open_api_spec, tag):
     """
     Replace the paths in the open API spec with only the public paths, and validate the tags for
     each endpoint.
     """
-    # Filter out the paths that do not have a public tag
-    # TODO add preview APIs in with a disclaimer here
-    public_paths = {
+    # Parse paths
+    paths = {
         url: {
-            method: path_info
+            method: Endpoint(url, method, path_info)
             for method, path_info in methods_for_url.items()
-            if is_public_api(method, url, path_info["tags"])
         }
         for (url, methods_for_url) in open_api_spec["paths"].items()
     }
-    # Filter out URLs that have no public paths
+    # Filter out the endpoints that do not have a public tag
+    public_paths = {
+        url: {
+            method: endpoint.serialize()
+            for (method, endpoint) in methods_for_url.items()
+            if endpoint.matches(tag)
+        }
+        for (url, methods_for_url) in paths.items()
+    }
+    # Filter out empty URLs
     public_paths = {
         url: methods_for_url
         for (url, methods_for_url) in public_paths.items()
@@ -49,8 +77,13 @@ def get_public_apis(open_api_spec):
 if __name__ == "__main__":
     # If running this script, actually output the new open api spec
     open_api_spec = requests.get("https://api.onefootprint.com/docs-spec-v3").json()
-    open_api_spec = get_public_apis(open_api_spec)
     os.makedirs("out", exist_ok=True)
+    public_open_api_spec = get_apis(open_api_spec, "PublicApi")
     with open("out/public-api.json", "w") as f:
-        f.write(json.dumps(open_api_spec))
+        f.write(json.dumps(public_open_api_spec))
+        f.close()
+    preview_open_api_spec = get_apis(open_api_spec, "Preview")
+    os.makedirs("out", exist_ok=True)
+    with open("out/preview-api.json", "w") as f:
+        f.write(json.dumps(preview_open_api_spec))
         f.close()
--- backend/ci/tests/test_open_api.py
+++ backend/ci/tests/test_open_api.py
@@ -1,15 +1,21 @@
 from tests.utils import get
-from scripts.update_open_api import get_public_apis
+from scripts.update_open_api import get_apis
 
 
 def test_open_api():
     # Validate that the public APIs we expose are sane
     open_api_spec = get("docs-spec-v3")
-    public_open_api_spec = get_public_apis(open_api_spec)
-    public_paths_str = [
-        f"{method.upper()} {url}"
-        for (url, methods_for_path) in public_open_api_spec["paths"].items()
-        for method in methods_for_path
-    ]
-    print("===== Publicly exposed APIs =====")
-    print("\n".join(public_paths_str))
+    public_open_api_spec = get_apis(open_api_spec, "PublicApi")
+    preview_open_api_spec = get_apis(open_api_spec, "Preview")
+
+    def print_apis(open_api_spec, title):
+        paths_str = [
+            f"{method.upper()} {url}"
+            for (url, methods_for_path) in open_api_spec["paths"].items()
+            for method in methods_for_path
+        ]
+        print(f"===== {title} =====")
+        print("\n".join(paths_str))
+
+    print_apis(public_open_api_spec, "Publicly-exposed APIs")
+    print_apis(preview_open_api_spec, "Preview APIs")
