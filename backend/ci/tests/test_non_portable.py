11c1593fe9fc6a09d9033dfa1c7dcd0ef2c70ccf
Elliott Forde <elliott@onefootprint.com>
2023-03-20 18:48:38 -0700

    Simplify non-portable vault tests (#2743)
    ## Describe your changes
    
    These tests became redundant after we merged the custom + id data
    codepaths - just beefed up the unified test and removed the id-only and
    custom-only tests

--- backend/ci/tests/test_non_portable.py
+++ backend/ci/tests/test_non_portable.py
@@ -31,7 +31,7 @@ class TestNonPortableVaultApi:
             ),
         ],
     )
-    def test_identity_validation(self, tenant, key, value, expected_error):
+    def test_data_validation(self, tenant, key, value, expected_error):
         # Can't create the user inline with invalid data
         data = {key: value}
         post("users/", data, tenant.sk.key, status_code=400)
@@ -47,178 +47,70 @@ class TestNonPortableVaultApi:
         # Validate endpoint should also fail
         post(f"users/{fp_id}/vault/validate", data, tenant.sk.key, status_code=400)
 
-    def test_vault_create_write_decrypt(self, tenant):
-        # create the vault with data
-        data = {
-            "id.phone_number": PHONE_NUMBER,
-            "id.email": EMAIL,
-            "custom.hi": "bye",
-            **build_user_data(),
-        }
-        body = post("users/", data, tenant.sk.key)
-        user = body
-        fp_id = user["id"]
-        assert fp_id
 
-        # check that the data is there now
-        response = get(f"users/{fp_id}/vault", None, tenant.sk.key)
-        assert response["id.first_name"]
-        assert response["id.last_name"]
-        assert response["id.zip"]
-        assert response["id.ssn9"]
-        assert response["id.city"]
-        assert response["id.phone_number"]
-        assert response["id.email"]
-        assert response["custom.hi"]
+def test_vault_create_write_decrypt(tenant):
+    # create the vault
+    initial_data = {
+        "id.phone_number": PHONE_NUMBER,
+        "id.email": EMAIL,
+        "custom.hi": "bye",
+        **build_user_data(),
+    }
+    body = post("users/", initial_data, tenant.sk.key)
+    user = body
+    fp_id = user["id"]
+    assert fp_id
 
-        # decrypt the data
-        data = dict(
-            reason="test",
-            fields=[
-                "id.first_name",
-                "id.zip",
-                "id.city",
-                "id.phone_number",
-                "id.email",
-                "custom.hi",
-            ],
-        )
-        body = post(f"users/{fp_id}/vault/decrypt", data, tenant.sk.key)
-        data = body
-        assert data["id.first_name"] == "Sandbox"
-        assert data["id.zip"] == "10009"
-        assert data["id.city"] == "Enclave"
-        assert data["id.phone_number"] == PHONE_NUMBER.replace(" ", "")
-        assert data["id.email"] == EMAIL
-        assert data["custom.hi"] == "bye"
+    # add some more data to the vault
+    update_data = {
+        "custom.ach_account_number": "123467890",
+        "custom.cc4": "4242",
+    }
+    put(f"users/{fp_id}/vault", update_data, tenant.sk.key)
 
-        # verify access events created
-        body = get(
-            "org/access_events",
-            dict(footprint_user_id=fp_id),
-            tenant.sk.key,
-        )
-        access_events = body["data"]
-        assert access_events[0]["kind"] == "decrypt"
-        assert set(access_events[0]["targets"]) == {
-            "id.first_name",
-            "id.zip",
-            "id.city",
-            "id.phone_number",
-            "id.email",
-            "custom.hi",
-        }
+    # check that the data is there now
+    all_data = {
+        **initial_data,
+        **update_data,
+    }
+    fields_to_check = [i for i in all_data] + ["custom.insurance_id"]
+    params = {"fields": ", ".join(fields_to_check)}
 
-    def test_custom_data(self, tenant):
-        # create the vault
-        body = post("users/", None, tenant.sk.key)
-        user = body
-        fp_id = user["id"]
-        assert fp_id
+    response = get(f"users/{fp_id}/vault", params, tenant.sk.key)
+    for k in all_data:
+        assert response[k]
+    assert response["custom.insurance_id"] == False
 
-        # post data to it
-        data = {"custom.ach_account_number": "123467890", "custom.cc4": "4242"}
-        put(f"users/{fp_id}/vault", data, tenant.sk.key)
-
-        # verify access events created
-        body = get(
-            "org/access_events",
-            dict(footprint_user_id=fp_id),
-            tenant.sk.key,
-        )
-        access_events = body["data"]
-        assert access_events[0]["kind"] == "update"
-        assert set(access_events[0]["targets"]) == {
-            "custom.ach_account_number",
-            "custom.cc4",
-        }
-
-        # check status of the data
-        params = {"fields": "custom.cc4,custom.ach_account_number, custom.insurance_id"}
-        response = get(f"users/{fp_id}/vault", params, tenant.sk.key)
-        assert response["custom.ach_account_number"] == True
-        assert response["custom.cc4"] == True
-        assert response["custom.insurance_id"] == False
-
-        # decrypt the data
-        # check status of the data
-        data = dict(reason="test", fields=["custom.cc4", "custom.ach_account_number"])
-        response = post(f"users/{fp_id}/vault/decrypt", data, tenant.sk.key)
-        assert response["custom.ach_account_number"] == "123467890"
-        assert response["custom.cc4"] == "4242"
-
-        # verify access events created
-        body = get(
-            "org/access_events",
-            dict(footprint_user_id=fp_id),
-            tenant.sk.key,
-        )
-        access_events = body["data"]
-        assert access_events[0]["kind"] == "decrypt"
-        assert set(access_events[0]["targets"]) == {
-            "custom.ach_account_number",
-            "custom.cc4",
-        }
-
-
-class TestUnifiedVaultApi:
-    def test_unified_vault_create_write_decrypt(self, tenant):
-        # create the vault
-        body = post("users/", None, tenant.sk.key)
-        user = body
-        fp_id = user["id"]
-        assert fp_id
-
-        # post data to it
-        data = {
-            "custom.ach_account_number": "123467890",
-            "custom.cc4": "4242",
-            **build_user_data(),
-        }
-        put(f"users/{fp_id}/vault", data, tenant.sk.key)
-
-        # check that the data is there now
-        params = {
-            "fields": "id.last_name, id.ssn9, custom.ach_account_number,custom.cc4, custom.insurance_id"
-        }
-
-        response = get(f"users/{fp_id}/vault", params, tenant.sk.key)
-        assert response["id.last_name"] == True
-        assert response["id.ssn9"] == True
-        assert response["custom.ach_account_number"] == True
-        assert response["custom.cc4"] == True
-        assert response["custom.insurance_id"] == False
-
-        # decrypt the data
-        data = dict(
-            reason="test",
-            fields=[
-                "id.first_name",
-                "id.zip",
-                "custom.ach_account_number",
-                "custom.cc4",
-            ],
-        )
-        body = post(f"users/{fp_id}/vault/decrypt", data, tenant.sk.key)
-        data = body
-        assert data["id.first_name"] == "Sandbox"
-        assert data["id.zip"] == "10009"
-        assert data["custom.ach_account_number"] == "123467890"
-        assert data["custom.cc4"] == "4242"
-
-        # verify access events created
-        body = get(
-            "org/access_events",
-            dict(footprint_user_id=fp_id),
-            tenant.sk.key,
-        )
-        access_events = body["data"]
-        assert access_events[0]["kind"] == "decrypt"
-
-        events = set(access_events[0]["targets"])
-        assert events == {
+    # decrypt the data
+    data = dict(
+        reason="test",
+        fields=[
             "id.first_name",
             "id.zip",
             "custom.ach_account_number",
             "custom.cc4",
-        }
+        ],
+    )
+    body = post(f"users/{fp_id}/vault/decrypt", data, tenant.sk.key)
+    data = body
+    assert data["id.first_name"] == "Sandbox"
+    assert data["id.zip"] == "10009"
+    assert data["custom.ach_account_number"] == "123467890"
+    assert data["custom.cc4"] == "4242"
+
+    # verify access events created
+    body = get(
+        "org/access_events",
+        dict(footprint_user_id=fp_id),
+        tenant.sk.key,
+    )
+    access_events = body["data"]
+    assert access_events[0]["kind"] == "decrypt"
+
+    events = set(access_events[0]["targets"])
+    assert events == {
+        "id.first_name",
+        "id.zip",
+        "custom.ach_account_number",
+        "custom.cc4",
+    }
