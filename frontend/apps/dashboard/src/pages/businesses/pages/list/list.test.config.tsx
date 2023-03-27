f1e270df8724a077e19fda29b9be8e339af4df92
Rafael Michels Motta <rafael@onefootprint.com>
2023-03-27 10:51:15 -0300

    Fix business list test url (#2829
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- frontend/apps/dashboard/src/pages/businesses/pages/list/list.test.config.tsx
+++ frontend/apps/dashboard/src/pages/businesses/pages/list/list.test.config.tsx
@@ -43,7 +43,7 @@ export const withBusinesses = (
 ) =>
   mockRequest({
     method: 'get',
-    path: '/users',
+    path: '/entities',
     response: {
       data: businesses,
       meta: {},
@@ -55,7 +55,7 @@ export const withBusinesses = (
 export const withBusinessesError = () =>
   mockRequest({
     method: 'get',
-    path: '/users',
+    path: '/entities',
     statusCode: 400,
     response: {
       error: {
