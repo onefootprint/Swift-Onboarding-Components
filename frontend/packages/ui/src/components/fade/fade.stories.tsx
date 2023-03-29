7fce8fea9b4844ab67f57256dc8376cb18c39c5f
Claudio Angrigiani <angrigianicr@gmail.com>
2023-03-29 14:34:50 -0400

    [FP-3144] New Fade component (#2872)

---
+++ frontend/packages/ui/src/components/fade/fade.stories.tsx
@@ -0,0 +1,57 @@
+import { Meta, Story } from '@storybook/react';
+import React, { useState } from 'react';
+
+import Button from '../button/button';
+import Fade from './fade';
+
+export default {
+  title: 'Components/Fade',
+  argTypes: {
+    from: {
+      control: {
+        type: 'select',
+        options: ['left', 'right', 'top', 'bottom', 'center'],
+      },
+    },
+    to: {
+      control: {
+        type: 'select',
+        options: ['left', 'right', 'top', 'bottom', 'center'],
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story = ({ from, to }) => {
+  const [isVisible, setisVisible] = useState(false);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Button onClick={() => setisVisible(!isVisible)}>Trigger</Button>
+      <Fade isVisible={isVisible} from={from} to={to}>
+        <div
+          style={{
+            backgroundColor: 'red',
+            width: 200,
+            height: 200,
+            marginTop: 8,
+            borderRadius: '6px',
+          }}
+        />
+      </Fade>
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  from: 'left',
+  to: 'right',
+};
