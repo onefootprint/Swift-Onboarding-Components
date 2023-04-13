bf3354006eefbb542da696971cb8cdafb2afc750
Claudio Angrigiani <angrigianicr@gmail.com>
2023-04-13 18:31:35 -0400

    [FP-1177] Add story to Pagination component (#3059)
    Co-authored-by: Rafael Michels Motta <rafael@onefootprint.com>

---
+++ frontend/packages/ui/src/components/pagination/pagination.stories.tsx
@@ -0,0 +1,66 @@
+import { Meta, Story } from '@storybook/react';
+import React, { useState } from 'react';
+
+import Pagination from './pagination';
+
+export default {
+  component: Pagination,
+  title: 'Components/Pagination',
+  argTypes: {
+    hasNextPage: {
+      control: 'boolean',
+      description: 'Whether there is a next page',
+      name: 'hasNextPage',
+    },
+    hasPrevPage: {
+      control: 'boolean',
+      description: 'Whether there is a previous page',
+      name: 'hasPrevPage',
+    },
+    onNextPage: {
+      description: 'Callback for when the next page button is clicked',
+      name: 'onNextPage',
+    },
+    onPrevPage: {
+      description: 'Callback for when the previous page button is clicked',
+      name: 'onPrevPage',
+    },
+    pageIndex: {
+      description: 'The current page index',
+      name: 'pageIndex',
+    },
+    pageSize: {
+      description: 'The number of results per page',
+      name: 'pageSize',
+    },
+    totalNumResults: {
+      description: 'The total number of results',
+      name: 'totalNumResults',
+    },
+  },
+} as Meta;
+
+const Template: Story = ({ pageIndex, pageSize, totalNumResults }) => {
+  const [currentPage, setCurrentPage] = useState(pageIndex);
+  const hasPrevPage = currentPage > 0;
+  const hasNextPage = currentPage < Math.ceil(totalNumResults / pageSize) - 1;
+
+  return (
+    <Pagination
+      pageIndex={currentPage}
+      pageSize={pageSize}
+      totalNumResults={totalNumResults}
+      hasPrevPage={hasPrevPage}
+      hasNextPage={hasNextPage}
+      onPrevPage={() => setCurrentPage(currentPage - 1)}
+      onNextPage={() => setCurrentPage(currentPage + 1)}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  pageIndex: 0,
+  pageSize: 10,
+  totalNumResults: 100,
+};
