9f4faaa33324b6ee706d7941b6d2cd6595106123
Rafael Michels Motta <rafael@onefootprint.com>
2023-04-17 20:18:52 -0300

    [FP-3526] Appclip structure (#3101
    ## Describe your changes
    
    Created a new app and a new package, using react-native:
    
    - `my-mobile` (my one footprint, written with react-native)
    - `ui-mobile` (our design system, for react-native)
    
    * Let me know if you have some thoughts about the `-mobile`
    
    Right now, they are pretty simple and not very useful. They just having
    a skeleton, all the configuration required to run as app clip, some
    configurations to make it work with mono-repo, as well as some files to
    do the whole automation to deploy to the apple store
    
    I decided to create a new script `yarn dev:mobile`, because I don't want
    to open xcode for our regular projects.
    
    
    <img width="565" alt="Screenshot 2023-04-17 at 14 41 58"
    src="https://user-images.githubusercontent.com/446731/232566951-7673281a-0868-4091-a010-0f5e30739f82.png">
    
    <img width="565" alt="Screenshot 2023-04-17 at 14 42 17"
    src="https://user-images.githubusercontent.com/446731/232567019-e37d7c3b-9c89-4ea9-bea2-3d203520b146.png">
    
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

---
+++ frontend/apps/my-mobile/src/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { isClip } from 'react-native-app-clip';
+
+const App = () => (
+  <View style={styles.container}>
+    {isClip() ? (
+      <View>
+        <Text style={{ color: '#333', fontSize: 24 }}>App Clip!</Text>
+      </View>
+    ) : (
+      <View>
+        <Text style={{ color: '#333', fontSize: 24 }}>Regular App!</Text>
+      </View>
+    )}
+    <StatusBar />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F2F2F2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
