dd35cea98d90b1aa30634840fe6b3fb9ef3f3c66
Rafael Michels Motta <rafael@onefootprint.com>
2023-04-18 12:56:02 -0300

    Fix tenant request

--- frontend/apps/frontpage/src/pages/tenant/tenant.server.ts
+++ frontend/apps/frontpage/src/pages/tenant/tenant.server.ts
@@ -3,30 +3,32 @@ import type { GetServerSideProps } from 'next';
 import { API_BASE_URL } from '../../config/constants';
 
 const getServerSideProps: GetServerSideProps = async context => {
-  console.log('tenant request', API_BASE_URL);
   const obKey = context.query['ob-key'];
   if (!obKey) {
     return { notFound: true };
   }
-  const response = await fetch(`${API_BASE_URL}/org/onboarding_config`, {
-    headers: {
-      'X-Onboarding-Config-Key': obKey as string,
-    },
-  }).then(res => res.json());
-
-  if (response.error || !response.ok) {
+  try {
+    const request = await fetch(`${API_BASE_URL}/org/onboarding_config`, {
+      headers: {
+        'X-Onboarding-Config-Key': obKey as string,
+      },
+    });
+    if (!request.ok) {
+      throw new Error('Network response was not OK');
+    }
+    const response = await request.json();
+    return {
+      props: {
+        tenant: {
+          logoUrl: response.logo_url,
+          name: response.org_name,
+          obKey,
+        },
+      },
+    };
+  } catch (e) {
     return { notFound: true };
   }
-
-  return {
-    props: {
-      tenant: {
-        logoUrl: response.logo_url,
-        name: response.org_name,
-        obKey,
-      },
-    },
-  };
 };
 
 export default getServerSideProps;
