cf2d19a951dded31cd37cc507b77a7687a439387
Rafael Michels Motta <rafael@onefootprint.com>
2023-04-18 12:36:12 -0300

    Debugging node api base url

--- frontend/apps/frontpage/src/pages/tenant/tenant.server.ts
+++ frontend/apps/frontpage/src/pages/tenant/tenant.server.ts
@@ -1,13 +1,13 @@
 import type { GetServerSideProps } from 'next';
 
-import { API_BASE_URL } from '../../config/constants';
+const baseUrl = process.env.API_BASE_URL;
 
 const getServerSideProps: GetServerSideProps = async context => {
   const obKey = context.query['ob-key'];
   if (!obKey) {
     return { notFound: true };
   }
-  const response = await fetch(`${API_BASE_URL}/org/onboarding_config`, {
+  const response = await fetch(`${baseUrl}/org/onboarding_config`, {
     headers: {
       'X-Onboarding-Config-Key': obKey as string,
     },
