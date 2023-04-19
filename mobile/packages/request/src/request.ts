bde5b02be3572cf8a7fe98b7fa9ae5aa63875218
Rafael Michels Motta <rafael@onefootprint.com>
2023-04-19 14:28:51 -0300

    [FP-3618] App clip: add request pkg (#3148
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

---
+++ mobile/packages/request/src/request.ts
@@ -0,0 +1,79 @@
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import applyCaseMiddleware from 'axios-case-converter';
+
+const LOGOUT_ERROR = 'Session expired or does not exist';
+
+export type FootprintServerError = {
+  message: string;
+  statusCode: number;
+  supportId: string;
+};
+
+export type RequestError = AxiosError<{
+  error: FootprintServerError;
+}>;
+
+export type PaginatedRequestResponse<T> = {
+  data: T;
+  meta: {
+    next?: string | null;
+    nextPage?: string | null;
+    count: number;
+  };
+};
+
+export const isFootprintError = (error: unknown): error is RequestError =>
+  (error as RequestError)?.response?.data !== undefined;
+
+export const isUnhandledError = (error: unknown): error is Error =>
+  (error as Error)?.message !== undefined;
+
+export const isFootprintServerError = (
+  error: unknown,
+): FootprintServerError | undefined => {
+  const errorData = (error as RequestError).response?.data?.error;
+  if (errorData?.message && errorData?.statusCode && errorData?.supportId) {
+    return errorData;
+  }
+  return undefined;
+};
+
+export const isLogoutError = (error: unknown) => {
+  const serverError = isFootprintServerError(error);
+  return !!(
+    serverError?.statusCode === 401 &&
+    serverError?.message?.includes(LOGOUT_ERROR)
+  );
+};
+
+export const getErrorMessage = (error?: unknown | Error): string => {
+  if (isFootprintError(error)) {
+    if (error?.response?.data.error.message) {
+      return error.response.data.error.message;
+    }
+  }
+  if (isUnhandledError(error)) {
+    return error.message;
+  }
+  return 'Something went wrong';
+};
+
+const getRequestOptions = (requestConfig: AxiosRequestConfig) => {
+  return {
+    baseURL: process.env.API_BASE_URL,
+    timeout: 60000,
+    withCredentials: true,
+    ...requestConfig,
+    headers: {
+      ...requestConfig.headers,
+    },
+  };
+};
+
+const request = <Response = any>(requestConfig: AxiosRequestConfig = {}) => {
+  const client = applyCaseMiddleware(axios.create());
+  const options = getRequestOptions(requestConfig);
+  return client.request<Response>(options);
+};
+
+export default request;
