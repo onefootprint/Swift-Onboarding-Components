ed37523c8de6e440bb61668f033d9c353971ca36
Elliott Forde <elliott@onefootprint.com>
2023-04-18 18:44:27 -0700

    Update docs on standalone user vaults (#3140)
    ## Describe your changes
    
    Brings the standalone vault section of our docs up to more recent
    standards
    
    ## Test Plan
    
    Added more docs tests

--- backend/ci/tests/test_docs.py
+++ backend/ci/tests/test_docs.py
@@ -4,7 +4,7 @@ tests breaks, you are probably making a breaking change to our public API and ne
 do it OR do it VERY carefully.
 """
 
-from tests.utils import get
+from tests.utils import get, post, put
 
 
 def test_get_users(sandbox_user, sandbox_tenant):
@@ -14,6 +14,27 @@ def test_get_users(sandbox_user, sandbox_tenant):
     email = sandbox_user.client.data["id.email"]
     body = get("/users", dict(search=email), sandbox_tenant.sk.key)
     assert any(i["id"] == sandbox_user.fp_id for i in body["data"])
+
+
+def test_standalone_vaults(sandbox_tenant):
+    body = post("/users", None, sandbox_tenant.sk.key)
+    assert body["id"]
+
+    data = {
+        "id.first_name": "Jane",
+        "id.last_name": "Joe",
+        "id.dob": "1988-12-30",
+        "id.ssn9": "12-121-1212",
+        "custom.cc4": "4242",
+    }
+    body = post("/users", data, sandbox_tenant.sk.key)
+    fp_id = body["id"]
+
+    data = {"id.email": "jane@acmebank.com", "custom.ach_account": "111122224444"}
+    put(f"/users/{fp_id}/vault", data, sandbox_tenant.sk.key)
+
+    body = get("/users", dict(search="Jane"), sandbox_tenant.sk.key)
+    assert any(i["id"] == fp_id for i in body["data"])
 
 
 # TODO add tests for everything else documented
