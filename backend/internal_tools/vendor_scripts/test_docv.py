d67d6b84bc8f9442aa4f3536ff0460f782f03bc6
Keagan Long <keagan@onefootprint.com>
2023-04-20 09:59:01 -0700

    Fix test_docv.py (#3164)
    ## Describe your changes
    
    This py script isn't wrapped in `main` so its giving me problems in
    vscode:
    ```
    ==================================== ERRORS ====================================
    _____ ERROR collecting backend/internal_tools/vendor_scripts/test_docv.py ______
    backend/internal_tools/vendor_scripts/test_docv.py:124: in <module>
        args = parser.parse_args()
    /opt/homebrew/Cellar/python@3.10/3.10.10_1/Frameworks/Python.framework/Versions/3.10/lib/python3.10/argparse.py:1829: in parse_args
        self.error(msg % ' '.join(argv))
    /opt/homebrew/Cellar/python@3.10/3.10.10_1/Frameworks/Python.framework/Versions/3.10/lib/python3.10/argparse.py:2587: in error
        self.exit(2, _('%(prog)s: error: %(message)s\n') % args)
    /opt/homebrew/Cellar/python@3.10/3.10.10_1/Frameworks/Python.framework/Versions/3.10/lib/python3.10/argparse.py:2574: in exit
        _sys.exit(status)
    E   SystemExit: 2
    =========================== short test summary info ============================
    ERROR backend/internal_tools/vendor_scripts/test_docv.py - SystemExit: 2
    !!!!!!!!!!!!!!!!!!!! Interrupted: 1 error during collection !!!!!!!!!!!!!!!!!!!!
    ==================== 168 tests collected, 1 error in 1.09s =====================
    
    ```
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/internal_tools/vendor_scripts/test_docv.py
+++ backend/internal_tools/vendor_scripts/test_docv.py
@@ -11,127 +11,135 @@ def load_secrets():
     load_dotenv()
 
     return {
-        'incode_api': os.getenv('INCODE_API_KEY'),
-        'idology_un': os.getenv('IDOLOGY_USERNAME'),
-        'idology_pw': os.getenv('IDOLOGY_PASSWORD')
-        }
+        "incode_api": os.getenv("INCODE_API_KEY"),
+        "idology_un": os.getenv("IDOLOGY_USERNAME"),
+        "idology_pw": os.getenv("IDOLOGY_PASSWORD"),
+    }
+
 
 def encode_as_b64(path):
     with open(path, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode('ascii')
+        encoded_string = base64.b64encode(image_file.read()).decode("ascii")
 
     return encoded_string
 
 
 def incode_url(s):
-    return f'https://demo-api.incodesmile.com/{s}'
+    return f"https://demo-api.incodesmile.com/{s}"
+
 
 def default_headers():
-    api_key = load_secrets()['incode_api']
-    return {'api-version': '1.0', 'Content-Type': 'application/json', 'x-api-key': api_key}
+    api_key = load_secrets()["incode_api"]
+    return {
+        "api-version": "1.0",
+        "Content-Type": "application/json",
+        "x-api-key": api_key,
+    }
+
 
 def start_session():
     headers = default_headers()
-    data = {
-        'countryCode': 'ALL',
-        'configurationId': '643450886f6f92d20b27599b'
-    }
-    resp = r.post(incode_url('omni/start'), headers=headers, json=data)
-    return resp.json()['token']
+    data = {"countryCode": "ALL", "configurationId": "643450886f6f92d20b27599b"}
+    resp = r.post(incode_url("omni/start"), headers=headers, json=data)
+    return resp.json()["token"]
 
-def add_side(token, b64_image, side): 
-    if side == 'front':
-        url = incode_url('omni/add/front-id/v2?onlyFront=false')
+
+def add_side(token, b64_image, side):
+    if side == "front":
+        url = incode_url("omni/add/front-id/v2?onlyFront=false")
     else:
-        url = incode_url('omni/add/back-id/v2?retry=false')
-    
+        url = incode_url("omni/add/back-id/v2?retry=false")
+
     headers = default_headers()
-    headers['X-Incode-Hardware-Id'] = token
-    data = {
-        'base64Image': b64_image
-    }
+    headers["X-Incode-Hardware-Id"] = token
+    data = {"base64Image": b64_image}
 
     resp = r.post(url, headers=headers, json=data)
     j = resp.json()
-    if j['classification'] and j['readability']:
+    if j["classification"] and j["readability"]:
         print(f"{side} accepted")
     return resp.json()
 
+
 def process_id(token):
-    url = incode_url('omni/process/id')
+    url = incode_url("omni/process/id")
     headers = default_headers()
-    headers['X-Incode-Hardware-Id'] = token
+    headers["X-Incode-Hardware-Id"] = token
     resp = r.post(url, headers=headers)
 
-    return resp.json()['success']
+    return resp.json()["success"]
+
 
 def fetch_score(token):
-    url = incode_url('omni/get/score')
+    url = incode_url("omni/get/score")
     headers = default_headers()
-    headers['X-Incode-Hardware-Id'] = token
+    headers["X-Incode-Hardware-Id"] = token
 
     resp = r.get(url, headers=headers)
     j = resp.json()
     print("~~~=====INCODE====~~~\n\n")
     print(f"=====OVERALL=====\n{j['overall']}\n\n")
-    print(f'=====ID VALIDATION=====\n')
-    for t in list(j['idValidation']['photoSecurityAndQuality']):
+    print(f"=====ID VALIDATION=====\n")
+    for t in list(j["idValidation"]["photoSecurityAndQuality"]):
         print(f"test: {t['key']} result: {t['status']}")
-    for t in list(j['idValidation']['idSpecific']):
+    for t in list(j["idValidation"]["idSpecific"]):
         print(f"test: {t['key']} result: {t['status']}")
     print("=====END INCODE=====\n\n\n")
+
 
 def send_incode(front, back):
     print("....sending incode request")
     token = start_session()
-    resp_front = add_side(token, front, 'front')
-    resp_back = add_side(token, back, 'back')
+    resp_front = add_side(token, front, "front")
+    resp_back = add_side(token, back, "back")
     result = process_id(token)
     fetch_score(token)
 
-##### 
+
+#####
 # idology
+
 
 def idology_req(un, pw, front, back, country_code, doc_type):
     return {
-        'username': un,
-        'password': pw,
-        'output': 'json',
-        'image': front,
-        'backImage': back,
-        'countryCode': country_code,
-        'scanDocumentType': doc_type,
+        "username": un,
+        "password": pw,
+        "output": "json",
+        "image": front,
+        "backImage": back,
+        "countryCode": country_code,
+        "scanDocumentType": doc_type,
     }
-
 
 
 def send_idology(front, back):
     secrets = load_secrets()
     print("....sending idology request")
-    un = secrets['idology_un']
-    pw = secrets['idology_pw']
-    req = idology_req(un, pw, front, back, 'USA', 'driverLicense')
-    resp = r.post('https://web.idologylive.com/api/scan-capture.svc', data=req)
+    un = secrets["idology_un"]
+    pw = secrets["idology_pw"]
+    req = idology_req(un, pw, front, back, "USA", "driverLicense")
+    resp = r.post("https://web.idologylive.com/api/scan-capture.svc", data=req)
     j = resp.json()
     print("~~======IDOLOGY======~~")
     print(f"result: {j['response']['capture-result']['key']}")
     print(f"qualifiers: {j['response']['qualifiers']}")
 
-try: 
-    parser = argparse.ArgumentParser()
-    parser.add_argument(dest='front_path', help="front")
-    parser.add_argument(dest='back_path', help="back")
-    args = parser.parse_args()
-    front = encode_as_b64(args.front_path)
-    back = encode_as_b64(args.back_path)
 
-    # send reqs
-    send_incode(front, back)
-    send_idology(front,back)
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(dest="front_path", help="front")
+        parser.add_argument(dest="back_path", help="back")
+        args = parser.parse_args()
+        front = encode_as_b64(args.front_path)
+        back = encode_as_b64(args.back_path)
 
-except KeyboardInterrupt:
-    import sys
-    print("\nkeyboard interrupt, exiting")
-    sys.exit()
+        # send reqs
+        send_incode(front, back)
+        send_idology(front, back)
 
+    except KeyboardInterrupt:
+        import sys
 
+        print("\nkeyboard interrupt, exiting")
+        sys.exit()
