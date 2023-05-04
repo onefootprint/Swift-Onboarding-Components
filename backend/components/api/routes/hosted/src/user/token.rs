011b56b24351765c96983e985540daf7a8e6831d
Elliott Forde <elliott@onefootprint.com>
2023-05-03 21:36:07 -0700

    [FP-3802] Clean up GET user/token api (#3288)
    ## Describe your changes
    
    Frontend will start using this to see if the auth token has expired. Just making one cleanup to allow this to be hit with any auth token
    
    ## Caveats
    
    This does expose auth token scopes publically, but we have been doing that for a while. no real risk in it since security is not via obscurity

--- backend/components/api/routes/hosted/src/user/token.rs
+++ backend/components/api/routes/hosted/src/user/token.rs
@@ -1,14 +1,12 @@
-use crate::auth::user::{UserAuth, UserAuthContext, UserAuthGuard};
+use crate::auth::user::{UserAuthContext, UserAuthGuard};
 use crate::errors::ApiError;
 use crate::types::response::ResponseData;
-use api_core::auth::IsGuardMet;
+use api_core::auth::Any;
 use chrono::{DateTime, Utc};
-use newtypes::VaultId;
 use paperclip::actix::{self, api_v2_operation, web::Json, Apiv2Schema};
 
 #[derive(Debug, Clone, Apiv2Schema, serde::Serialize)]
 pub struct TokenResponse {
-    pub user_vault_id: VaultId,
     pub scopes: Vec<UserAuthGuard>,
     pub expires_at: DateTime<Utc>,
 }
@@ -18,11 +16,10 @@ pub struct TokenResponse {
 pub async fn get(
     user_auth: UserAuthContext,
 ) -> actix_web::Result<Json<ResponseData<TokenResponse>>, ApiError> {
-    let user_auth = user_auth.check_guard(UserAuthGuard::SignUp.or(UserAuthGuard::BasicProfile))?;
+    let user_auth = user_auth.check_guard(Any)?;
     let scopes = user_auth.scopes.iter().map(|x| x.into()).collect();
 
     Ok(Json(ResponseData::ok(TokenResponse {
-        user_vault_id: user_auth.user_vault_id().clone(),
         expires_at: user_auth.expires_at(),
         scopes,
     })))
