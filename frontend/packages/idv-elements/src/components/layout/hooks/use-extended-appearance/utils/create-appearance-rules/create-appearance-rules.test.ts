1c53f03f432b7290b2ee78e90c4d5b966f67defa
Belce Dogru <belce.dogru@gmail.com>
2023-05-03 22:48:07 -0400

    fix tests

--- frontend/packages/idv-elements/src/components/layout/hooks/use-extended-appearance/utils/create-appearance-rules/create-appearance-rules.test.ts
+++ frontend/packages/idv-elements/src/components/layout/hooks/use-extended-appearance/utils/create-appearance-rules/create-appearance-rules.test.ts
@@ -9,14 +9,9 @@ describe('createAppearanceRules', () => {
   describe('createAppearanceRules', () => {
     it('should generate the right styles', () => {
       expect(
-        createAppearanceRules({
-          button: {
-            background: 'red',
-          },
-          'button:hover': {
-            background: 'green',
-          },
-        }),
+        createAppearanceRules(
+          '{"button":{"background":"red"},"button:hover":{"background":"green"}}',
+        ),
       ).toEqual(
         '.fp-button{background:red;} .fp-button:hover{background:green;}',
       );
