94e9e04506c3ff4ba3ed16d65f4ccbcc491a8dd8
Rafael Michels Motta <rafael@onefootprint.com>
2023-05-04 21:18:37 -0300

    [FP-3869] Fix sessionStorage not available (#3334)

--- frontend/packages/dev-tools/src/utils/session-id/session-id.test.ts
+++ frontend/packages/dev-tools/src/utils/session-id/session-id.test.ts
@@ -1,14 +1,50 @@
 import getSessionId from './session-id';
 
 describe('getSessionId', () => {
-  it('should return a session id', () => {
-    const sessionId = getSessionId();
-    expect(typeof sessionId).toBe('string');
+  describe('when sessionStorage is available', () => {
+    beforeEach(() => {
+      Object.defineProperty(window, 'sessionStorage', {
+        value: {
+          getItem: jest.fn(() => null),
+          setItem: jest.fn(),
+          removeItem: jest.fn(),
+        },
+        writable: true,
+      });
+    });
+
+    it('should store session id in sessionStorage', () => {
+      const sessionId = getSessionId();
+      expect(window.sessionStorage.setItem).toHaveBeenCalledWith(
+        'fp-session-id',
+        sessionId,
+      );
+    });
+
+    it('should retrieve session id from sessionStorage', () => {
+      const sessionId = getSessionId();
+      // @ts-ignore
+      window.sessionStorage.getItem.mockReturnValueOnce(sessionId);
+      const sessionId2 = getSessionId();
+      expect(sessionId).toEqual(sessionId2);
+    });
   });
 
-  it('should return the same session id that was created previously', () => {
-    const sessionId = getSessionId();
-    const sessionId2 = getSessionId();
-    expect(sessionId).toEqual(sessionId2);
+  describe('when sessionStorage is not available', () => {
+    beforeEach(() => {
+      Object.defineProperty(window, 'sessionStorage', {
+        value: undefined,
+        writable: true,
+      });
+    });
+
+    it('should return a session id even if sessionStorage is not available', () => {
+      const sessionId = getSessionId();
+      expect(typeof sessionId).toBe('string');
+    });
+
+    it('should not throw an error when sessionStorage is not available', () => {
+      expect(() => getSessionId()).not.toThrow();
+    });
   });
 });
--- frontend/packages/dev-tools/src/utils/session-id/session-id.ts
+++ frontend/packages/dev-tools/src/utils/session-id/session-id.ts
@@ -2,13 +2,27 @@ import { v4 as uuidv4 } from 'uuid';
 
 const key = 'fp-session-id';
 
+const isSessionStorageAvailable = () => {
+  try {
+    const testKey = 'test';
+    sessionStorage.setItem(testKey, testKey);
+    sessionStorage.removeItem(testKey);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
 const create = () => {
   const sessionId = uuidv4();
-  sessionStorage?.setItem(key, sessionId);
+  if (isSessionStorageAvailable()) {
+    sessionStorage.setItem(key, sessionId);
+  }
   return sessionId;
 };
 
-const get = () => sessionStorage?.getItem(key);
+const get = () =>
+  isSessionStorageAvailable() ? sessionStorage.getItem(key) : null;
 
 const getSessionId = () => get() || create();
 
