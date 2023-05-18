94e64a590ccd1f127a2b0f4d723dedbbf0a5883e
Keagan Long <keagan@onefootprint.com>
2023-05-18 14:55:01 -0700

    Remove Socure from TVC default apis in get_enabled_vendor_apis (#3498)
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/api/core/src/decision/tests/requirements.rs
+++ backend/components/api/core/src/decision/tests/requirements.rs
@@ -59,7 +59,7 @@ async fn create_tvc_for_requirements(
     IdentityDataKind::State,
     IdentityDataKind::Country,
     IdentityDataKind::City,
-], Some(vec![Vendor::Idology]) => vec![VendorAPI::IdologyExpectID, VendorAPI::SocureIDPlus])]
+], Some(vec![Vendor::Idology]) => vec![VendorAPI::IdologyExpectID])]
 #[test_case(vec![
     IdentityDataKind::FirstName,
     IdentityDataKind::LastName,
@@ -68,19 +68,7 @@ async fn create_tvc_for_requirements(
     IdentityDataKind::State,
     IdentityDataKind::Country,
     IdentityDataKind::City,
-], None => vec![VendorAPI::IdologyExpectID, VendorAPI::SocureIDPlus])]
-#[test_case(vec![
-    IdentityDataKind::FirstName,
-    IdentityDataKind::LastName,
-    IdentityDataKind::AddressLine1,
-    IdentityDataKind::Zip,
-    IdentityDataKind::State,
-    IdentityDataKind::Country,
-    IdentityDataKind::City,
-    IdentityDataKind::PhoneNumber,
-    IdentityDataKind::Ssn9,
-    IdentityDataKind::Ssn4,
-], Some(vec![Vendor::Idology, Vendor::Experian]) => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2, VendorAPI::SocureIDPlus, VendorAPI::ExperianPreciseID])]
+], None => vec![VendorAPI::IdologyExpectID])]
 #[test_case(vec![
     IdentityDataKind::FirstName,
     IdentityDataKind::LastName,
@@ -92,24 +80,38 @@ async fn create_tvc_for_requirements(
     IdentityDataKind::PhoneNumber,
     IdentityDataKind::Ssn9,
     IdentityDataKind::Ssn4,
-], None => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2, VendorAPI::SocureIDPlus]; "no tenant vendor control means no experian")]
+], Some(vec![Vendor::Idology, Vendor::Experian]) => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2, VendorAPI::ExperianPreciseID])]
+#[test_case(vec![
+    IdentityDataKind::FirstName,
+    IdentityDataKind::LastName,
+    IdentityDataKind::AddressLine1,
+    IdentityDataKind::Zip,
+    IdentityDataKind::State,
+    IdentityDataKind::Country,
+    IdentityDataKind::City,
+    IdentityDataKind::PhoneNumber,
+    IdentityDataKind::Ssn9,
+    IdentityDataKind::Ssn4,
+], None => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2]; "no tenant vendor control means no experian")]
 // not enough info for experian, but tenant is xpn enabled
 #[test_case(vec![
     IdentityDataKind::FirstName,
     IdentityDataKind::LastName,
-] , Some(vec![Vendor::Idology, Vendor::Experian]) => vec![VendorAPI::SocureIDPlus])]
+    IdentityDataKind::AddressLine1,
+] , Some(vec![Vendor::Idology, Vendor::Experian]) => vec![VendorAPI::IdologyExpectID])]
 #[test_case(vec![
     IdentityDataKind::FirstName,
     IdentityDataKind::LastName,
-] , None => vec![VendorAPI::SocureIDPlus])]
+    IdentityDataKind::AddressLine1,
+] , None => vec![VendorAPI::IdologyExpectID])]
 // Catch all case for vendors we expect if we had a full vault
 #[test_case(
     IdentityDataKind::iter().collect()
-, None => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2, VendorAPI::SocureIDPlus])]
+, None => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2])]
 // Catch all case for vendors we expect if we had a full vault and TVC
 #[test_case(
     IdentityDataKind::iter().collect()
-, Some(vec![Vendor::Idology, Vendor::Experian]) => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2, VendorAPI::SocureIDPlus, VendorAPI::ExperianPreciseID])]
+, Some(vec![Vendor::Idology, Vendor::Experian]) => vec![VendorAPI::IdologyExpectID, VendorAPI::TwilioLookupV2, VendorAPI::ExperianPreciseID])]
 #[tokio::test]
 async fn test_get_vendor_apis_for_verification_requests(
     data_lifetime_kinds: Vec<IdentityDataKind>,
