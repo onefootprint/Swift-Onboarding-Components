4fbd34289164bac8510ee5fe47ad765ffd426cae
Claudio Angrigiani <angrigianicr@gmail.com>
2023-05-22 12:25:59 -0400

    [FP-4006] New CardExpInput component (#3525)

---
+++ frontend/packages/ui/src/components/card-exp-date-input/card-exp-date-input.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable no-param-reassign */
+import { isPast, parse } from 'date-fns';
+import React, { forwardRef, useState } from 'react';
+
+import Input, { InputProps } from '../internal/input';
+
+export type CardExpDateInputProps = Omit<
+  InputProps,
+  | 'autoComplete'
+  | 'inputMode'
+  | 'mask'
+  | 'maxLength'
+  | 'minLength'
+  | 'placeholder'
+  | 'size'
+  | 'type'
+> & {
+  invalidMessage?: string;
+};
+
+const CardExpDateInput = forwardRef<HTMLInputElement, CardExpDateInputProps>(
+  (
+    {
+      hasError,
+      hint,
+      onChange,
+      onBlur,
+      invalidMessage = 'Date must be in the future',
+      label = 'Exp. Date',
+      value,
+      ...props
+    }: CardExpDateInputProps,
+    ref,
+  ) => {
+    const parsedDate = parse(value || '', 'MM/yy', new Date());
+    const [blurred, setBlurred] = useState(false);
+    const shouldShowError = blurred && (hasError || isPast(parsedDate));
+    const errorMessage = shouldShowError ? invalidMessage : undefined;
+
+    const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+      setBlurred(true);
+      onBlur?.(event);
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setBlurred(false);
+      onChange?.(event);
+    };
+
+    return (
+      <Input
+        {...props}
+        autoComplete="cc-exp"
+        className="fp-input-exp-date"
+        hasError={shouldShowError}
+        hint={errorMessage || hint}
+        inputMode="numeric"
+        label={label}
+        mask={{
+          date: true,
+
+          datePattern: ['m', 'y'],
+        }}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        placeholder="MM / YY"
+        ref={ref}
+        size="default"
+        value={value}
+      />
+    );
+  },
+);
+
+export default CardExpDateInput;
---
+++ frontend/packages/ui/src/components/card-exp-date-input/index.ts
@@ -0,0 +1,1 @@
+export { type CardExpDateInputProps, default } from './card-exp-date-input';
