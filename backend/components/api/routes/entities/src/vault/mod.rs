b3df722fef1a6e02634ad6023f996c5ab08dc6e3
Elliott Forde <elliott@onefootprint.com>
2023-05-21 22:16:29 -0700

    Separate entities POST vault/validate from PATCH /vault (#3535)
    ## Describe your changes
    
    These will get messier when we have the versions that take the client
    auth token, so just splitting into separate files

--- backend/components/api/routes/entities/src/vault/mod.rs
+++ backend/components/api/routes/entities/src/vault/mod.rs
@@ -1,19 +1,20 @@
 use paperclip::actix::web;
 
-pub mod decrypt;
-pub mod get;
-pub mod patch;
+mod decrypt;
+mod get;
+mod patch;
+mod validate;
 
 pub fn routes(config: &mut web::ServiceConfig) {
     config
         .service(get::get)
         .service(decrypt::post)
         .service(decrypt::post_client)
-        .service(patch::post_validate)
+        .service(validate::post)
         .service(patch::patch);
 
     get::configure_get_aliases(config);
     decrypt::configure_post_aliases(config);
     patch::configure_patch_aliases(config);
-    patch::configure_post_validate_aliases(config);
+    validate::configure_post_aliases(config);
 }
--- backend/components/api/routes/entities/src/vault/patch.rs
+++ backend/components/api/routes/entities/src/vault/patch.rs
@@ -5,7 +5,7 @@ use crate::types::{EmptyResponse, JsonApiResponse};
 use crate::utils::headers::InsightHeaders;
 use crate::utils::vault_wrapper::VaultWrapper;
 use crate::State;
-use api_core::utils::vault_wrapper::{Any, TenantVw};
+use api_core::utils::vault_wrapper::Any;
 use db::models::access_event::NewAccessEvent;
 use db::models::insight_event::CreateInsightEvent;
 use db::models::scoped_vault::ScopedVault;
@@ -14,53 +14,6 @@ use macros::route_alias;
 use newtypes::put_data_request::RawDataRequest;
 use newtypes::{AccessEventKind, FpId, ValidateArgs};
 use paperclip::actix::{self, api_v2_operation, web, web::Json, web::Path};
-
-#[route_alias(
-    actix::post(
-        "/users/{footprint_user_id}/vault/validate",
-        tags(Users, Vault, PublicApi),
-        description = "Checks if provided data is valid before adding it to the vault."
-    ),
-    actix::post(
-        "/businesses/{footprint_biz_id}/vault/validate",
-        tags(Businesses, Vault, PublicApi),
-        description = "Checks if provided data is valid before adding it to the vault."
-    )
-)]
-#[api_v2_operation(
-    description = "Works for either person or business entities. Checks if provided data is valid before adding it to the vault.",
-    tags(Vault, Entities, Preview)
-)]
-#[actix::post("/entities/{fp_id}/vault/validate")]
-pub async fn post_validate(
-    state: web::Data<State>,
-    path: Path<FpId>,
-    request: Json<RawDataRequest>,
-    tenant_auth: SecretTenantAuthContext,
-) -> JsonApiResponse<EmptyResponse> {
-    let fp_id = path.into_inner();
-
-    let tenant_auth = tenant_auth.check_guard(TenantGuard::Admin)?;
-    let tenant_id = tenant_auth.tenant().id.clone();
-    let is_live = tenant_auth.is_live()?;
-
-    let request = request
-        .into_inner()
-        .clean_and_validate(ValidateArgs::for_non_portable(is_live))?;
-    let uvw = state
-        .db_pool
-        .db_query(move |conn| -> ApiResult<_> {
-            let scoped_user = ScopedVault::get(conn, (&fp_id, &tenant_id, is_live))?;
-            let uvw: TenantVw = VaultWrapper::build_for_tenant(conn, &scoped_user.id)?;
-            Ok(uvw)
-        })
-        .await??;
-    request.assert_allowable_identifiers(uvw.vault.kind)?;
-    let request = request.no_fingerprints(); // No fingerprints to check speculatively
-    uvw.validate_request(request)?;
-
-    EmptyResponse::ok().json()
-}
 
 #[route_alias(
     actix::patch(
