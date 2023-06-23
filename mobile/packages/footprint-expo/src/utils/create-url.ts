09a6e4830dda3390f2c0335374b6baf04744b058
Belce Dogru <belce.dogru@gmail.com>
2023-06-23 08:52:49 -0700

    Add options to mobile urls
    Load event should be returning an async function we can wait on

--- mobile/packages/footprint-expo/src/utils/create-url.ts
+++ mobile/packages/footprint-expo/src/utils/create-url.ts
@@ -1,20 +1,24 @@
 import type {
   FootprintAppearance,
+  FootprintOptions,
   FootprintUserData,
 } from '../footprint.types';
 import getAppearance from './get-appearance';
+import getOptions from './get-options';
 import getUserData from './get-user-data';
 
 const getURL = (params: {
   appearance?: FootprintAppearance;
   publicKey?: string;
   userData?: FootprintUserData;
+  options?: FootprintOptions;
   redirectUrl?: string;
 }) => {
   const url = 'http://id.onefootprint.com';
   const { redirectUrl, publicKey } = params;
   const { fontSrc, rules, variables } = getAppearance(params.appearance);
   const userData = getUserData(params.userData);
+  const options = getOptions(params.options);
   const searchParams = new URLSearchParams();
   if (publicKey) {
     searchParams.append('public_key', publicKey);
@@ -31,8 +35,17 @@ const getURL = (params: {
   if (redirectUrl) {
     searchParams.append('redirect_url', redirectUrl);
   }
+
+  let urlFragment = '';
   if (userData) {
-    return `${url}?${searchParams.toString()}#${userData}`;
+    urlFragment = `${userData}`;
+  }
+  if (options) {
+    urlFragment = `${urlFragment}__${options}`;
+  }
+
+  if (urlFragment) {
+    return `${url}?${searchParams.toString()}#${urlFragment}`;
   } else {
     return `${url}?${searchParams.toString()}`;
   }
