11df367b1b8ffc504086ccbc2ab29883d3c3b040
Elliott Forde <elliott@onefootprint.com>
2023-07-05 11:10:51 -0700

    Revert "[FP-4856] Migrate document_data.kind to full DIs (#4267)"
    This reverts commit 415c778ee8649a8618cdef943e9b84f7ccdad5e4.

--- backend/components/db/schema/migrations/2023-07-05-172707_update_document_data_kind/down.sql
+++
@@ -1,3 +0,0 @@
-UPDATE document_data
-SET kind = REPLACE(kind, 'document.', '')
-WHERE kind ILIKE 'document.%';
--- backend/components/db/schema/migrations/2023-07-05-172707_update_document_data_kind/up.sql
+++
@@ -1,5 +0,0 @@
-UPDATE document_data
-SET kind = 'document.' || kind
-WHERE
-    kind NOT ILIKE 'document.%' AND
-    kind NOT ILIKE 'custom.%';
