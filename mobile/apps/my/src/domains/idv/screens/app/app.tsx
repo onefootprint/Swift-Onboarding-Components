3068807e62b4740dfe1e5a51d79e7fd8ce3f6fa7
Rafael Michels Motta <rafael@onefootprint.com>
2023-07-07 11:46:49 -0300

    Fix linking

--- mobile/apps/my/src/domains/idv/screens/app/app.tsx
+++ mobile/apps/my/src/domains/idv/screens/app/app.tsx
@@ -3,17 +3,21 @@ import styled, { css } from '@onefootprint/styled';
 import { DesignSystemProvider } from '@onefootprint/ui';
 import React from 'react';
 
+import { REVIEW_AUTH_TOKEN } from '@/config/constants';
+
+import AppStoreReview from '../app-store-review';
 import Error from '../error';
 import Router from '../router';
 import ThemeProvider from './components/theme-provider';
 import useParseHandoffUrl from './hooks/use-parse-handoff-url';
 
 type AppProps = {
+  linkingUrl?: string;
   onLoad: () => void;
 };
 
-const App = ({ onLoad }: AppProps) => {
-  const { isError, data } = useParseHandoffUrl();
+const App = ({ linkingUrl, onLoad }: AppProps) => {
+  const { isError, data } = useParseHandoffUrl(linkingUrl);
 
   if (isError) {
     return (
@@ -28,7 +32,11 @@ const App = ({ onLoad }: AppProps) => {
     return (
       <ThemeProvider authToken={data.authToken}>
         <Container onLayout={onLoad}>
-          <Router authToken={data.authToken} />
+          {data.authToken === REVIEW_AUTH_TOKEN ? (
+            <AppStoreReview authToken={data.authToken} />
+          ) : (
+            <Router authToken={data.authToken} />
+          )}
         </Container>
       </ThemeProvider>
     );
