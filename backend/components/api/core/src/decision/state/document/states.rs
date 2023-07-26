759e5979ef2c11f5bed95ef74fe2e4bcfd1b8545
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2023-07-26 16:43:57 -0400

    Redo doc v0 (#4545)
    ## Describe your changes
    Collect document and re-run decisioning logic, hiding previous doc risk signals in the process
    
    
    potentially we'd want another doc failure or potentially a doc pass to raise a review but that's tbd
    
    
    **note** I also disabled the failing alpaca tests for now
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    PR above in stack

--- backend/components/api/core/src/decision/state/document/states.rs
+++ backend/components/api/core/src/decision/state/document/states.rs
@@ -2,15 +2,14 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use db::models::{vault::Vault, workflow::Workflow as DbWorkflow};
+
 use feature_flag::FeatureFlagClient;
-use newtypes::{DecisionStatus, DocumentConfig, VendorAPI};
+use newtypes::DocumentConfig;
 use newtypes::{OnboardingId, ScopedVaultId, TenantId, WorkflowId};
 use webhooks::WebhookClient;
 
 use super::{DocumentState, MakeDecision};
-use crate::decision::onboarding::{
-    Decision, DecisionResult, OnboardingRulesDecisionOutput, WaterfallOnboardingRulesDecisionOutput,
-};
+use crate::decision::features::risk_signals::fetch_latest_risk_signals_map;
 use crate::decision::{
     onboarding::rules::KycRuleGroup,
     rule::rule_sets,
@@ -155,7 +154,6 @@ impl OnAction<MakeDecision, DocumentState> for DocumentDecisioning {
         _action: MakeDecision,
         state: &State,
     ) -> ApiResult<Self::AsyncRes> {
-        // TODO
         let vendor_results =
             common::assert_kyc_vendor_calls_completed(state, &self.ob_id, &self.sv_id).await?;
 
@@ -171,32 +169,16 @@ impl OnAction<MakeDecision, DocumentState> for DocumentDecisioning {
         let (ff_client, vendor_results, webhook_client) = async_res;
         let vault = Vault::get(conn, &self.sv_id)?;
         let fixture_decision = decision::utils::get_fixture_data_decision(ff_client, &vault, &self.t_id)?;
+        let risk_signals = fetch_latest_risk_signals_map(conn, &self.sv_id)?;
 
         let decision = if let Some(fixture_decision) = fixture_decision {
             common::kyc_decision_from_fixture(fixture_decision)?
         } else {
-            // TODO: fixed in PR up stack
-            let decision = OnboardingRulesDecisionOutput {
-                decision: Decision {
-                    decision_status: DecisionStatus::Pass,
-                    should_commit: false,
-                    create_manual_review: false,
-                    vendor_api: VendorAPI::IncodeFetchScores,
-                },
-                // in future we could have the wc_reason_codes.is_empty expresses as a rule and append that rule result here. This only impacts a log
-                rules_triggered: vec![],
-                rules_not_triggered: vec![],
-            };
-
-            WaterfallOnboardingRulesDecisionOutput::new(
-                DecisionResult::NotRequired,
-                DecisionResult::Evaluated(decision),
-                DecisionResult::NotRequired,
-                vec![],
-            )
+            // Rerun decisioning, but with the latest doc risk signals
+            // TODO: what's the review strategy for this case?
+            common::get_decision(&self, conn, risk_signals, &self.sv_id, &self.wf_id)?
         };
 
-        // TODO: doc wf shouldn't really be calling this
         common::save_kyc_decision(
             conn,
             webhook_client,
