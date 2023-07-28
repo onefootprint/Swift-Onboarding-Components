6ef3d2b0cf2cfd7f244c5c99a8858ca3487167bc
Keagan Long <keagan@onefootprint.com>
2023-07-28 08:05:31 -1000

    Parse Stytch errors (#4685)
    ## Describe your changes
    
    Handle parsing either error or success responses from Stytch
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/idv/src/stytch/error.rs
+++ backend/components/idv/src/stytch/error.rs
@@ -1,7 +1,11 @@
+use serde_with::{DeserializeFromStr, SerializeDisplay};
 use std::fmt::Debug;
+use strum::Display;
+use strum_macros::EnumString;
+
+use super::response::StytchErrorResponse;
 
 #[derive(Debug, thiserror::Error)]
-
 pub enum Error {
     #[error("Json error: {0}")]
     SerdeJson(#[from] serde_json::Error),
@@ -9,4 +13,14 @@ pub enum Error {
     InternalError(#[from] reqwest::Error),
     #[error("error sending request to fingerprint js api: {0}")]
     RequestError(String),
+    #[error("Stytch error response: {0:?}")]
+    StytchError(StytchErrorResponse),
+}
+
+#[derive(Clone, Debug, Display, EnumString, SerializeDisplay, DeserializeFromStr, Eq, PartialEq)]
+pub enum StytchError {
+    #[strum(serialize = "The telemety_id was not found.")]
+    TelemetryIdNotFound,
+    #[strum(default)]
+    Unknown(String),
 }
--- backend/components/idv/src/stytch/response.rs
+++ backend/components/idv/src/stytch/response.rs
@@ -1,6 +1,26 @@
+use super::error::{Error, StytchError};
 use serde::*;
 
-// TODO
+pub fn parse_response(value: serde_json::Value) -> Result<LookupResponse, Error> {
+    let response: Response = serde_json::value::from_value(value)?;
+    match response {
+        Response::Success(r) => Ok(r),
+        Response::Error(r) => Err(Error::StytchError(r)),
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Response {
+    Success(LookupResponse),
+    Error(StytchErrorResponse),
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct StytchErrorResponse {
+    pub error_message: StytchError,
+    pub status_code: Option<u16>,
+}
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct LookupResponse {
@@ -13,11 +33,41 @@ mod tests {
     use serde_json::json;
 
     #[test]
-    fn test_parse() {
+    fn parse_success() {
         let json = json!({
             "telemetry_id": "abc_123",
         });
-        let parsed: LookupResponse = serde_json::from_value(json).unwrap();
+        let parsed = parse_response(json).unwrap();
         assert_eq!("abc_123".to_owned(), parsed.telemetry_id);
+    }
+
+    #[test]
+    fn parse_error() {
+        let json = json!({
+            "error_message": "The telemety_id was not found.",
+        });
+        let parsed = parse_response(json).unwrap_err();
+
+        let Error::StytchError(e) = parsed else {
+            panic!("Expected StytchError, got {:?}", parsed);
+        };
+        assert_eq!(StytchError::TelemetryIdNotFound, e.error_message);
+    }
+
+    #[test]
+    fn parse_unknown_error() {
+        let json = json!({
+            "error_message": "Oh shoot",
+        });
+        let parsed = parse_response(json).unwrap_err();
+
+        let Error::StytchError(e) = parsed else {
+            panic!("Expected StytchError, got {:?}", parsed);
+        };
+
+        let StytchError::Unknown(s) = e.error_message else {
+            panic!("Expected StytchError::Unknown");
+        };
+        assert_eq!("Oh shoot".to_owned(), s);
     }
 }
