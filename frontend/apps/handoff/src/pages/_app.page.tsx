08ee734f989a1ddf223e2b6b9d2a938675267b3f
Rafael Michels Motta <rafael@onefootprint.com>
2023-08-01 11:16:12 -0300

    [FP-5386] Appclip debug: show smart banner (#4774)

--- frontend/apps/handoff/src/pages/_app.page.tsx
+++ frontend/apps/handoff/src/pages/_app.page.tsx
@@ -6,6 +6,8 @@ import { createGlobalStyle, css } from '@onefootprint/styled';
 import { DesignSystemProvider } from '@onefootprint/ui';
 import { QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import MachineProvider from '../components/machine-provider';
@@ -16,18 +18,34 @@ import configureSentry from '../config/initializers/sentry';
 configureSentry();
 configureReactI18next();
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <QueryClientProvider client={queryClient}>
-    <ObserveCollectorProvider appName="handoff">
-      <MachineProvider>
-        <DesignSystemProvider theme={themes.light}>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </DesignSystemProvider>
-      </MachineProvider>
-    </ObserveCollectorProvider>
-  </QueryClientProvider>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  // https://developer.apple.com/documentation/app_clips/supporting_invocations_from_your_website_and_the_messages_app
+  const shouldShowAppClipSmartBanner = router.pathname === '/appclip';
+
+  return (
+    <>
+      {shouldShowAppClipSmartBanner ? (
+        <Head>
+          <meta
+            name="apple-itunes-app"
+            content="app-id=1632436468, app-clip-bundle-id=com.onefootprint.my.Clip, app-clip-display=card"
+          />
+        </Head>
+      ) : null}
+      <QueryClientProvider client={queryClient}>
+        <ObserveCollectorProvider appName="handoff">
+          <MachineProvider>
+            <DesignSystemProvider theme={themes.light}>
+              <GlobalStyle />
+              <Component {...pageProps} />
+            </DesignSystemProvider>
+          </MachineProvider>
+        </ObserveCollectorProvider>
+      </QueryClientProvider>
+    </>
+  );
+};
 
 const GlobalStyle = createGlobalStyle`
   ${({ theme }) => css`
