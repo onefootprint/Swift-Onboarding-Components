a69a4f97304fe1fde905499e3a6401733171e245
Rafael Michels Motta <rafael@onefootprint.com>
2023-08-08 20:12:59 -0300

    Appclip: fix buffer not being loaded (#4950)

--- mobile/apps/my/index.appclip.js
+++ mobile/apps/my/index.appclip.js
@@ -1,4 +1,6 @@
-import { AppRegistry, View, Text } from 'react-native';
+import { AppRegistry } from 'react-native';
 import IdvApp from './src/idv-app';
+
+global.Buffer = global.Buffer || require('buffer').Buffer;
 
 AppRegistry.registerComponent('appclip', () => IdvApp);
