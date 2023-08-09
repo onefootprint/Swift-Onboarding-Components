a0aea438de80402d2661f93cc1c812284c4e8a7f
Rafael Michels Motta <rafael@onefootprint.com>
2023-08-09 08:43:30 -0300

    Testing cache

--- frontend/apps/bifrost/src/pages/root/root.tsx
+++ frontend/apps/bifrost/src/pages/root/root.tsx
@@ -60,7 +60,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   res.setHeader(
     'Cache-Control',
-    'public, s-maxage=60, stale-while-revalidate=3600',
+    'public, s-maxage=30, stale-while-revalidate=3600',
   );
 
   const obConfig = query.public_key as string | undefined;
