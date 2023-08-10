274abce708d6f57f278a5319aa813abc70645a75
Alex Grinman <356333+agrinman@users.noreply.github.com>
2023-08-10 18:04:47 -0400

    API-access for /liveness endpoint (#5011)
    ## Describe your changes
    
    Context:
    https://onefootprint.slack.com/archives/C05768Z3JRY/p1691680395238669

--- backend/components/api/routes/entities/src/liveness.rs
+++ backend/components/api/routes/entities/src/liveness.rs
@@ -1,10 +1,12 @@
 use crate::auth::tenant::CheckTenantGuard;
 use crate::auth::tenant::TenantGuard;
 use crate::auth::tenant::TenantSessionAuth;
+use crate::auth::Either;
 use crate::errors::ApiError;
 use crate::types::response::ResponseData;
 use crate::utils::db2api::DbToApi;
 use crate::State;
+use api_core::auth::tenant::SecretTenantAuthContext;
 use newtypes::FpId;
 use paperclip::actix::{api_v2_operation, get, web, web::Json};
 
@@ -16,7 +18,7 @@ use paperclip::actix::{api_v2_operation, get, web, web::Json};
 pub async fn get(
     state: web::Data<State>,
     request: web::Path<FpId>,
-    auth: TenantSessionAuth,
+    auth: Either<TenantSessionAuth, SecretTenantAuthContext>,
 ) -> actix_web::Result<Json<ResponseData<Vec<api_wire_types::LivenessEvent>>>, ApiError> {
     let auth = auth.check_guard(TenantGuard::Read)?;
     let tenant_id = auth.tenant().id.clone();
