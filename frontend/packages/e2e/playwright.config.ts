ee7492d4dc9d00e0a792b282d5ad7ad7d8eab00d
Rafael Michels Motta <rafael@onefootprint.com>
2023-08-11 08:39:12 -0300

    [E2E] Adjust timeout and retry

--- frontend/packages/e2e/playwright.config.ts
+++ frontend/packages/e2e/playwright.config.ts
@@ -11,14 +11,14 @@ dotenv.config();
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 60000 * 10,
+  timeout: 30000,
   testDir: './src',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 5 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
