5b3c5128669b0ce8dc63ba723292eaf219503183
Keagan Long <keagan@onefootprint.com>
2023-08-14 13:28:31 -1000

    Alert for pending users/workflows (#5066)
    ## Describe your changes
    
    Airplane cron job to alert on ScopedVault's/Workflow's created in the
    past few days that have been in `pending` for more than 2 minutes. Now
    that we've worked through a great deal of the vendor quirks, we should
    have very few situations where this happens and when it does we need to
    react quickly + fix.
    
    When we start getting KYB users and are beholden to Middesk's async
    flow, we'll likely need to use a bigger threshold for kyb sv's/wf's
    since that's expected to take many minutes in common cases
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

---
+++ backend/internal_tools/airplane/tasks/report_pending_users.airplane.ts
@@ -0,0 +1,72 @@
+import airplane from 'airplane';
+import { pg_query } from '../utils';
+
+export default airplane.task(
+  {
+    slug: 'report_pending_tasks',
+    name: "Report Pending ScopedVault's/Workflow's",
+    parameters: {},
+    envVars: {
+      DATABASE_URL: { config: 'DATABASE_URL' },
+    },
+    schedules:
+      process.env.AIRPLANE_ENV_SLUG === 'prod'
+        ? {
+            every_three_minutes: {
+              cron: '*/3 * * * *',
+              description: 'Runs every 3 minutes',
+            },
+          }
+        : {},
+  },
+  async params => {
+    const dbUrl = process.env.DATABASE_URL;
+    let query = `
+      with pending_scoped_vaults as (
+        select 
+          id sv_id,
+        start_timestamp sv_created_at, 
+          status sv_status
+        from scoped_vault 
+        where 
+          status = 'pending'
+      ),
+      pending_workflows as (
+        select
+        id wf_id,
+          scoped_vault_id sv_id,
+          created_at wf_created_at,
+        status wf_status
+        from workflow
+        where 
+        status = 'pending'
+      )
+
+      select 
+        t.name tenant_name, t.id tenant_id,
+        coalesce(psv.sv_id, wf.sv_id) sv_id,
+        psv.sv_created_at, psv.sv_status, wf.wf_created_at, wf.wf_status, wf.wf_id
+      from pending_scoped_vaults psv
+      full outer join pending_workflows wf on psv.sv_id = wf.sv_id
+      inner join scoped_vault sv on sv.id = coalesce(psv.sv_id, wf.sv_id)
+      inner join tenant t on sv.tenant_id = t.id
+      where 
+        t.id != '_private_it_org_2'
+        and greatest(psv.sv_created_at, wf.wf_created_at) >= now() - interval '3 day'
+        and greatest(psv.sv_created_at, wf.wf_created_at) < now() - interval '2 minute'
+    `;
+
+    const rows = await pg_query(dbUrl, query);
+    if (rows.length > 0) {
+      const message = `* ${
+        rows.length
+      } ScopedVault's/Workflow's Stuck in Pending* (showing first 30)\n${rows
+        .slice(0, 30) // only show first 30 so we don't barf too hard on Slack
+        .map(e => JSON.stringify(e))
+        .join(', ')}`;
+      await airplane.slack.message('risk-alerts', message);
+      throw message;
+    }
+    return rows;
+  },
+);
