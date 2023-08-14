d6c6d45e7c4e95e8cd0f122e618833fddc28feae
Alex Grinman <356333+agrinman@users.noreply.github.com>
2023-08-14 16:21:59 -0400

    yet another android passkey workaround (#5070)
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/api/core/src/utils/liveness.rs
+++ backend/components/api/core/src/utils/liveness.rs
@@ -27,17 +27,21 @@ impl LivenessWebauthnConfig {
                 Some(true),
                 Some(true),
             ),
-            /// TEMPORARY: workaround for this bug: https://github.com/android/identity-samples/issues/49
-            android_webauthn: WebauthnCore::new_unsafe_experts_only(
-                "Footprint",
-                &config.rp_id,
-                #[allow(clippy::unwrap_used)]
-                &url::Url::parse("android:apk-key-hash:D_woKFaP1yeRthdVOKrD03l1Dx6xKjgv7cCoE13UXcg").unwrap(),
-                Some(120 * 1000),
-                Some(true),
-                Some(true),
-            ),
+            android_webauthn: Self::android_config(&config.rp_id),
         }
+    }
+
+    /// TEMPORARY: workaround for this bug: https://github.com/android/identity-samples/issues/49
+    pub fn android_config(rp_id: &str) -> WebauthnCore {
+        WebauthnCore::new_unsafe_experts_only(
+            "Footprint",
+            rp_id,
+            #[allow(clippy::unwrap_used)]
+            &url::Url::parse("android:apk-key-hash:D_woKFaP1yeRthdVOKrD03l1Dx6xKjgv7cCoE13UXcg").unwrap(),
+            Some(120 * 1000),
+            Some(false),
+            Some(true),
+        )
     }
 
     pub fn android_workaround(&self) -> &WebauthnCore {
