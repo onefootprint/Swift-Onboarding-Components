7b6e81e1f947e55c9531439ee63ce796d4a0f56d
Lucas Gelfond <lucas@onefootprint.com>
2023-08-14 09:22:03 -0700

    [FP-5633] improve app clip dev experience (#5057)

--- mobile/apps/my/src/hooks/use-should-open-idv.ts
+++ mobile/apps/my/src/hooks/use-should-open-idv.ts
@@ -1,7 +1,12 @@
+import { DEBUG_HANDOFF_URL, IS_DEV } from '@/domains/idv/config/constants';
+
 import useURL from './use-url';
 
 const useShouldOpenIdv = () => {
   const linkingUrl = useURL();
+  if (DEBUG_HANDOFF_URL && IS_DEV) {
+    return { shouldOpen: true, linkingUrl: DEBUG_HANDOFF_URL };
+  }
   const shouldOpen = linkingUrl?.includes('https://handoff');
   return { shouldOpen, linkingUrl };
 };
