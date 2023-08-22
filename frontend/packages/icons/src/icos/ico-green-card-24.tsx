bc1e0d613d9d876fbaa5b3b8d023c65eec6e005f
Claudio Angrigiani <angrigianicr@gmail.com>
2023-08-22 14:34:16 -0400

    New icons id-docs (#5292)
    ![image](https://github.com/onefootprint/monorepo/assets/12160059/acca6edd-18c3-4c9c-b6d5-74edc96d74f6)

---
+++ frontend/packages/icons/src/icos/ico-green-card-24.tsx
@@ -0,0 +1,31 @@
+import { useTheme } from '@onefootprint/styled';
+import React from 'react';
+
+import type { IconProps } from '../types';
+
+const IcoGreenCard24 = ({
+  color = 'primary',
+  className,
+  testID,
+}: IconProps) => {
+  const theme = useTheme();
+  return (
+    <svg
+      width={24}
+      height={24}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      data-testid={testID}
+      className={className}
+      aria-hidden="true"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4.241 6.23a.491.491 0 0 0-.491.491v10.552c0 .271.22.492.491.492H19.76c.271 0 .491-.22.491-.492V6.721a.491.491 0 0 0-.491-.491H4.24Zm-1.991.491c0-1.1.892-1.991 1.991-1.991H19.76c1.1 0 1.991.892 1.991 1.991v10.552c0 1.1-.892 1.992-1.991 1.992H4.24c-1.1 0-1.991-.892-1.991-1.992V6.721Zm5.384 2.302a1.422 1.422 0 1 0 0 2.845 1.422 1.422 0 0 0 0-2.845Zm-2.922 1.422a2.922 2.922 0 1 1 5.845 0 2.922 2.922 0 0 1-5.845 0Zm7.848 0a.75.75 0 0 1 .75-.75h3.966a.75.75 0 0 1 0 1.5H13.31a.75.75 0 0 1-.75-.75Zm1.403 3.725a.75.75 0 0 1 .75-.75h2.563a.75.75 0 0 1 0 1.5h-2.563a.75.75 0 0 1-.75-.75Z"
+        fill={theme.color[color]}
+      />
+    </svg>
+  );
+};
+export default IcoGreenCard24;
---
+++ frontend/packages/icons/src/icos/ico-work-24.tsx
@@ -0,0 +1,27 @@
+import { useTheme } from '@onefootprint/styled';
+import React from 'react';
+
+import type { IconProps } from '../types';
+
+const IcoWork24 = ({ color = 'primary', className, testID }: IconProps) => {
+  const theme = useTheme();
+  return (
+    <svg
+      width={24}
+      height={24}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      data-testid={testID}
+      className={className}
+      aria-hidden="true"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13.5 7.875v-1.5h-3v1.5h3ZM6 9.375v8.25h7.413c.03.535.166 1.042.387 1.5H6c-.832 0-1.5-.668-1.5-1.5l.008-8.25c0-.832.66-1.5 1.492-1.5h3v-1.5c0-.832.668-1.5 1.5-1.5h3c.832 0 1.5.668 1.5 1.5v1.5h3c.832 0 1.5.668 1.5 1.5v4.61a3.976 3.976 0 0 0-1.5-.547V9.375H6Zm13.824 7.112a.59.59 0 1 0-.835-.835l-2.231 2.231-.934-.933a.59.59 0 1 0-.835.835l1.351 1.351a.59.59 0 0 0 .835 0l2.65-2.65Z"
+        fill={theme.color[color]}
+      />
+    </svg>
+  );
+};
+export default IcoWork24;
