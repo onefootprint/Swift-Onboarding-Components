eda0cf352af92d0738e7b2eedbe209920ecf28ec
ahsan1578 <54286440+ahsan1578@users.noreply.github.com>
2023-08-22 14:32:36 -0700

    Demo app camera issue fixed (#5277)
    ## Describe your changes
    
    This is the same issue as we had before for hosted and hand-off where
    Chrome wouldn't allow camera access. We fixed the bug by updating the
    bifrost, handoff, and hosted next config. We never changed the config in
    the demo app.
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- frontend/apps/demos/next.config.js
+++ frontend/apps/demos/next.config.js
@@ -32,7 +32,7 @@ const securityHeaders = [
   },
   {
     key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
+    value: 'camera=*, microphone=(), geolocation=()',
   },
   {
     key: 'X-Content-Type-Options',
