8e6aaa17e75be41e3e52bd35c0153bfd6d915d51
Elliott Forde <elliott@onefootprint.com>
2023-08-29 17:11:37 -0700

    Add integration test for one active workflow (#5441)
    ## Describe your changes
    
    ^

--- backend/ci/tests/bifrost/test_reonboard.py
+++ backend/ci/tests/bifrost/test_reonboard.py
@@ -1,5 +1,6 @@
 from tests.utils import get, patch
 from tests.bifrost_client import BifrostClient
+from tests.utils import create_ob_config
 
 
 def test_reonboard(sandbox_tenant, twilio, sandbox_user):
@@ -22,3 +23,37 @@ def test_reonboard(sandbox_tenant, twilio, sandbox_user):
     )
     obds = [i for i in timeline if i["event"]["kind"] == "onboarding_decision"]
     assert len(obds) == 1
+
+
+def test_abort_then_reonboard(sandbox_tenant, twilio, must_collect_data):
+    obc1 = create_ob_config(
+        sandbox_tenant,
+        "Reonboard OBC 1",
+        must_collect_data,
+        must_collect_data,
+    )
+    obc2 = create_ob_config(
+        sandbox_tenant,
+        "Reonboard OBC 2",
+        must_collect_data,
+        must_collect_data,
+    )
+
+    # Start onboarding onto obc1, then deactivate it by onboarding onto obc2
+    bifrost1 = BifrostClient.new(obc1, twilio)
+    phone_number = bifrost1.data["id.phone_number"]
+    sandbox_id = bifrost1.sandbox_id
+    bifrost2 = BifrostClient.inherit(obc2, twilio, phone_number, sandbox_id)
+
+    # Shouldn't be able to do anything with bifrost1's workflow/auth token
+    body = patch("hosted/user/vault", dict(), bifrost1.auth_token, status_code=401)
+    assert (
+        body["error"]["message"] == "Workflow is deactivated. Cannot perform add_data"
+    )
+
+    # But should be able to use bifrost2's auth token
+    patch("hosted/user/vault", dict(), bifrost2.auth_token)
+
+    # And, can re-start onboarding onto obc1 and run to completion
+    bifrost1 = BifrostClient.inherit(obc1, twilio, phone_number, sandbox_id)
+    bifrost1.run()
