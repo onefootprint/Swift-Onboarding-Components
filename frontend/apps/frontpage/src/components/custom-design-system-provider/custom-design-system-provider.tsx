bb3cc321868055103932e21066f163178b64491e
Rafael Michels Motta <rafael@onefootprint.com>
2023-09-05 15:33:11 -0300

    [Design system provider]: Remove mounted state

--- frontend/apps/frontpage/src/components/custom-design-system-provider/custom-design-system-provider.tsx
+++ frontend/apps/frontpage/src/components/custom-design-system-provider/custom-design-system-provider.tsx
@@ -2,7 +2,7 @@ import themes from '@onefootprint/design-tokens';
 import { DesignSystemProvider } from '@onefootprint/ui';
 import { useRouter } from 'next/router';
 import { ThemeProvider, useTheme } from 'next-themes';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 type CustomDesignSystemProviderProps = {
   children: React.ReactNode;
@@ -19,19 +19,10 @@ const CustomDesignSystemProvider = ({
 const DesignSystemProviderWrapper = ({
   children,
 }: CustomDesignSystemProviderProps) => {
-  const [mounted, setMounted] = useState(false);
   const router = useRouter();
   const isVaultingSection = router.pathname.includes('/vaulting');
   const { theme } = useTheme();
   const isDark = theme === 'dark' || isVaultingSection;
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
     <DesignSystemProvider theme={themes[isDark ? 'dark' : 'light']}>
