56af2530519ca8475d44ffc1ae63709966ecaaf9
Belce Dogru <belce.dogru@gmail.com>
2023-09-06 15:50:04 -0700

    Fix error messages (#5576)

--- frontend/packages/idv-elements/src/components/app-error-boundary/components/error/error.tsx
+++ frontend/packages/idv-elements/src/components/app-error-boundary/components/error/error.tsx
@@ -3,23 +3,28 @@ import { IcoForbid40 } from '@onefootprint/icons';
 import { EmptyState } from '@onefootprint/ui';
 import React from 'react';
 
+import { NavigationHeader } from '../../../layout';
+
 type ErrorProps = {
   resetErrorBoundary: () => void;
 };
 
 const Error = ({ resetErrorBoundary }: ErrorProps) => {
-  const { t } = useTranslation('pages.error');
+  const { t } = useTranslation('global.errors.uncaught-error');
 
   return (
-    <EmptyState
-      description={t('description')}
-      iconComponent={IcoForbid40}
-      title={t('title')}
-      cta={{
-        label: t('cta'),
-        onClick: resetErrorBoundary,
-      }}
-    />
+    <>
+      <NavigationHeader button={{ variant: 'close', confirmClose: true }} />
+      <EmptyState
+        description={t('description')}
+        iconComponent={IcoForbid40}
+        title={t('title')}
+        cta={{
+          label: t('cta'),
+          onClick: resetErrorBoundary,
+        }}
+      />
+    </>
   );
 };
 
