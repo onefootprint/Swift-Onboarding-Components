ed49779bc3f6b2a52507dbb8820ac7103b03408d
Claudio Angrigiani <angrigianicr@gmail.com>
2023-09-11 13:42:52 -0400

    Add unify config (#5598)
    This is a configuration that is needed for the tool Unify, used by Peter

--- frontend/apps/frontpage/next.config.js
+++ frontend/apps/frontpage/next.config.js
@@ -20,9 +20,9 @@ const ContentSecurityPolicy = `
   form-action 'self';
   frame-ancestors 'self';
   frame-src 'self' localhost:3000 *.onefootprint.com vercel.live www.youtube.com form.typeform.com; 
-  img-src 'self' data: footprint-blog.ghost.io assets.vercel.com vercel.live vercel.com usefathom.com *.usefathom.com i.onefp.net i-dev.onefp.net *.i-dev.onefp.net;
+  img-src 'self' data: footprint-blog.ghost.io assets.vercel.com vercel.live vercel.com usefathom.com *.usefathom.com i.onefp.net i-dev.onefp.net *.i-dev.onefp.net cdn.unifygtm.com *.unifygtm.com;
   media-src 'self' https footprint-blog.ghost.io;
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' usefathom.com *.usefathom.com vercel.live vitals.vercel-insights.com tagmanager.google.com www.googletagmanager.com platform.twitter.com www.youtube.com static.ads-twitter.com connect.facebook.net www.facebook.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' usefathom.com *.usefathom.com vercel.live vitals.vercel-insights.com tagmanager.google.com www.googletagmanager.com platform.twitter.com www.youtube.com static.ads-twitter.com connect.facebook.net www.facebook.com cdn.unifygtm.com *.unifygtm.com;
   style-src 'self' 'unsafe-inline' tagmanager.google.com fonts.googleapis.com;
   worker-src 'self' blob:;
 `;
