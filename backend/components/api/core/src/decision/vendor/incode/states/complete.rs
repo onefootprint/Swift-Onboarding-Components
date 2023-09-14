042cfefe3089b00777d8e4fe60aedbdc25a2201d
Keagan Long <keagan@onefootprint.com>
2023-09-13 18:05:18 -0700

    [FP-5996] Don't write id. data with OCR for is_doc_first flows if Workflow is redo (#5670)
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/components/api/core/src/decision/vendor/incode/states/complete.rs
+++ backend/components/api/core/src/decision/vendor/incode/states/complete.rs
@@ -18,6 +18,7 @@ use db::models::ob_configuration::ObConfiguration;
 use db::models::risk_signal::RiskSignal;
 use db::models::user_timeline::UserTimeline;
 use db::models::vault::Vault;
+use db::models::workflow::Workflow;
 use db::DbPool;
 use db::TxnPgConn;
 use idv::incode::doc::response::FetchOCRResponse;
@@ -67,6 +68,7 @@ impl Complete {
         let uvw = VaultWrapper::lock_for_onboarding(conn, sv_id)?;
         let (id_doc, doc_request) = IdentityDocument::get(conn, id_doc_id)?;
         let (obc, _) = ObConfiguration::get(conn, &doc_request.workflow_id)?;
+        let wf = Workflow::get(conn, &doc_request.workflow_id)?;
 
         // Create a timeline event
         let info = newtypes::IdentityDocumentUploadedInfo {
@@ -123,7 +125,7 @@ impl Complete {
         // A little bit of a hack to allow vaulting all address fields that exist
         // But, bifrost will display an empty address screen if ANY address field is missing
         validate_args.allow_dangling_keys = true;
-        let id_data: Vec<(DataIdentifier, PiiString)> = if obc.is_doc_first {
+        let id_data: Vec<(DataIdentifier, PiiString)> = if obc.is_doc_first && !wf.config.is_redo() {
             let r = &fetch_ocr_response;
             let name = r.name.as_ref();
             let address = r.checked_address_bean.as_ref().or(r.address_fields.as_ref());
