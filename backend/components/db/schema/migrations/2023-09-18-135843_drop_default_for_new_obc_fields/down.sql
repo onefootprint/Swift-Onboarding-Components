769bb50572df8d82311128d167de4d52a67ddf78
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2023-09-19 12:33:24 -0400

    drop default for new country fields (#5730)
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    confirmed no null in PG

---
+++ backend/components/db/schema/migrations/2023-09-18-135843_drop_default_for_new_obc_fields/down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE ob_configuration 
+    ALTER COLUMN allow_us_residents SET DEFAULT true,
+    ALTER COLUMN allow_us_territory_residents SET DEFAULT false;
---
+++ backend/components/db/schema/migrations/2023-09-18-135843_drop_default_for_new_obc_fields/up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE ob_configuration 
+    ALTER COLUMN allow_us_residents DROP DEFAULT,
+    ALTER COLUMN allow_us_territory_residents DROP DEFAULT;
