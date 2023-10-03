d0f0380f997ff0d7d181bc00bec5cde6c6d6980d
Claudio Angrigiani <angrigianicr@gmail.com>
2023-10-03 12:51:30 -0400

    [FP-6167] Remove hotjar (#5997)
    https://uploads.linear.app/b298b4c8-ed5e-478e-aed7-d30fb01f7e0e/e0cf5006-79e9-4c0f-af01-c1ce056a4b72/a9565381-e6dc-4c48-9755-53f10f33a88f
    
    We don't use hotjar and was throwing a security policy error in
    frontpage

--- frontend/apps/frontpage/src/pages/_app.page.tsx
+++ frontend/apps/frontpage/src/pages/_app.page.tsx
@@ -65,20 +65,6 @@ const App = ({ Component, pageProps }: AppProps) => (
       />
     )}
     <Script />
-    <Script
-      id="hotjar"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `(function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:3586342,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-      }}
-    />
     {UNIFY_API_KEY && (
       <Script
         async
