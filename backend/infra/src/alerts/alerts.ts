29da77ef867639b1a3b28950132f2e5861c15b85
Elliott Forde <elliott@onefootprint.com>
2023-10-05 13:01:49 -0700

    Add new upload endpoint to list of latent APIs (#6094)
    ## Describe your changes
    
    ^
    https://onefootprint.slack.com/archives/C03DQ288QQM/p1696535889928699

--- backend/infra/src/alerts/alerts.ts
+++ backend/infra/src/alerts/alerts.ts
@@ -15,7 +15,10 @@ export interface Alert {
 }
 
 /// Generally higher-latency HTTP requests that we want to have a higher alert threshold
-const LATENT_HTTP_ROUTES = ['/hosted/user/documents/{id}/upload'];
+const LATENT_HTTP_ROUTES = [
+  '/hosted/user/documents/{id}/upload',
+  '/hosted/user/documents/{id}/upload/{side}',
+];
 
 /// Note, add alert runbooks at: https://www.notion.so/onefootprint/Alert-Runbooks-17f53ed91bb64a09b446bf2c0eb1cb25
 const staticAlerts: Alert[] = [
