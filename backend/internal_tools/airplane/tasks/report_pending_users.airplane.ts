ffebcdbb057157d3cc844c4d1766c161bf0ec180
Keagan Long <keagan@onefootprint.com>
2023-10-06 18:40:25 -0700

    [FP-6273] Fix Report Pending users alert (#6145)
    ## Describe your changes
    
    Doesn't alert a `pending` user if their latest `workflow_event` was in
    the past 2min to mitigate cases where we are reporting pending users who
    quickly transition out of pending moments after the alert fires.
    
    ## Issue: FP-6273
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/internal_tools/airplane/tasks/report_pending_users.airplane.ts
+++ backend/internal_tools/airplane/tasks/report_pending_users.airplane.ts
@@ -40,20 +40,30 @@ export default airplane.task(
         from workflow
         where 
         status = 'pending'
+      ),
+      pending_workflows_latest_workflow_event as (
+        select
+          distinct on (wf.wf_id)
+          wf.wf_id, 
+          created_at wfe_created_at
+        from pending_workflows wf
+        inner join workflow_event wfe on wf.wf_id = wfe.workflow_id
+        order by wf.wf_id, wfe.created_at desc
       )
 
       select 
         t.name tenant_name, t.id tenant_id,
         coalesce(psv.sv_id, wf.sv_id) sv_id,
-        psv.sv_created_at, psv.sv_status, wf.wf_created_at, wf.wf_status, wf.wf_id
+        psv.sv_created_at, psv.sv_status, wf.wf_created_at, wfe_created_at, wf.wf_status, wf.wf_id
       from pending_scoped_vaults psv
       full outer join pending_workflows wf on psv.sv_id = wf.sv_id
+      left join pending_workflows_latest_workflow_event wfe on wf.wf_id = wfe.wf_id
       inner join scoped_vault sv on sv.id = coalesce(psv.sv_id, wf.sv_id)
       inner join tenant t on sv.tenant_id = t.id
       where 
         t.id != '_private_it_org_2'
-        and greatest(psv.sv_created_at, wf.wf_created_at) >= now() - interval '3 day'
-        and greatest(psv.sv_created_at, wf.wf_created_at) < now() - interval '2 minute'
+        and greatest(psv.sv_created_at, wf.wf_created_at, wfe.wfe_created_at) >= now() - interval '3 day'
+        and greatest(psv.sv_created_at, wf.wf_created_at, wfe.wfe_created_at) < now() - interval '2 minute'
     `;
 
     const rows = await pg_query(dbUrl, query);
