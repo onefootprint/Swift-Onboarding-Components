d59a1e7a691b4715f78ddcc7cc6428dff1e55bf6
Rafael Michels Motta <rafael@onefootprint.com>
2023-10-12 17:26:06 -0500

    Dashboard: add api keys page titke

--- frontend/apps/dashboard/src/pages/api-keys/api-keys.tsx
+++ frontend/apps/dashboard/src/pages/api-keys/api-keys.tsx
@@ -1,6 +1,7 @@
 import { useToggle, useTranslation } from '@onefootprint/hooks';
 import { RoleScopeKind } from '@onefootprint/types';
 import { Box, Button, Stack, Typography } from '@onefootprint/ui';
+import Head from 'next/head';
 import React from 'react';
 import PermissionGate from 'src/components/permission-gate';
 
@@ -14,28 +15,33 @@ const ApiKeys = () => {
     useToggle(false);
 
   return (
-    <section data-testid="api-keys-section">
-      <Stack align="center" justify="space-between" marginBottom={7}>
-        <Stack direction="column" gap={2}>
-          <Typography variant="heading-2">{t('header.title')}</Typography>
-          <Typography variant="body-2" color="secondary">
-            {t('header.subtitle')}
-          </Typography>
+    <>
+      <Head>
+        <title>{t('page-title')}</title>
+      </Head>
+      <Box>
+        <Stack align="center" justify="space-between" marginBottom={7}>
+          <Stack direction="column" gap={2}>
+            <Typography variant="heading-2">{t('header.title')}</Typography>
+            <Typography variant="body-2" color="secondary">
+              {t('header.subtitle')}
+            </Typography>
+          </Stack>
+          <PermissionGate
+            fallbackText={t('header.cta-not-allowed')}
+            scopeKind={RoleScopeKind.apiKeys}
+          >
+            <Button onClick={openCreateDialog} size="small" variant="primary">
+              {t('header.cta')}
+            </Button>
+          </PermissionGate>
         </Stack>
-        <PermissionGate
-          fallbackText={t('header.cta-not-allowed')}
-          scopeKind={RoleScopeKind.apiKeys}
-        >
-          <Button onClick={openCreateDialog} size="small" variant="primary">
-            {t('header.cta')}
-          </Button>
-        </PermissionGate>
-      </Stack>
-      <Table />
-      <CreateDialog open={isCreateDialogOpen} onClose={closeCreateDialog} />
-      <Box marginTop={9} />
-      <Roles />
-    </section>
+        <Table />
+        <CreateDialog open={isCreateDialogOpen} onClose={closeCreateDialog} />
+        <Box marginTop={9} />
+        <Roles />
+      </Box>
+    </>
   );
 };
 
