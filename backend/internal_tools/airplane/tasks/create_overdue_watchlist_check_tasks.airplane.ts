26d3bb4e70eddec6d87401c0dbb282442f9ae074
Keagan Long <keagan@onefootprint.com>
2023-10-27 10:03:45 -0700

    Unpause airplane schedule to create WatchlistCheck Task's (#6538)
    ## Describe your changes
    
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects.
    
    Example:
    - before/after screenshots (if appropriate).
    - Sample payloads (if it's a API change).
    - Changes x feature to such that y
    - Added x
    - Removed y -->
    
    ## Issue: <FP-???>
    
    ## Caveats
    
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    
    <!--- How did you test your changes? :) -->

--- backend/internal_tools/airplane/tasks/create_overdue_watchlist_check_tasks.airplane.ts
+++ backend/internal_tools/airplane/tasks/create_overdue_watchlist_check_tasks.airplane.ts
@@ -17,17 +17,16 @@ export default airplane.task(
       FPC_PRIVATE_PROECTED_TOKEN: { config: 'FPC_PRIVATE_PROECTED_TOKEN' },
       API_URL: { config: 'API_URL' },
     },
-    // Turning off schedule for now so I can manually slow rollout and test new logic
-    // schedules:
-    //   process.env.AIRPLANE_ENV_SLUG === 'prod'
-    //     ? {
-    //         every_day: {
-    //           cron: '0 17 * * *',
-    //           description: 'Runs once per day at 17:00 UTC',
-    //           paramValues: { tenant_id: 'org_PtnIJT4VR35BS9xy0wITgF' },
-    //         },
-    //       }
-    //     : {},
+    schedules:
+      process.env.AIRPLANE_ENV_SLUG === 'prod'
+        ? {
+            every_day: {
+              cron: '0 17 * * *',
+              description: 'Runs once per day at 17:00 UTC',
+              paramValues: { limit: 1000 },
+            },
+          }
+        : {},
   },
   async params => {
     return await protected_custodian_api_call(
