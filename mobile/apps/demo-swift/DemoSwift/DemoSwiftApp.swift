43eb3fe38a4bddc325a311d0f3e1a475dc5c905c
Rafael Michels Motta <rafael@onefootprint.com>
2023-11-07 11:14:17 -0300

    [SDK] Add swift demo app (#6760)

---
+++ mobile/apps/demo-swift/DemoSwift/DemoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftApp.swift
+//  DemoSwift
+//
+//  Created by Rafael Michels Motta on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
