d9784d49374388d01ce48824c36621ee2da5fcaa
Keagan Long <keagan@onefootprint.com>
2023-11-06 17:50:22 -0800

    Tweak pending users alert to be less noisy (#6751)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    - Fixes start_datetime to do what it should be doing which is query back
    3 minutes plus an additional 10s buffer
    - Changes end_datetime so it subtracts a 5s buffer so that users who are
    transitioning out of pending right in the few seconds the task is
    running are not erronesouly alerted, eg:
    https://onefootprint.slack.com/archives/C04N71Q0CMP/p1699318082550199
    - Adds some log lines just so its easy to see what timestamps were used
    and # of rows found
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/internal_tools/airplane/tasks/report_recent_pending_users.airplane.ts
+++ backend/internal_tools/airplane/tasks/report_recent_pending_users.airplane.ts
@@ -17,16 +17,19 @@ export default airplane.task(
         : {},
   },
   async params => {
-    let end_datetime = new Date().toISOString();
+    let end_datetime = new Date(new Date().getTime() - 1000 * 5).toISOString(); // query up to current time minus a 5 second buffer so users who are transitioning out of pending in the few seconds this task runs are not erroneously alerted
     let start_datetime = new Date(
-      new Date().getTime() - 1000 * 60 * 3 - 10,
+      new Date().getTime() - 1000 * (60 * 3 + 10), // query starting 3 minutes ago, plus another 10 second buffer since this task may start a few seconds after its strict starting time
     ).toISOString();
+
+    console.log('start_datetime', start_datetime, 'end_datetime', end_datetime);
 
     const run = await airplane.execute('query_pending_users', {
       start_datetime: start_datetime,
       end_datetime: end_datetime,
     });
     let rows = (run.output as object)['Q1'];
+    console.log('rows.length', rows.length);
 
     if (rows.length > 0) {
       function row_to_string(row) {
