1f1798db0f3d034d806ab88ac8bdda12b35a0469
Rafael Michels Motta <rafael@onefootprint.com>
2023-11-14 16:53:03 -0300

    Bootstrap stress tests (#6879)

---
+++ frontend/packages/e2e/src/kyc.bootstrap.stress.spec.ts
@@ -0,0 +1,47 @@
+import { expect, test } from '@playwright/test';
+
+import {
+  clickOnContinue,
+  selectOutcomeOptional,
+  waitForVerifyButton,
+} from './utils/commands';
+
+const email = 'piip@onefootprint.com';
+const phoneNumber = '+15555550100';
+
+const numberOfTests = 30;
+
+for (let i = 0; i < numberOfTests; i++) {
+  test(`E2E.Bootstrap ${i} #stress`, async ({ browserName, page }) => {
+    test.setTimeout(120000);
+    const flowId = `${browserName}-${Math.floor(Math.random() * 100000) + 1}`;
+
+    await page.route('**/*.{png,jpg,jpeg,woff,woff2}', route => route.abort());
+    const bootstrapData = {
+      'id.email': email,
+      'id.phone_number': phoneNumber,
+    };
+
+    await page.goto(
+      `/e2e?flow=${flowId}&user_data=${encodeURIComponent(
+        JSON.stringify(bootstrapData),
+      )}`,
+    );
+    await page.waitForLoadState();
+
+    await waitForVerifyButton({ page });
+
+    await page.getByRole('button', { name: 'Verify with Footprint' }).click();
+    await page.waitForLoadState();
+
+    const frame = page.frameLocator('iframe[name^="footprint-iframe-"]');
+
+    await selectOutcomeOptional({ frame }, 'Success');
+    await clickOnContinue({ frame });
+    await page.waitForLoadState();
+
+    await expect(
+      frame.getByText('Login with a different account'),
+    ).toBeAttached();
+  });
+}
