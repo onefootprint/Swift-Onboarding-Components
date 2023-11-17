c279e4cb3d6fd11cca11074e839944790ee700df
ahsan1578 <54286440+ahsan1578@users.noreply.github.com>
2023-11-16 17:53:44 -0800

    [FP-6638, FP-6639]  mobile ssn (#6906)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
     ### Fix the right padding
    
    <img width="388" alt="Screenshot 2023-11-15 at 9 34 51 PM"
    src="https://github.com/onefootprint/monorepo/assets/54286440/816d2371-0990-4527-a99f-0e656a5ce29f">
    
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

---
+++ mobile/apps/verify/src/screens/ssn/ssn.tsx
@@ -0,0 +1,62 @@
+import { IcoShield40 } from '@onefootprint/icons';
+import styled, { css } from '@onefootprint/styled';
+import {
+  Box,
+  Button,
+  Container,
+  TextInput,
+  Typography,
+} from '@onefootprint/ui';
+import React from 'react';
+
+import Header from '@/components/header';
+import useTranslation from '@/hooks/use-translation';
+
+const Ssn = () => {
+  const { t } = useTranslation('pages.ssn');
+  const ssnKind = 'ssn-full';
+
+  return (
+    <Container scroll>
+      <Header title={t('title')} subtitle={t('subtitle')} />
+      <Box gap={7}>
+        {ssnKind === 'ssn-full' ? (
+          <TextInput
+            label={t('ssn-input.full-ssn.label')}
+            placeholder={t('ssn-input.full-ssn.placeholder')}
+          />
+        ) : (
+          <TextInput
+            label={t('ssn-input.last-4.label')}
+            placeholder={t('ssn-input.last-4.placeholder')}
+          />
+        )}
+        {ssnKind === 'ssn-full' && (
+          <Disclaimer>
+            <IcoShield40 />
+            <Box gap={3} paddingTop={1} flex={1}>
+              <Typography variant="label-3">{t('disclaimer.title')}</Typography>
+              <Typography variant="body-3" color="secondary">
+                {t('disclaimer.description')}
+              </Typography>
+            </Box>
+          </Disclaimer>
+        )}
+        <Button variant="primary">{t('cta')}</Button>
+      </Box>
+    </Container>
+  );
+};
+
+const Disclaimer = styled.View`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: row;
+    background: ${theme.backgroundColor.secondary};
+    gap: ${theme.spacing[3]};
+    padding: ${theme.spacing[5]};
+    border-radius: ${theme.borderRadius.default};
+  `}
+`;
+
+export default Ssn;
