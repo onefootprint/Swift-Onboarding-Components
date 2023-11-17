d5f399d332bb6cb3adb3a5c0c10cc92c7fd5adb3
ahsan1578 <54286440+ahsan1578@users.noreply.github.com>
2023-11-16 17:48:01 -0800

    [FP-6641] mobile legal status (#6905)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    <img width="392" alt="Screenshot 2023-11-15 at 8 57 39 PM"
    src="https://github.com/onefootprint/monorepo/assets/54286440/41afa225-cb7e-4707-ad7c-98f85adafdc0">
    
    
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

---
+++ mobile/apps/verify/src/types/visa-kind.ts
@@ -0,0 +1,14 @@
+enum VisaKind {
+  e1 = 'e1',
+  e2 = 'e2',
+  e3 = 'e3',
+  f1 = 'f1',
+  g4 = 'g4',
+  h1b = 'h1b',
+  l1 = 'l1',
+  o1 = 'o1',
+  tn1 = 'tn1',
+  other = 'other',
+}
+
+export default VisaKind;
