41845a574ebaf867b934350efc7b831aac289682
ahsan1578 <54286440+ahsan1578@users.noreply.github.com>
2023-11-16 17:35:46 -0800

    [FP-6636] Mobile basic data skeleton (#6899)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    <img width="389" alt="Screenshot 2023-11-15 at 7 20 46 PM"
    src="https://github.com/onefootprint/monorepo/assets/54286440/5c5adc52-6527-457e-92c1-cd43e6968ca2">
    
    
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

---
+++ mobile/apps/verify/src/screens/basic-information/basic-information.tsx
@@ -0,0 +1,60 @@
+import styled, { css } from '@onefootprint/styled';
+import {
+  Box,
+  Button,
+  Container,
+  CountrySelect,
+  TextInput,
+} from '@onefootprint/ui';
+import React from 'react';
+
+import Header from '@/components/header';
+import useTranslation from '@/hooks/use-translation';
+
+const BasicInformation = () => {
+  const { t } = useTranslation('pages.basic-information');
+
+  // TODO: we don't always show country of birth, so we should make this dynamic
+  return (
+    <Container scroll>
+      <Header title={t('title')} subtitle={t('subtitle')} />
+      <Box gap={7}>
+        <Row>
+          <Box flex={1}>
+            <TextInput
+              label={t('first-name-input.label')}
+              placeholder={t('first-name-input.placeholder')}
+              autoFocus
+            />
+          </Box>
+          <Box flex={1}>
+            <TextInput
+              label={t('middle-name-input.label')}
+              placeholder={t('middle-name-input.placeholder')}
+            />
+          </Box>
+        </Row>
+        <TextInput
+          label={t('last-name-input.label')}
+          placeholder={t('last-name-input.placeholder')}
+        />
+        <TextInput
+          label={t('dob-input.label')}
+          placeholder={t('dob-input.placeholder')}
+        />
+        <CountrySelect />
+        <Button variant="primary">{t('cta')}</Button>
+      </Box>
+    </Container>
+  );
+};
+
+const Row = styled.View`
+  ${({ theme }) => css`
+    display: flex;
+    gap: ${theme.spacing[4]};
+    flex-direction: row;
+  `}
+`;
+
+export default BasicInformation;
