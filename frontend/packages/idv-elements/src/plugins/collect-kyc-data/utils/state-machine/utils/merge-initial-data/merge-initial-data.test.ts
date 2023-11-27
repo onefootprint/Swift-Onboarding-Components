8837ec862277fac621c27503ff5745b8f0f191fe
Bruno Batista <bruno@onefootprint.com>
2023-11-27 14:26:29 +0100

    fix(idv-elements): FP-6699 avoid vault same data (#7011)

--- frontend/packages/idv-elements/src/plugins/collect-kyc-data/utils/state-machine/utils/merge-initial-data/merge-initial-data.test.ts
+++ frontend/packages/idv-elements/src/plugins/collect-kyc-data/utils/state-machine/utils/merge-initial-data/merge-initial-data.test.ts
@@ -1,10 +1,10 @@
 import { IdDI } from '@onefootprint/types';
 
-import mergeInitialData from './merge-initial-data';
+import mergeInitialData, { isEqArray } from './merge-initial-data';
 
-describe('getInitData', () => {
+describe('mergeInitialData', () => {
   it('should take bootstrap data over decrypted data', () => {
-    const initialData = {
+    const bootstrapData = {
       [IdDI.ssn9]: { value: '111111111', bootstrap: true },
       [IdDI.middleName]: { value: 'C.', decrypted: true },
     };
@@ -14,11 +14,44 @@ describe('getInitData', () => {
       [IdDI.lastName]: { value: 'Smith', decrypted: true },
       [IdDI.ssn9]: { value: '101010101', decrypted: true },
     };
-    expect(mergeInitialData(initialData, decryptedData)).toEqual({
+    expect(mergeInitialData(bootstrapData, decryptedData)).toEqual({
       [IdDI.firstName]: { value: 'John', decrypted: true },
       [IdDI.middleName]: { value: 'C.', decrypted: true },
       [IdDI.lastName]: { value: 'Smith', decrypted: true },
       [IdDI.ssn9]: { value: '111111111', bootstrap: true },
     });
+  });
+
+  it('should replace bootstrap data when value is the same', () => {
+    const bootstrapData = {
+      [IdDI.middleName]: { value: 'Lee', bootstrap: true },
+    };
+    const decryptedData = {
+      [IdDI.firstName]: { value: 'Bob', decrypted: true },
+      [IdDI.middleName]: { value: 'Lee', decrypted: true },
+      [IdDI.lastName]: { value: 'Swagger', decrypted: true },
+    };
+    expect(mergeInitialData(bootstrapData, decryptedData)).toEqual({
+      [IdDI.firstName]: { value: 'Bob', decrypted: true },
+      [IdDI.middleName]: { value: 'Lee', decrypted: true },
+      [IdDI.lastName]: { value: 'Swagger', decrypted: true },
+    });
+  });
+});
+
+describe('isEqArray', () => {
+  it.each`
+    arrA          | arrB          | output
+    ${undefined}  | ${undefined}  | ${false}
+    ${null}       | ${null}       | ${false}
+    ${[]}         | ${[]}         | ${true}
+    ${[1]}        | ${[1]}        | ${true}
+    ${[1]}        | ${[1, 2]}     | ${false}
+    ${[3, 2, 1]}  | ${[1, 2, 3]}  | ${true}
+    ${['a']}      | ${['a']}      | ${true}
+    ${['a']}      | ${['a', 'b']} | ${false}
+    ${['b', 'a']} | ${['a', 'b']} | ${true}
+  `(`for $arrB`, ({ arrA, arrB, output }) => {
+    expect(isEqArray(arrA, arrB)).toBe(output);
   });
 });
--- frontend/packages/idv-elements/src/plugins/collect-kyc-data/utils/state-machine/utils/merge-initial-data/merge-initial-data.ts
+++ frontend/packages/idv-elements/src/plugins/collect-kyc-data/utils/state-machine/utils/merge-initial-data/merge-initial-data.ts
@@ -1,17 +1,46 @@
 import type { IdDI } from '@onefootprint/types';
+import isEqual from 'lodash/isEqual';
 
 import type { KycData } from '../../../data-types';
+
+interface Comparator {
+  (a?: unknown, b?: unknown): boolean;
+  (a?: unknown[], b?: unknown[]): boolean;
+}
+
+const isMissingKey = (obj: Record<string, unknown>, key: string): boolean =>
+  !Object.hasOwn(obj, key);
+
+export const isEqArray: Comparator = (a, b) => {
+  if (!Array.isArray(a) || !Array.isArray(b) || a.length !== b.length) {
+    return false;
+  }
+
+  const sortedA = [...a].sort();
+  const sortedB = [...b].sort();
+
+  return sortedA.every((x, idx) => x === sortedB[idx]);
+};
 
 const mergeInitialData = (
   initData: KycData,
   decryptedFields: KycData,
 ): KycData => {
-  const newValues = Object.fromEntries(
-    Object.entries(decryptedFields).filter(
-      // Don't allow replacing bootstrap data with decrypted data - the bootstrap
-      // data should take precedent
-      ([k]) => !initData[k as IdDI],
-    ),
+  const newValues = Object.entries(decryptedFields).reduce<KycData>(
+    (obj, [k, val]) => {
+      const initValue = initData?.[k as IdDI]?.value;
+      const decryptedValue = val.value;
+      const hasEqValue = Array.isArray(decryptedValue) ? isEqArray : isEqual;
+
+      /** Don't allow replacing bootstrap data with decrypted data. The bootstrap data should take precedent */
+      if (isMissingKey(initData, k) || hasEqValue(initValue, decryptedValue)) {
+        /* @ts-ignore: key string vs enum comparison */
+        obj[k] = val; // eslint-disable-line no-param-reassign
+      }
+
+      return obj;
+    },
+    {},
   );
 
   return {
