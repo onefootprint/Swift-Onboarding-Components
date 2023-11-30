b6713c852d66d58cd792e62281092b67d8a71f3f
Elliott Forde <elliott@onefootprint.com>
2023-11-30 11:12:36 -0800

    Fix build for migrations on master

--- backend/components/db/schema/migrations/2023-11-30-013630_drop_one_portable_dl_constraint/down.sql
+++ backend/components/db/schema/migrations/2023-11-30-013630_drop_one_portable_dl_constraint/down.sql
@@ -1,7 +1,1 @@
--- NOTE this has to normally happen CONCURRENTLY...  can't do that with diesel, so i dropped them manually in dev and prod
-CREATE UNIQUE INDEX IF NOT EXISTS unique_portable_data_lifetime_per_user_vault ON data_lifetime(kind, vault_id)
-WHERE
-    deactivated_seqno IS NULL AND
-    portablized_seqno IS NOT NULL AND
-    -- id documents are not yet unique
-    NOT starts_with(kind, 'id_document.');
+SELECT 1;
