0e21623d1df76ec36fbf762ac430c0a114f4adfc
Claudio Angrigiani <angrigianicr@gmail.com>
2023-12-12 19:13:02 -0300

    Allow sorting pinned notes (#7275)
    https://github.com/onefootprint/monorepo/assets/12160059/f868df8e-e0da-4395-aa41-c878d3e354d3
    
    Full demo: 
    
    https://github.com/onefootprint/monorepo/assets/12160059/d8bf3432-3a4e-457e-8b5e-51bd4c679868

--- frontend/apps/dashboard/src/components/entities/components/details/components/content/components/pinned-notes/pinned-notes.tsx
+++ frontend/apps/dashboard/src/components/entities/components/details/components/content/components/pinned-notes/pinned-notes.tsx
@@ -1,29 +1,88 @@
 import { useTranslation } from '@onefootprint/hooks';
-import { IcoPin24 } from '@onefootprint/icons';
+import { IcoArrowDown16, IcoPin24 } from '@onefootprint/icons';
 import styled, { css } from '@onefootprint/styled';
 import { ActorKind } from '@onefootprint/types';
-import { Divider, Typography } from '@onefootprint/ui';
-import React, { Fragment } from 'react';
+import { createFontStyles, Divider, Stack, Typography } from '@onefootprint/ui';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import { useHover } from 'usehooks-ts';
 
 import useCurrentEntityAnnotations from '@/entity/hooks/use-current-entity-annotations';
 
 import PinnedNote from './components/pinned-note';
 
+enum SortOrder {
+  ascending = 'ascending',
+  descending = 'descending',
+}
+
 const PinnedNotes = () => {
   const { data } = useCurrentEntityAnnotations();
   const { t } = useTranslation('pages.entity.pinned-notes');
+  const [sortedData, setSortedData] = useState(data);
+  const [sortDirection, setSortDirection] = useState(SortOrder.descending);
+  const sortButtonRef = useRef<HTMLButtonElement>(null);
+  const isHover = useHover(sortButtonRef);
+
+  useEffect(() => {
+    if (data && sortDirection === SortOrder.descending) {
+      setSortedData(data.sort(sortDataDescending));
+    } else if (data && sortDirection === SortOrder.ascending) {
+      setSortedData(data.sort(sortDataAscending));
+    }
+  }, [data, sortDirection]);
+
+  const sortDataAscending = (
+    a: { timestamp: string },
+    b: { timestamp: string },
+  ) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime();
+
+  const sortDataDescending = (
+    a: { timestamp: string },
+    b: { timestamp: string },
+  ) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime();
+
+  const handleSortData = () => {
+    if (sortDirection === SortOrder.descending) {
+      setSortDirection(SortOrder.ascending);
+    } else {
+      setSortDirection(SortOrder.descending);
+    }
+  };
 
   if (!data || data.length === 0) {
     return null;
   }
+
   return (
     <Container>
       <HeaderTitle>
-        <IcoPin24 />
-        <Typography variant="label-2">{t('title')}</Typography>
+        <Stack direction="row" gap={2}>
+          <IcoPin24 />
+          <Typography variant="label-2">{t('title')}</Typography>
+        </Stack>
+        {data.length > 1 ? (
+          <SortButton
+            as="button"
+            onClick={() => handleSortData()}
+            ref={sortButtonRef}
+            data-hover={isHover}
+          >
+            <Stack direction="row" gap={3} align="center" justify="center">
+              {sortDirection === SortOrder.descending
+                ? t('sort-ascending')
+                : t('sort-descending')}
+              <Stack height="100%">
+                <StyledIconArrow
+                  direction={sortDirection}
+                  color={isHover ? 'primary' : 'tertiary'}
+                />
+              </Stack>
+            </Stack>
+          </SortButton>
+        ) : null}
       </HeaderTitle>
       <NotesContainer>
-        {data.map(({ note, id, source, timestamp }, i) => (
+        {sortedData?.map(({ note, id, source, timestamp }, i) => (
           <Fragment key={id}>
             <PinnedNote
               author={
@@ -44,6 +103,30 @@ const PinnedNotes = () => {
   );
 };
 
+const SortButton = styled(Stack)`
+  ${({ theme }) => css`
+    ${createFontStyles('label-3')}
+    all: unset;
+    color: ${theme.color.tertiary};
+    cursor: pointer;
+    user-select: none;
+    transition: color 0.2s ease-in-out;
+
+    &[data-hover='true'] {
+      color: ${theme.color.primary};
+    }
+  `}
+`;
+
+const StyledIconArrow = styled(IcoArrowDown16)<{ direction: SortOrder }>`
+  ${({ direction }) => css`
+    transform: ${direction === SortOrder.descending
+      ? 'rotate(180deg)'
+      : 'rotate(0deg)'};
+    transition: transform 0.2s ease-in-out;
+  `}
+`;
+
 const Container = styled.div`
   ${({ theme }) => css`
     gap: ${theme.spacing[4]};
@@ -55,6 +138,7 @@ const HeaderTitle = styled.div`
   ${({ theme }) => css`
     display: flex;
     align-items: center;
+    justify-content: space-between;
     gap: ${theme.spacing[2]};
     margin-bottom: ${theme.spacing[3]};
   `}
