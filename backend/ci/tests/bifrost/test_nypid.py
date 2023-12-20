403eafdd210c7bcf70dddd488428cd71fef6aa65
Elliott Forde <elliott@onefootprint.com>
2023-12-20 14:51:33 -0800

    Add test that we can portablize an NYPID via a KYC playbook (#7404)
    ## Describe your changes
    Grid is going to be something kind of like this
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/ci/tests/bifrost/test_nypid.py
+++ backend/ci/tests/bifrost/test_nypid.py
@@ -4,9 +4,11 @@ from tests.headers import SandboxId
 from tests.bifrost_client import BifrostClient
 
 
-def test_portablize_nypid(sandbox_tenant, foo_sandbox_tenant, auth_playbook, twilio):
+def test_portablize_nypid_via_auth(
+    sandbox_tenant, foo_sandbox_tenant, auth_playbook, twilio
+):
     """
-    Portablize a vault created via API at sandbox_tenant.
+    Portablize a vault created via API at sandbox_tenant by onboarding onto an auth playbook.
     Then onboard the user onto foo_sandbox_tenant and ensure the data was prefilled
     """
     sandbox_id = _gen_random_sandbox_id()
@@ -27,6 +29,56 @@ def test_portablize_nypid(sandbox_tenant, foo_sandbox_tenant, auth_playbook, twi
 
     # Log into the user with an auth playbook, which will portablize it
     inherit_user(twilio, FIXTURE_PHONE_NUMBER, "auth", auth_playbook.key, sandbox_id_h)
+
+    # Now, when one-click onboarding onto another tenant, we should prefill this data instead of
+    # using the data that is filled in by the Bifrost client
+    bifrost = BifrostClient.inherit(
+        foo_sandbox_tenant.default_ob_config, twilio, FIXTURE_PHONE_NUMBER, sandbox_id
+    )
+    user = bifrost.run()
+    # Double check the data was prefilled
+    body = get(f"entities/{user.fp_id}", None, *foo_sandbox_tenant.db_auths)
+    assert all(
+        d["source"] == "prefill"
+        for d in body["data"]
+        if d["identifier"] in initial_data
+    )
+
+    data = dict(fields=[k for k in initial_data], reason="Foobar")
+    body = post(
+        f"entities/{user.fp_id}/vault/decrypt", data, *foo_sandbox_tenant.db_auths
+    )
+    assert all(body[k] == v for k, v in initial_data.items())
+
+
+def test_portablize_nypid_via_kyc(
+    sandbox_tenant, foo_sandbox_tenant, auth_playbook, twilio
+):
+    """
+    Portablize a vault created via API at sandbox_tenant by onboarding onto a KYC playbook.
+    Then onboard the user onto foo_sandbox_tenant and ensure the data was prefilled
+    """
+    sandbox_id = _gen_random_sandbox_id()
+    sandbox_id_h = SandboxId(sandbox_id)
+    # This data is slightly different from that in constants.py
+    initial_data = {
+        "id.first_name": "Nonportable",
+        "id.last_name": "Penguin",
+        "id.address_line1": "1 Hayes St",
+        "id.city": "San Francisco",
+        "id.state": "CA",
+        "id.zip": "94117",
+        "id.country": "US",
+        "id.phone_number": FIXTURE_PHONE_NUMBER,
+        "id.email": FIXTURE_EMAIL,
+    }
+    post("users", initial_data, sandbox_tenant.s_sk, sandbox_id_h)
+
+    # Inherit the user and run them through a KYC playbook
+    bifrost = BifrostClient.inherit(
+        sandbox_tenant.default_ob_config, twilio, FIXTURE_PHONE_NUMBER, sandbox_id
+    )
+    bifrost.run()
 
     # Now, when one-click onboarding onto another tenant, we should prefill this data instead of
     # using the data that is filled in by the Bifrost client
