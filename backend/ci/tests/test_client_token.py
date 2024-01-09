7e3995d94b244c08c5106ddc31417e323306a0e9
Elliott Forde <elliott@onefootprint.com>
2024-01-09 15:56:47 -0600

    Update tests to use modern `scope` when creating client token (#7564)
    ## Describe your changes
    Testing changes made in #7556, in separate PR to make sure backcompat is
    maintained
    
    ## Caveats
    This adds one test for the legacy serialization because grid is still
    using it
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/ci/tests/test_client_token.py
+++ backend/ci/tests/test_client_token.py
@@ -5,13 +5,13 @@ from tests.dashboard.utils import latest_access_event_for
 from tests.headers import ClientTokenAuth
 
 
-def client_token_with_scopes(user, **kwargs):
+def client_token_with_scope(user, **kwargs):
     body = post(f"entities/{user.fp_id}/client_token", kwargs, user.tenant.sk.key)
     return ClientTokenAuth(body["token"]), body
 
 
 def test_generate(sandbox_user):
-    data = dict(fields=["id.ssn9"], ttl=60 * 60, scopes=["decrypt"])
+    data = dict(fields=["id.ssn9"], ttl=60 * 60, scope="decrypt")
     tenant = sandbox_user.tenant
     body = post(f"entities/{sandbox_user.fp_id}/client_token", data, tenant.sk.key)
     assert body["expires_at"]
@@ -29,8 +29,8 @@ def test_generate(sandbox_user):
 def test_decrypt(sandbox_user, attrs_to_decrypt):
     tenant = sandbox_user.tenant
     # Generate the token that can only be used to decrypt this info
-    auth_token, _ = client_token_with_scopes(
-        sandbox_user, fields=attrs_to_decrypt, scopes=["decrypt"]
+    auth_token, _ = client_token_with_scope(
+        sandbox_user, fields=attrs_to_decrypt, scope="decrypt"
     )
 
     # Use the token to decrypt it
@@ -48,18 +48,18 @@ def test_decrypt(sandbox_user, attrs_to_decrypt):
 
 def test_decrypt_reason(sandbox_user):
     tenant = sandbox_user.tenant
-    auth_token, _ = client_token_with_scopes(
-        sandbox_user, fields=["id.first_name"], scopes=["decrypt"]
+    auth_token, _ = client_token_with_scope(
+        sandbox_user, fields=["id.first_name"], scope="decrypt"
     )
     # Shouldn't be able to decrypt without a reason
     data = dict(fields=["id.first_name"])
     post(f"entities/vault/decrypt", data, auth_token, status_code=400)
 
     # Should work with reason specified on auth token
-    auth_token, _ = client_token_with_scopes(
+    auth_token, _ = client_token_with_scope(
         sandbox_user,
         fields=["id.first_name"],
-        scopes=["decrypt"],
+        scope="decrypt",
         decrypt_reason="Hayes valley",
     )
     post(f"entities/vault/decrypt", data, auth_token)
@@ -77,11 +77,10 @@ def test_decrypt_reason(sandbox_user):
 
 
 def test_vault(sandbox_user, sandbox_tenant):
-    auth_token, _ = client_token_with_scopes(
+    auth_token, _ = client_token_with_scope(
         sandbox_user,
         fields=["id.first_name", "id.last_name"],
-        # TODO update other tests to only use scope
-        scopes=["decrypt", "vault"],
+        scope="vault_and_decrypt",
     )
 
     body = get(f"entities/client_token", None, auth_token)
@@ -105,7 +104,7 @@ def test_vault(sandbox_user, sandbox_tenant):
 
 
 def test_vault_card(sandbox_user, sandbox_tenant):
-    auth_token, body = client_token_with_scopes(
+    auth_token, body = client_token_with_scope(
         sandbox_user,
         scope="vault_card",
     )
@@ -144,11 +143,11 @@ def test_vault_card(sandbox_user, sandbox_tenant):
 def test_insufficient_permissions(sandbox_user):
     auth_tokens = [
         # Token with incorrect decrypt perms
-        client_token_with_scopes(
-            sandbox_user, fields=["id.first_name"], scopes=["decrypt"]
+        client_token_with_scope(
+            sandbox_user, fields=["id.first_name"], scope="decrypt"
         )[0],
         # Token with vault perms
-        client_token_with_scopes(sandbox_user, fields=["id.ssn9"], scopes=["vault"])[0],
+        client_token_with_scope(sandbox_user, fields=["id.ssn9"], scope="vault")[0],
     ]
 
     # Try to use the token to decrypt ssn - but we have insufficient permissions
@@ -161,13 +160,11 @@ def test_insufficient_permissions(sandbox_user):
 
     auth_tokens = [
         # Token with incorrect vault perms
-        client_token_with_scopes(
-            sandbox_user, fields=["id.first_name"], scopes=["vault"]
-        )[0],
-        # Token with decrypt perms
-        client_token_with_scopes(sandbox_user, fields=["id.ssn9"], scopes=["decrypt"])[
+        client_token_with_scope(sandbox_user, fields=["id.first_name"], scope="vault")[
             0
         ],
+        # Token with decrypt perms
+        client_token_with_scope(sandbox_user, fields=["id.ssn9"], scope="decrypt")[0],
     ]
     for auth_token in auth_tokens:
         data = {
@@ -177,9 +174,40 @@ def test_insufficient_permissions(sandbox_user):
         patch(f"entities/vault", data, auth_token, status_code=401)
 
 
+def test_vault_legacy(sandbox_user, sandbox_tenant):
+    """
+    Grid (and some other tenants) are still using the old version of this API that allows providing
+    multiple scopes. Keep this integration test for as long as tenants are still using the legacy version
+    """
+    auth_token, _ = client_token_with_scope(
+        sandbox_user,
+        fields=["id.first_name", "id.last_name"],
+        scopes=["vault", "decrypt"],
+    )
+
+    body = get(f"entities/client_token", None, auth_token)
+    assert set(body["vault_fields"]) == set(["id.first_name", "id.last_name"])
+    assert body["tenant"]["name"] == sandbox_tenant.name
+
+    data = {
+        "id.first_name": "Hayes",
+        "id.last_name": "Valley",
+    }
+    post(f"entities/vault/validate", data, auth_token)
+    patch(f"entities/vault", data, auth_token)
+
+    data = dict(
+        fields=["id.first_name", "id.last_name"],
+        reason="Hayes valley",
+    )
+    body = post(f"entities/vault/decrypt", data, auth_token)
+    assert body["id.first_name"] == "Hayes"
+    assert body["id.last_name"] == "Valley"
+
+
 def test_large_objects(sandbox_user):
-    auth_token, _ = client_token_with_scopes(
-        sandbox_user, fields=["custom.large_id"], scopes=["decrypt", "vault"]
+    auth_token, _ = client_token_with_scope(
+        sandbox_user, fields=["custom.large_id"], scope="vault_and_decrypt"
     )
 
     di = "custom.large_id"
@@ -202,10 +230,10 @@ def test_large_objects(sandbox_user):
     assert json.loads(obj_out)["some_key"] == obj["some_key"]
 
     # Test decrypt downloading
-    token, _ = client_token_with_scopes(
+    token, _ = client_token_with_scope(
         sandbox_user,
         fields=["custom.large_id"],
-        scopes=["decrypt_download"],
+        scope="decrypt_download",
         decrypt_reason="flerp",
     )
 
