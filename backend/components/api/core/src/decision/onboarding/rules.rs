b39ad6588399e753f54478f21944607c851b9e8d
Keagan Long <keagan@onefootprint.com>
2024-01-19 11:13:35 -0800

    nuke some more wild code (#7727)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/api/core/src/decision/onboarding/rules.rs
+++ backend/components/api/core/src/decision/onboarding/rules.rs
@@ -1,46 +1,10 @@
-use db::models::onboarding_decision::OnboardingDecision;
-use newtypes::{DecisionStatus, RuleAction, RuleName};
-
+use super::OnboardingRulesDecision;
 use crate::decision::features::kyb_features::KybFeatureVector;
 use crate::decision::features::risk_signals::risk_signal_group_struct::Kyb;
+use crate::decision::features::risk_signals::RiskSignalGroupStruct;
 use crate::decision::onboarding::FeatureVector;
-use crate::decision::{
-    features::risk_signals::RiskSignalGroupStruct, rule::rules_engine::OnboardingEvaluationResult,
-};
 use crate::errors::ApiResult;
-
-use super::{Decision, OnboardingRulesDecision, OnboardingRulesDecisionOutput};
-
-impl From<OnboardingEvaluationResult> for OnboardingRulesDecisionOutput {
-    fn from(result: OnboardingEvaluationResult) -> Self {
-        // If we no rules that triggered, we consider that a pass
-        let (decision_status, create_manual_review) = match result.triggered_action.as_ref() {
-            Some(a) => (a.into(), a.should_create_review()),
-            None => (DecisionStatus::Pass, false),
-        };
-
-        OnboardingRulesDecisionOutput {
-            decision: Decision {
-                // TODO: fix this
-                should_commit: should_commit(&result.rules_triggered, &result.triggered_action),
-                decision_status,
-                create_manual_review,
-            },
-            rules_triggered: result.rules_triggered.to_owned(),
-            rules_not_triggered: result.rules_not_triggered.to_owned(),
-        }
-    }
-}
-
-// For now, we have very simple logic to decide when to commit which is just "if the only thing
-// that failed this user is a watchlist hit, commit"
-// More thoughts: https://www.notion.so/onefootprint/Design-Doc-Portabilization-Decision-71f1cfb945234c58b74e97f005211917?pvs=4
-pub fn should_commit(rules_triggered: &Vec<RuleName>, triggered_action: &Option<RuleAction>) -> bool {
-    // TODO: codify our own proper set of Rule's for determining should_commit
-    rules_triggered.is_empty() // probs redundant now 
-        || (rules_triggered.len() == 1 && rules_triggered.contains(&RuleName::WatchlistHit))
-        || matches!(triggered_action, Some(RuleAction::StepUp) | None) // TODO: when we handle enforcing that StepUp cannot be a triggered_action if doc was already collected, we can remove this hack- ok jk still can't remove this
-}
+use db::models::onboarding_decision::OnboardingDecision;
 
 pub fn evaluate_kyb_rules(
     rsg: RiskSignalGroupStruct<Kyb>,
--- backend/components/api/core/src/decision/rule/rules_engine.rs
+++ backend/components/api/core/src/decision/rule/rules_engine.rs
@@ -1,59 +1,6 @@
-use newtypes::{FootprintReasonCode, RuleAction, RuleName, VendorAPI};
-
-use super::{
-    rule_set::{Rule, RuleEvaluationSummary, RuleSet, RuleSetResult},
-    *,
-};
+use super::rule_set::{RuleSet, RuleSetResult};
 use crate::decision::{onboarding::FeatureSet, rule::RULE_LOG_LINE};
-use itertools::Itertools;
-use strum::IntoEnumIterator;
-
-pub fn evaluate_reason_code_rules(
-    rules: Vec<Rule<Vec<FootprintReasonCode>>>,
-    footprint_reason_codes: &Vec<FootprintReasonCode>,
-    vendor_apis: Vec<VendorAPI>,
-    allow_stepup: bool,
-) -> OnboardingEvaluationResult {
-    // for the rules in the rule set, evaluate each rule
-    let evaluated = rules.iter().cloned().map(|rule| RuleEvaluationSummary {
-        name: rule.name,
-        action: rule.action,
-        triggered: (rule.rule)(footprint_reason_codes),
-    });
-
-    // partition rules by whether or not the rule evaluated to true
-    let (rules_triggered, rules_not_triggered): (Vec<_>, Vec<_>) = evaluated.partition(|r| r.triggered);
-
-    // overall action for the rule set
-    let allowed_rule_actions = RuleAction::iter()
-        .filter(|r| allow_stepup || !matches!(r, RuleAction::StepUp))
-        .collect_vec();
-    let action_for_rule_set = rules_triggered
-        .iter()
-        .filter(|r| allowed_rule_actions.contains(&r.action))
-        .map(|r| r.action)
-        .max();
-
-    // Build a struct that represents the result of evaluating the rule set
-    let evaluated_ruleset = RuleSetResult {
-        ruleset_name: RuleSetName::AllFootprintReasonCodeRules,
-        rules_triggered,
-        rules_not_triggered,
-        action: action_for_rule_set,
-    };
-    let triggered_action = evaluated_ruleset.action;
-
-    // Log evaluation of a single rule set
-    log_ruleset_evaluation(&evaluated_ruleset, vendor_apis.clone());
-
-    OnboardingEvaluationResult {
-        rules_triggered: evaluated_ruleset.triggered_rule_names(),
-        rules_not_triggered: evaluated_ruleset.not_triggered_rule_names(),
-        triggered_action,
-        vendor_apis,
-    }
-}
-
+use newtypes::{RuleAction, RuleName, VendorAPI};
 /// Evaluate a list of rulesets for a given input type T
 pub fn evaluate_onboarding_rules<T>(
     rulesets: Vec<RuleSet<T>>,
