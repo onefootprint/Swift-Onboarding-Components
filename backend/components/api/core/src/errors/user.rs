1ff668f679897da9dc76f0e5f7d1e902ec5a92f2
Elliott Forde <elliott@onefootprint.com>
2024-01-22 15:12:40 -0700

    Some refactors for auth methods (#7772)
    ## Describe your changes
    I'm thinking we'll postpone the ability to log in with an unverified
    email until we have more fully-fledged account recovery
    
    So, this just pulls out the refactors from #7690 that we want to keep,
    but doesn't actually enable logging in with an unverified email
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/api/core/src/errors/user.rs
+++ backend/components/api/core/src/errors/user.rs
@@ -1,18 +1,8 @@
-use newtypes::{ContactInfoKind, DataIdentifier, IdentityDataKind};
+use newtypes::ContactInfoKind;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum UserError {
-    #[error("Data update is not allowed without providing the associated tenant")]
-    NotAllowedWithoutTenant,
-    #[error("Unable to add {0} in this method")]
-    InvalidDataKind(IdentityDataKind),
-    #[error("Cannot decrypt {0}")]
-    CannotDecrypt(DataIdentifier),
-    #[error("Cannot send SMS communications to a phone number that isn't verified")]
-    PhoneNumberNotVerified,
-    #[error("Cannot send email communications to an email that isn't verified")]
-    EmailNotVerified,
     #[error("Cannot send communications to a {0} that isn't verified")]
     ContactInfoKindNotVerified(ContactInfoKind),
     #[error("No contact info found for user")]
@@ -26,8 +16,6 @@ pub enum UserError {
     SandboxMismatch,
     #[error("Invalid auth session: {0}")]
     InvalidAuthSession(String),
-    #[error("Must provide either a playbook public key or a user auth with a playbook attached in order to create an onboarding token")]
-    ObConfigRequiredForSignUp,
     #[error("Must provide an auth playbook public key to create an auth token")]
     PlaybookMissingForAuth,
     #[error("Cannot reonboard user - user has no complete onboardings.")]
