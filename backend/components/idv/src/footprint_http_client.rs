9217913f628ef29fcd1656b051ff147b863c9d13
Elliott Forde <21066341+evforde@users.noreply.github.com>
2024-01-24 20:49:08 +0000

    Add logging for soft timeout exceeded in FootprintHttpClient (#7822)
    ## Describe your changes
    We've been decreasing the number of retries on this HTTP client to now only 1 retry. I didn't realize this was used for ALL vendors, which scares me a bit :P
    
    Even with only one retry, we're seeing some incode requests take 30s, so a single retry is causing the whole HTTP request to take > 60s.
    
    This adds two things:
    - Tracing middleware, so we can actually see the status code from incode
    - A new "soft timeout" middleware that just logs when requests are latent. in the future, i might add a hard 20s timeout to all requests made by this client, but i want to first see what this would impact
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/idv/src/footprint_http_client.rs
+++ backend/components/idv/src/footprint_http_client.rs
@@ -1,12 +1,15 @@
 use std::time::Duration;
 
-use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use chrono::Utc;
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware, Middleware};
 use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
+use reqwest_tracing::TracingMiddleware;
 
 #[derive(Clone)]
 pub struct FootprintVendorHttpClient {
     pub client: ClientWithMiddleware,
 }
+
 impl FootprintVendorHttpClient {
     pub fn new() -> Result<Self, reqwest::Error> {
         let retry_policy = ExponentialBackoff::builder()
@@ -14,6 +17,8 @@ impl FootprintVendorHttpClient {
             .base(1)
             .build_with_max_retries(1);
         let client = ClientBuilder::new(reqwest::Client::new())
+            .with(TracingMiddleware::default())
+            .with(SoftTimeoutMiddleware)
             // Will only retry if:
             // * The status was 5XX (server error)
             // * The status was 408 (request timeout) or 429 (too many requests)
@@ -28,5 +33,26 @@ impl std::ops::Deref for FootprintVendorHttpClient {
     type Target = ClientWithMiddleware;
     fn deref(&self) -> &Self::Target {
         &self.client
+    }
+}
+
+/// For now, just log when a timeout is reached. One day, we will actually enforce a timeout
+struct SoftTimeoutMiddleware;
+
+#[async_trait::async_trait]
+impl Middleware for SoftTimeoutMiddleware {
+    async fn handle(
+        &self,
+        req: reqwest::Request,
+        extensions: &mut task_local_extensions::Extensions,
+        next: reqwest_middleware::Next<'_>,
+    ) -> reqwest_middleware::Result<reqwest::Response> {
+        let start = Utc::now();
+        let res = next.run(req, extensions).await;
+        let elapsed = Utc::now() - start;
+        if elapsed.num_seconds() > 20 {
+            tracing::warn!(num_seconds=%elapsed.num_seconds(), "Vendor request soft timeout exceeded");
+        }
+        res
     }
 }
