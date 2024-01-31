355ac5c9dfc7d4f1808df04ce15a7db39003c656
Elliott Forde <elliott@onefootprint.com>
2024-01-30 21:00:20 -0700

    Create Cloudfront 5xx alert instead of ALB 5xx alert (#7948)
    ## Describe your changes
    Cloudfront is the outermost layer of our loadbalancing/firewalls. So,
    all errors that occur as HTTP requests are routed through our stack will
    be visible in Cloudfront.
    
    We used to have alerts on the ALB target (ie the webserver) and ALB
    itself throwing 5xxs. But those are only part of the picture since they
    miss all errors that happen downstream of the ALB. Now, we'll also alert
    on errors that originate at cloudfront to better catch cases like
    https://onefootprint.slack.com/archives/C03DQ288QQM/p1706659855800229
    
    ## Caveats
    This alert will fire when `HTTP 5xx Errors` fires as well. That other
    alert is useful because you can immediately see the traces for the
    requests that 5xxed. But maybe it's superfluous to have both of these
    and we should just alert at cloudfront?
    
    This also removes alerting on 5xxs from the nitro service ALB. But those
    errors will bubble up into our application and should error loudly
    
    ## Test Plan
    Ran this query in honeycomb:
    https://ui.honeycomb.io/footprint-2e/environments/prod/datasets/cloudfront/result/aT21imNvnxD

--- backend/infra/src/alerts/alerts.ts
+++ backend/infra/src/alerts/alerts.ts
@@ -1,4 +1,4 @@
-import { Trigger, Threshold } from './trigger';
+import { Threshold } from './trigger';
 import { Query } from './query';
 import { GlobalState } from '../main';
 
@@ -229,56 +229,25 @@ const staticAlerts: Alert[] = [
     },
   },
   {
-    name: 'ALB Target 5xx Errors',
+    name: 'Cloudfront 5xx Errors',
     description:
-      'Elevated 5xx that originated from the loadbalancer target. This will likely fire any time the HTTP 5xx errors alert fires',
-    runbookUrl:
-      'https://www.notion.so/onefootprint/Alert-Runbooks-17f53ed91bb64a09b446bf2c0eb1cb25?pvs=4#73efd0bc1e48446db7ab38e735b8925e',
-    datasetName: 'aws',
-    query: {
-      time_range: 240,
-      breakdowns: ['LoadBalancer'],
-      calculations: [
-        {
-          op: 'SUM',
-          column:
-            'amazonaws.com/AWS/ApplicationELB/HTTPCode_Target_5XX_Count.avg',
-        },
-      ],
-      filters: [
-        {
-          column: 'LoadBalancer',
-          op: 'exists',
-        },
-      ],
-      filter_combination: 'AND',
-    },
-    // These won't make a ton of sense to page on for a while - we might want to page on a 5xx percentage
-    pageThreshold: {
-      op: '>',
-      value: 10,
-    },
-  },
-  {
-    name: 'ALB 5xx Errors',
-    description:
-      'Elevated 5xx that originated from the loadbalancer. This specifically does not includes 5xx that originated from the loadbalancer target (the application)',
-    datasetName: 'aws',
+      'Elevated 5xx seen at cloudfront. This includes 5xxs from the application, ALB, and cloudfront.',
+    datasetName: 'cloudfront',
     runbookUrl:
       'https://www.notion.so/onefootprint/Alert-Runbooks-17f53ed91bb64a09b446bf2c0eb1cb25?pvs=4#b6d06909bf0e4a1689bbaaef62ebd8f0',
     query: {
       time_range: 240,
-      breakdowns: ['LoadBalancer'],
+      breakdowns: ['cs_uri_stem'],
       calculations: [
         {
-          op: 'SUM',
-          column: 'amazonaws.com/AWS/ApplicationELB/HTTPCode_ELB_5XX_Count.avg',
+          op: 'COUNT',
         },
       ],
       filters: [
         {
-          column: 'LoadBalancer',
-          op: 'exists',
+          column: 'sc_status',
+          op: '>=',
+          value: 500,
         },
       ],
       filter_combination: 'AND',
