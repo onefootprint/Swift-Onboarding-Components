a7ce155e9e651a5968b197e0858e93f960b337ea
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2024-01-31 20:07:46 -0500

    Ungate POA from dash (#7971)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <img width="331" alt="Screen Shot 2024-01-31 at 8 07 25 PM"
    src="https://github.com/onefootprint/monorepo/assets/105253942/cae91c5b-2770-4cfc-a908-1b7ff02900e0">
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- frontend/apps/dashboard/src/components/entities/components/details/components/content/components/vault/components/vault-actions/components/retrigger-kyc-dialog/components/retrigger-kyc-form/retrigger-kyc-form.tsx
+++ frontend/apps/dashboard/src/components/entities/components/details/components/content/components/vault/components/vault-actions/components/retrigger-kyc-dialog/components/retrigger-kyc-form/retrigger-kyc-form.tsx
@@ -13,7 +13,6 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import AnimatedContainer from 'src/components/animated-container';
 import FrequentNotesTextArea from 'src/components/frequent-notes-text-area';
-import useSession from 'src/hooks/use-session';
 
 import useEntity from '@/entity/hooks/use-entity';
 import useEntityId from '@/entity/hooks/use-entity-id';
@@ -46,10 +45,6 @@ const RetriggerKYCForm = ({ onSubmit, formId }: RetriggerKYCFormProps) => {
     watch,
   } = methods;
   const triggerKind = watch('kind');
-
-  const {
-    data: { user },
-  } = useSession();
 
   const handleBeforeSubmit = (data: RetriggerKYCFormData) => {
     onSubmit({
@@ -86,16 +81,14 @@ const RetriggerKYCForm = ({ onSubmit, formId }: RetriggerKYCFormProps) => {
             hint={t('form.proof-of-ssn.description')}
             {...register('kind', { required: true })}
           />
-          {user?.isFirmEmployee && (
-            <div>
-              <Radio
-                value={TriggerKind.ProofOfAddress}
-                label={t('form.proof-of-address.title')}
-                hint={t('form.proof-of-address.description')}
-                {...register('kind', { required: true })}
-              />
-            </div>
-          )}
+          <div>
+            <Radio
+              value={TriggerKind.ProofOfAddress}
+              label={t('form.proof-of-address.title')}
+              hint={t('form.proof-of-address.description')}
+              {...register('kind', { required: true })}
+            />
+          </div>
           <Tooltip
             disabled={shouldShowRetriggerKyc}
             position="left"
