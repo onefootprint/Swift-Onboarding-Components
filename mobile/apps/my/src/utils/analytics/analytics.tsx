1074f172d8c25cad1c117f5405d4385e4a1a8716
Rafael Michels Motta <rafael@onefootprint.com>
2024-02-01 15:23:49 -0300

    Appclip: fix types (part 1)

--- mobile/apps/my/src/utils/analytics/analytics.tsx
+++ mobile/apps/my/src/utils/analytics/analytics.tsx
@@ -1,4 +1,4 @@
-import { Mixpanel } from 'mixpanel-react-native';
+import { type MixpanelProperties, Mixpanel } from 'mixpanel-react-native';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
 import { IS_DEV, MIXPANEL_TOKEN } from '../../config/constants';
@@ -8,22 +8,27 @@ export type AnalyticsProps = {
   children: React.ReactNode;
 };
 
-export const AnalyticsContext = createContext(null);
+type AnalyticsContextType = {
+  track: (eventName: string, properties?: MixpanelProperties) => void;
+  timeEvent: (eventName: string) => void;
+};
 
-export const useAnalytics = (): Mixpanel => useContext(AnalyticsContext);
+export const AnalyticsContext = createContext<AnalyticsContextType>({
+  track: (eventName: string, properties?: MixpanelProperties) =>
+    console.log('track', eventName, properties),
+  timeEvent: (eventName: string) => console.log('timeEvent', eventName),
+});
+
+export const useAnalytics = () => useContext(AnalyticsContext);
 
 const Provider = ({ debug, children }: AnalyticsProps) => {
-  const [analytics, setAnalytics] = useState(null);
+  const [analytics, setAnalytics] = useState<AnalyticsContextType>({
+    track: args => console.log('track', args),
+    timeEvent: args => console.log('timeEvent', args),
+  });
 
   useEffect(() => {
-    if (IS_DEV || debug) {
-      setAnalytics({
-        track: args => console.log('track', args),
-        timeEvent: args => console.log('timeEvent', args),
-      });
-      return;
-    }
-
+    if (IS_DEV || debug) return;
     const trackAutomaticEvents = true;
     const mixpanel = new Mixpanel(MIXPANEL_TOKEN, trackAutomaticEvents);
     mixpanel.init();
