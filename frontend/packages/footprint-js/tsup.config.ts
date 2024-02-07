8b660e6802e33af5ff622173d5ef3fb160b8d460
Elliott Forde <elliott@onefootprint.com>
2024-02-06 16:22:46 -0800

    Have footprint.js respect NEXT_PUBLIC_API_BASE_URL (#8079)

--- frontend/packages/footprint-js/tsup.config.ts
+++ frontend/packages/footprint-js/tsup.config.ts
@@ -5,6 +5,7 @@ import { version } from './package.json';
 const isE2E = process.env.IS_E2E === 'true';
 const forceFootprintToUseLocal =
   process.env.FORCE_FOOTPRINT_JS_TO_USE_LOCAL === 'true';
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 const currentBranch = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF;
 const isVercelPreview = process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview';
 const isDevelopment = currentBranch === 'development' && isVercelPreview;
@@ -71,7 +72,8 @@ export default defineConfig(options => ({
   watch: options.watch,
   minify: !options.watch,
   env: {
-    API_BASE_URL: getApiUrl(!!options.watch || forceFootprintToUseLocal),
+    API_BASE_URL:
+      apiBaseUrl || getApiUrl(!!options.watch || forceFootprintToUseLocal),
     BIFROST_URL: getBifrostUrl(!!options.watch || forceFootprintToUseLocal),
     AUTH_URL: getAuthUrl(!!options.watch || forceFootprintToUseLocal),
     COMPONENTS_URL: getComponentsUrl(
