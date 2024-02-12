e1b32c6fd2728a553172b9e2898e88f1f2d8aadc
Claudio Angrigiani <angrigianicr@gmail.com>
2024-02-12 16:47:12 -0500

    Fix bottom sheet header (#8147)
    before: 
    
    ![image](https://github.com/onefootprint/monorepo/assets/12160059/18b8e491-5798-4f73-b5af-fecd65986658)
    
    after
    
    ![5FE55149-43E9-47C9-8D88-C86CBE3C3CE7_4_5005_c](https://github.com/onefootprint/monorepo/assets/12160059/d399b588-c3f9-4891-8e32-654ccda444e9)

--- frontend/packages/ui/src/components/bottom-sheet/header/header.tsx
+++ frontend/packages/ui/src/components/bottom-sheet/header/header.tsx
@@ -3,6 +3,7 @@ import * as DialogPrimitive from '@radix-ui/react-dialog';
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+import Box from '../../box';
 import IconButton from '../../icon-button';
 import Stack from '../../stack';
 import Typography from '../../typography';
@@ -18,17 +19,16 @@ type HeaderProps = {
 const Header = ({ title, closeAriaLabel = 'Close', onClose }: HeaderProps) => (
   <Container hasBorder={!!title} flexGrow={0}>
     <DialogPrimitive.Close asChild>
-      <Stack align="center" justify="center" height="100%">
-        <IconButton aria-label={closeAriaLabel} onClick={onClose}>
-          <IcoClose24 />
-        </IconButton>
-      </Stack>
+      <IconButton aria-label={closeAriaLabel} onClick={onClose}>
+        <IcoClose24 />
+      </IconButton>
     </DialogPrimitive.Close>
     {title && (
-      <DialogPrimitive.Title asChild>
+      <Title>
         <Typography variant="label-2">{title}</Typography>
-      </DialogPrimitive.Title>
+      </Title>
     )}
+    <Box height="32px" width="32px" as="span" />
   </Container>
 );
 
@@ -36,15 +36,24 @@ const Container = styled(Stack)<{ hasBorder: boolean }>`
   ${({ theme, hasBorder }) => css`
     height: 52px;
     display: flex;
-    align-items: start;
-    justify-content: start;
+    align-items: center;
+    justify-content: space-between;
     padding: ${theme.spacing[3]};
+    width: 100%;
 
     ${hasBorder &&
     css`
       border-bottom: 1px solid ${theme.borderColor.tertiary};
     `}
   `}
+`;
+
+const Title = styled(DialogPrimitive.Title)`
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 `;
 
 export default Header;
