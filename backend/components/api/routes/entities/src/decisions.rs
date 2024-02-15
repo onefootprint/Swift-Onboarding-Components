c120588f0d5a213c86d3dcb0b724b8f919947941
Keagan Long <keagan@onefootprint.com>
2024-02-14 17:11:14 -0800

    Remove workflow run from review decision (#8163)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/api/routes/entities/src/decisions.rs
+++ backend/components/api/routes/entities/src/decisions.rs
@@ -5,17 +5,12 @@ use crate::{
 };
 use api_core::{
     decision,
-    decision::state::{actions::WorkflowActions, ReviewCompleted, StateError, WorkflowWrapper},
     errors::{onboarding::OnboardingError, ApiResult},
     task,
     utils::fp_id_path::FpIdPath,
-    ApiErrorKind,
 };
 use api_wire_types::DecisionRequest;
-use db::{
-    models::{scoped_vault::ScopedVault, workflow::Workflow},
-    DbResult,
-};
+use db::models::{scoped_vault::ScopedVault, workflow::Workflow};
 use paperclip::actix::{api_v2_operation, post, web};
 
 #[api_v2_operation(
@@ -38,45 +33,17 @@ pub async fn post(
 
     let fpid = fp_id.clone();
     let tid = tenant_id.clone();
-    let wf = state
-        .db_pool
-        .db_query(move |conn| -> DbResult<_> {
-            let sv = ScopedVault::get(conn, (&fpid, &tid, is_live))?;
-            let wf = Workflow::get_active(conn, &sv.id)?;
-            Ok(wf)
-        })
-        .await?;
-    let wf = wf.ok_or(OnboardingError::NoWorkflow)?;
-
-    let wf_id = wf.id.clone();
-    let ww = WorkflowWrapper::init(&state, wf.clone()).await?;
-    // TODO: add a ww.expects_action method here to check if the workflow is expecting ReviewCompleted or not. If not, for now we should probably gracefully
-    // just continue and do what this route would have done anyway (ie call save_review_decision). But in the future, we may instead query here for
-    // an existing *active* workflow and if there is one, then strictly error if a review is being made when that workflow isn't expecting it
-    let action = WorkflowActions::ReviewCompleted(ReviewCompleted {
-        decision: request.clone(),
-        actor: actor.clone(),
-    });
-    let res = ww.run(&state, action).await;
-    match res {
-        Ok(_) => return EmptyResponse::ok().json(),
-        Err(e) => match e.kind() {
-            ApiErrorKind::StateError(StateError::UnexpectedActionForState) => {
-                tracing::info!(workflow_id=?wf.id, state=?wf.state, "ReviewCompleted called on workflow not expecting it");
-            }
-            _ => Err(e)?,
-        },
-    }
-
     state
         .db_pool
-        // TODO how does this work when there are multiple KYC workflows for one scoped vault?
         .db_transaction(move |conn| -> ApiResult<_> {
-            let wf = Workflow::lock(conn, &wf_id)?;
+            let sv = ScopedVault::get(conn, (&fpid, &tid, is_live))?;
+            let wf = Workflow::get_active(conn, &sv.id)?.ok_or(OnboardingError::NoWorkflow)?;
+            let wf = Workflow::lock(conn, &wf.id)?;
             decision::review::save_review_decision(conn, wf, request, actor)?;
             Ok(())
         })
         .await?;
+
     // Since we may have updated users onboarding status
     task::execute_webhook_tasks((*state.clone().into_inner()).clone());
 
