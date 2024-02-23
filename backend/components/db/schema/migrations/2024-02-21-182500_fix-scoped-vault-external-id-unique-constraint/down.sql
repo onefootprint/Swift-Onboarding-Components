c9144bbbfae257d620ac1807972fd0d3b214dd38
Ethan Lowman <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-02-23 13:47:28 -0500

    Remove down migration for scoped vault external ID unique constraint (#8278)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    This is causing builds to fail until we create a new migration:
    https://github.com/onefootprint/monorepo/actions/runs/8011264066/job/21884103677
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/db/schema/migrations/2024-02-21-182500_fix-scoped-vault-external-id-unique-constraint/down.sql
+++ backend/components/db/schema/migrations/2024-02-21-182500_fix-scoped-vault-external-id-unique-constraint/down.sql
@@ -1,2 +1,8 @@
-CREATE UNIQUE INDEX IF NOT EXISTS scoped_vault_unique_external_id_tenant_id ON scoped_vault(tenant_id, external_id);
-DROP INDEX scoped_vault_active_unique_external_id_tenant_id;
+-- It's actually more involved than this to revert this migration since the (tenant_id, external_id) unique constraint may be broken by new deactivated scoped vaults.
+-- We'd probably have to hand-delete deactivated scoped vaults, and then recreate the old index using the commands below.
+
+-- CREATE UNIQUE INDEX IF NOT EXISTS scoped_vault_unique_external_id_tenant_id ON scoped_vault(tenant_id, external_id);
+-- DROP INDEX scoped_vault_active_unique_external_id_tenant_id;
+
+-- Need a non-empty query to make the migration pass.
+SELECT 1;
--- backend/components/db/schema/migrations/2024-02-21-182500_fix-scoped-vault-external-id-unique-constraint/up.sql
+++ backend/components/db/schema/migrations/2024-02-21-182500_fix-scoped-vault-external-id-unique-constraint/up.sql
@@ -1,2 +1,2 @@
 CREATE UNIQUE INDEX IF NOT EXISTS scoped_vault_active_unique_external_id_tenant_id ON scoped_vault(tenant_id, external_id) WHERE deactivated_at IS NULL;
-DROP INDEX scoped_vault_unique_external_id_tenant_id;
+DROP INDEX IF EXISTS scoped_vault_unique_external_id_tenant_id;
