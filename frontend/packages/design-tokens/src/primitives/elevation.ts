33ef352fd7501abc64a7219cbf43f173355270dd
Claudio Angrigiani <angrigianicr@gmail.com>
2024-02-27 18:58:09 -0500

    Fix elevations (#8325)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- frontend/packages/design-tokens/src/primitives/elevation.ts
+++ frontend/packages/design-tokens/src/primitives/elevation.ts
@@ -1,20 +1,12 @@
 const generateElevation = (shadowColorVar: string) => ({
-  flat: `0px 0px 0px 0px rgba(${shadowColorVar}, 0)`,
-  low: `0px 1px 3px rgba(${shadowColorVar}, 0.1),
-        0px 1px 3px rgba(${shadowColorVar}, 0.1),
-        0px 2px 4px rgba(${shadowColorVar}, 0.1)`,
-  medium: `0px 2px 3px rgba(${shadowColorVar}, 0.11),
-           0px 2px 3px rgba(${shadowColorVar}, 0.11),
-           0px 4px 6px rgba(${shadowColorVar}, 0.10)`,
-  high: `0px 2px 4px rgba(${shadowColorVar}, 0.14),
-         0px 2px 4px rgba(${shadowColorVar}, 0.13),
-         0px 5px 7px rgba(${shadowColorVar}, 0.12)`,
-  extraHigh: `0px 10px 8px rgba(${shadowColorVar}, 0.20),
-              0px 12px 10px rgba(${shadowColorVar}, 0.18),
-              0px 15px 12px rgba(${shadowColorVar}, 0.16)`,
+  flat: `0px 0px 0px 0px rgba(${shadowColorVar}, 0);`,
+  low: `0px 1px 4px 0px rgba(${shadowColorVar}, 0.12);`,
+  medium: `0px 1px 8px 0px rgba(${shadowColorVar}, 0.14);`,
+  high: `0px 1px 12px 0px rgba(${shadowColorVar}, 0.18);`,
+  extraHigh: `0px 10px 8px rgba(${shadowColorVar}, 0.20);`,
 });
-const shadowColorLight = '5, 5, 50';
-const shadowColorDark = '5, 5, 10';
+const shadowColorLight = '5, 5, 5';
+const shadowColorDark = '0, 0, 0';
 
 export const elevationLight = generateElevation(shadowColorLight);
 export const elevationDark = generateElevation(shadowColorDark);
