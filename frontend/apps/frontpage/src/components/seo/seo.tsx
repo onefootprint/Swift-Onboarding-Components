b562d78de737625b41b6c8ca83e353e6a7036265
Rafael Michels Motta <rafael@onefootprint.com>
2024-02-29 15:37:39 -0300

    SEO: tweaks

--- frontend/apps/frontpage/src/components/seo/seo.tsx
+++ frontend/apps/frontpage/src/components/seo/seo.tsx
@@ -41,13 +41,17 @@ const Seo = ({
     <meta name="description" content={description} />
     <meta name="keywords" content={keywords} />
     <meta property="og:type" content={kind} />
-    <meta property="og:title" content={og.title || title} />
-    <meta property="og:description" content={og.description || description} />
+    <meta name="title" property="og:title" content={og.title || title} />
+    <meta
+      name="description"
+      property="og:description"
+      content={og.description || description}
+    />
     {og.author && (
       <meta name="author" property="og:author" content={og.author} />
     )}
     <meta property="og:url" content={`https://onefootprint.com${slug}`} />
-    <meta property="og:image" content={og.image || image} />
+    <meta name="image" property="og:image" content={og.image || image} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content={twitter.title || title} />
     {twitter.extraData &&
