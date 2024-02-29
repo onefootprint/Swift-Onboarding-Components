39daeb26ec4248094054572f56683dc018f84a35
Rafael Michels Motta <rafael@onefootprint.com>
2024-02-29 15:09:06 -0300

    Debug blog og image

--- frontend/apps/frontpage/src/components/seo/seo.tsx
+++ frontend/apps/frontpage/src/components/seo/seo.tsx
@@ -34,36 +34,39 @@ const Seo = ({
   title,
   og = {},
   twitter = {},
-}: SeoProps) => (
-  <Head>
-    <title>{title}</title>
-    <link rel="canonical" href={`https://onefootprint.com${slug}`} />
-    <meta name="description" content={description} />
-    <meta name="keywords" content={keywords} />
-    <meta property="og:type" content={kind} />
-    <meta property="og:title" content={og.title || title} />
-    <meta property="og:description" content={og.description || description} />
-    {og.author && (
-      <meta name="author" property="og:author" content={og.author} />
-    )}
-    <meta property="og:url" content={`https://onefootprint.com${slug}`} />
-    <meta property="og:image" content={og.image || image} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={twitter.title || title} />
-    {twitter.extraData &&
-      twitter.extraData.map(({ label, data }, index) => (
-        <Fragment key={label}>
-          <meta name={`twitter:label${index + 1}`} content={label} />
-          <meta name={`twitter:data${index + 1}`} content={data} />
-        </Fragment>
-      ))}
-    <meta
-      name="twitter:description"
-      content={twitter.description || description}
-    />
-    <meta name="twitter:url" content={`https://onefootprint.com${slug}`} />
-    <meta name="twitter:image" content={twitter.image || image} />
-  </Head>
-);
+}: SeoProps) => {
+  console.log(og);
+  return (
+    <Head>
+      <title>{title}</title>
+      <link rel="canonical" href={`https://onefootprint.com${slug}`} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta property="og:type" content={kind} />
+      <meta property="og:title" content={og.title || title} />
+      <meta property="og:description" content={og.description || description} />
+      {og.author && (
+        <meta name="author" property="og:author" content={og.author} />
+      )}
+      <meta property="og:url" content={`https://onefootprint.com${slug}`} />
+      <meta property="og:image" content={og.image || image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={twitter.title || title} />
+      {twitter.extraData &&
+        twitter.extraData.map(({ label, data }, index) => (
+          <Fragment key={label}>
+            <meta name={`twitter:label${index + 1}`} content={label} />
+            <meta name={`twitter:data${index + 1}`} content={data} />
+          </Fragment>
+        ))}
+      <meta
+        name="twitter:description"
+        content={twitter.description || description}
+      />
+      <meta name="twitter:url" content={`https://onefootprint.com${slug}`} />
+      <meta name="twitter:image" content={twitter.image || image} />
+    </Head>
+  );
+};
 
 export default Seo;
