onefootprint/Swift-Onboarding-Components

commit 477748feaacc86ae42658ed85a957b01157e7fba

[FP-7275] Always use the token issued by the challenge in identify verify (#8430)
## Describe your changes
Migrate to the modern identify flow:

- Login flow: token is issued during the matching `POST
/hosted/identify` request. This token should be passed into `POST
/hosted/identify/login_challenge` (to make sure we initiate a login
challenge to the identified user) and then also to `POST
/hosted/identify/verify`.
- Signup flow: `POST /hosted/identify/signup_challenge` issues a token
that we should pass back in the `POST /hosted/identify/verify` API
- Signup flow race condition: If we somehow end up issuing a signup
challenge for a vault that already exists, the signup challenge API will
return a token we can use to instead issue a login challenge to log into
the existing vault

## Caveats
<!--- Are there any caveats to this changeset? -->

## Test Plan
Tested locally

Login challenge:

https://github.com/onefootprint/monorepo/assets/21066341/e743c460-5656-4e19-9980-103f7f6098d0

Signup challenge:

https://github.com/onefootprint/monorepo/assets/21066341/ec167a5d-10af-4dcf-9a78-327bd78519ea

Signup challenge race condition:

https://github.com/onefootprint/monorepo/assets/21066341/79d381ea-f231-4c4b-9625-028579a9c1b2

frontend/packages/idv/src/components/identify/components/pin-verification/pin-verification.tsx
@@ -11,12 +11,7 @@ import type { ComponentProps } from 'react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import {
-  getLogger,
-  isEmailOrPhoneIdentifier,
-  isPhoneIdentifier,
-  isStringValid,
-} from '../../../../utils';
+import { getLogger, isPhoneIdentifier } from '../../../../utils';
 import useEffectOnceStrict from '../../hooks/use-effect-once-strict';
 import {
   useIdentifyVerify,
@@ -50,22 +45,22 @@ const PinVerification = ({
   const [state, send] = useIdentifyMachine();
   const {
     challenge: { challengeData: data },
-    identify: { email, identifyToken, phoneNumber, sandboxId, user },
+    identify: { email, phoneNumber, sandboxId, user },
     variant,
     obConfigAuth,
   } = state.context;
-  const commonMutationProps = {
-    authToken: state.context.initialAuthToken,
-    obConfigAuth,
-    sandboxId,
-    scope: getTokenScope(variant),
-  };
   const requestError = useRequestError();
   const { t } = useTranslation('identify', { keyPrefix: 'pin-verification' });
   const showRequestErrorToast = useRequestErrorToast();
-  const mutLoginChallenge = useLoginChallenge(commonMutationProps);
-  const mutSignupChallenge = useSignupChallenge(commonMutationProps);
-  const mutIdentifyVerify = useIdentifyVerify(commonMutationProps);
+
+  const scope = getTokenScope(variant);
+  const mutLoginChallenge = useLoginChallenge();
+  const mutSignupChallenge = useSignupChallenge({
+    obConfigAuth,
+    sandboxId,
+    scope,
+  });
+  const mutIdentifyVerify = useIdentifyVerify({ scope });
 
   const challengeData: ChallengeData | undefined =
     data ||
@@ -86,9 +81,9 @@ const PinVerification = ({
       return;
     }
 
-    const { challengeToken } = challengeData;
+    const { challengeToken, token } = challengeData;
     mutIdentifyVerify.mutate(
-      { challengeResponse: pin, challengeToken },
+      { challengeResponse: pin, challengeToken, authToken: token },
       {
         onError: error => {
           logWarn('Failed to verify pin: ', error);
@@ -140,11 +135,14 @@ const PinVerification = ({
       },
       {
         onError: (error: unknown) => {
-          if (requestError.getErrorCode(error) === 'E120') {
+          const isExistingVaultError =
+            requestError.getErrorCode(error) === 'E120';
+          const { token } = requestError.getErrorContext(error);
+          if (isExistingVaultError && token) {
             logError(
               'Entered signup challenge when the user already has a vault. Initiating login challenge',
             );
-            initiatePhoneOrEmailLoginChallenge();
+            initiatePhoneOrEmailLoginChallenge(token);
             return;
           }
           logError('Failed to initiate signup challenge: ', error);
@@ -155,23 +153,14 @@ const PinVerification = ({
     );
   };
 
-  const initiatePhoneOrEmailLoginChallenge = () => {
+  const initiatePhoneOrEmailLoginChallenge = (authToken: string) => {
     if (mutLoginChallenge.isLoading) {
       return;
     }
 
-    // We'll be able to simplify this soon with the token-based login challenges
-    const conditionalPayload = isStringValid(identifyToken)
-      ? { authToken: identifyToken }
-      : {
-          identifier: isEmailOrPhoneIdentifier(identifier)
-            ? identifier
-            : undefined,
-        };
-
     mutLoginChallenge.mutate(
       {
-        ...conditionalPayload,
+        authToken,
         isResend: !!challengeData, // Check whether is resend, but isResend state might not have updated yet
         preferredChallengeKind,
       },
@@ -198,7 +187,7 @@ const PinVerification = ({
     }
 
     if (user) {
-      initiatePhoneOrEmailLoginChallenge();
+      initiatePhoneOrEmailLoginChallenge(user.token);
     } else {
       initiateSignupChallenge();
     }

frontend/apps/auth/src/components/user-container/identify.tsx
@@ -1,25 +1,0 @@
-import { useIdentify } from '@onefootprint/idv/src/components/identify/queries';
-import type { IdentifyResponse } from '@onefootprint/types';
-import React from 'react';
-
-import { useEffectOnceStrict } from '@/src/hooks';
-
-import Loading from '../../app/user/loading';
-
-type IdentifyUserProps = {
-  authToken: string;
-  onError: (error: unknown) => void;
-  onSuccess: (res: IdentifyResponse) => void;
-};
-
-const IdentifyUser = ({ authToken, onError, onSuccess }: IdentifyUserProps) => {
-  const mutIdentify = useIdentify({});
-
-  useEffectOnceStrict(() => {
-    mutIdentify.mutate({ identifier: { authToken } }, { onError, onSuccess });
-  });
-
-  return <Loading />;
-};
-
-export default IdentifyUser;

frontend/packages/idv/src/components/identify/components/challenge-select-or-passkey/hooks/run-passkey.ts
@@ -19,9 +19,7 @@ const { logError } = getLogger('run-passkey');
 const useRunPasskey = ({ onSuccess }: UseRunPasskeyArgs) => {
   const [state] = useIdentifyMachine();
   const {
-    identify: { successfulIdentifier, sandboxId },
-    obConfigAuth,
-    initialAuthToken,
+    identify: { user },
     variant,
   } = state.context;
   const { t } = useTranslation('identify', {
@@ -29,21 +27,18 @@ const useRunPasskey = ({ onSuccess }: UseRunPasskeyArgs) => {
   });
   const showRequestErrorToast = useRequestErrorToast();
   const toast = useToast();
-  const commonMutationProps = {
-    authToken: initialAuthToken,
-    obConfigAuth,
-    sandboxId,
+  const mutLoginChallenge = useLoginChallenge();
+  const mutIdentifyVerify = useIdentifyVerify({
     scope: getTokenScope(variant),
-  };
-  const mutLoginChallenge = useLoginChallenge(commonMutationProps);
-  const mutIdentifyVerify = useIdentifyVerify(commonMutationProps);
+  });
+
   const [isRunningWebauthn, setIsRunningWebauthn] = useState(false);
   const isWaiting = isRunningWebauthn || mutIdentifyVerify.isLoading;
 
   const initiatePasskeyChallenge = () => {
-    if (!successfulIdentifier) {
+    if (!user?.token) {
       logError(
-        'No successful identifier found while initiating login biometric challenge',
+        'No identifying token found while initiating login biometric challenge',
       );
       return;
     }
@@ -52,14 +47,9 @@ const useRunPasskey = ({ onSuccess }: UseRunPasskeyArgs) => {
       return;
     }
 
-    // We'll be able to simplify this soon with the token-based login challenges
-    const loginIdentifier =
-      'email' in successfulIdentifier || 'phoneNumber' in successfulIdentifier
-        ? successfulIdentifier
-        : undefined;
     mutLoginChallenge.mutate(
       {
-        identifier: loginIdentifier,
+        authToken: user?.token,
         preferredChallengeKind: ChallengeKind.biometric,
       },
       {
@@ -75,7 +65,7 @@ const useRunPasskey = ({ onSuccess }: UseRunPasskeyArgs) => {
   const handleRequestChallengeSuccess = async (
     payload: LoginChallengeResponse,
   ) => {
-    const { biometricChallengeJson, challengeToken, challengeKind } =
+    const { token, biometricChallengeJson, challengeToken, challengeKind } =
       payload.challengeData || {};
 
     if (challengeKind !== ChallengeKind.biometric) {
@@ -118,6 +108,7 @@ const useRunPasskey = ({ onSuccess }: UseRunPasskeyArgs) => {
 
     mutIdentifyVerify.mutate(
       {
+        authToken: token,
         challengeResponse,
         challengeToken,
       },

frontend/packages/idv/src/components/identify/components/step-bootstrap/utils.ts
@@ -17,7 +17,7 @@ export const identifyMutationCaller = async (
   mutation: UseMutationResult<
     IdentifyResponse,
     unknown,
-    IdentifyRequest,
+    Pick<IdentifyRequest, 'identifier'>,
     unknown
   >,
   identifier: EmailOrPhoneIdentifier,

frontend/packages/idv/src/components/identify/identify.test.config.ts
@@ -86,6 +86,7 @@ export const withIdentify = (
     path: '/hosted/identify',
     response: {
       user: userFound && {
+        token: 'utok_xxx',
         isUnverified: isUnverified ?? false,
         availableChallengeKinds,
         authMethods,
@@ -103,6 +104,7 @@ export const withLoginChallenge = (challengeKind: string) =>
     path: '/hosted/identify/login_challenge',
     response: {
       challengeData: {
+        token: 'utok_xxx',
         scrubbedPhoneNumber: '+1 (•••) •••-••99',
         biometricChallengeJson: {},
         challengeToken: 'token',
@@ -117,6 +119,7 @@ export const withSignupChallenge = (challengeKind?: string) =>
     path: '/hosted/identify/signup_challenge',
     response: {
       challengeData: {
+        token: 'utok_xxx',
         scrubbedPhoneNumber: '+1 (•••) •••-••99',
         challengeToken: 'token',
         challengeKind: challengeKind ?? 'sms',

frontend/packages/idv/src/components/identify/queries/use-identify-verify.ts
@@ -3,30 +3,17 @@ import type {
   IdentifyVerifyRequest,
   IdentifyVerifyResponse,
 } from '@onefootprint/types';
-import { AUTH_HEADER, SANDBOX_ID_HEADER } from '@onefootprint/types';
+import { AUTH_HEADER } from '@onefootprint/types';
 import { useMutation } from '@tanstack/react-query';
 
-type PayloadPartKey = 'obConfigAuth' | 'sandboxId' | 'authToken' | 'scope';
+type PayloadPartKey = 'scope';
 type Payload = Omit<IdentifyVerifyRequest, 'identifier'> & {
   authToken?: string;
 };
 
 const requestFn = async (payload: Payload) => {
-  const {
-    challengeResponse,
-    challengeToken,
-    authToken,
-    obConfigAuth,
-    sandboxId,
-    scope,
-  } = payload;
-  const headers: Record<string, string> = { ...obConfigAuth };
-  if (sandboxId) {
-    headers[SANDBOX_ID_HEADER] = sandboxId;
-  }
-  if (authToken) {
-    headers[AUTH_HEADER] = authToken;
-  }
+  const { challengeResponse, challengeToken, authToken, scope } = payload;
+  const headers: Record<string, string> = { [AUTH_HEADER]: authToken };
 
   const response = await request<IdentifyVerifyResponse>({
     method: 'POST',

frontend/packages/idv/src/components/identify/queries/use-login-challenge.ts
@@ -3,54 +3,34 @@ import type {
   LoginChallengeRequest,
   LoginChallengeResponse,
 } from '@onefootprint/types';
-import { AUTH_HEADER, SANDBOX_ID_HEADER } from '@onefootprint/types';
+import { AUTH_HEADER } from '@onefootprint/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import type { EmailAndOrPhone } from '../types';
 import calculateRetryTime from './get-retry-time';
 
-type ToIgnore = 'identifier' | 'preferredChallengeKind';
-type PayloadPartial = 'obConfigAuth' | 'sandboxId' | 'authToken';
-type BasePayload = Pick<Payload, PayloadPartial>;
-type RestOfPayload = Omit<Payload, PayloadPartial>;
-type Payload = Omit<LoginChallengeRequest, ToIgnore> & {
-  authToken?: string;
-  identifier?: EmailAndOrPhone;
-  preferredChallengeKind: `${LoginChallengeRequest['preferredChallengeKind']}`;
+type Payload = LoginChallengeRequest & {
+  authToken: string;
 };
 
 const FIVE_MINUTES = 1000 * 60 * 5; // challenges expire after 5 mins
 
 const getQueryKeyForPayload = (payload: Payload) => {
-  const { identifier, preferredChallengeKind } = payload;
-  return ['login-challenge', identifier, preferredChallengeKind];
+  const { authToken, preferredChallengeKind } = payload;
+  return ['login-challenge', authToken, preferredChallengeKind];
 };
 
-const requestFn = async ({
-  authToken,
-  identifier,
-  obConfigAuth,
-  preferredChallengeKind,
-  sandboxId,
-}: Payload) => {
-  const headers: Record<string, string> = { ...obConfigAuth };
-  if (sandboxId) {
-    headers[SANDBOX_ID_HEADER] = sandboxId;
-  }
-  if (authToken) {
-    headers[AUTH_HEADER] = authToken;
-  }
+const requestFn = async ({ authToken, preferredChallengeKind }: Payload) => {
+  const headers: Record<string, string> = { [AUTH_HEADER]: authToken };
 
   const response = await request<LoginChallengeResponse>({
     method: 'POST',
     url: '/hosted/identify/login_challenge',
     data: {
       preferredChallengeKind,
-      identifier,
     },
     headers,
   });
-  const { challengeData, error } = { ...response.data };
+  const { challengeData, ...restOfResponse } = { ...response.data };
   if (challengeData.scrubbedPhoneNumber) {
     challengeData.scrubbedPhoneNumber =
       challengeData.scrubbedPhoneNumber.replaceAll('*', '•');
@@ -61,19 +41,15 @@ const requestFn = async ({
 
   return {
     challengeData,
-    error,
+    ...restOfResponse,
   };
 };
 
-const useLoginChallenge = (basePayload: BasePayload) => {
+const useLoginChallenge = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: ({
-      isResend,
-      ...restOfPayload
-    }: Partial<BasePayload> & RestOfPayload) => {
-      const payload = { ...basePayload, ...restOfPayload };
+    mutationFn: ({ isResend, ...payload }: Payload) => {
       const queryKey = getQueryKeyForPayload(payload);
       const queryState =
         queryClient.getQueryState<LoginChallengeResponse>(queryKey);

frontend/packages/idv/src/components/identify/queries/use-signup-challenge.ts
@@ -3,27 +3,25 @@ import type {
   ObConfigAuth,
   SignupChallengeResponse,
 } from '@onefootprint/types';
-import { AUTH_HEADER, SANDBOX_ID_HEADER } from '@onefootprint/types';
+import { SANDBOX_ID_HEADER } from '@onefootprint/types';
+import type { IdentifyTokenScope } from '@onefootprint/types/src/api/identify-verify';
 import { useMutation } from '@tanstack/react-query';
 
 import type { EmailAndOrPhone } from '../types';
 import calculateRetryTime from './get-retry-time';
 
-type PayloadPartKey = 'obConfigAuth' | 'sandboxId' | 'authToken';
+type PayloadPartKey = 'obConfigAuth' | 'sandboxId' | 'scope';
 type Payload = EmailAndOrPhone & {
-  authToken?: string;
   obConfigAuth?: ObConfigAuth;
   sandboxId?: string;
+  scope: IdentifyTokenScope;
 };
 
 const requestFn = async (payload: Payload) => {
-  const { obConfigAuth, sandboxId, authToken, ...identifier } = payload;
+  const { obConfigAuth, sandboxId, ...identifier } = payload;
   const headers: Record<string, string> = { ...obConfigAuth };
   if (sandboxId) {
     headers[SANDBOX_ID_HEADER] = sandboxId;
-  }
-  if (authToken) {
-    headers[AUTH_HEADER] = authToken;
   }
   const response = await request<SignupChallengeResponse>({
     method: 'POST',

commit 2fcd7c5db043bd9170fe7735bed66b888cc09a0d

Fix recent bug in "sign in with email instead" flow (#8448)
## Describe your changes
This bug was introduced in #8430, where we started using the token from
`POST /hosted/identify` to initiate a login challenge. This broke the
KBA flow for "sign in with email instead" because the KBA flow issues a
different token that is needed to initiate the login challenge

## Caveats
<!--- Are there any caveats to this changeset? -->

## Test Plan
Tested locally. Will explore some ways to add automated coverage for
this so we don't introduce another regression...

frontend/packages/idv/src/components/identify/components/pin-verification/pin-verification.tsx
@@ -45,7 +45,7 @@ const PinVerification = ({
   const [state, send] = useIdentifyMachine();
   const {
     challenge: { challengeData: data },
-    identify: { email, phoneNumber, sandboxId, user },
+    identify: { email, identifyToken, phoneNumber, sandboxId },
     variant,
     obConfigAuth,
   } = state.context;
@@ -186,8 +186,8 @@ const PinVerification = ({
       return;
     }
 
-    if (user) {
-      initiatePhoneOrEmailLoginChallenge(user.token);
+    if (identifyToken) {
+      initiatePhoneOrEmailLoginChallenge(identifyToken);
     } else {
       initiateSignupChallenge();
     }

frontend/packages/idv/src/components/identify/state/machine.ts
@@ -433,6 +433,9 @@ const createIdentifyMachine = (args: IdentifyMachineArgs) =>
           const { email, phoneNumber, user, successfulIdentifier } =
             event.payload;
           context.identify.user = user;
+          if (user) {
+            context.identify.identifyToken = user.token;
+          }
           const isEmailChanged = email && context.identify.email !== email;
           const isPhoneChanged =
             phoneNumber && context.identify.phoneNumber !== phoneNumber;