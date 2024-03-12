68ad263625d6dfea391e84ff2e721e219363ea87
Bruno Batista <bruno@onefootprint.com>
2024-03-12 18:04:36 +0100

    docs(onAuth): update docs and examples (#8519)
    ## Describe your changes
    This PR updates the documentation regarding the new optional callback
    `onAuth`
    
    ---------
    
    Co-authored-by: Elliott Forde <elliott@onefootprint.com>

--- frontend/apps/demos/src/pages/components/all/components/verify-button-vanilla/verify-button-vanilla.tsx
+++ frontend/apps/demos/src/pages/components/all/components/verify-button-vanilla/verify-button-vanilla.tsx
@@ -11,7 +11,14 @@ const VerifyButtonVanilla = () => {
     // @ts-expect-error: footprintCallbacks is custom
     window.footprintCallbacks = {
       onComplete: (validationToken: string) => {
+        // TODO: User has finished the flow. This validation token can be used to see the fp_id of
+        // the user, the auth method they used to log in, and their KYC status
         console.log('complete ', validationToken);
+      },
+      onAuth: (validationToken: string) => {
+        // User has authenticated. This validation token can optionally be used to see the fp_id
+        // of the authenticated user and the auth method they used to log in
+        console.log(validationToken);
       },
       onClose: () => {
         console.log('close');
