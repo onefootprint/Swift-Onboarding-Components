68497c1aa355cd4a78a2ff5d6d3e31007ac3d4f4
Rafael Michels Motta <rafael@onefootprint.com>
2024-03-13 17:58:19 -0300

    [FP-7325] Investor profile: fix finra upload limit (#8557)
    ## Describe your changes
    
    We were not displaying any errors when uploading a file with size > 5
    MB. This PR fixes this issue.
    
    Demo:
    
    
    https://github.com/onefootprint/monorepo/assets/446731/07540a4e-a9b9-4eeb-87d8-50314e0d8fdb
    
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- frontend/packages/idv/src/plugins/investor-profile/pages/declarations/components/upload-compliance-letter/components/upload-files/upload-files.tsx
+++ frontend/packages/idv/src/plugins/investor-profile/pages/declarations/components/upload-compliance-letter/components/upload-files/upload-files.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Divider, Grid } from '@onefootprint/ui';
+import { IcoInfo16 } from '@onefootprint/icons';
+import { Box, Button, Divider, Grid, Stack, Text } from '@onefootprint/ui';
 import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
@@ -17,26 +18,33 @@ const UploadFiles = ({ onChange }: UploadFilesProps) => {
   });
   const inputRef = useRef<HTMLInputElement>(null);
   const [files, setFiles] = useState<File[]>([]);
+  const [showFileSizeError, setShowFileSizeError] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files?.length) {
       return;
     }
     const { files: uploads } = event.target;
+    const sizeLimit = 5 * 1024 * 1024; // 5MB
+    const isOverLimit = Array.from(uploads).some(file => file.size > sizeLimit);
+    if (isOverLimit) {
+      setShowFileSizeError(true);
+    } else {
+      // TODO: uncomment when we support multiple files
+      // Update the state with new files
+      // setFiles(current => {
+      //   // Filter out files with colluding names
+      //   const names = new Set(files.map(file => file.name));
+      //   const filtered = Array.from(uploads).filter(
+      //     file => !names.has(file.name),
+      //   );
+      //   return [...current, ...filtered];
+      // });
 
-    // TODO: uncomment when we support multiple files
-    // Update the state with new files
-    // setFiles(current => {
-    //   // Filter out files with colluding names
-    //   const names = new Set(files.map(file => file.name));
-    //   const filtered = Array.from(uploads).filter(
-    //     file => !names.has(file.name),
-    //   );
-    //   return [...current, ...filtered];
-    // });
-
-    // Only save the first file
-    setFiles([uploads[0]]);
+      // Only save the first file
+      setShowFileSizeError(false);
+      setFiles([uploads[0]]);
+    }
   };
 
   // TODO: uncomment when we support multiple files
@@ -56,56 +64,71 @@ const UploadFiles = ({ onChange }: UploadFilesProps) => {
   }, [files]);
 
   return (
-    <Box
-      backgroundColor="secondary"
-      padding={5}
-      borderRadius="default"
-      gap={5}
-      display="grid"
-    >
-      <Box>
-        <Button
-          fullWidth
-          onClick={() => {
-            inputRef.current?.click();
-          }}
-          variant="secondary"
-          size="large"
-        >
-          {/* TODO: uncomment when we support multiple files */}
-          {/* {files.length > 0 ? t('cta-multiple') : t('cta')} */}
-          {t('cta')}
-        </Button>
+    <Stack direction="column" gap={3}>
+      <Box
+        backgroundColor="secondary"
+        padding={5}
+        borderRadius="default"
+        gap={5}
+        display="grid"
+      >
+        <Box>
+          <Button
+            fullWidth
+            onClick={() => {
+              inputRef.current?.click();
+            }}
+            variant="secondary"
+            size="large"
+          >
+            {/* TODO: uncomment when we support multiple files */}
+            {/* {files.length > 0 ? t('cta-multiple') : t('cta')} */}
+            {t('cta')}
+          </Button>
+        </Box>
+        <HiddenInput
+          accept=".pdf"
+          // TODO: uncomment when we support multiple files
+          // multiple
+          type="file"
+          ref={inputRef}
+          data-testid="file-upload-input"
+          onChange={handleChange}
+        />
+        {files.length > 0 && (
+          <>
+            <Divider />
+            <Grid.Container gap={2}>
+              {files.map(file => (
+                <FileEntry
+                  // We enforce unique file names, so there won't be duplicate keys
+                  key={file.name}
+                  file={file}
+                  onRemove={() => {
+                    // TODO: uncomment when we support multiple files
+                    // removeFileAtIndex(index);
+                    setFiles([]);
+                    setShowFileSizeError(false);
+                  }}
+                />
+              ))}
+            </Grid.Container>
+          </>
+        )}
       </Box>
-      <HiddenInput
-        accept=".pdf"
-        // TODO: uncomment when we support multiple files
-        // multiple
-        type="file"
-        ref={inputRef}
-        data-testid="file-upload-input"
-        onChange={handleChange}
-      />
-      {files.length > 0 && (
-        <>
-          <Divider />
-          <Grid.Container gap={2}>
-            {files.map(file => (
-              <FileEntry
-                // We enforce unique file names, so there won't be duplicate keys
-                key={file.name}
-                file={file}
-                onRemove={() => {
-                  // TODO: uncomment when we support multiple files
-                  // removeFileAtIndex(index);
-                  setFiles([]);
-                }}
-              />
-            ))}
-          </Grid.Container>
-        </>
-      )}
-    </Box>
+      <Stack center>
+        {showFileSizeError ? (
+          <Text color="error" variant="caption-3">
+            {t('size-limit-error')}
+          </Text>
+        ) : (
+          <Text color="quaternary" variant="caption-3" display="flex" gap={2}>
+            <IcoInfo16 color="quaternary" />
+            {t('hint')}
+          </Text>
+        )}
+      </Stack>
+    </Stack>
   );
 };
 
