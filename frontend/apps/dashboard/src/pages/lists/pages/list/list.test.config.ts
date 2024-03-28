e5808e488e75e48233e7fb06947a2e89276e547b
Elliott Forde <elliott@onefootprint.com>
2024-03-28 09:56:06 -0700

    Fix lists test (#8852)
    ## Describe your changes
    https://onefootprint.slack.com/archives/C06ARQZARUG/p1711637205305269
    
    From lots of the build failures last night
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- frontend/apps/dashboard/src/pages/lists/pages/list/list.test.config.ts
+++ frontend/apps/dashboard/src/pages/lists/pages/list/list.test.config.ts
@@ -1,33 +1,39 @@
+import type { PaginatedRequestResponse } from '@onefootprint/request/src';
 import { mockRequest } from '@onefootprint/test-utils';
 import type { List } from '@onefootprint/types';
 import { ActorKind, ListKind } from '@onefootprint/types';
 
-export const listsFixture: List[] = [
-  {
-    id: '1',
-    actor: {
-      kind: ActorKind.footprint,
+export const listsFixture: PaginatedRequestResponse<List[]> = {
+  data: [
+    {
+      id: '1',
+      actor: {
+        kind: ActorKind.footprint,
+      },
+      alias: 'my_list',
+      createdAt: 'date',
+      kind: ListKind.emailAddress,
+      name: 'Email List',
+      entriesCount: 0,
+      playbooks: [],
     },
-    alias: 'my_list',
-    createdAt: 'date',
-    kind: ListKind.emailAddress,
-    name: 'Email List',
-    entriesCount: 0,
-    playbooks: [],
-  },
-  {
-    id: '2',
-    actor: {
-      kind: ActorKind.footprint,
+    {
+      id: '2',
+      actor: {
+        kind: ActorKind.footprint,
+      },
+      alias: 'my_list2',
+      createdAt: 'date',
+      kind: ListKind.ssn9,
+      name: 'SSN List',
+      entriesCount: 0,
+      playbooks: [],
     },
-    alias: 'my_list2',
-    createdAt: 'date',
-    kind: ListKind.ssn9,
-    name: 'SSN List',
-    entriesCount: 0,
-    playbooks: [],
+  ],
+  meta: {
+    count: 10,
   },
-];
+};
 
 export const withLists = () =>
   mockRequest({
