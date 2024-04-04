bf556cb705e76fe4140df041d096c7c33b6f725c
Keagan Long <keagan@onefootprint.com>
2024-04-03 19:35:28 -0700

    Remove Airplane stuff I accidently added back (#8972)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    Think I just did a bad rebase that re-introduced these 🙈 
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/internal_tools/airplane/.airplane-view/render.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-// A render function that works for React 18.
-export const render = (content) => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    content,
-    document.getElementById("root")
-  );
-};
