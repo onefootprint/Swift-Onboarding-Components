807ada74d5cf36ed80894a3343cc80f1555113b5
ahsan1578 <54286440+ahsan1578@users.noreply.github.com>
2024-04-12 17:05:26 -0700

    Don't show dupes for businesses (#9161)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    ^
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- frontend/apps/dashboard/src/components/entities/components/details/components/content/content.tsx
+++ frontend/apps/dashboard/src/components/entities/components/details/components/content/content.tsx
@@ -1,6 +1,8 @@
+import { EntityKind } from '@onefootprint/types';
 import { Box, Divider } from '@onefootprint/ui';
 import React from 'react';
 
+import { useEntityContext } from '../../hooks/use-entity-context';
 import {
   AuditTrail,
   Banner,
@@ -13,37 +15,43 @@ import {
 } from './components';
 import DuplicateData from './components/duplicate-data';
 
-const Content = () => (
-  <Box tag="section" testID="entity-content">
-    <Box marginBottom={7}>
-      <Banner />
+const Content = () => {
+  const { kind } = useEntityContext();
+
+  return (
+    <Box tag="section" testID="entity-content">
+      <Box marginBottom={7}>
+        <Banner />
+      </Box>
+      <Box marginBottom={7}>
+        <Breadcrumb />
+      </Box>
+      <Box>
+        <PinnedNotes />
+      </Box>
+      <Box marginBottom={5}>
+        <Header />
+      </Box>
+      <Box marginBottom={5}>
+        <Divider />
+      </Box>
+      <Box marginBottom={9}>
+        <Vault />
+      </Box>
+      <Box marginBottom={9}>
+        <AuditTrail />
+      </Box>
+      <Box marginBottom={9}>
+        <RiskSignals />
+      </Box>
+      {kind === EntityKind.person && (
+        <Box marginBottom={9}>
+          <DuplicateData />
+        </Box>
+      )}
+      <DeviceInsights />
     </Box>
-    <Box marginBottom={7}>
-      <Breadcrumb />
-    </Box>
-    <Box>
-      <PinnedNotes />
-    </Box>
-    <Box marginBottom={5}>
-      <Header />
-    </Box>
-    <Box marginBottom={5}>
-      <Divider />
-    </Box>
-    <Box marginBottom={9}>
-      <Vault />
-    </Box>
-    <Box marginBottom={9}>
-      <AuditTrail />
-    </Box>
-    <Box marginBottom={9}>
-      <RiskSignals />
-    </Box>
-    <Box marginBottom={9}>
-      <DuplicateData />
-    </Box>
-    <DeviceInsights />
-  </Box>
-);
+  );
+};
 
 export default Content;
