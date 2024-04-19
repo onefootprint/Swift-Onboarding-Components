7301e89e18f1c5513a1aa2efc20b779d2e6b69ca
evforde <21066341+evforde@users.noreply.github.com>
2024-04-19 19:44:09 +0000

    [api] Remove selfie_collected from document_uploaded timeline event (#9251)
    ## Describe your changes
    The frontend hasn't been reading this, safe to RM
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/api/core/src/serializers/identity_document.rs
+++ backend/components/api/core/src/serializers/identity_document.rs
@@ -5,7 +5,6 @@ use crate::utils::db2api::DbToApi;
 
 impl DbToApi<(IdentityDocument, DocumentRequest)> for api_wire_types::DocumentUploadedTimelineEvent {
     fn from_db((identity_doc, document_request): (IdentityDocument, DocumentRequest)) -> Self {
-        let skip_selfie = identity_doc.should_skip_selfie();
         let IdentityDocument {
             document_type,
             status,
@@ -13,12 +12,9 @@ impl DbToApi<(IdentityDocument, DocumentRequest)> for api_wire_types::DocumentUp
             ..
         } = identity_doc;
 
-        let selfie_collected = document_request.should_collect_selfie() && !skip_selfie;
-
         Self {
             status,
             document_type,
-            selfie_collected,
             device_type: device_type.unwrap_or(DocumentScanDeviceType::Mobile),
             config: document_request.config,
         }
--- backend/components/api/wire_types/src/resources/identity_document_timeline_event.rs
+++ backend/components/api/wire_types/src/resources/identity_document_timeline_event.rs
@@ -5,8 +5,6 @@ use newtypes::{DocumentRequestConfig, DocumentScanDeviceType, IdDocKind, Identit
 pub struct DocumentUploadedTimelineEvent {
     pub status: IdentityDocumentStatus,
     pub document_type: IdDocKind,
-    // TODO rm
-    pub selfie_collected: bool,
     pub device_type: DocumentScanDeviceType,
     pub config: DocumentRequestConfig,
 }
