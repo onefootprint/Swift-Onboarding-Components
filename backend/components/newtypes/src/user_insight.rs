9adab203f4c8eab560f7dc29069524c5508e2094
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2024-04-23 10:40:10 -0400

    Snake case for user insight scope (#9288)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/newtypes/src/user_insight.rs
+++ backend/components/newtypes/src/user_insight.rs
@@ -1,7 +1,11 @@
 use paperclip::actix::Apiv2Schema;
 use serde::Serialize;
+use strum::Display;
+use strum_macros::EnumString;
 
-#[derive(Clone, Serialize, Debug, Apiv2Schema)]
+
+#[derive(Clone, Serialize, EnumString, Display, Debug, Apiv2Schema)]
+#[strum(serialize_all = "snake_case")]
 pub enum UserInsightScope {
     Behavior,
     Device,
