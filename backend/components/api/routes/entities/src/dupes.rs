e4a864c84d88cb210c7495ff6e8a7df4850f5290
dave-argoff <105253942+dave-argoff@users.noreply.github.com>
2024-04-26 14:08:05 +0000

    Load dupes in dupe endpoint for neuro (#9328)
    ## Describe your changes
    thread through the previous PR to the dupes endpoint
    
    Still needs FE to render DeviceID and CookieID in the dash, but is behind a flag now
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/api/routes/entities/src/dupes.rs
+++ backend/components/api/routes/entities/src/dupes.rs
@@ -10,15 +10,18 @@ use crate::{
     State,
 };
 use api_core::{
-    errors::{ApiResult, AssertionError},
+    decision::vendor::neuro_id::tenant_can_view_neuro,
+    errors::ApiResult,
     utils::{
         db2api::DbToApi,
         fp_id_path::FpIdPath,
         vault_wrapper::{TenantVw, VaultWrapper},
     },
 };
-use db::models::{fingerprint::Fingerprint, scoped_vault::ScopedVault};
-use itertools::Itertools;
+use db::models::{
+    fingerprint::Fingerprint, neuro_id_analytics_event::NeuroIdAnalyticsEvent, scoped_vault::ScopedVault,
+};
+use itertools::{chain, Itertools};
 use newtypes::{DupeKind, ScopedVaultId};
 use paperclip::actix::{api_v2_operation, get, web};
 
@@ -36,19 +39,28 @@ pub async fn get_dupes(
     let tenant_id = auth.tenant().id.clone();
     let is_live = auth.is_live()?;
     let fp_id = request.into_inner();
+    let can_view_neuro = tenant_can_view_neuro(&state, &tenant_id);
 
-    let (dupes, vws) = state
+    let (dupes, vws, neuro_dupes) = state
         .db_pool
         .db_query(move |conn| -> ApiResult<_> {
             let sv = ScopedVault::get(conn, (&fp_id, &tenant_id, is_live))?;
-
+            // Load the dupes
             let dupes = Fingerprint::get_dupes(conn, &sv)?;
-            let sv_ids = dupes.internal.iter().map(|fp| &fp.scoped_vault_id).collect_vec();
+            let neuro_dupes = NeuroIdAnalyticsEvent::get_dupes_for_tenant(conn, &sv)?;
+
+            // Get VWs for all the dupes
+            let sv_ids = chain(
+                dupes.internal.iter().map(|fp| &fp.scoped_vault_id),
+                neuro_dupes.internal.iter().map(|(_, sv_id)| sv_id),
+            )
+            .collect_vec();
             let vaults = ScopedVault::bulk_get(conn, sv_ids, &tenant_id, is_live)?;
             let vws: HashMap<ScopedVaultId, TenantVw> =
                 VaultWrapper::multi_get_for_tenant(conn, vaults, None)?;
 
-            Ok((dupes, vws))
+
+            Ok((dupes, vws, neuro_dupes))
         })
         .await?;
 
@@ -59,28 +71,39 @@ pub async fn get_dupes(
         .into_iter()
         .map(|fp| (fp.scoped_vault_id, fp.kind))
         .into_group_map();
+    let mut sv_id_to_dupe_neuro = neuro_dupes
+        .internal
+        .into_iter()
+        .map(|(kind, sv)| (sv, kind))
+        .into_group_map();
 
     let same_tenant = vws
         .into_iter()
         .map(|(sv_id, vw)| {
             let decrypted_data = decrypted_results.remove(&sv_id).unwrap_or_default();
-            let duplicate_dis = sv_id_to_dupe_fps
-                .remove(&sv_id)
-                .ok_or(AssertionError("VW not found"))?;
-            let duplicate_kinds = duplicate_dis
-                .into_iter()
-                .map(DupeKind::try_from)
-                .collect::<newtypes::NtResult<Vec<_>>>()?
-                // TODO: filter out neuro here based on flag
-                .into_iter()
-                .unique()
-                .collect_vec();
+            let duplicate_dis = sv_id_to_dupe_fps.remove(&sv_id).unwrap_or_default();
+            let duplicate_neuro_kinds = if can_view_neuro {
+                sv_id_to_dupe_neuro.remove(&sv_id).unwrap_or_default()
+            } else {
+                vec![]
+            };
+
+            let duplicate_kinds = chain(
+                duplicate_dis
+                    .into_iter()
+                    .map(DupeKind::try_from)
+                    .collect::<newtypes::NtResult<Vec<_>>>()?,
+                duplicate_neuro_kinds,
+            )
+            .unique()
+            .collect_vec();
             Ok((duplicate_kinds, vw, decrypted_data))
         })
         .collect::<ApiResult<Vec<_>>>()?
         .into_iter()
         .map(|(d, vw, data)| api_wire_types::SameTenantDupe::from_db((d, vw, &auth, data)))
         .collect();
+
     let other_tenant = dupes.external.map(|d| api_wire_types::OtherTenantDupes {
         num_matches: d.num_users,
         num_tenants: d.num_tenants,
