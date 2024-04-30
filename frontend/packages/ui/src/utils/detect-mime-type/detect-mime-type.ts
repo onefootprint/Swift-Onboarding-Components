99511336dadaf5ac30bb11fdf1fc09a33a926b0d
Ethan Lowman <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-04-30 17:12:08 -0400

    Add support for rendering HEIC images with object URLs (#9377)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    This won't work in Chrome, but it does work in Safari.
    
    Chrome:
    <img width="517" alt="image"
    src="https://github.com/onefootprint/monorepo/assets/155459438/41c6b069-36b3-49f0-86b4-3e4dcf0eb0e7">
    
    
    Safari:
    <img width="514" alt="image"
    src="https://github.com/onefootprint/monorepo/assets/155459438/7213a53f-daa9-4deb-aed7-2466041dd9a5">
    
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- frontend/packages/ui/src/utils/detect-mime-type/detect-mime-type.ts
+++ frontend/packages/ui/src/utils/detect-mime-type/detect-mime-type.ts
@@ -1,18 +1,36 @@
 import type { Buffer } from 'buffer';
 
 const magicNumbers = {
-  'application/pdf': [0x25, 0x50, 0x44, 0x46],
-  'image/jpeg': [0xff, 0xd8, 0xff, 0xe0],
-  'image/png': [0x89, 0x50, 0x4e, 0x47],
-  'image/gif': [0x47, 0x49, 0x46, 0x38],
+  'application/pdf': {
+    signature: [0x25, 0x50, 0x44, 0x46],
+    offset: 0,
+  },
+  'image/jpeg': {
+    signature: [0xff, 0xd8, 0xff, 0xe0],
+    offset: 0,
+  },
+  'image/png': {
+    signature: [0x89, 0x50, 0x4e, 0x47],
+    offset: 0,
+  },
+  'image/gif': {
+    signature: [0x47, 0x49, 0x46, 0x38],
+    offset: 0,
+  },
+  'image/heic': {
+    signature: [0x66, 0x74, 0x79, 0x70, 0x68, 0x65, 0x69, 0x63],
+    offset: 4,
+  },
 };
 
 const detectMimeType = (bytes: Buffer): string => {
-  for (const [mimeType, magicNumber] of Object.entries(magicNumbers)) {
+  for (const [mimeType, { signature, offset }] of Object.entries(
+    magicNumbers,
+  )) {
     if (
       bytes
-        .subarray(0, magicNumber.length)
-        .every((byte, i) => byte === magicNumber[i])
+        .subarray(offset, offset + signature.length)
+        .every((byte, i) => byte === signature[i])
     ) {
       return mimeType;
     }
