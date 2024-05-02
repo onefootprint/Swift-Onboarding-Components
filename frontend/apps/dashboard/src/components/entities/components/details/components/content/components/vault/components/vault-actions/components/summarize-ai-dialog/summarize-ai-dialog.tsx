d47186bca75d00c5ecef57832506738888bc62c8
Claudio Angrigiani <angrigianicr@gmail.com>
2024-05-02 16:20:48 -0400

    Improvements to ai dialog (#9426)

--- frontend/apps/dashboard/src/components/entities/components/details/components/content/components/vault/components/vault-actions/components/summarize-ai-dialog/summarize-ai-dialog.tsx
+++ frontend/apps/dashboard/src/components/entities/components/details/components/content/components/vault/components/vault-actions/components/summarize-ai-dialog/summarize-ai-dialog.tsx
@@ -1,16 +1,11 @@
 import { useRequestErrorToast } from '@onefootprint/hooks';
 import { IcoWand24 } from '@onefootprint/icons';
-import {
-  Banner,
-  Dialog,
-  Divider,
-  Shimmer,
-  Stack,
-  Text,
-  Tooltip,
-} from '@onefootprint/ui';
+import { AnimatedLoadingSpinner, Dialog, Stack, Text } from '@onefootprint/ui';
+import { AnimatePresence, motion } from 'framer-motion';
+import type { ParseKeys } from 'i18next';
 import React, { useCallback, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import styled, { css } from 'styled-components';
 
 import useEntityId from '@/entity/hooks/use-entity-id';
 
@@ -62,54 +57,104 @@ const SummarizeAiDialogue = ({ open, onClose }: SummarizeAiDialogueProps) => {
     showRequestErrorToast,
   ]);
 
+  const summaryContent = [
+    {
+      title: 'summary',
+      content: generateSummaryMutation.data?.highLevelSummary,
+    },
+    {
+      title: 'details',
+      content: generateSummaryMutation.data?.detailedSummary,
+    },
+    {
+      title: 'risk-signal-analysis',
+      content: generateSummaryMutation.data?.riskSignalSummary,
+    },
+    {
+      title: 'conclusion',
+      content: generateSummaryMutation.data?.conclusion,
+    },
+  ];
+
+  const containerVariants = {
+    hidden: { opacity: 0, filter: 'blur(5px)' },
+    visible: { opacity: 1, filter: 'blur(0px)', transition: { duration: 0.5 } },
+  };
+
   return (
-    <Dialog size="large" title={t('label')} onClose={handleClose} open={open}>
-      <Stack gap={3} direction="column">
-        {generateSummaryMutation.isLoading ? (
-          <Stack gap={4} direction="column">
-            <Shimmer sx={{ width: '100%', height: '500px' }} />
-
-            <Banner variant="info">
-              <Stack gap={2} direction="row">
-                <Tooltip alignment="start" position="top" text={t('title')}>
-                  <IcoWand24 />
-                </Tooltip>
-                <Text variant="body-4">{t('detail')}</Text>
-              </Stack>
-            </Banner>
-          </Stack>
-        ) : (
-          generateSummaryMutation.data && (
-            <>
-              <Text variant="heading-3">Summary</Text>
-              <Divider />
-              <Text variant="body-3">
-                {generateSummaryMutation.data.highLevelSummary}
-              </Text>
-
-              <Text variant="heading-3">Details</Text>
-              <Divider />
-              <Text variant="body-3">
-                {generateSummaryMutation.data.detailedSummary}
-              </Text>
-
-              <Text variant="heading-3">Risk Signal Analysis</Text>
-              <Divider />
-              <Text variant="body-3">
-                {generateSummaryMutation.data.riskSignalSummary}
-              </Text>
-
-              <Text variant="heading-3">Conclusion</Text>
-              <Divider />
-              <Text variant="body-3">
-                {generateSummaryMutation.data.conclusion}
-              </Text>
-            </>
-          )
-        )}
+    <Dialog size="default" title={t('label')} onClose={handleClose} open={open}>
+      <Stack gap={3} direction="column" paddingBottom={4}>
+        <AnimatePresence>
+          {generateSummaryMutation.isLoading ? (
+            <EmptyState
+              gap={5}
+              direction="column"
+              width="100%"
+              height="420px"
+              align="center"
+              justify="center"
+            >
+              <AnimatedLoadingSpinner animationStart />
+              <Text variant="label-3">{t('generating-summary')}</Text>
+            </EmptyState>
+          ) : (
+            generateSummaryMutation.data && (
+              <ResponseContainer
+                initial="hidden"
+                animate="visible"
+                variants={containerVariants}
+                direction="column"
+                gap={7}
+              >
+                {summaryContent.map(({ title, content }) => (
+                  <Section direction="column" gap={2}>
+                    <Text variant="label-2" color="secondary">
+                      {t(title as unknown as ParseKeys<'common'>)}
+                    </Text>
+                    <Text variant="body-3">{content}</Text>
+                  </Section>
+                ))}
+              </ResponseContainer>
+            )
+          )}
+          <InlineMessage role="alert" gap={4}>
+            <Stack flexShrink={0}>
+              <IcoWand24 color="info" />
+            </Stack>
+            <Stack direction="column" alignItems="flex-start">
+              <Text variant="body-4">{t('detail')}</Text>
+            </Stack>
+          </InlineMessage>
+        </AnimatePresence>
       </Stack>
     </Dialog>
   );
 };
+
+const EmptyState = styled(Stack)`
+  ${({ theme }) => css`
+    border-radius: ${theme.borderRadius.default};
+    overflow: hidden;
+    border: ${theme.borderWidth[1]} dashed ${theme.borderColor.tertiary};
+  `}
+`;
+
+const InlineMessage = styled(Stack)`
+  ${({ theme }) => css`
+    padding: ${theme.spacing[4]};
+    background-color: ${theme.backgroundColor.info};
+    border-radius: ${theme.borderRadius.default};
+    overflow: hidden;
+    color: ${theme.color.info};
+  `}
+`;
+
+const ResponseContainer = styled(motion(Stack))`
+  ${({ theme }) => css`
+    padding-bottom: ${theme.spacing[5]};
+  `}
+`;
+
+const Section = styled(motion(Stack))``;
 
 export default SummarizeAiDialogue;
