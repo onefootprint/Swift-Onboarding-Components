71d3d136f0951c78c5b27727e907320e2a16159e
Elliott Forde <elliott@onefootprint.com>
2024-05-03 16:26:18 -0700

    Update backfill_user_timeline_seqno/up.sql to better handle local environments

--- backend/components/db/schema/migrations/2024-05-03-040225_backfill_user_timeline_seqno/up.sql
+++ backend/components/db/schema/migrations/2024-05-03-040225_backfill_user_timeline_seqno/up.sql
@@ -1,13 +1,13 @@
--- CREATE INDEX CONCURRENTLY user_timeline_for_backfill ON user_timeline(timestamp, id) WHERE seqno IS NULL;
--- CREATE INDEX CONCURRENTLY data_lifetime_for_backfill ON data_lifetime(created_at, created_seqno);
+-- I've made some modifications to this backfill after it's been run in dev and prod to better support folks' local environments
+CREATE INDEX CONCURRENTLY user_timeline_for_backfill ON user_timeline(timestamp, id) WHERE seqno IS NULL;
+CREATE INDEX CONCURRENTLY data_lifetime_for_backfill ON data_lifetime(created_at, created_seqno);
 
--- TODO(eforde) run in batches before merging
+-- Lookahead of 1 min to backfill seqnos
 WITH user_timelines_to_update AS (
     SELECT id, timestamp
     FROM user_timeline
     WHERE seqno IS NULL
     ORDER BY timestamp DESC
-    LIMIT 10000
 ),
 seqnos_to_set AS (
     -- Grab the DataLifetime that was created most recently after this timeline event and use its seqno - 1.
@@ -21,11 +21,41 @@ seqnos_to_set AS (
     INNER JOIN data_lifetime dl ON dl.created_at >= ut.timestamp AND dl.created_at < ut.timestamp + INTERVAL '1 minutes'
     ORDER BY ut.id, dl.created_at ASC
 )
--- SELECT * from seqnos_to_set ORDER BY seqno;
 UPDATE user_timeline
--- If we couldn't find a seqno within a minute, put a non-null but obviously incorrect value.
--- e'll come back and fix these later with a bigger lookahead window - but will do small window first.
-SET seqno = coalesce(s.seqno, -10)
+SET seqno = s.seqno
 FROM user_timelines_to_update ut
-LEFT JOIN seqnos_to_set s ON s.id = ut.id
+INNER JOIN seqnos_to_set s ON s.id = ut.id
 WHERE user_timeline.id = ut.id;
+
+
+-- Then do a slower lookahead of 1 day to backfill any stragglers
+WITH user_timelines_to_update AS (
+    SELECT id, timestamp
+    FROM user_timeline
+    WHERE seqno IS NULL
+    ORDER BY timestamp DESC
+),
+seqnos_to_set AS (
+    -- Grab the DataLifetime that was created most recently after this timeline event and use its seqno - 1.
+    -- This was the seqno at the time that the timeline event was created.
+    SELECT DISTINCT ON(ut.id)
+        ut.id,
+        dl.created_seqno - 1 as seqno,
+        ut.timestamp,
+        dl.created_at
+    FROM user_timelines_to_update ut
+    INNER JOIN data_lifetime dl ON dl.created_at >= ut.timestamp AND dl.created_at < ut.timestamp + INTERVAL '1 day'
+    ORDER BY ut.id, dl.created_at ASC
+)
+UPDATE user_timeline
+SET seqno = s.seqno
+FROM user_timelines_to_update ut
+INNER JOIN seqnos_to_set s ON s.id = ut.id
+WHERE user_timeline.id = ut.id;
+
+-- Catch all for local environments since it doesn't matter so much
+UPDATE user_timeline SET seqno = 0 WHERE seqno IS NULL;
+
+
+DROP INDEX CONCURRENTLY user_timeline_for_backfill;
+DROP INDEX CONCURRENTLY data_lifetime_for_backfill;
