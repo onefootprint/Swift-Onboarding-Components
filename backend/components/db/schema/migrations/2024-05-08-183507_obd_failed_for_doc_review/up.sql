306f070876ec1bbc29424bc0e808b78a1cd4b869
Elliott Forde <elliott@onefootprint.com>
2024-05-09 16:03:45 -0700

    [2/2] Remove default value for obd.failed_for_doc_review (#9518)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/db/schema/migrations/2024-05-08-183507_obd_failed_for_doc_review/up.sql
+++ backend/components/db/schema/migrations/2024-05-08-183507_obd_failed_for_doc_review/up.sql
@@ -1,2 +1,1 @@
--- TODO remove default
 ALTER TABLE onboarding_decision ADD COLUMN failed_for_doc_review BOOLEAN NOT NULL DEFAULT 'f';
---
+++ backend/components/db/schema/migrations/2024-05-08-184853_drop_default_obd_failed_for_doc_review/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE onboarding_decision ALTER COLUMN failed_for_doc_review SET DEFAULT 'f';
