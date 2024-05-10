34ef2f19a2f2b30ec3bc83807a92289975057ab7
ethan-lowman-fp <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-05-10 15:17:10 +0000

    Remove card expiration migration endpoint (#9551)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical details and possible side effects. -->
    All done!
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/api/core/src/migrations/mod.rs
+++ backend/components/api/core/src/migrations/mod.rs
@@ -3,4 +3,4 @@
 // pub mod m112223_backfill_portable_data;
 // #[cfg(test)]
 // mod test_m112223_backfill_portable_data;
-pub mod m050624_fix_card_expiration_year;
+// pub mod m050624_fix_card_expiration_year;
