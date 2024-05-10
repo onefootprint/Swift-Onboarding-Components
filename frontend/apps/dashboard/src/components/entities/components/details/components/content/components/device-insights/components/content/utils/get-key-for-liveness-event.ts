b682470ec576599639739918ff3fdcb52092d4ba
Belce Dogru <belce.dogru@gmail.com>
2024-05-10 13:25:19 -0700

    [Device insights] Style fixes (#9566)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    <img width="572" alt="Screenshot 2024-05-10 at 11 27 17 AM"
    src="https://github.com/onefootprint/monorepo/assets/18564210/c5c9681c-efbc-4983-a596-7ae7eb2ce8fd">
    <img width="594" alt="Screenshot 2024-05-10 at 11 27 14 AM"
    src="https://github.com/onefootprint/monorepo/assets/18564210/cc5e9281-0f28-414a-8f89-901fd06ddc87">
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- frontend/apps/dashboard/src/components/entities/components/details/components/content/components/device-insights/components/content/utils/get-key-for-liveness-event.ts
+++ frontend/apps/dashboard/src/components/entities/components/details/components/content/components/device-insights/components/content/utils/get-key-for-liveness-event.ts
@@ -1,6 +1,6 @@
 import type { Liveness } from '@onefootprint/types';
 
 const getKeyForLiveness = (liveness: Liveness) =>
-  `${liveness.insight.timestamp}-${liveness.insight.latitude}-${liveness.insight.longitude}`;
+  `${liveness.insight.timestamp}-${liveness.insight.latitude}-${liveness.insight.longitude}-${liveness.kind}-${liveness.scope}`;
 
 export default getKeyForLiveness;
