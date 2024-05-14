7973958f31d9cb83d3853058f2415ea8a9ee96e6
evforde <21066341+evforde@users.noreply.github.com>
2024-05-14 19:51:28 +0000

    Reorganize db_types mod.rs to avoid future merge conflicts (#9619)
    ## Describe your changes
    Two changes adding a new db type would always conflict with the way this file was organized before... i've had to resolve one too many merge conflicts 😛
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/newtypes/src/db_types/mod.rs
+++ backend/components/newtypes/src/db_types/mod.rs
@@ -1,80 +1,128 @@
 mod access_event_kind;
+pub use access_event_kind::*;
 mod access_event_purpose;
+pub use access_event_purpose::*;
 mod actor;
+pub use actor::*;
 mod android_verdict_types;
+pub use android_verdict_types::*;
 mod apple_attestation;
+pub use apple_attestation::*;
 mod attestation_type;
+pub use attestation_type::*;
 mod audit_event;
+pub use audit_event::*;
 mod auth_event_kind;
+pub use auth_event_kind::*;
 mod billing_event_kind;
+pub use billing_event_kind::*;
 mod business_owner_kind;
+pub use business_owner_kind::*;
 mod company_size;
+pub use company_size::*;
 mod compliance_doc;
+pub use compliance_doc::*;
 mod compliance_status;
+pub use compliance_status::*;
 mod contact_info_priority;
+pub use contact_info_priority::*;
 mod d2p_session_status;
+pub use d2p_session_status::*;
 mod data_lifetime_source;
+pub use data_lifetime_source::*;
 mod decision_intent_kind;
+pub use decision_intent_kind::*;
 mod decision_status;
+pub use decision_status::*;
 mod document_request;
+pub use document_request::*;
 mod document_scan_device_type;
+pub use document_scan_device_type::*;
 mod document_side;
+pub use document_side::*;
 mod dupes;
+pub use dupes::*;
 mod fingerprint;
+pub use fingerprint::*;
 mod fingerprint_meta;
+pub use fingerprint_meta::*;
 mod footprint_reason_code;
+pub use footprint_reason_code::*;
 mod identify_scope;
+pub use identify_scope::*;
 mod identity_document_fixture_result;
+pub use identity_document_fixture_result::*;
 mod identity_document_status;
+pub use identity_document_status::*;
 mod incode;
+pub use incode::*;
 mod insight;
+pub use insight::*;
 mod label;
+pub use label::*;
 mod list;
+pub use list::*;
 mod liveness_source;
+pub use liveness_source::*;
 mod manual_review;
+pub use manual_review::*;
 mod middesk_request_state;
+pub use middesk_request_state::*;
 mod ob_config;
+pub use ob_config::*;
 mod onboarding_status;
+pub use onboarding_status::*;
 mod org_member_email;
+pub use org_member_email::*;
 mod preview_api;
+pub use preview_api::*;
 mod proxy_ingress_kind;
+pub use proxy_ingress_kind::*;
 mod risk_signal_group;
+pub use risk_signal_group::*;
 mod rule_expression;
+pub use rule_expression::*;
 mod rules;
+pub use rules::*;
 mod sealed_bytes;
+pub use sealed_bytes::*;
 mod session_kind;
+pub use session_kind::*;
 mod supported_document_and_country_mapping;
+pub use supported_document_and_country_mapping::*;
 mod task_status;
+pub use task_status::*;
 mod tenant_frequent_notes;
+pub use tenant_frequent_notes::*;
 mod tenant_kind;
+pub use tenant_kind::*;
 mod tenant_role_kind;
+pub use tenant_role_kind::*;
 mod tenant_scope;
+pub use tenant_scope::*;
 mod user_timeline_event;
+pub use user_timeline_event::*;
 mod vault_data_format;
+pub use vault_data_format::*;
 mod vault_key_pair;
+pub use vault_key_pair::*;
 mod vault_kind;
+pub use vault_kind::*;
 mod vault_proxy_permission;
+pub use vault_proxy_permission::*;
 mod vendor;
+pub use vendor::*;
 mod watchlist_check;
+pub use watchlist_check::*;
 mod waterfall;
+pub use waterfall::*;
 mod workflow;
+pub use workflow::*;
 mod workflow_fixture_result;
+pub use workflow_fixture_result::*;
 mod workflow_request;
+pub use workflow_request::*;
 mod workflow_source;
+pub use workflow_source::*;
 mod workos_auth_method;
-pub use self::{
-    access_event_kind::*, access_event_purpose::*, actor::*, android_verdict_types::*, apple_attestation::*,
-    attestation_type::*, audit_event::*, auth_event_kind::*, billing_event_kind::*, business_owner_kind::*,
-    company_size::*, compliance_doc::*, compliance_status::*, contact_info_priority::*,
-    d2p_session_status::*, data_lifetime_source::*, decision_intent_kind::*, decision_status::*,
-    document_request::*, document_scan_device_type::*, document_side::*, dupes::*, fingerprint::*,
-    fingerprint_meta::*, footprint_reason_code::*, identify_scope::*, identity_document_fixture_result::*,
-    identity_document_status::*, incode::*, insight::*, label::*, list::*, liveness_source::*,
-    manual_review::*, middesk_request_state::*, ob_config::*, onboarding_status::*, org_member_email::*,
-    preview_api::*, proxy_ingress_kind::*, risk_signal_group::*, rule_expression::*, rules::*,
-    sealed_bytes::*, session_kind::*, supported_document_and_country_mapping::*, task_status::*,
-    tenant_frequent_notes::*, tenant_kind::*, tenant_role_kind::*, tenant_scope::*, user_timeline_event::*,
-    vault_data_format::*, vault_key_pair::*, vault_kind::*, vault_proxy_permission::*, vendor::*,
-    watchlist_check::*, waterfall::*, workflow::*, workflow_fixture_result::*, workflow_request::*,
-    workflow_source::*, workos_auth_method::*,
-};
+pub use workos_auth_method::*;
