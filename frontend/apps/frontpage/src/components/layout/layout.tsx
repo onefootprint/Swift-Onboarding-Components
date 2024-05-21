49c8123610c8292dbd38a2fadf909453835a7a80
Claudio Angrigiani <angrigianicr@gmail.com>
2024-05-21 09:09:54 -0400

    Bring banner back (#9710)
    When we release the announcement, this should link to our blog post

--- frontend/apps/frontpage/src/components/layout/layout.tsx
+++ frontend/apps/frontpage/src/components/layout/layout.tsx
@@ -1,52 +1,63 @@
 import { media } from '@onefootprint/ui';
-// import { getCookie, setCookie } from 'cookies-next';
-// import { useRouter } from 'next/router';
-import React from 'react';
-import styled from 'styled-components';
+import { setCookie } from 'cookies-next';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 
 import Footer from '../footer';
 import Navbar from '../navbar';
-// import CaseStudyBanner from './message-banner';
+import CaseStudyBanner from './message-banner';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
-// const ARTICLE_URL = '/';
+const ARTICLE_URL = '/blog/footprint-13m-series-a-led-by-qed';
 
-const Layout = ({ children }: LayoutProps) => (
-  // const router = useRouter();
-  // const isArticlePage = router.pathname.includes(ARTICLE_URL);
+const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter();
+  const isArticlePage = router.pathname.includes(ARTICLE_URL);
+  // const isCookieSet = getCookie('should-show-case-study-banner') !== 'false';
 
-  // const [isBannerVisible, setIsBannerVisible] = useState(
-  //   getCookie('should-show-case-study-banner') !== 'false' && !isArticlePage,
-  // );
+  const [isBannerVisible, setIsBannerVisible] = useState<boolean>(
+    !isArticlePage,
+  );
 
-  // const handleCloseBanner = () => {
-  //   setCookie('should-show-case-study-banner', 'false');
-  //   setIsBannerVisible(false);
-  // };
+  const handleCloseBanner = () => {
+    setCookie('should-show-case-study-banner', 'false');
+    setIsBannerVisible(false);
+  };
 
-  <>
-    {/* {isBannerVisible && (
+  return (
+    <>
+      {isBannerVisible && (
         <CaseStudyBanner
           onClose={handleCloseBanner}
           articleUrl={ARTICLE_URL}
-        >
-          Footprint raised $13M Series A led by QED Investors
-        </CaseStudyBanner>
-      )} */}
-    <Navbar />
-    <Content>{children}</Content>
-    <Footer />
-  </>
-);
-const Content = styled.div`
-  position: relative;
-  padding-top: calc(var(--mobile-header-height) + var(--mobile-spacing));
+          text="Footprint raised $13M Series A led by QED Investors"
+        />
+      )}
+      <Navbar />
+      <Content $shouldShowBanner={isBannerVisible}>{children}</Content>
+      <Footer />
+    </>
+  );
+};
 
-  ${media.greaterThan('lg')`
-    padding-top: calc(var(--desktop-header-height) + var(--desktop-spacing));
+const Content = styled.div<{ $shouldShowBanner: boolean }>`
+  ${({ $shouldShowBanner }) => css`
+    position: relative;
+    padding-top: ${$shouldShowBanner
+      ? 'calc(var(--mobile-header-height) + var(--mobile-spacing) + 120px)'
+      : 'calc(var(--mobile-header-height) + var(--mobile-spacing))'};
+
+    ${media.greaterThan('lg')`
+      padding-top: ${
+        $shouldShowBanner
+          ? 'calc(var(--desktop-header-height) + var(--desktop-spacing) + 56px)'
+          : 'calc(var(--desktop-header-height) + var(--desktop-spacing))'
+      };
+    `}
   `}
 `;
 
