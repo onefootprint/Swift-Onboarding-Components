651f05563edb4257bd13ee0acb45000fe1216679
Claudio Angrigiani <angrigianicr@gmail.com>
2024-05-22 12:37:44 -0400

    Fix org selector (#9747)

--- frontend/apps/partners/src/app/[lang]/auth/organizations/content.tsx
+++ frontend/apps/partners/src/app/[lang]/auth/organizations/content.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { ThemedLogoFpDefault } from '@onefootprint/icons';
-import { createFontStyles, Text, Tooltip, useToast } from '@onefootprint/ui';
+import {
+  createFontStyles,
+  Stack,
+  Text,
+  Tooltip,
+  useToast,
+} from '@onefootprint/ui';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -55,116 +61,85 @@ const OrganizationsPageContent = ({
 
   return (
     <Container>
-      <Inner>
-        <ThemedLogoFpDefault />
-        <Text variant="label-1" color="primary">
-          {t('select-an-organization')}
-        </Text>
-        <ButtonGroup>
-          {orgs.map(org => (
-            <Tooltip
-              key={org.id}
-              text={t('disabled-org-selection', { org: org.name })}
-              position="right"
-              disabled={org.isAuthMethodSupported}
+      <ThemedLogoFpDefault />
+      <Text variant="label-1" color="primary">
+        {t('select-an-organization')}
+      </Text>
+      <ButtonGroup>
+        {orgs.map(org => (
+          <Tooltip
+            key={org.id}
+            text={t('disabled-org-selection', { org: org.name })}
+            position="right"
+            disabled={org.isAuthMethodSupported}
+          >
+            <EllipsisButton
+              disabled={!org.isAuthMethodSupported}
+              onClick={handleClick(org.id)}
+              type="button"
             >
-              <EllipsisButton
-                disabled={!org.isAuthMethodSupported}
-                onClick={handleClick(org.id)}
-                type="button"
-              >
-                {org.name}
-              </EllipsisButton>
-            </Tooltip>
-          ))}
-        </ButtonGroup>
-      </Inner>
+              {org.name}
+            </EllipsisButton>
+          </Tooltip>
+        ))}
+      </ButtonGroup>
     </Container>
   );
 };
 
-const ButtonGroup = styled.div<{ isLoading?: boolean }>`
-  ${({ theme, isLoading = false }) => css`
+const Container = styled(Stack)`
+  ${({ theme }) => css`
+    height: 100vh;
     display: flex;
     flex-direction: column;
-    width: 100%;
+    align-items: center;
+    justify-content: center;
+    width: 320px;
+    max-width: 95%;
+    text-align: center;
+    gap: ${theme.spacing[7]};
+  `}
+`;
 
-    span {
-      height: 46px;
-      margin: 0;
+const ButtonGroup = styled.div<{ isLoading?: boolean }>`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    border-radius: ${theme.borderRadius.default};
+    overflow: hidden;
+    width: 100%;
+    border: ${theme.borderWidth[1]} solid ${theme.borderColor.tertiary};
+
+    & > div {
       width: 100%;
-      padding: 0 ${theme.spacing[6]};
-      co-align: left;
 
       &:not(:last-child) {
-        button {
-          border-bottom: unset;
-        }
+        border-bottom: ${theme.borderWidth[1]} solid
+          ${theme.borderColor.tertiary};
       }
-
-      &:first-child {
-        button {
-          border-top-left-radius: ${theme.borderRadius.default};
-          border-top-right-radius: ${theme.borderRadius.default};
-        }
-      }
-
-      &:last-child {
-        button {
-          border-bottom-left-radius: ${theme.borderRadius.default};
-          border-bottom-right-radius: ${theme.borderRadius.default};
-        }
-      }
-    }
-
-    button {
-      ${createFontStyles('label-2')};
-      background-color: ${theme.backgroundColor.primary};
-      border: ${theme.borderWidth[1]} solid ${theme.borderColor.tertiary};
-      cursor: pointer;
-      height: 100%;
-      width: 100%;
-      margin: 0;
-      padding: 0 ${theme.spacing[6]};
-      text-align: left;
-      overflow: hidden;
-
-      ${!isLoading &&
-      css`
-        @media (hover: hover) {
-          &:hover {
-            background-color: ${theme.backgroundColor.secondary};
-          }
-        }
-        &:focus {
-          background-color: ${theme.backgroundColor.secondary};
-        }
-      `}
     }
   `}
 `;
 
 const EllipsisButton = styled.button`
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
-
-const Container = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  justify-content: center;
-  text-align: center;
-`;
-
-const Inner = styled.div`
   ${({ theme }) => css`
+    all: unset;
+    ${createFontStyles('label-2')};
+    background-color: ${theme.backgroundColor.primary};
     display: flex;
-    max-width: 350px;
-    flex-direction: column;
-    gap: ${theme.spacing[7]};
     align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    width: 100%;
+    padding: ${theme.spacing[4]} ${theme.spacing[5]};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+
+    &:hover,
+    &:focus {
+      background-color: ${theme.backgroundColor.secondary};
+    }
   `}
 `;
 
