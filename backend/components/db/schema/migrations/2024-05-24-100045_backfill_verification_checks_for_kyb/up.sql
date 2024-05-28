0433b3351ca0dc0322ce0e86e78ec580ab415a99
Dave Argoff <105253942+dave-argoff@users.noreply.github.com>
2024-05-28 11:18:34 -0400

    fix kyb migration for future (#9797)
    ## Describe your changes
    i fixed the rows manually in a shell
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/components/db/schema/migrations/2024-05-24-100045_backfill_verification_checks_for_kyb/up.sql
+++ backend/components/db/schema/migrations/2024-05-24-100045_backfill_verification_checks_for_kyb/up.sql
@@ -1,6 +1,6 @@
 -- 12.2K records in prod
 UPDATE ob_configuration
-SET verification_checks = ARRAY[json_build_object('kind', 'kyb', 'data', json_build_object('ein_only', 'false'))]::jsonb[]
+SET verification_checks = ARRAY[json_build_object('kind', 'kyb', 'data', json_build_object('ein_only', false))]::jsonb[]
 WHERE 
     kind = 'kyb' and 
     not skip_kyb and 
