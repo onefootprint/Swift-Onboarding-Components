477ba0180625426272ec38d39fbaac1c3b56279c
Bruno Batista <bruno@onefootprint.com>
2024-05-29 10:49:25 +0200

    chore(handoff): csp violations errors (#9799)
    ## Describe your changes
    
    This pull request incorporates the missing entries for the handoff CSP
    configuration.
    
    
    ![image](https://github.com/onefootprint/monorepo/assets/5114769/4efb65ed-fa44-4002-ac58-89a44e75cc0c)

--- frontend/apps/handoff/next.config.js
+++ frontend/apps/handoff/next.config.js
@@ -47,7 +47,7 @@ const DEV_CONNECT_SRC = (
 
 const ContentSecurityPolicy = `
   child-src blob: onefootprint.com;
-  connect-src 'self' ${DEV_CONNECT_SRC} vitals.vercel-insights.com vercel.live *.onefootprint.com *.pusher.com wss://*.pusher.com dvnfo.com maps.googleapis.com unpkg.com https://*.fptls.com https://*.fptls2.com https://*.fptls3.com https://api.fpjs.io https://*.api.fpjs.io telemetry.stytch.com https://*.fptls.com https://*.fptls2.com https://*.fptls3.com https://api.fpjs.io https://*.api.fpjs.io telemetry.stytch.com *.launchdarkly.com ${OBSERVE_CONNECT_SRC} ${LOG_ROCKET_CONNECT_SRC} ${SENTRY_CONNECT_SRC} ${DATADOG_SRC};
+  connect-src 'self' data: ${DEV_CONNECT_SRC} vitals.vercel-insights.com vercel.live *.onefootprint.com *.pusher.com wss://*.pusher.com dvnfo.com maps.googleapis.com unpkg.com https://*.fptls.com https://*.fptls2.com https://*.fptls3.com https://api.fpjs.io https://*.api.fpjs.io telemetry.stytch.com https://*.fptls.com https://*.fptls2.com https://*.fptls3.com https://api.fpjs.io https://*.api.fpjs.io telemetry.stytch.com *.launchdarkly.com ${OBSERVE_CONNECT_SRC} ${LOG_ROCKET_CONNECT_SRC} ${SENTRY_CONNECT_SRC} ${DATADOG_SRC};
   default-src 'self' vitals.vercel-insights.com;
   font-src 'self' fonts.googleapis.com fonts.gstatic.com;
   form-action 'self';
@@ -56,7 +56,7 @@ const ContentSecurityPolicy = `
   img-src 'self' data: assets.vercel.com vercel.live vercel.com i.onefp.net i-dev.onefp.net *.i-dev.onefp.net cdn.jsdelivr.net;
   media-src 'self' https;
   script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: vercel.live vitals.vercel-insights.com maps.googleapis.com fpnpmcdn.net js.dvnfo.com blob: https://cdn.jsdelivr.net docs.opencv.org elements.stytch.com ${LOG_ROCKET_SCRIPT_SRC} ${SENTRY_SCRIPT_SRC} ${DATADOG_SRC};
-  style-src 'self' 'unsafe-inline' fonts.googleapis.com cdn.jsdelivr.net;
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com cdn.jsdelivr.net i.onefp.net;
   worker-src 'self' blob:;
 `;
 
