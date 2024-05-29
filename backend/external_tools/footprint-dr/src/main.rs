bfae98187efece1f66d2965ee8fe79c9dffa5d2a
Ethan Lowman <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-05-29 10:32:33 -0400

    [Vault DR] Fetch org private key from a script (#9810)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical
    details and possible side effects. -->
    This PR has a bit of cleanup, but the main objective is changing the
    `--org-private-key` flag to `--org-private-key-plugin`. By adding this
    layer of indirection for fetching the key, we allow for a wide variety
    of storage mechanisms for the private key, and reduce the risk of the
    private key being leaked to shell history or left on the filesystem. For
    example, the plugin can be a shell script that fetches an AWS SSM
    parameter.
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse
    output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

--- backend/external_tools/footprint-dr/src/main.rs
+++ backend/external_tools/footprint-dr/src/main.rs
@@ -24,6 +24,11 @@ struct Command {
     help: Option<bool>,
 }
 
+const VAULT_SELECTOR_GROUP: &str = "vault selector";
+const RECORD_SELECTOR_GROUP: &str = "record selector";
+const PAGINATION_OR_SAMPLE_GROUP: &str = "pagination or sample";
+const NUMBER_OF_VAULTS_GROUP: &str = "number of vaults";
+
 #[derive(Subcommand, Debug)]
 enum SubCommand {
     /// Authenticate the CLI with Footprint
@@ -49,7 +54,7 @@ enum SubCommand {
         sandbox: SandboxFlag,
 
         /// List all vaults
-        #[arg(long, group = "vault selector", group = "number of vaults")]
+        #[arg(long, group = VAULT_SELECTOR_GROUP, group = NUMBER_OF_VAULTS_GROUP)]
         all: bool,
 
         #[clap(flatten)]
@@ -61,31 +66,46 @@ enum SubCommand {
         sandbox: SandboxFlag,
 
         /// List all vault records
-        #[arg(long, group = "vault selector", group = "number of vaults")]
+        #[arg(long, group = VAULT_SELECTOR_GROUP, group = NUMBER_OF_VAULTS_GROUP)]
         all: bool,
 
         #[clap(flatten)]
         vault_filter: VaultSelector,
 
         /// List of FP IDs
-        #[arg(name = "FP ID", group = "vault selector", group = "number of vaults")]
+        #[arg(name = "FP ID", group = VAULT_SELECTOR_GROUP, group = NUMBER_OF_VAULTS_GROUP)]
         fp_ids: Vec<String>,
     },
-    /// Decrypt vault records
+    /// Decrypt vault records from the Disaster Recovery backups
     ///
-    /// FOO
-    #[command(group = ArgGroup::new("record selector").required(true))]
+    /// Requires the --org-private-key-plugin flag, pointing to an executable that emits the org
+    /// private key to stdout.
+    ///
+    /// Test recovery:
+    ///     If the --wrapped-recovery-key flag is not provided, the CLI will run a test recovery.
+    ///     This is supported by online Footprint services, and will generate audit logs for all
+    ///     decryptions.
+    ///
+    /// Full recovery:
+    ///     If the --wrapped-recovery-key flag is provided, the CLI will decrypt the records
+    ///     in the cloud storage bucket without relying on Footprint services.
+    #[command(verbatim_doc_comment)]
+    #[command(group = ArgGroup::new(RECORD_SELECTOR_GROUP).required(true))]
     Decrypt {
         #[clap(flatten)]
         sandbox: SandboxFlag,
 
         /// Decrypt all records
-        #[arg(long, group = "record selector")]
+        #[arg(long, group = RECORD_SELECTOR_GROUP)]
         all: bool,
 
         /// Decrypt records from the given line-separated JSON file (.jsonl)
-        #[arg(value_name = "records file path", group = "record selector")]
+        #[arg(value_name = "path", group = RECORD_SELECTOR_GROUP)]
         records: Option<PathBuf>,
+
+        /// An that emits the organization private key to stdout
+        #[arg(long, value_name = "path")]
+        org_private_key_plugin: Option<PathBuf>,
 
         /// Wrapped recovery key file (.age)
         #[arg(long, value_name = "path")]
@@ -106,20 +126,20 @@ struct VaultSelector {
     #[arg(
         long,
         group = "vault selector",
-        group = "pagination or sample",
+        group = PAGINATION_OR_SAMPLE_GROUP,
         value_name = "FP ID"
     )]
     vault_gt: Option<String>,
 
     /// Randomly sample vaults.
-    #[arg(long, group = "vault selector", group = "pagination or sample")]
+    #[arg(long, group = "vault selector", group = PAGINATION_OR_SAMPLE_GROUP)]
     sample: bool,
 
     /// The maximum number of vaults to list.
     #[arg(
         short = 'n',
         long,
-        group = "number of vaults",
+        group = NUMBER_OF_VAULTS_GROUP,
         value_name = "count",
         default_value = "100"
     )]
