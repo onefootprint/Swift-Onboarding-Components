b5fed4c46419d6f3bda212fc2d2e74e3c91eadeb
ethan-lowman-fp <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-05-30 21:15:08 +0000

    [CI cleanup] Factor out diesel migration tests (#9852)
    ## Describe your changes
    <!--- An in-depth description of the changes made by this PR. Technical details and possible side effects. -->
    
    ## Caveats
    <!--- Are there any caveats to this changeset? -->
    
    ## Test Plan
    <!--- How did you test your changes? -->
    
    <!---
    ### Frontend/Design Checklist
    - Can we add any unit tests? What about E2E tests?
    - Do we need to notify any customers?
    - Do we need to update any developer docs?
    - If doing SDK changes
        - Are they backwards compatible?
        - Which NPM packages do we need to release, and when?
    - Are there any performance implications? What does the lighthouse output show?
    
    ### Screenshots / videos (attachment)
    - [ ] IDV
      - [ ] Mobile & Desktop
      - [ ] Bifrost & Hosted & Handoff
      - [ ] Integration with SDKs + appearance customizations
    - [ ] Dashboard
    - [ ] Form
      - [ ] Different variants/fields
     -->

---
+++ backend/ci/scripts/check_db_migrations.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd ../../components/db/schema
+
+DIESEL_VERSION="2.0.0"
+cargo install diesel_cli --version $DIESEL_VERSION --locked --no-default-features --features postgres
+
+NUM_FILES=`ls migrations | wc -l`
+NUM_TO_REVERT=$(( $NUM_FILES < 1 ? $NUM_FILES : 1 ))
+echo "Reverting $NUM_TO_REVERT migrations"
+
+for ((i=0; i<$NUM_TO_REVERT; i++))
+do
+  echo $i
+  diesel migration revert
+  if [ $? -ne 0 ]; then
+    echo "failed to properly revert"
+    exit 1
+  fi
+done
+
+echo "Reverted $NUM_TO_REVERT migrations, running again"
+
+diesel migration run
+if [ $? -ne 0 ]; then
+  echo "failed to properly restore"
+  exit 1
+fi
