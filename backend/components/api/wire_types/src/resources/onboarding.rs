d60bcd2cacfc3895502cff28d6940e6fe7be21b0
Elliott Forde <elliott@onefootprint.com>
2024-06-05 16:00:34 -0700

    Add open API example for playbook_key in

--- backend/components/api/wire_types/src/resources/onboarding.rs
+++ backend/components/api/wire_types/src/resources/onboarding.rs
@@ -12,6 +12,7 @@ use serde::Serialize;
 #[derive(Debug, Clone, Serialize, Apiv2Schema)]
 
 pub struct PublicOnboarding {
+    #[openapi(example = "ob_live_fZvYlX3JpanlQ3MAwE45g0")]
     pub playbook_key: ObConfigurationKey,
     pub status: OnboardingStatus,
     pub timestamp: DateTime<Utc>,
