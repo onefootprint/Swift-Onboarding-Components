b318e3d742a8ef60d2d5ceffff0de8b847346e5c
Rafael Michels Motta <rafael@onefootprint.com>
2024-06-13 08:22:15 -0300

    OC: Remove un-used files

--- frontend/packages/footprint-react/src/utils/dom-utils.ts
+++
@@ -1,9 +0,0 @@
-const className = 'footprint-body-locked';
-
-export const unlockBody = () => {
-  document.body.classList.remove(className);
-};
-
-export const lockBody = () => {
-  document.body.classList.add(className);
-};
