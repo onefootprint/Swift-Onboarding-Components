10b429875b636622a4dcd5f8d2a8cc88a2f6c549
Claudio Angrigiani <angrigianicr@gmail.com>
2024-06-27 17:03:58 -0400

    add next-sitemap (#10299)
    ## Describe your changes
    
    
    ## Caveats
    
    
    ## Test Plan

---
+++ frontend/apps/frontpage/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://onefootprint.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 1000,
+};
