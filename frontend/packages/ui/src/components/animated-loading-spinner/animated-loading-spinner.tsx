c7665141bcfe5d7fb9c1f000234125e2c1b03864
Claudio Angrigiani <angrigianicr@gmail.com>
2024-06-27 17:05:08 -0400

    Remove framer-motion from loader-animation (#10294)
    In some cases this loader animation was rendering a bit buggy in Safari.
    I suspect it's because of framer motion applied to svgs. This new
    implementation prescinds from framer motion and uses `keyframes`
    instead.
    
    
    
    https://github.com/onefootprint/monorepo/assets/12160059/23a145d4-610d-4024-9e4e-ecff71e5f711

--- frontend/packages/ui/src/components/animated-loading-spinner/animated-loading-spinner.tsx
+++ frontend/packages/ui/src/components/animated-loading-spinner/animated-loading-spinner.tsx
@@ -1,7 +1,6 @@
 import type { Color } from '@onefootprint/design-tokens';
-import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
-import styled, { useTheme } from 'styled-components';
+import styled, { keyframes, useTheme } from 'styled-components';
 
 import Stack from '../stack';
 
@@ -11,22 +10,6 @@ export type AnimatedLoadingSpinnerProps = {
   color?: Color;
   ariaLabel?: string;
 };
-
-const getSpinnerVariants = (size: number) => ({
-  animate: {
-    strokeDashoffset: [0, size / 4, 0],
-    rotate: [0, 360],
-    transition: {
-      repeat: Infinity,
-      duration: 2,
-      ease: 'linear',
-    },
-  },
-  initial: {
-    strokeDashoffset: 0,
-    rotate: 0,
-  },
-});
 
 const AnimatedLoadingSpinner = ({
   animationStart = true,
@@ -42,58 +25,69 @@ const AnimatedLoadingSpinner = ({
   const radius = (size - strokeWidth) / 3;
   const circumference = radius * 2 * Math.PI;
   const adjustedCircumference = circumference * 0.75;
-  const spinnerVariants = getSpinnerVariants(adjustedCircumference);
 
   return (
     <Container $height={size} $width={size} align="center" justify="center" aria-label={ariaLabel} role="progressbar">
-      <AnimatePresence>
-        {animationStart && (
-          <motion.svg
-            initial="initial"
-            animate="animate"
-            exit="initial"
-            width={size}
-            height={size}
-            viewBox={`0 0 ${size} ${size}`}
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              stroke={color ? theme.backgroundColor.transparent : theme.backgroundColor.secondary}
-              strokeWidth={strokeWidth}
-            />
-            <motion.circle
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              stroke={spinnerColor}
-              strokeWidth={strokeWidth}
-              strokeLinecap="round"
-              variants={spinnerVariants}
-              style={{
-                pathLength: 1,
-                pathOffset: 0.5,
-                strokeDasharray: adjustedCircumference,
-              }}
-            />
-          </motion.svg>
-        )}
-      </AnimatePresence>
+      {animationStart && (
+        <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`} fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            stroke={color ? theme.backgroundColor.transparent : theme.backgroundColor.secondary}
+            strokeWidth={strokeWidth}
+          />
+          <AnimatedCircle
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            stroke={spinnerColor}
+            strokeWidth={strokeWidth}
+            strokeLinecap="round"
+            $circumference={adjustedCircumference}
+          />
+        </svg>
+      )}
     </Container>
   );
 };
 
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const dash = (size: number) => keyframes`
+  0% {
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dashoffset: ${size / 3};
+  }
+  100% {
+    stroke-dashoffset: 0;
+  }
+`;
+
 const Container = styled(Stack)<{ $height: number; $width: number }>`
   ${({ $height, $width }) => `
-        height: ${$height}px;
-        width: ${$width}px;
-        display: flex; 
-        justify-content: center;
-        align-items: center; 
-    `}
+      height: ${$height}px;
+      width: ${$width}px;
+      display: flex; 
+      justify-content: center;
+      align-items: center; 
+  `}
+`;
+
+const AnimatedCircle = styled.circle<{ $circumference: number }>`
+  animation: ${rotate} 2s linear infinite, ${({ $circumference }) => dash($circumference)} 2s ease-in-out infinite;
+  stroke-dasharray: ${({ $circumference }) => $circumference};
+  stroke-dashoffset: 0;
+  transform-origin: center;
 `;
 
 export default AnimatedLoadingSpinner;
