20c1a2fd978b52cadc61e024076f184a2f7010bd
ahsan1578 <54286440+ahsan1578@users.noreply.github.com>
2024-07-02 22:37:36 +0000

    Error prop with Field (#10336)
    ## Describe your changes
    
    ## Caveats
    
    ## Test Plan

--- mobile/packages/footprint-react-native/src/onboarding-components/field/field.tsx
+++ mobile/packages/footprint-react-native/src/onboarding-components/field/field.tsx
@@ -1,20 +1,30 @@
+import { get } from 'lodash';
 import React, { useId, useMemo } from 'react';
+import type { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 
 import FieldContext from '../field-context';
 import type { Di } from '../types/dis';
 
 export type FieldProps = {
   name: keyof Di;
-  children?: React.ReactNode;
+  children?: (
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    error: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined,
+  ) => React.ReactNode;
 };
 
 const Field = ({ name, children }: FieldProps) => {
   const id = useId();
   const contextValues = useMemo(() => ({ name, id }), [name, id]);
+  const {
+    formState: { errors },
+  } = useFormContext();
+  const error = get(errors, name);
 
   return (
     <FieldContext.Provider value={contextValues}>
-      {children}
+      {children?.(error)}
     </FieldContext.Provider>
   );
 };
