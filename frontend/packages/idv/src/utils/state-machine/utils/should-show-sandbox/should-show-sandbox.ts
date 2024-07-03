a0621c0fe7de25739fba624892df19604b301ec8
ahsan1578 <54286440+ahsan1578@users.noreply.github.com>
2024-07-03 16:57:35 -0700

    Skip asking for sandbox outcome the second time the handoff open in mobile onborading components SDK (#10409)
    ## Describe your changes
    
    
    ## Caveats
    
    
    ## Test Plan

--- frontend/packages/idv/src/utils/state-machine/utils/should-show-sandbox/should-show-sandbox.ts
+++ frontend/packages/idv/src/utils/state-machine/utils/should-show-sandbox/should-show-sandbox.ts
@@ -1,7 +1,15 @@
-import type { MachineContext, MachineEvents } from '../../types';
+import { ComponentsSdkTypes, type MachineContext, type MachineEvents } from '../../types';
 
 const shouldShowSandbox = (context: MachineContext, event: MachineEvents): boolean => {
   if (event.type !== 'initContextUpdated') {
+    return false;
+  }
+
+  const { componentsSdkContext } = context;
+  const isMobileComponentSdk = componentsSdkContext?.componentsSdkType === ComponentsSdkTypes.MOBILE;
+  const skipRelayToComponents = componentsSdkContext?.skipRelayToComponents;
+  if (isMobileComponentSdk && skipRelayToComponents) {
+    // this means we already have the auth token i.e. we asked for sandbox outcome already before the identify step
     return false;
   }
 
