c40eb41f4085945d43ddf0957b3455088e34525a
Elliott Forde <elliott@onefootprint.com>
2024-07-08 13:36:48 -0700

    Revert "[BE-328] Try remove span around DB queries (#10421)"
    This reverts commit eed52f51431ff03610c1db366a596224babe8a54.

--- backend/components/db/core/src/lib.rs
+++ backend/components/db/core/src/lib.rs
@@ -69,12 +69,17 @@ impl DbPool {
         E: From<DbError> + Send + 'static + std::fmt::Debug,
         R: Send + 'static,
     {
+        let current_span = tracing::info_span!("db_query::interact");
         let result = self
             .0
             .get()
             .await
             .map_err(DbError::from)?
-            .interact(move |conn| f(conn))
+            .interact(move |conn| {
+                // Without adding a span inside here, none of the traces inside f will appear...
+                let _guard = current_span.enter();
+                f(conn)
+            })
             .await
             .map_err(DbError::from)??;
         Ok(result)
