5a0a86610ee6e5d17c600670d254a18af858a304
Claudio Angrigiani <angrigianicr@gmail.com>
2024-07-11 09:15:10 -0400

    Allow more sources in `ContentSecurityPolicy` (#10476)

--- frontend/apps/frontpage/next.config.js
+++ frontend/apps/frontpage/next.config.js
@@ -19,7 +19,7 @@ const DEV_FRAME_SRC = (IS_DEV ? ['http://localhost:3000'] : []).join(' ');
 
 const ContentSecurityPolicy = `
   child-src onefootprint.com;
-  connect-src 'self' *.onefootprint.com vitals.vercel-insights.com getform.io/f/pbygomeb *.pusher.com wss://*.pusher.com vercel.live aplo-evnt.com usefathom.com *.usefathom.com www.youtube.com api.onefootprint.com unifyintent.com *.unifyintent.com;
+  connect-src 'self' *.onefootprint.com vitals.vercel-insights.com getform.io/f/pbygomeb *.pusher.com wss://*.pusher.com vercel.live aplo-evnt.com usefathom.com *.usefathom.com www.youtube.com api.onefootprint.com unifyintent.com *.unifyintent.com px.ads.linkedin.com;
   default-src 'self' vitals.vercel-insights.com;
   font-src 'self' fonts.googleapis.com fonts.gstatic.com;
   form-action 'self';
@@ -28,7 +28,7 @@ const ContentSecurityPolicy = `
   img-src 'self' data: footprint-blog.ghost.io assets.vercel.com vercel.live vercel.com usefathom.com *.usefathom.com i.onefp.net i-dev.onefp.net *.i-dev.onefp.net unifyintent.com *.unifyintent.com cdn.jsdelivr.net px.ads.linkedin.com;
   media-src 'self' https footprint-blog.ghost.io;
   object-src 'self' data:;
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' usefathom.com *.usefathom.com vercel.live vitals.vercel-insights.com tagmanager.google.com www.googletagmanager.com platform.twitter.com www.youtube.com static.ads-twitter.com connect.facebook.net www.facebook.com unifyintent.com *.unifyintent.com *.apollo.io *.vercel-scripts.com snap.licdn.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' usefathom.com *.usefathom.com vercel.live vitals.vercel-insights.com tagmanager.google.com www.googletagmanager.com platform.twitter.com www.youtube.com static.ads-twitter.com connect.facebook.net www.facebook.com unifyintent.com *.unifyintent.com *.apollo.io *.vercel-scripts.com snap.licdn.com googleads.g.doubleclick.net www.googleadservices.com;
   style-src 'self' 'unsafe-inline' tagmanager.google.com fonts.googleapis.com cdn.jsdelivr.net;
   worker-src 'self' blob:;
 `;
