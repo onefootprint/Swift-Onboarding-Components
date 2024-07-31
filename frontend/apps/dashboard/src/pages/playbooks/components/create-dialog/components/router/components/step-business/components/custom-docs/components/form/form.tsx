bf481d3a615a0535f47667749a0121432293359f
Rafael Michels Motta <rafael@onefootprint.com>
2024-07-31 15:57:31 -0300

    Playbook create (5): remove business docs save btn

--- frontend/apps/dashboard/src/pages/playbooks/components/create-dialog/components/router/components/step-business/components/custom-docs/components/form/form.tsx
+++ frontend/apps/dashboard/src/pages/playbooks/components/create-dialog/components/router/components/step-business/components/custom-docs/components/form/form.tsx
@@ -1,63 +1,105 @@
-import { type DataToCollectFormData } from '@/playbooks/utils/machine/types';
-import { IcoTrash24 } from '@onefootprint/icons';
-import { Button, LinkButton, Stack } from '@onefootprint/ui';
-import React, { useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+import type { CustomDoc, DataToCollectFormData } from '@/playbooks/utils/machine/types';
+import { IcoPlusSmall16 } from '@onefootprint/icons';
+import { Box, LinkButton, Stack, Text } from '@onefootprint/ui';
+import React, { useEffect, useState } from 'react';
+import { useFieldArray, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import useCustomDocsValues from '../../hooks/use-custom-docs-values';
-import CustomDocs from './components/custom-docs';
+import CustomDocsForm from './components/custom-docs-form';
 
-type FormProps = {
+type CustomProps = {
   onClose: () => void;
 };
 
-const Form = ({ onClose }: FormProps) => {
+const CustomDocs = ({ onClose }: CustomProps) => {
   const { t } = useTranslation('common');
-  const { watch, setValue } = useFormContext<DataToCollectFormData>();
-  const [originalValue] = useState(() => {
-    return {
-      custom: watch('business.docs.custom'),
-    };
+  const { control } = useFormContext<DataToCollectFormData>();
+  const [formIndex, setFormIndex] = useState<number | null>(null);
+  const { fields, append, remove, update } = useFieldArray({
+    control,
+    name: 'business.docs.custom',
   });
-  const {
-    meta: { hasDoc },
-  } = useCustomDocsValues();
 
-  const handleCancel = () => {
-    setValue('business.docs.custom', originalValue.custom);
-    onClose();
+  useEffect(() => {
+    if (fields.length === 0) {
+      handleAdd();
+    }
+  }, []);
+
+  const handleAdd = () => {
+    const length = fields.length;
+    append({ name: '', description: '', identifier: '' });
+    setFormIndex(length);
   };
 
-  const handleRemoveAll = () => {
-    setValue('business.docs.custom', []);
-    onClose();
+  const handleEdit = (index: number) => () => {
+    setFormIndex(index);
   };
 
-  return (
-    <Stack direction="column" gap={8}>
-      <CustomDocs onCancel={handleCancel} />
-      <Stack gap={4} direction="column">
-        <Button variant="primary" fullWidth onClick={onClose}>
-          {t('save')}
-        </Button>
-        <Button variant="secondary" fullWidth onClick={handleCancel}>
-          {t('cancel')}
-        </Button>
-        <Stack justifyContent="center">
-          <LinkButton
-            destructive
-            iconComponent={IcoTrash24}
-            iconPosition="left"
-            onClick={handleRemoveAll}
-            variant="label-4"
-            disabled={!hasDoc}
-          >
-            {t('remove-all')}
+  const handleDelete = (index: number) => {
+    const shouldCancel = fields.length === 1;
+    remove(index);
+    setFormIndex(null);
+
+    if (shouldCancel) {
+      onClose();
+    }
+  };
+
+  const handleCancel = (index: number, meta: { isCreating: boolean; isValid: boolean }) => {
+    if (meta.isCreating && !meta.isValid) {
+      remove(index);
+      if (fields.length === 1) {
+        onClose();
+      }
+    }
+    setFormIndex(null);
+  };
+
+  const handleSubmit = (index: number, customDoc: CustomDoc) => {
+    update(index, customDoc);
+    setFormIndex(null);
+  };
+
+  return fields.length > 0 ? (
+    <Stack gap={3} direction="column">
+      {formIndex == null ? (
+        <Stack direction="column" gap={5}>
+          {fields.map((field, index) => (
+            <Stack key={field.id} justifyContent="space-between" gap={5}>
+              <Stack gap={3} alignItems="center" overflow="hidden">
+                <Text variant="label-4">{field.name}</Text>
+                <Box
+                  backgroundColor="secondary"
+                  borderColor="tertiary"
+                  borderRadius="sm"
+                  borderStyle="solid"
+                  borderWidth={1}
+                  overflow="hidden"
+                  paddingBlock={1}
+                  paddingInline={2}
+                  textOverflow="ellipsis"
+                  userSelect="none"
+                  whiteSpace="nowrap"
+                >
+                  <Text variant="snippet-2" color="tertiary" truncate>
+                    document.custom.{field.identifier}
+                  </Text>
+                </Box>
+              </Stack>
+              <LinkButton variant="label-4" onClick={handleEdit(index)}>
+                {t('edit')}
+              </LinkButton>
+            </Stack>
+          ))}
+          <LinkButton iconComponent={IcoPlusSmall16} iconPosition="left" variant="label-4" onClick={handleAdd}>
+            {t('add')}
           </LinkButton>
         </Stack>
-      </Stack>
+      ) : (
+        <CustomDocsForm index={formIndex} onCancel={handleCancel} onDelete={handleDelete} onSubmit={handleSubmit} />
+      )}
     </Stack>
-  );
+  ) : null;
 };
 
-export default Form;
+export default CustomDocs;
