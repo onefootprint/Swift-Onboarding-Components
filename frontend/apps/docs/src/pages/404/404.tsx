9a7ca4773d0e866b8d2342b92c55cc940c5c674d
Claudio Angrigiani <angrigianicr@gmail.com>
2024-08-09 13:27:13 -0400

    Add redirect from 404 to api-reference and docs (#11133)

--- frontend/apps/docs/src/pages/404/404.tsx
+++ frontend/apps/docs/src/pages/404/404.tsx
@@ -3,19 +3,25 @@ import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
+import router from 'next/router';
+import { API_REFERENCE_PATH } from 'src/config/constants';
 import SEO from '../../components/seo';
 
 const Page404 = () => {
   const { t } = useTranslation('common', { keyPrefix: 'pages.404' });
+  const isApiReference = router.asPath.startsWith(API_REFERENCE_PATH);
+  const redirectUrl = isApiReference ? `${API_REFERENCE_PATH}` : '/';
+  console.log('redirectUrl', redirectUrl);
   return (
     <>
-      <SEO title={t('title')} slug="/" />
+      <SEO title={t('title')} />
       <Container>
         <Inner>
           <EmptyState
             title={t('title')}
             description={t('description')}
             renderHeader={() => <Image alt={t('image.alt')} src="/404.png" height={212} width={298} priority />}
+            cta={{ label: t('cta.label'), onClick: () => router.push(redirectUrl) }}
           />
         </Inner>
       </Container>
