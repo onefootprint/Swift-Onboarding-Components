30fe8e37aee340944417d45cf215de24480e4aa8
ethan-lowman-fp <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-08-27 20:25:06 +0000

    Fix bug in vault DR batch query (#11481)
    ## Describe your changes
    The new test describes the bug in the query. If a seqno isn't completely handled by a single batch, some DLs for that seqno won't every be written. We'll have to rewrite this query entirely to accommodate manifests, so we can fix any impact of the bug in that backfill.
    
    ## Caveats
    
    ## Test Plan

--- backend/components/db/core/src/helpers/vault_dr.rs
+++ backend/components/db/core/src/helpers/vault_dr.rs
@@ -44,7 +44,7 @@ pub fn load_vault_dr_data_lifetime_batch(
         .inner_join(scoped_vault::table)
         .filter(scoped_vault::tenant_id.eq(tenant_id))
         .filter(scoped_vault::is_live.eq(is_live))
-        .filter(data_lifetime::created_seqno.gt(max_processed_seqno))
+        .filter(data_lifetime::created_seqno.ge(max_processed_seqno))
         .filter(diesel::dsl::not(diesel::dsl::exists(
             vault_dr_blob::table
                 .filter(vault_dr_blob::data_lifetime_id.eq(data_lifetime::id))
@@ -77,4 +77,161 @@ pub fn get_latest_vault_dr_backup_record_timestamp(
         .first(conn)
         .optional()?;
     Ok(ts)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::vault_dr::NewVaultDrAwsPreEnrollment;
+    use crate::models::vault_dr::NewVaultDrBlob;
+    use crate::models::vault_dr::NewVaultDrConfig;
+    use crate::models::vault_dr::VaultDrAwsPreEnrollment;
+    use crate::models::vault_dr::VaultDrBlob;
+    use crate::models::vault_dr::VaultDrConfig;
+    use crate::test_helpers::assert_have_same_elements;
+    use crate::tests::fixtures;
+    use crate::tests::prelude::*;
+    use hex::ToHex;
+    use itertools::Itertools;
+    use macros::db_test;
+    use newtypes::DataIdentifier;
+    use newtypes::IdentityDataKind;
+
+    #[db_test]
+    fn test_vault_dr_batch_query(conn: &mut TestPgConn) {
+        let tenant_id = fixtures::tenant::create(conn).id;
+        let ob_config_id = fixtures::ob_configuration::create(conn, &tenant_id, true).id;
+        let v1_id = fixtures::vault::create_person(conn, true).into_inner().id;
+        let sv1 = fixtures::scoped_vault::create(conn, &v1_id, &ob_config_id);
+
+        let new_ape = NewVaultDrAwsPreEnrollment {
+            tenant_id: &tenant_id,
+            is_live: true,
+            aws_external_id: crypto::random::gen_rand_bytes(16).encode_hex::<String>().into(),
+        };
+        let ape = VaultDrAwsPreEnrollment::get_or_create(conn, new_ape).unwrap();
+
+        let new_vdr_config = NewVaultDrConfig {
+            created_at: Utc::now(),
+            tenant_id: &tenant_id,
+            is_live: true,
+            aws_pre_enrollment_id: &ape.id,
+            aws_account_id: "12345678".to_owned(),
+            aws_role_name: "my-role".to_owned(),
+            s3_bucket_name: "my-bucket".to_owned(),
+            recovery_public_key: "pub-key".to_owned(),
+            wrapped_recovery_key: "wrapped-recovery-key".to_owned(),
+            org_public_keys: vec!["org-pub-key".to_owned()],
+            bucket_path_namespace: "the-namespace".to_owned(),
+        };
+        let vdr_config = VaultDrConfig::create(conn, new_vdr_config).unwrap();
+
+        let batch_size: usize = 5;
+        let mut seqno_timeline = vec![];
+        let mut dl_ids = vec![];
+        for _ in 0..=5 {
+            let seqno1 = DataLifetime::get_next_seqno(conn).unwrap();
+            DataLifetime::bulk_deactivate_kinds(
+                conn,
+                &sv1,
+                vec![DataIdentifier::Id(IdentityDataKind::Dob)],
+                seqno1,
+            )
+            .unwrap();
+            let dl =
+                fixtures::data_lifetime::build(conn, &v1_id, &sv1, seqno1, None, None, IdentityDataKind::Dob);
+            seqno_timeline.push(seqno1);
+            dl_ids.push(dl.id);
+
+            let seqno2 = DataLifetime::get_next_seqno(conn).unwrap();
+            DataLifetime::bulk_deactivate_kinds(
+                conn,
+                &sv1,
+                vec![DataIdentifier::Id(IdentityDataKind::Email)],
+                seqno1,
+            )
+            .unwrap();
+            let dl = fixtures::data_lifetime::build(
+                conn,
+                &v1_id,
+                &sv1,
+                seqno2,
+                None,
+                None,
+                IdentityDataKind::Email,
+            );
+            seqno_timeline.push(seqno1);
+            dl_ids.push(dl.id);
+
+            // Create another DL at the same seqno.
+            DataLifetime::bulk_deactivate_kinds(
+                conn,
+                &sv1,
+                vec![DataIdentifier::Id(IdentityDataKind::FirstName)],
+                seqno1,
+            )
+            .unwrap();
+            let dl = fixtures::data_lifetime::build(
+                conn,
+                &v1_id,
+                &sv1,
+                seqno2,
+                None,
+                None,
+                IdentityDataKind::FirstName,
+            );
+            seqno_timeline.push(seqno1);
+            dl_ids.push(dl.id);
+        }
+
+        assert_eq!(seqno_timeline.len(), dl_ids.len());
+
+        // Make sure we're triggering a condition where a batch doesn't contain all DLs for a
+        // seqno.
+        assert_eq!(seqno_timeline[batch_size - 1], seqno_timeline[batch_size]);
+
+        let expected_batch_sizes = vec![batch_size, batch_size, batch_size, 3, 0, 0];
+        assert_eq!(dl_ids.len(), expected_batch_sizes.iter().sum::<usize>());
+
+
+        let mut dl_ids = dl_ids.into_iter();
+        for (i, expected_batch_size) in expected_batch_sizes.into_iter().enumerate() {
+            let batch = load_vault_dr_data_lifetime_batch(
+                conn,
+                &tenant_id,
+                true,
+                &vdr_config.id,
+                batch_size as u32,
+                Some(vec![sv1.fp_id.clone()]),
+            )
+            .unwrap();
+            assert_eq!(batch.len(), expected_batch_size, "Batch {} size", i);
+
+            let expected_dl_ids = (&mut dl_ids).take(expected_batch_size).collect_vec();
+            assert_have_same_elements(
+                batch.iter().map(|dl| dl.id.clone()).collect_vec(),
+                expected_dl_ids,
+            );
+
+            write_fake_blobs(conn, &vdr_config.id, batch);
+        }
+    }
+
+    fn write_fake_blobs(conn: &mut TestPgConn, config_id: &VaultDrConfigId, dls: Vec<DataLifetime>) {
+        let new_blobs = dls
+            .into_iter()
+            .map(|dl| NewVaultDrBlob {
+                created_at: Utc::now(),
+                config_id: config_id.clone(),
+                data_lifetime_id: dl.id,
+                dl_created_seqno: dl.created_seqno,
+                bucket_path: crypto::random::gen_rand_bytes(32).encode_hex(),
+                content_etag: "content-etag".to_owned(),
+                wrapped_record_key: "wrapped-record-key".into(),
+                content_length_bytes: 123,
+            })
+            .collect_vec();
+
+        VaultDrBlob::bulk_create(conn, new_blobs).unwrap();
+    }
 }
