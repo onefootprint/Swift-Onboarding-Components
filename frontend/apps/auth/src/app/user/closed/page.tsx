0e1f12c71b9faa35416f596a70f9fa0d603492c8
Bruno Batista <bruno@onefootprint.com>
2024-08-29 17:00:25 +0200

    fix(auth): font family in ssr pages (#11562)
    ## Describe your changes
    
    
    ![image](https://github.com/user-attachments/assets/1cdcf12c-6ca7-4103-b255-3ef5a2978062)

--- frontend/apps/auth/src/app/user/closed/page.tsx
+++ frontend/apps/auth/src/app/user/closed/page.tsx
@@ -6,8 +6,10 @@ const UserClosedPage = () => (
       <div style={{ padding: '23px' }}>
         <SvgSafeAndPenguin />
       </div>
-      <h3 style={{ fontSize: '19px', marginBottom: '11px' }}>Your data is safe and secure!</h3>
-      <p style={{ fontSize: '15px' }}>You can now close this tab.</p>
+      <h3 style={{ fontFamily: 'var(--font-family-default)', fontSize: '19px', marginBottom: '11px' }}>
+        Your data is safe and secure!
+      </h3>
+      <p style={{ fontFamily: 'var(--font-family-default)', fontSize: '15px' }}>You can now close this tab.</p>
     </div>
   </main>
 );
