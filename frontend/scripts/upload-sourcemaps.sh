ab321d600e194249a5f2efaaebc2b43aabef866e
Rafael Michels Motta <rafael@onefootprint.com>
2024-09-15 10:09:15 -0300

    Upload source maps debug 2

--- frontend/scripts/upload-sourcemaps.sh
+++ frontend/scripts/upload-sourcemaps.sh
@@ -35,6 +35,7 @@ done
 
 # Source the .env file from the calling directory
 if [ -f ".env.local" ]; then
+  echo "loaded .env.local"
   . ./.env.local
 fi
 
@@ -43,14 +44,14 @@ if [ -z "$SERVICE" ]; then
   exit 0
 fi
 
-if [ -z "$DATADOG_API_KEY" ]; then
-  echo 'DATADOG_API_KEY is not set. Skipping datadog-ci sourcemaps upload.'
-  exit 0
-fi
-
 if [ -z "$VERCEL_GIT_COMMIT_SHA" ]; then
   echo 'VERCEL_GIT_COMMIT_SHA is not set. Using "unknown" as fallback.'
   VERCEL_GIT_COMMIT_SHA="unknown"
+fi
+
+if [ -z "$DATADOG_API_KEY" ]; then
+  echo 'DATADOG_API_KEY is not set. Skipping datadog-ci sourcemaps upload.'
+  exit 0
 fi
 
 datadog-ci sourcemaps upload .next/static --service="$SERVICE" --release-version="${SERVICE}-${VERCEL_GIT_COMMIT_SHA}" --minified-path-prefix=https://$SERVICE.onefootprint.com/_next/static
