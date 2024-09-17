a8aaa1db9a508c16850a9a3850213fadb5481877
Bruno Batista <bruno@onefootprint.com>
2024-09-17 15:17:37 +0200

    chore(auth): delete unused util (#11917)
    ## Describe your changes
    
    Delete unused `getErrorToastVariant` util

--- frontend/apps/auth/src/utils/index.ts
+++ frontend/apps/auth/src/utils/index.ts
@@ -1,4 +1,3 @@
 export { default as getAuthBootstrapData } from './get-bootstrap-data';
-export { default as getErrorToastVariant } from './toast-error-variant';
 export { default as isEmbeddedInIframe } from './is-in-iframe';
 export { default as isSdkUrlAllowed } from './verify-allowed-domain';
--- frontend/apps/auth/src/utils/toast-error-variant/toast-error-variant.ts
+++
@@ -1,9 +0,0 @@
-import { getErrorMessage } from '@onefootprint/request';
-
-const getErrorToastVariant = (err?: unknown) => ({
-  description: getErrorMessage(err),
-  title: 'Uh-oh!',
-  variant: 'error' as const,
-});
-
-export default getErrorToastVariant;
