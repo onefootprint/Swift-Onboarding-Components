43fd1f2061589079ab3ad13971c43d929eeb27b3
Bruno Batista <bruno@onefootprint.com>
2024-09-27 19:55:47 +0200

    fix(auth): set viewport max scale 1x (#12202)
    ## Describe your changes
    
    This fixes the zoom strange behavior when auth is in a web view

--- frontend/apps/auth/src/app/layout.tsx
+++ frontend/apps/auth/src/app/layout.tsx
@@ -50,6 +50,7 @@ const RootLayout = ({ children }: RootLayoutProps) => (
       <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
       <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
       <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </head>
     <body style={{ backgroundColor: 'inherit' }}>{children}</body>
     {mapsSrc ? <Script src={mapsSrc} strategy="lazyOnload" /> : null}
