f431efef110e619fe09b2aa1d4e88e4caff74ef8
aranwmurphy-footprint <176972215+aranwmurphy-footprint@users.noreply.github.com>
2024-10-03 14:32:25 +0000

    Add integration tests for card.*.fingerprint and bank.*.fingerprint (#12316)
    ## Describe your changes
    Add integration test cases to ensure that card.*.fingerprint and bank.*.fingerprint are generated for new users, as well as when underlying vault data changes.
    
    ## Caveats
    N/A
    
    ## Test Plan
    Integration tests pass

--- backend/ci/tests/api/test_duplicates.py
+++ backend/ci/tests/api/test_duplicates.py
@@ -173,3 +173,118 @@ def test_composite_dupes(sandbox_tenant, faker):
         "card_number_cvc",
         "bank_routing_account",
     }
+
+
+def test_composite_fingerprint_dis(sandbox_tenant, faker):
+    nonce = _gen_random_str(8)
+    ssn = _gen_random_ssn()
+    data = {
+        "id.first_name": f"Hayes {nonce}",
+        "id.last_name": "Valley",
+        "id.dob": faker.date(),
+        "id.ssn4": ssn[-4:],
+        "card.hayes.number": faker.credit_card_number("visa"),
+        "card.hayes.expiration": faker.credit_card_expire(),
+        "card.hayes.cvc": faker.credit_card_security_code("visa"),
+        "bank.hayes.ach_routing_number": faker.aba(),
+        "bank.hayes.ach_account_number": faker.bban(),
+    }
+
+    fp_id1 = post("users", data, sandbox_tenant.s_sk, IgnoreCardValidation("true"))[
+        "id"
+    ]
+
+    data["id.last_name"] = data["id.last_name"].lower()
+    fp_id2 = post("users", data, sandbox_tenant.s_sk, IgnoreCardValidation("true"))[
+        "id"
+    ]
+
+    ssn2 = _gen_random_ssn()
+    other_data = {
+        **data,
+        "id.first_name": f"Noe {nonce}",
+        "id.ssn4": ssn2[-4:],
+        "card.hayes.number": faker.credit_card_number("visa"),
+        "bank.hayes.ach_account_number": faker.bban(),
+    }
+    fp_id3 = post(
+        "users", other_data, sandbox_tenant.s_sk, IgnoreCardValidation("true")
+    )["id"]
+
+    # Add an additional card and bank account, ensure that dupes with the original card are not affected
+    ssn3 = _gen_random_ssn()
+    multi_cards_banks = {
+        **data,
+        "id.first_name": f"Mountain {nonce}",
+        "id.ssn4": ssn3[-4:],
+        "card.mountain.number": faker.credit_card_number("visa"),
+        "card.mountain.expiration": faker.credit_card_expire(),
+        "card.mountain.cvc": faker.credit_card_security_code("visa"),
+        "bank.mountain.ach_routing_number": faker.aba(),
+        "bank.mountain.ach_account_number": faker.bban(),
+    }
+    fp_id4 = post(
+        "users", multi_cards_banks, sandbox_tenant.s_sk, IgnoreCardValidation("true")
+    )["id"]
+
+    # decrypt the data
+    fields_to_decrypt = [
+        "bank.hayes.fingerprint",
+        "card.hayes.fingerprint",
+        "bank.mountain.fingerprint",
+        "card.mountain.fingerprint",
+    ]
+    data = dict(
+        reason="test",
+        fields=fields_to_decrypt,
+    )
+
+    body1 = post(f"entities/{fp_id1}/vault/decrypt", data, sandbox_tenant.s_sk)
+    body2 = post(f"entities/{fp_id2}/vault/decrypt", data, sandbox_tenant.s_sk)
+    body3 = post(f"entities/{fp_id3}/vault/decrypt", data, sandbox_tenant.s_sk)
+    body4 = post(f"entities/{fp_id4}/vault/decrypt", data, sandbox_tenant.s_sk)
+
+    assert {
+        body1["bank.hayes.fingerprint"],
+        body2["bank.hayes.fingerprint"],
+        body3["bank.hayes.fingerprint"],
+        body4["bank.hayes.fingerprint"],
+    } == {
+        body1["bank.hayes.fingerprint"],
+        body3["bank.hayes.fingerprint"],
+    }
+
+    assert {
+        body1["card.hayes.fingerprint"],
+        body2["card.hayes.fingerprint"],
+        body3["card.hayes.fingerprint"],
+        body4["card.hayes.fingerprint"],
+    } == {
+        body1["card.hayes.fingerprint"],
+        body3["card.hayes.fingerprint"],
+    }
+
+    assert body4["card.mountain.fingerprint"] != body4["card.hayes.fingerprint"]
+    assert body4["bank.mountain.fingerprint"] != body4["bank.hayes.fingerprint"]
+
+    # Attempt to set card and bank fingerprint DI's
+    patch_data = {
+        "card.hayes.fingerprint": "foo",
+        "bank.hayes.fingerprint": "bar",
+    }
+    body = patch(
+        f"users/{fp_id3}/vault",
+        patch_data,
+        sandbox_tenant.s_sk,
+        IgnoreCardValidation("true"),
+        status_code=400,
+    )
+    assert body["code"] == "T120"
+    assert (
+        body["context"]["bank.hayes.fingerprint"]
+        == "Cannot specify this piece of data. It will automatically be derived."
+    )
+    assert (
+        body["context"]["card.hayes.fingerprint"]
+        == "Cannot specify this piece of data. It will automatically be derived."
+    )
