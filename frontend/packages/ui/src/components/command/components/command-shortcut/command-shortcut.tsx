77d211b5c085656be084bd775fe14aa7f5c1a601
Rafael Michels Motta <rafael@onefootprint.com>
2024-10-03 21:11:52 -0300

    UI:Fix command shortcut crashing (#12474)
    ## Describe your changes
    
    
    ## Caveats
    
    
    ## Test Plan

--- frontend/packages/ui/src/components/command/components/command-shortcut/command-shortcut.tsx
+++ frontend/packages/ui/src/components/command/components/command-shortcut/command-shortcut.tsx
@@ -1,5 +1,5 @@
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
-import { useCallback, useEffect } from 'react';
+import { useEventListener } from 'usehooks-ts';
 
 type CommandShortcutProps = {
   baseKey?: 'Meta' | 'Control' | 'Alt' | 'Shift';
@@ -9,33 +9,27 @@ type CommandShortcutProps = {
 };
 
 const CommandShortcut = ({ baseKey = 'Meta', ctrlKey, onShortcut, onClose }: CommandShortcutProps) => {
-  const handleKeyDown = useCallback(
-    (event: KeyboardEvent) => {
-      const keyMap: { [key: string]: keyof KeyboardEvent } = {
-        Meta: 'metaKey',
-        Control: 'ctrlKey',
-        Alt: 'altKey',
-        Shift: 'shiftKey',
-      };
+  const handleKeyDown = (event: KeyboardEvent) => {
+    const keyMap: Record<string, keyof KeyboardEvent> = {
+      Meta: 'metaKey',
+      Control: 'ctrlKey',
+      Alt: 'altKey',
+      Shift: 'shiftKey',
+    };
 
-      const isBaseKeyPressed = event[keyMap[baseKey] as keyof KeyboardEvent] || false;
-      const isCtrlKeyPressed = event.key.toLowerCase() === ctrlKey.toLowerCase();
+    const isBaseKeyPressed = event[keyMap[baseKey] as keyof KeyboardEvent];
+    const isCtrlKeyPressed = event.key?.toLowerCase() === ctrlKey.toLowerCase();
 
-      if (isBaseKeyPressed && isCtrlKeyPressed) {
-        event.preventDefault();
-        onShortcut();
-      } else if (event.key === 'Escape') {
-        event.preventDefault();
-        onClose();
-      }
-    },
-    [baseKey, ctrlKey, onShortcut, onClose],
-  );
+    if (isBaseKeyPressed && isCtrlKeyPressed) {
+      event.preventDefault();
+      onShortcut();
+    } else if (event.key === 'Escape') {
+      event.preventDefault();
+      onClose();
+    }
+  };
 
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-    return () => document.removeEventListener('keydown', handleKeyDown);
-  }, [handleKeyDown]);
+  useEventListener('keydown', handleKeyDown);
 
   return <VisuallyHidden>{`${baseKey}+${ctrlKey}`}</VisuallyHidden>;
 };
