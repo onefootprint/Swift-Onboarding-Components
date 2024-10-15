5528254ee4f0def80ef83d1a0b405ad78ebb1c1c
Claudio Angrigiani <angrigianicr@gmail.com>
2024-10-15 16:03:00 -0400

    Inner drawer component (#12786)
    https://github.com/user-attachments/assets/0c865192-7077-4b51-b2ce-cf174e151340
    
    
    
    component for @zamandalee's id-doc view project
    
    👉 she'll move from `ui` to local folder -- this component will not
    remain on our UI system for now

---
+++ frontend/packages/ui/src/components/dialog/inner-drawer/inner-drawer.tsx
@@ -0,0 +1,62 @@
+import { IcoChevronRight16 } from '@onefootprint/icons';
+import styled, { css } from 'styled-components';
+import { Box, Stack } from '../../..';
+
+type InnerDrawerProps = {
+  children: React.ReactNode;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onClose: () => void;
+};
+
+const InnerDrawer = ({ children, open, onClose }: InnerDrawerProps) => {
+  return (
+    <Container>
+      <ChevronButtonContainer onClick={onClose}>
+        <Stack
+          alignItems="center"
+          justifyContent="center"
+          style={{ transform: open ? 'rotate(180deg)' : 'rotate(0deg)' }}
+        >
+          <IcoChevronRight16 />
+        </Stack>
+      </ChevronButtonContainer>
+      {children}
+    </Container>
+  );
+};
+
+const Container = styled(Box)`
+${({ theme }) => css`
+    grid-area: drawer;
+    display: flex;
+    flex-direction: column;
+    background-color: ${theme.backgroundColor.primary};
+    border-right: ${theme.borderWidth[1]} solid ${theme.borderColor.tertiary};
+    position: relative;
+`}
+`;
+
+const ChevronButtonContainer = styled(Box)`
+${({ theme }) => css`
+    position: absolute;
+    top: ${theme.spacing[3]};
+    right: 0;
+    transform: translateX(100%);
+    cursor: pointer;
+    height: 32px;
+    padding: ${theme.spacing[3]} ${theme.spacing[2]};
+    border-radius: 0 ${theme.borderRadius.sm} ${theme.borderRadius.sm} 0;
+    border: ${theme.borderWidth[1]} solid ${theme.borderColor.tertiary};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${theme.backgroundColor.primary};
+
+    &:hover {
+      background-color: ${theme.backgroundColor.secondary};
+    }
+`}
+`;
+
+export default InnerDrawer;
