57ba6695249a1d89792746fc403dfcffe219acc5
lucas gelfond <lucas@onefootprint.com>
2024-10-16 14:50:01 -0700

    [PR-275] use-sentilink-signal hook (#12819)
    ## Describe your changes
    
    Title! Uses the API defined here:
    https://docs.onefootprint.com/internal-api/dashboard#post-entities-fp-id-sentilink-signal-id
    
    ## Caveats
    
    N/A - not visible to users yet 
    
    ## Test Plan
    
    N/A

---
+++ frontend/apps/dashboard/src/components/entities/components/details/components/content/components/risk-signals/components/risk-signals-list/hooks/use-entity-sentilink-signal/use-entity-sentilink-signal.ts
@@ -0,0 +1,33 @@
+import request from '@onefootprint/request';
+import type { GetEntitySentilinkSignalRequest, GetEntitySentilinkSignalResponse } from '@onefootprint/types';
+import { useQuery } from '@tanstack/react-query';
+import useSession from 'src/hooks/use-session';
+import type { AuthHeaders } from 'src/hooks/use-session';
+
+type UseEntitySentilinkSignalProps = {
+  entityId: string;
+  riskSignalId: string;
+};
+
+const getSentilinkSignal = async (payload: GetEntitySentilinkSignalRequest, authHeaders: AuthHeaders) => {
+  const { data: response } = await request<GetEntitySentilinkSignalResponse>({
+    headers: authHeaders,
+    method: 'POST',
+    url: `/entities/${payload.entityId}/sentilink/${payload.riskSignalId}`,
+  });
+  return response;
+};
+
+const useEntitySentilinkSignal = ({ entityId, riskSignalId }: UseEntitySentilinkSignalProps) => {
+  const { authHeaders } = useSession();
+
+  const sentilinkSignalQuery = useQuery({
+    queryKey: ['entity', entityId, 'sentilink', riskSignalId],
+    queryFn: () => getSentilinkSignal({ entityId, riskSignalId }, authHeaders),
+    enabled: !!riskSignalId,
+  });
+
+  return sentilinkSignalQuery;
+};
+
+export default useEntitySentilinkSignal;
