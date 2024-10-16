c43db1e42f551f9e652c14f24b6c4fff6f901176
lucas gelfond <lucas@onefootprint.com>
2024-10-16 16:58:13 -0700

    Fix overflow bug (#12880)
    ## Describe your changes
    
    Title.
    
    Before:
    
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/9077d67a-12ee-4c47-9a49-89e02396bbce.png)
    
    After:
    
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/25f5c938-6370-4273-9ecc-05267380c18f.png)
    
    ## Caveats
    
    
    ## Test Plan
    
    In UI

--- frontend/apps/dashboard/src/components/field-or-placeholder/field-or-placeholder.tsx
+++ frontend/apps/dashboard/src/components/field-or-placeholder/field-or-placeholder.tsx
@@ -17,7 +17,7 @@ const FieldOrPlaceholder = ({ data, transforms }: FieldOrPlaceholderProps) => {
 
   if (isVaultDataText(data)) {
     return (
-      <Text isPrivate variant="body-3" color="primary">
+      <Text isPrivate variant="body-3" color="primary" flexWrap="wrap" wordBreak="break-all" textAlign="end">
         {data}
       </Text>
     );
