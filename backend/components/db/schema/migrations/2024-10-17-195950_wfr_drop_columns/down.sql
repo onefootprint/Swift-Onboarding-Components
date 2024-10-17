89f1c9c4449ce4ac7f6eadca6bb2ec749a04f454
Elliott Forde <elliott@onefootprint.com>
2024-10-17 15:48:32 -0700

    [4/4][BE-1712] Drop legacy WFR columns (#12955)
    ## Describe your changes
    
    
    ## Caveats
    
    
    ## Test Plan
    ```
    footprint=> select * from workflow_request where not exists (select 1 from workflow_request_junction where workflow_request_id = workflow_request.id and scoped_vault_id = workflow_request.scoped_Vault_id);
     id | _created_at | _updated_at | timestamp | deactivated_at | scoped_vault_id | ob_configuration_id | created_by | workflow_id | config | note
    ----+-------------+-------------+-----------+----------------+-----------------+---------------------+------------+-------------+--------+------
    (0 rows)
    ```
    double checked the backfill

---
+++ backend/components/db/schema/migrations/2024-10-17-195950_wfr_drop_columns/down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE workflow_request ADD COLUMN scoped_vault_id TEXT;
+ALTER TABLE workflow_request ADD COLUMN workflow_id TEXT;
---
+++ backend/components/db/schema/migrations/2024-10-17-195950_wfr_drop_columns/up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE workflow_request DROP COLUMN scoped_vault_id;
+ALTER TABLE workflow_request DROP COLUMN workflow_id;
