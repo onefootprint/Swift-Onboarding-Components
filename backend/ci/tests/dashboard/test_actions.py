00e2e2a70543a7d9edf5628a44d655fa534385d8
Elliott Forde <elliott@onefootprint.com>
2024-10-18 09:24:43 -0700

    Add integration test coverage of businesses in POST /entities/<>/actions (#12992)
    ## Describe your changes
    
    https://onefootprint.slack.com/archives/C05U1CAD6FQ/p1729263130403349?thread_ts=1729257141.075119&cid=C05U1CAD6FQ
    
    ## Caveats
    
    
    ## Test Plan

--- backend/ci/tests/dashboard/test_actions.py
+++ backend/ci/tests/dashboard/test_actions.py
@@ -4,10 +4,10 @@ from tests.utils import post, get
 from tests.headers import FpAuth
 
 
+# TODO also test business
 def test_trigger_action(sandbox_tenant):
     bifrost = BifrostClient.new_user(sandbox_tenant.default_ob_config)
     user = bifrost.run()
-
     trigger = dict(
         kind="onboard", data=dict(playbook_id=sandbox_tenant.default_ob_config.id)
     )
@@ -26,42 +26,61 @@ def test_trigger_action(sandbox_tenant):
     bifrost.run()
 
 
-def test_manual_decision_action(sandbox_tenant):
-    bifrost = BifrostClient.new_user(sandbox_tenant.default_ob_config)
+def test_cannot_trigger_action_business(sandbox_tenant, kyb_sandbox_ob_config):
+    bifrost = BifrostClient.new_user(kyb_sandbox_ob_config)
+    user = bifrost.run()
+    trigger = dict(kind="onboard", data=dict(playbook_id=kyb_sandbox_ob_config.id))
+    trigger_action = dict(kind="trigger", trigger=trigger, note="Flerp")
+    data = dict(actions=[trigger_action])
+    fp_bid = user.fp_bid
+    body = post(
+        f"entities/{fp_bid}/actions", data, *sandbox_tenant.db_auths, status_code=400
+    )
+    assert body["message"] == "Must be a person vault"
+
+
+def test_manual_decision_action(sandbox_tenant, kyb_sandbox_ob_config):
+    bifrost = BifrostClient.new_user(kyb_sandbox_ob_config)
     user = bifrost.run()
     assert user.client.validate_response["user"]["status"] == "pass"
+    assert user.client.validate_response["business"]["status"] == "pass"
 
-    md_action = dict(
-        kind="manual_decision",
-        status="fail",
-        annotation=dict(note="Flerp", is_pinned=False),
-    )
-    data = dict(actions=[md_action])
-    post(f"entities/{user.fp_id}/actions", data, *sandbox_tenant.db_auths)
-    body = get(f"entities/{user.fp_id}", data, *sandbox_tenant.db_auths)
-    assert body["status"] == "fail"
+    # Should be able to manually review both the user and the business
+    for fp_id in [user.fp_id, user.fp_bid]:
+        md_action = dict(
+            kind="manual_decision",
+            status="fail",
+            annotation=dict(note="Flerp", is_pinned=False),
+        )
+        data = dict(actions=[md_action])
+        post(f"entities/{fp_id}/actions", data, *sandbox_tenant.db_auths)
+        body = get(f"entities/{fp_id}", data, *sandbox_tenant.db_auths)
+        assert body["status"] == "fail"
 
 
-def test_clear_review(sandbox_tenant):
+def test_clear_review(sandbox_tenant, kyb_sandbox_ob_config):
     bifrost = BifrostClient.new_user(
-        sandbox_tenant.default_ob_config, fixture_result="manual_review"
+        kyb_sandbox_ob_config, fixture_result="manual_review"
     )
     user = bifrost.run()
     assert user.client.validate_response["user"]["status"] == "fail"
     assert user.client.validate_response["user"]["requires_manual_review"]
+    assert user.client.validate_response["business"]["status"] == "fail"
+    assert user.client.validate_response["business"]["requires_manual_review"]
 
-    body = get(f"users/{user.fp_id}/onboardings", None, sandbox_tenant.s_sk)
-    assert body["data"][0]["status"] == "fail"
+    for fp_id in [user.fp_id, user.fp_bid]:
+        body = get(f"users/{fp_id}/onboardings", None, sandbox_tenant.s_sk)
+        assert body["data"][0]["status"] == "fail"
 
-    clear_review_action = dict(kind="clear_review")
-    data = dict(actions=[clear_review_action])
-    post(f"entities/{user.fp_id}/actions", data, *sandbox_tenant.db_auths)
-    body = get(f"entities/{user.fp_id}", data, *sandbox_tenant.db_auths)
-    assert not body["requires_manual_review"]
+        clear_review_action = dict(kind="clear_review")
+        data = dict(actions=[clear_review_action])
+        post(f"entities/{fp_id}/actions", data, *sandbox_tenant.db_auths)
+        body = get(f"entities/{fp_id}", data, *sandbox_tenant.db_auths)
+        assert not body["requires_manual_review"]
 
-    # Make sure the onboarding status hasn't changed
-    body = get(f"users/{user.fp_id}/onboardings", None, sandbox_tenant.s_sk)
-    assert body["data"][0]["status"] == "fail"
+        # Make sure the onboarding status hasn't changed
+        body = get(f"users/{fp_id}/onboardings", None, sandbox_tenant.s_sk)
+        assert body["data"][0]["status"] == "fail"
 
 
 def test_trigger_and_clear_review(sandbox_tenant):
