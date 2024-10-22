48002062f1de4314c08ebbc1e87ff59717ac5d6f
Ethan Lowman <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-10-22 18:23:56 -0400

    Make fk_vault_dr_blob_dl_created_at_svv_id not valid (#13170)
    ## Describe your changes
    It's timing out.
    
    ## Caveats
    
    
    ## Test Plan

--- backend/components/db/schema/migrations/2024-10-22-145656_vdr-blob-denormalize-created-at-svv/up.sql
+++ backend/components/db/schema/migrations/2024-10-22-145656_vdr-blob-denormalize-created-at-svv/up.sql
@@ -4,4 +4,4 @@ ALTER TABLE vault_dr_blob
 ADD CONSTRAINT fk_vault_dr_blob_dl_created_at_svv_id
     FOREIGN KEY (dl_created_at_svv_id)
     REFERENCES scoped_vault_version(id)
-    DEFERRABLE INITIALLY DEFERRED;
+    DEFERRABLE INITIALLY DEFERRED NOT VALID;
