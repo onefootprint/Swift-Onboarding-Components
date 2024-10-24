a5d379263c7c47651ff6fcf31e9f5524ad483de0
Bruno Batista <bruno@onefootprint.com>
2024-10-24 08:20:18 -0300

    chore(idv) replace styled.div for Box (#13198)
    ## Describe your changes
    
    This replaces a `styled.div` for a `Box` component

--- frontend/packages/idv/src/plugins/collect-kyb-data/pages/loading/loading.tsx
+++ frontend/packages/idv/src/plugins/collect-kyb-data/pages/loading/loading.tsx
@@ -1,11 +1,10 @@
 import { Box, Shimmer, Stack } from '@onefootprint/ui';
-import styled from 'styled-components';
 
 type LoadingProps = { children?: JSX.Element | null };
 
 const Loading = ({ children }: LoadingProps): JSX.Element => (
   <Box>
-    <NavHeader />
+    <Box width="100%" height="var(--navigation-header-height)" />
     <Stack flexDirection="column" justifyContent="center" alignItems="center" marginBottom={8}>
       <Shimmer height="28px" width="272px" marginBottom={5} />
       <Shimmer height="70px" width="340px" />
@@ -16,10 +15,5 @@ const Loading = ({ children }: LoadingProps): JSX.Element => (
     {children}
   </Box>
 );
-
-const NavHeader = styled.div`
-  width: 100%;
-  height: var(--navigation-header-height);
-`;
 
 export default Loading;
