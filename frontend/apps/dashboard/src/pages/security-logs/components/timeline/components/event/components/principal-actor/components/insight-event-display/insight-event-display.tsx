7c26033ab73675e00df1195b0b933ef465863337
lucasgelfond <8399069+lucasgelfond@users.noreply.github.com>
2024-11-01 16:40:01 +0000

    [PR-357] redesign insight display card (#13580)
    ## Describe your changes
    
    Pedro wanted these to look a bit different, so we changed em!
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/1d56f454-f68b-4cfe-bb6f-144d23c9e3f7.png)
    
    ## Caveats
    
    ## Test Plan
    
    Pedro and in UI

--- frontend/apps/dashboard/src/pages/security-logs/components/timeline/components/event/components/principal-actor/components/insight-event-display/insight-event-display.tsx
+++ frontend/apps/dashboard/src/pages/security-logs/components/timeline/components/event/components/principal-actor/components/insight-event-display/insight-event-display.tsx
@@ -1,3 +1,4 @@
+import { IcoSparkles16 } from '@onefootprint/icons';
 import type { InsightEvent } from '@onefootprint/types';
 import { Stack, Text } from '@onefootprint/ui';
 import { useTranslation } from 'react-i18next';
@@ -17,7 +18,7 @@ const InsightEventDisplay = ({ insightEvent }: InsightEventDisplayProps) => {
       direction="column"
       paddingInline={5}
       paddingBlock={4}
-      gap={6}
+      gap={5}
       backgroundColor="primary"
       aria-label={t('aria-label')}
       borderColor="tertiary"
@@ -25,33 +26,47 @@ const InsightEventDisplay = ({ insightEvent }: InsightEventDisplayProps) => {
       borderWidth={1}
       borderRadius="default"
     >
-      <Text variant="label-3">{t('title')}</Text>
-      <Stack justifyContent="space-between" width="443px">
-        <Stack direction="column" gap={3}>
-          <Text variant="body-3" color="tertiary">
-            {t('labels.region')}
-          </Text>
-          <Text variant="body-3" color="tertiary">
-            {t('labels.country')}
-          </Text>
-          <Text variant="body-3" color="tertiary">
-            {t('labels.postal-code')}
-          </Text>
-          <Text variant="body-3" color="tertiary">
-            {t('labels.ip-address')}
-          </Text>
-          <Text variant="body-3" color="tertiary">
-            {t('labels.user-agent')}
-          </Text>
+      <Stack gap={1}>
+        <IcoSparkles16 />
+        <Text variant="label-3">{t('title')}</Text>
+      </Stack>
+      <Stack justifyContent="space-between" width="420px">
+        <Stack direction="column" gap={5}>
+          <Stack direction="column" gap={2}>
+            <Text variant="body-3" color="tertiary">
+              {t('labels.region')}
+            </Text>
+            <Text variant="body-3">{insightEvent?.region || '-'}</Text>
+          </Stack>
+
+          <Stack direction="column" gap={2}>
+            <Text variant="body-3" color="tertiary">
+              {t('labels.country')}
+            </Text>
+            <Text variant="body-3">{insightEvent?.country || '-'}</Text>
+          </Stack>
+
+          <Stack direction="column" gap={2}>
+            <Text variant="body-3" color="tertiary">
+              {t('labels.postal-code')}
+            </Text>
+            <Text variant="body-3">{insightEvent?.postalCode || '-'}</Text>
+          </Stack>
         </Stack>
-        <Stack direction="column" gap={3} width="221px">
-          <Text variant="body-3">{insightEvent?.region || '-'}</Text>
-          <Text variant="body-3">{insightEvent?.country || '-'}</Text>
-          <Text variant="body-3">{insightEvent?.postalCode || '-'}</Text>
-          <Text variant="body-3">{insightEvent?.ipAddress || '-'}</Text>
-          <Text variant="body-3" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
-            {insightEvent?.userAgent || '-'}
-          </Text>
+        <Stack direction="column" gap={5} width="221px">
+          <Stack direction="column" gap={2}>
+            <Text variant="body-3" color="tertiary">
+              {t('labels.ip-address')}
+            </Text>
+            <Text variant="body-3">{insightEvent?.ipAddress || '-'}</Text>
+          </Stack>
+
+          <Stack direction="column" gap={2}>
+            <Text variant="body-3" color="tertiary">
+              {t('labels.user-agent')}
+            </Text>
+            <Text variant="body-3">{insightEvent?.userAgent || '-'}</Text>
+          </Stack>
         </Stack>
       </Stack>
     </ShadowStack>
