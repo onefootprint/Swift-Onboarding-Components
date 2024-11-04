354622a9428555dab94cccccc7283e6df34c322b
Bruno Batista <bruno@onefootprint.com>
2024-11-04 14:40:36 -0300

    feat(apps): FE-752 set @/idv alias across apps (#13658)
    ## Describe your changes
    
    Added `@/idv/*` path on `@onefootprint/idv`
    ```
    "@/idv/*": ["./src/*"]
    ```
    
    Added `@/idv/*` path on every app that depends on `@onefootprint/idv`
    
    The `@/idv/*` alias, is not a replacement for `@onefootprint/idv`. 
    Every app (`auth, bifrost, components, handoff, hosted`) that use
    `@onefootprint/idv`, need to understand the internal paths within the
    `@onefootprint/idv` package
    
    ```
    "@/idv/*": ["node_modules/@onefootprint/idv/src/*"],
    ```

--- frontend/packages/idv/jest.config.ts
+++ frontend/packages/idv/jest.config.ts
@@ -95,7 +95,6 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '@/investor-profile/(.*)': '<rootDir>/src/plugins/investor-profile/$1',
-    '@/identify/(.*)': '<rootDir>/src/pages/identify/$1',
   },
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
