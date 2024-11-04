48b5d417afbcefd7ec3a85d89d4c85ce8041adf5
Rafael Michels Motta <rafael@onefootprint.com>
2024-11-04 20:56:19 -0300

    Dashboard: improve cache (#13670)
    ## Describe your changes
    
    
    ## Caveats
    
    
    ## Test Plan

--- frontend/apps/dashboard/src/components/layout/hooks/use-request-headers.ts
+++ frontend/apps/dashboard/src/components/layout/hooks/use-request-headers.ts
@@ -1,16 +1,34 @@
 import { client } from '@onefootprint/axios/dashboard';
 import { useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
+import {
+  DASHBOARD_ALLOW_ASSUMED_WRITES,
+  DASHBOARD_AUTHORIZATION_HEADER,
+  DASHBOARD_IS_LIVE_HEADER,
+} from 'src/config/constants';
 import useSession from 'src/hooks/use-session';
 
 const useRequestHeaders = () => {
-  const { authHeaders, isLive } = useSession();
+  const {
+    data: { auth },
+    isLive,
+    isAssumedSessionEditMode,
+  } = useSession();
   const queryClient = useQueryClient();
 
   useEffect(() => {
-    client.setConfig({ headers: authHeaders });
+    if (!auth) return;
+    client.setConfig({
+      headers: {
+        [DASHBOARD_AUTHORIZATION_HEADER]: auth,
+        [DASHBOARD_IS_LIVE_HEADER]: JSON.stringify(isLive),
+        ...(isAssumedSessionEditMode && {
+          [DASHBOARD_ALLOW_ASSUMED_WRITES]: JSON.stringify(true),
+        }),
+      },
+    });
     queryClient.resetQueries();
-  }, [isLive]);
+  }, [auth, isLive, isAssumedSessionEditMode]);
 };
 
 export default useRequestHeaders;
