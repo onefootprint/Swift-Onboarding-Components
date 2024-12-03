59e49def7cd5d5962ce0592a75a4e7cf9dbcfb34
lucas gelfond <lucas@onefootprint.com>
2024-12-03 13:36:23 -0800

    [PR-464] list locales (#14306)
    ## Describe your changes
    
    Copy-pasted directly. Easy change to isolate! Obviously this will change
    any all translations right now are unused
    
    ## Caveats
    
    
    ## Test Plan
    
    Typechecker / CI

--- frontend/apps/dashboard/src/config/initializers/react-i18next.ts
+++ frontend/apps/dashboard/src/config/initializers/react-i18next.ts
@@ -12,6 +12,7 @@ import entityDetails from '../locales/en/entity-details.json';
 import entityDocuments from '../locales/en/entity-documents.json';
 import home from '../locales/en/home.json';
 import internal from '../locales/en/internal.json';
+import lists from '../locales/en/lists.json';
 import onboarding from '../locales/en/onboarding.json';
 import playbookDetails from '../locales/en/playbook-details.json';
 import playbooks from '../locales/en/playbooks.json';
@@ -43,6 +44,7 @@ const configureReactI18next = () => {
         common,
         home,
         internal,
+        lists,
         onboarding,
         playbooks,
         roles,
