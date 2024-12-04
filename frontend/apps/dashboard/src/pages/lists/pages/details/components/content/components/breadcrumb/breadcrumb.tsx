ee63216edcd68c51743d4719e9fb8923bd89453f
lucasgelfond <8399069+lucasgelfond@users.noreply.github.com>
2024-12-04 21:26:40 +0000

    [PR-468] breadcrumb (#14335)
    ## Describe your changes
    
    Title! Identical to previous implementation
    
    ![Screenshot 2024-12-03 at 3.53.11 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/5b07c498-1a0c-4a1b-a43b-a711a8ec9540.png)
    
    ## Caveats
    
    ## Test Plan

---
+++ frontend/apps/dashboard/src/pages/lists/pages/details/components/content/components/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,19 @@
+import { Breadcrumb as UIBreadcrumb } from '@onefootprint/ui';
+import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
+
+const Breadcrumb = () => {
+  const { t } = useTranslation('lists', {
+    keyPrefix: 'details.breadcrumb',
+  });
+  return (
+    <UIBreadcrumb.List aria-label={t('title')}>
+      <UIBreadcrumb.Item href="/lists" as={Link}>
+        {t('title')}
+      </UIBreadcrumb.Item>
+      <UIBreadcrumb.Item>{t('details')}</UIBreadcrumb.Item>
+    </UIBreadcrumb.List>
+  );
+};
+
+export default Breadcrumb;
