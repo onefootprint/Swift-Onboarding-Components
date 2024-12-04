69c9749316248f9b4a031d6079eda056364f7114
lucasgelfond <8399069+lucasgelfond@users.noreply.github.com>
2024-12-04 21:26:50 +0000

    [PR-480] activity log loading shimmer (#14339)
    ## Describe your changes
    
    Title! With tailwind, fixed the gaps also.
    
    ![Screenshot 2024-12-03 at 4.16.53 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/8fa6031a-0e17-45dd-866c-f2832d61bc72.png)
    
    ## Caveats
    
    ## Test Plan

---
+++ frontend/apps/dashboard/src/pages/lists/pages/details/components/content/components/activity-log/components/loading/loading.tsx
@@ -0,0 +1,12 @@
+import { Shimmer } from '@onefootprint/ui';
+
+const Loading = () => (
+  <div className="flex flex-col gap-2">
+    <div className="w-full flex justify-between items-center flex-row gap-2 pb-2 border-b border-tertiary">
+      <Shimmer height="27px" width="300px" />
+    </div>
+    <Shimmer height="20px" width="100%" />
+  </div>
+);
+
+export default Loading;
