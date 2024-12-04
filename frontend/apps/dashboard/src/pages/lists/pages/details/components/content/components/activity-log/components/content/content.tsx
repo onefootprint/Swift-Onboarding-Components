2ebba9c3d6ff797f39d3cdff2af0b0998af0d52c
lucasgelfond <8399069+lucasgelfond@users.noreply.github.com>
2024-12-04 21:26:48 +0000

    [PR-479] activity log content (#14338)
    ## Describe your changes
    
    Title. I'm not positive I'm handling the member stuff here correctly, but easier to see when we have all of the pieces in. Mostly just updating the requests / tailwind of the code that previously was there:
    
    ![Screenshot 2024-12-03 at 4.09.08 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/8eba8160-9682-416e-bdb8-ce4c46594c04.png)
    
    ![Screenshot 2024-12-03 at 4.09.12 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/d4b3b42d-2ff7-4b83-a9d2-529a58fb88e4.png)
    
    ## Caveats
    
    ## Test Plan

---
+++ frontend/apps/dashboard/src/pages/lists/pages/details/components/content/components/activity-log/components/content/content.tsx
@@ -0,0 +1,48 @@
+import type { ListEvent } from '@onefootprint/request-types/dashboard';
+import { useTranslation } from 'react-i18next';
+import type { TimelineItem } from 'src/components/timeline';
+import Timeline from 'src/components/timeline';
+import ListEntryCreatedEventHeader from './components/list-entry-created-event-header';
+import ListEntryDeletedEventHeader from './components/list-entry-deleted-event-header';
+
+type ContentProps = {
+  timeline: ListEvent[];
+};
+
+const Content = ({ timeline }: ContentProps) => {
+  const { t } = useTranslation('lists', {
+    keyPrefix: 'details.activity-log',
+  });
+
+  const items: TimelineItem[] = [];
+
+  timeline.forEach(({ timestamp, principal, detail }: ListEvent) => {
+    const { kind } = detail;
+
+    // TK - timeline events for createList and updateList
+    if (kind === 'create_list_entry' && principal?.kind === 'user') {
+      items.push({
+        time: { timestamp },
+        // TK - possibly come back, not sure if we actually want to pass the ID
+        // down as the name / what was supposed to be here
+        // still can't see any of this so unclear
+        headerComponent: <ListEntryCreatedEventHeader user={principal.id} event={detail} />,
+      });
+    } else if (kind === 'delete_list_entry' && principal?.kind === 'user') {
+      items.push({
+        time: { timestamp },
+        headerComponent: <ListEntryDeletedEventHeader user={principal.id} event={detail} />,
+      });
+    }
+  });
+
+  return timeline.length > 0 ? (
+    <div className="w-full">
+      <Timeline items={items} />
+    </div>
+  ) : (
+    <div className="text-center text-body-3 text-tertiary">{t('empty')}</div>
+  );
+};
+
+export default Content;
