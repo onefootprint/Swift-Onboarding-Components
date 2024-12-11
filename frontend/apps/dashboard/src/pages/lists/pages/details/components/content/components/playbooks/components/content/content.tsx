253efc2b36fbb7f89c4cf009485a32bf82d97a09
lucasgelfond <8399069+lucasgelfond@users.noreply.github.com>
2024-12-11 16:53:13 +0000

    feat(lists-v2): [PR-490] playbooks content (#14422)
    ## Describe your changes
    
    Title - scaffolding so we're not going blind:
    
    ![Screenshot 2024-12-05 at 2.53.40 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/XsTtzwNbP0GdQMHxY1hw/e186011f-e2b1-45da-8ca6-f2028fe558a5.png)
    
    ## Caveats
    
    ## Test Plan

---
+++ frontend/apps/dashboard/src/pages/lists/pages/details/components/content/components/playbooks/components/content/content.tsx
@@ -0,0 +1,50 @@
+// import Rule from './components/rule';
+import type { ListDetails } from '@onefootprint/request-types/dashboard';
+import { LinkButton } from '@onefootprint/ui';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'react-i18next';
+
+type ContentProps = {
+  list: ListDetails;
+};
+
+const Content = ({ list }: ContentProps) => {
+  const { t } = useTranslation('lists', { keyPrefix: 'details.playbooks' });
+  const router = useRouter();
+
+  const handleClickPlaybook = (playbookId: string) => {
+    router.push({
+      pathname: `/playbooks/${playbookId}`,
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-6 mt-3">
+      {list.playbooks?.map(playbook => (
+        <div className="flex flex-col gap-3" key={playbook.id}>
+          <div className="flex flex-row gap-2 items-center">
+            <span className="text-primary text-label-3">{playbook.name}</span>
+            <span className="text-primary text-label-2">•</span>
+            <LinkButton onClick={() => handleClickPlaybook(playbook.id)} variant="label-3">
+              {t('details')}
+            </LinkButton>
+          </div>
+
+          <div className="flex flex-row gap-2 items-baseline pl-4">
+            <span className="text-tertiary text-label-3 min-w-[44px]">{t('in-rule')}</span>
+            <div className="flex flex-col gap-2">
+              {playbook.rules.map(rule => (
+                <div key={rule.ruleId}>{rule.ruleId}</div>
+                // <Rule key={rule.ruleId} rule={rule} />
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+
+      {!list.playbooks?.length && <div className="text-body-3 text-tertiary">{t('empty')}</div>}
+    </div>
+  );
+};
+
+export default Content;
