d945f963a94e28f4a0affcd99a05ebb7dcf9b350
dave-argoff <105253942+dave-argoff@users.noreply.github.com>
2024-12-12 00:51:24 +0000

    refactor biz insights to helper method (#14577)
    ## Describe your changes
    prep to add onboarding specific endpoint for biz insights
    
    ## Caveats
    
    ## Test Plan
    existing

--- backend/components/api/routes/entities/src/business_insight.rs
+++ backend/components/api/routes/entities/src/business_insight.rs
@@ -9,12 +9,15 @@ use api_core::utils::fp_id_path::FpIdPath;
 use api_core::utils::vault_wrapper::Any;
 use api_core::utils::vault_wrapper::VaultWrapper;
 use api_core::utils::vault_wrapper::VwArgs;
+use api_core::FpResult;
 use api_errors::BadRequest;
 use db::models::scoped_vault::ScopedVault;
 use db::models::verification_request::VReqIdentifier;
+use idv::middesk::response::business::BusinessResponse;
 use newtypes::vendor_api_struct::MiddeskBusinessUpdateWebhook;
 use newtypes::vendor_api_struct::MiddeskGetBusiness;
 use newtypes::BusinessDataKind as BDK;
+use newtypes::ScopedVaultId;
 use paperclip::actix::api_v2_operation;
 use paperclip::actix::get;
 use paperclip::actix::web;
@@ -33,7 +36,6 @@ pub async fn get_business_insights(
     let tenant_id = auth.tenant().id.clone();
     let is_live = auth.is_live()?;
     let fp_id = request.into_inner();
-
     let (sv, vw) = state
         .db_query(move |conn| {
             let sv = ScopedVault::get(conn, (&fp_id, &tenant_id, is_live))?;
@@ -42,9 +44,19 @@ pub async fn get_business_insights(
         })
         .await?;
 
+    let business_response = get_business_response(&state, sv.id, vw).await?;
+    Ok(BusinessInsights::from(business_response))
+}
+
+#[tracing::instrument(skip(state))]
+async fn get_business_response(
+    state: &State,
+    sv_id: ScopedVaultId,
+    vw: VaultWrapper<Any>,
+) -> FpResult<BusinessResponse> {
     let webhook_biz_response = load_response_for_vendor_api(
-        &state,
-        VReqIdentifier::LatestForSv(sv.id.clone()),
+        state,
+        VReqIdentifier::LatestForSv(sv_id.clone()),
         &vw.vault.e_private_key,
         MiddeskBusinessUpdateWebhook,
     )
@@ -52,20 +64,18 @@ pub async fn get_business_insights(
     .ok()
     .and_then(|(r, _)| r.business_response().cloned());
 
-    let biz_response = load_response_for_vendor_api(
-        &state,
-        VReqIdentifier::LatestForSv(sv.id.clone()),
-        &vw.vault.e_private_key,
-        MiddeskGetBusiness,
-    )
-    .await?
-    .ok()
-    .map(|(r, _)| r);
+    let business_response = match webhook_biz_response {
+        Some(response) => Some(response),
+        None => load_response_for_vendor_api(
+            state,
+            VReqIdentifier::LatestForSv(sv_id),
+            &vw.vault.e_private_key,
+            MiddeskGetBusiness,
+        )
+        .await?
+        .ok()
+        .map(|(r, _)| r),
+    };
 
-    let business_response = webhook_biz_response
-        .or(biz_response)
-        .ok_or(BadRequest("no vendor response found for middesk"))?;
-
-
-    Ok(BusinessInsights::from(business_response))
+    business_response.ok_or(BadRequest("no KYB response found"))
 }
