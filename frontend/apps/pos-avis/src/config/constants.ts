a9408a4977adc3774bbaac40db38fa04856eeb93
Rafael Michels Motta <rafael@onefootprint.com>
2024-12-17 13:24:10 -0300

    POS: prepare preview

--- frontend/apps/pos-avis/src/config/constants.ts
+++ frontend/apps/pos-avis/src/config/constants.ts
@@ -1,2 +1,7 @@
-export const DEV_OB_CONFIG_KEY = 'pb_test_uStuJNEaq6dS16QYu3CDOK';
-export const PROD_OB_CONFIG_KEY = 'pb_test_uStuJNEaq6dS16QYu3CDOK';
+export const SHOULD_USE_DEV =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development';
+
+const DEV_OB_CONFIG_KEY = 'pb_test_Ly508VDujEz1kQPrkwSyHu';
+const PROD_OB_CONFIG_KEY = 'pb_test_uStuJNEaq6dS16QYu3CDOK';
+
+export const OB_CONFIG_KEY = SHOULD_USE_DEV ? DEV_OB_CONFIG_KEY : PROD_OB_CONFIG_KEY;
--- frontend/apps/pos-avis/src/config/initializers/request.ts
+++ frontend/apps/pos-avis/src/config/initializers/request.ts
@@ -1,9 +1,9 @@
 import { client } from '@onefootprint/axios';
-import { DEV_OB_CONFIG_KEY } from '../../config/constants';
+import { OB_CONFIG_KEY, SHOULD_USE_DEV } from '../../config/constants';
 import createSandboxId from '../../utils/create-sandbox-id';
 
 const OVERRIDE_API_HOST_PARAM = 'apiHost';
-const DEFAULT_API_URL = 'https://api.onefootprint.com';
+const DEFAULT_API_URL = SHOULD_USE_DEV ? 'https://api.dev.onefootprint.com' : 'https://api.onefootprint.com';
 
 const getOverrideApiBaseUrl = (): string | null => {
   const urlParams = new URLSearchParams(window.location.search);
@@ -15,7 +15,7 @@ client.setConfig({
   baseURL: getOverrideApiBaseUrl() || process.env.REACT_APP_FP_API_URL || DEFAULT_API_URL,
   headers: {
     'X-Fp-Client-Version': 'frontend-avis',
-    'X-Onboarding-Config-Key': DEV_OB_CONFIG_KEY,
+    'X-Onboarding-Config-Key': OB_CONFIG_KEY,
     // TODO: Remove
     'X-Sandbox-Id': createSandboxId(),
   },
