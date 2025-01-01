7e1656973314db053079beaad88024c6871b9958
Claudio Angrigiani <angrigianicr@gmail.com>
2025-01-01 18:09:41 -0500

    Frontpage copy change (#14839)
    ![CleanShot 2025-01-01 at 15 51
    41@2x](https://github.com/user-attachments/assets/7c1e84af-c733-443f-8da3-a7ebc38e7931)

--- frontend/apps/frontpage/src/pages/home/sections/hero/hero.tsx
+++ frontend/apps/frontpage/src/pages/home/sections/hero/hero.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import { uniqueId } from 'lodash';
 import dynamic from 'next/dynamic';
 import { useTranslation } from 'react-i18next';
 import Ctas from 'src/components/ctas';
@@ -7,59 +6,39 @@ import FrontpageContainer from 'src/components/frontpage-container';
 import CustomersLogos from './components/customers-logos';
 const Screen = dynamic(() => import('./components/screen'));
 
-const letterStagger = {
-  hidden: { opacity: 0, filter: 'blur(10px)' },
-  visible: {
-    opacity: 1,
-    filter: 'blur(0px)',
-    transition: { duration: 0.1 },
-  },
+const staggerVariants = {
+  hidden: { opacity: 0, y: -5, filter: 'blur(10px)' },
+  visible: { opacity: 1, y: 0, filter: 'blur(0px)', transition: { staggerChildren: 0.1 } },
 };
 
-const buttonsAppear = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: { duration: 0.4, delay: 1.2 },
-  },
+const textVariants = {
+  hidden: { opacity: 0, y: -5, filter: 'blur(10px)' },
+  visible: { opacity: 1, y: 0, filter: 'blur(0px)', transition: { duration: 0.5 } },
 };
 
 const Hero = () => {
   const { t } = useTranslation('common', { keyPrefix: 'pages.home' });
 
-  const renderTextWithLetterStagger = (text: string) => {
-    return text.split('').map(char => (
-      <motion.span variants={letterStagger} key={uniqueId()}>
-        {char}
-      </motion.span>
-    ));
-  };
-
   return (
     <div className="relative">
       <div className="z-10 overflow-hidden">
         <FrontpageContainer className="relative flex flex-col items-center gap-24 py-10">
-          <div className="flex flex-col items-center justify-center">
-            <motion.h1
-              initial="hidden"
-              animate="visible"
-              variants={{ visible: { transition: { staggerChildren: 0.02 } } }}
-              className="text-center max-w-[580px] text-display-2 md:text-display-1"
-            >
-              {renderTextWithLetterStagger(t('hero.title'))}
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={staggerVariants}
+            className="flex flex-col items-center justify-center mx-4"
+          >
+            <motion.h1 variants={textVariants} className="text-center max-w-[580px] text-display-2 md:text-display-1">
+              {t('hero.title')}
             </motion.h1>
-            <motion.h2
-              initial="hidden"
-              animate="visible"
-              variants={{ visible: { transition: { delayChildren: 0.5, staggerChildren: 0.01 } } }}
-              className="text-center max-w-[520px] text-display-4 mt-2"
-            >
-              {renderTextWithLetterStagger(t('hero.subtitle'))}
+            <motion.h2 variants={textVariants} className="text-center text-secondary max-w-[620px] text-display-4 mt-2">
+              {t('hero.subtitle')}
             </motion.h2>
-            <motion.div variants={buttonsAppear} initial="hidden" animate="visible" className="w-full mt-6">
+            <motion.div variants={textVariants} className="w-full mt-6">
               <Ctas />
             </motion.div>
-          </div>
+          </motion.div>
           <Screen />
           <CustomersLogos />
         </FrontpageContainer>
--- frontend/apps/frontpage/src/pages/platform/kyc/components/sections/hero/hero.tsx
+++ frontend/apps/frontpage/src/pages/platform/kyc/components/sections/hero/hero.tsx
@@ -1,7 +1,5 @@
-import { Box, Container, Text } from '@onefootprint/ui';
 import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
-import styled, { css } from 'styled-components';
 
 import Ctas from 'src/components/ctas';
 import Illustration from './components/illustration/illustration';
@@ -9,41 +7,22 @@ import Illustration from './components/illustration/illustration';
 const Hero = () => {
   const { t } = useTranslation('common', { keyPrefix: 'pages.kyc' });
   return (
-    <HeroContainer initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, ease: 'easeInOut' }}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      className="flex flex-col items-center justify-center gap-10"
+    >
       <Illustration />
-      <TitleContainer>
-        <Text variant="display-2" textAlign="center" tag="h1">
-          {t('title')}
-        </Text>
-        <Text variant="display-4" color="tertiary" tag="h2">
-          {t('subtitle')}
-        </Text>
-        <Box paddingTop={3} width="100%">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <h1 className="text-center text-display-2">{t('title')}</h1>
+        <h2 className="text-display-4 text-tertiary">{t('subtitle')}</h2>
+        <div className="w-full pt-3">
           <Ctas />
-        </Box>
-      </TitleContainer>
-    </HeroContainer>
+        </div>
+      </div>
+    </motion.div>
   );
 };
-
-const HeroContainer = styled(motion.div)`
-  ${({ theme }) => css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: ${theme.spacing[9]};
-  `}
-`;
-
-const TitleContainer = styled(Container)`
-  ${({ theme }) => css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: ${theme.spacing[4]};
-  `}
-`;
 
 export default Hero;
