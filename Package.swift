af94834b31e4634e8e419cfddb37dfe88a5c1944
rodrigopagnuzzi <rodrigo@onefootprint.com>
2025-01-09 14:10:24 -0300

    Test CI for Swift KMM OBC
    test changelog
    
    test 44
    
    test 43
    
    test 42
    
    test 41
    
    test 40
    
    test 38
    
    test ssh
    
    test 36
    
    test 35
    
    test 34
    
    test 33
    
    test 32
    
    test 31
    
    test 30
    
    test 29
    
    test 28
    
    test 27
    
    test 26
    
    test 25
    
    test 24
    
    test 23
    
    test 22
    
    test 21
    
    test 20
    
    test 19
    
    test 18
    
    test 17
    
    test 16
    
    test 15
    
    test 14
    
    test 13
    
    test 12
    
    test 11
    
    test 10
    
    test 9
    
    test 8
    
    test again
    
    test
    
    Update swift-obc.yml
    
    Update Package.swift
    
    Update Package.swift
    
    Update swift-obc.yml
    
    Update Package.swift
    
    Update swift-obc.yml
    
    test

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// test 44
 
 import PackageDescription
 
@@ -19,9 +20,9 @@ let package = Package(
         // Define the binary target for the shared framework.
         .binaryTarget(
             name: "SwiftOnboardingComponentsShared",
-            path: "./Frameworks/SwiftOnboardingComponentsShared.xcframework"
-        ),
-        // Define the internal target that depends on the binary target.
+            url: "https://github.com/onefootprint/Swift-Onboarding-Components/releases/download/0.5.0/SwiftOnboardingComponentsShared.xcframework.zip",
+            checksum: "e1179a6037c43cbc857e9346e2a38e91b8b43499773255ed5f503ad202bd7376"
+        ),        // Define the internal target that depends on the binary target.
         .target(
             name: "Footprint",
             dependencies: ["SwiftOnboardingComponentsShared"]
---
+++ changelog.md
@@ -0,0 +1,10 @@
+### Changelog for Swift onboarding components SDK
+*Updating this file is the requirement for GitHub CI/CD for Swift package release*
+
+# v0.2.5
+Initial test release
+
+# v0.2.6
+Second test release
+
+
